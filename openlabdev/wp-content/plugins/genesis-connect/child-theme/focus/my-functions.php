f14179e9e97c8e722004ad8abaa8d9a4757c53d1
Boone Gorges <boonebgorges@gmail.com>
2011-10-04 12:08:26 -0400

    Removes openlabdev

--- openlabdev/wp-content/plugins/genesis-connect/child-theme/focus/my-functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* 
- * remove below nav option
- */
-function focus_bp_nav_filter( $nav ) {
-	unset( $nav['bpnav'] );
-	return $nav;
-}
-add_filter( 'gconnect_subnav_options', 'focus_bp_nav_filter' );
--- openlabdev/wp-content/themes/genesis/lib/classes/layouts.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * The file is a currently a placeholder for code coming in a future version of Genesis.
- *
- * @package Genesis
- */
