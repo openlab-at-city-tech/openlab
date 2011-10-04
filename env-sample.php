eb00a08d24c90906b81f09ee843b72d28a19888a
OpenLab <openlab@citytech.cuny.edu>
2011-10-04 11:34:23 -0400

    Updates env-sample.php

---
+++ env-sample.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * WordPress
+ */
+define( 'DB_NAME', '' );
+define( 'DB_USER', '' );
+define( 'DB_PASSWORD', '' );
+define( 'DB_HOST', '' );
+
+/**
+ * bbPress (BP version)
+ *
+ * These should be the same as your WP info
+ */
+define( 'BBDB_NAME', '' );
+define( 'BBDB_USER', '' );
+define( 'BBDB_PASSWORD', '' );
+define( 'BBDB_HOST', '' );
+
+/**
+ * Other environment specific constants
+ */
+define( 'WP_DEBUG', false );
+define( 'DOMAIN_CURRENT_SITE', 'openlab.citytech.cuny.edu' );
+define( 'PATH_CURRENT_SITE', '/' );
+
+@ini_set('log_errors','On');
+@ini_set('display_errors','Off');
+@ini_set('error_log','/usr/home/openlab/public_html/php_error.log');
+
+?>
