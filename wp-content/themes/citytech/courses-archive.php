55b932cd275860c5d1d57486ac8e51e288f47346
Joe Unander <joe_unander@alum.rit.edu>
2012-01-08 23:15:09 -0500

    Commit scope: finished first phase of filtering for courses.

--- wp-content/themes/citytech/courses-archive.php
+++ wp-content/themes/citytech/courses-archive.php
@@ -38,14 +38,34 @@ if ( !empty( $_GET['department'] ) ) {
 	$department=str_replace("-"," ",$_GET['department']);
 	$department=ucwords($department);
 }
+if ( !empty( $_GET['semester'] ) ) {
+	$semester=str_replace("-"," ",$_GET['semester']);
+	$semester=explode(" ",$semester);
+	$semester_season=ucwords($semester[0]);
+	$semester_year=ucwords($semester[1]);
+	$semester=ucwords($semester_season.' '.$semester_year);
+}
 
-if( !empty( $_GET['school'] ) && !empty( $_GET['department'] ) ) {
-	echo "<h3>".$school."<br>";
-	echo "Department: ".$department."</h3>";
+if( !empty( $_GET['school'] ) && !empty( $_GET['department'] ) && !empty($_GET['semester']) ) {
+	echo '<h3 id="bread-crumb">'.$school.'<span class="sep"> | </span>';
+	echo $department.'<span class="sep"> | </span>'.$semester.'</h3>';
+	$sql="SELECT a.group_id FROM {$bp->groups->table_name_groupmeta} a, {$bp->groups->table_name_groupmeta} b, {$bp->groups->table_name_groupmeta} c, {$bp->groups->table_name_groupmeta} d, {$bp->groups->table_name_groupmeta} e where a.group_id=b.group_id and a.group_id=c.group_id and a.group_id=d.group_id and a.group_id=e.group_id and a.meta_key='wds_group_type' and a.meta_value='course' and b.meta_key='wds_group_school' and b.meta_value like '%".$_GET['school']."%' and c.meta_key='wds_departments' and c.meta_value like '%".$department."%' and d.meta_key='wds_semester' and d.meta_value like '%".$semester_season."%' and e.meta_key='wds_year' and e.meta_value like '%".$semester_year."%'";
+}
+else if( !empty( $_GET['school'] ) && !empty( $_GET['department'] ) ) {
+	echo '<h3 id="bread-crumb">'.$school.'<span class="sep"> | </span>';
+	echo $department.'</h3>';
 	$sql="SELECT a.group_id FROM {$bp->groups->table_name_groupmeta} a, {$bp->groups->table_name_groupmeta} b, {$bp->groups->table_name_groupmeta} c where a.group_id=b.group_id and a.group_id=c.group_id and a.meta_key='wds_group_type' and a.meta_value='course' and b.meta_key='wds_group_school' and b.meta_value like '%".$_GET['school']."%' and c.meta_key='wds_departments' and c.meta_value like '%".$department."%'";
+
+}else if( !empty( $_GET['school'] ) && !empty( $_GET['semester'] ) ) {
+	echo '<h3 id="bread-crumb">'.$school.'<span class="sep"> | </span>';
+	echo $semester.'</h3>';
+	$sql="SELECT a.group_id FROM {$bp->groups->table_name_groupmeta} a, {$bp->groups->table_name_groupmeta} b, {$bp->groups->table_name_groupmeta} c, {$bp->groups->table_name_groupmeta} d where a.group_id=b.group_id and a.group_id=c.group_id and a.group_id=d.group_id and a.meta_key='wds_group_type' and a.meta_value='course' and b.meta_key='wds_group_school' and c.meta_value like '%".$_GET['school']."%' and c.meta_key='wds_semester' and b.meta_value like '%".$semester_season."%' and d.meta_key='wds_year' and d.meta_value like '%".$semester_year."%'";
 } elseif( !empty( $_GET['school'] ) ) {
-	echo "<h3>".$school."</h3>";
+	echo '<h3 id="bread-crumb">'.$school.'</h3>';
 	$sql="SELECT a.group_id FROM {$bp->groups->table_name_groupmeta} a, {$bp->groups->table_name_groupmeta} b where a.group_id=b.group_id and a.meta_key='wds_group_type' and a.meta_value='course' and b.meta_key='wds_group_school' and b.meta_value like '%".$_GET['school']."%'";
+} elseif( !empty( $_GET['semester'] ) ) {
+	echo '<h3 id="bread-crumb">'.$semester.'</h3>';
+	$sql="SELECT a.group_id FROM {$bp->groups->table_name_groupmeta} a, {$bp->groups->table_name_groupmeta} b, {$bp->groups->table_name_groupmeta} c where a.group_id=b.group_id and a.group_id=c.group_id and a.meta_key='wds_group_type' and a.meta_value='course' and b.meta_key='wds_semester' and b.meta_value like '%".$semester_season."%' and c.meta_key='wds_year' and c.meta_value like '%".$semester_year."%'";
 }else{
 	$sql="SELECT group_id FROM {$bp->groups->table_name_groupmeta} where meta_key='wds_group_type' and meta_value='course'";
 }
@@ -132,7 +152,7 @@ switch ($_GET['school']) {
 		$option_value_school = "arts";
 		break;
 	default: 
-		$display_option_school = "Select Desired Sequence";
+		$display_option_school = "Select School";
 		$option_value_school = "";
 		break;
 } 
@@ -178,12 +198,123 @@ switch ($_GET['department']) {
 		$display_option_dept = "Mechanical Engineering Technology";
 		$option_value_dept = "mechanical-engineering-technology";
 		break;
+    //School of Professional Studies
+    case "business":
+		$display_option_dept = "Business";
+		$option_value_dept = "business";
+		break;
+	case "career-and-technology-teacher-education":
+		$display_option_dept = "Career and Technology Teacher Education";
+		$option_value_dept = "career-and-technology-teacher-education";
+		break;
+	case "dental-hygiene":
+		$display_option_dept = "Dental Hygiene";
+		$option_value_dept = "dental-hygiene";
+		break;
+	case "health-services-administration":
+		$display_option_dept = "Health Services Administration";
+		$option_value_dept = "health-services-administration";
+		break;
+	case "hospitality-management":
+		$display_option_dept = "Hospitality Management";
+		$option_value_dept = "hospitality-management";
+		break;
+	case "human-services":
+		$display_option_dept = "Human Services";
+		$option_value_dept = "human-services";
+		break;
+	case "law-and-paralegal-studies":
+		$display_option_dept = "Law and Paralegal Studies";
+		$option_value_dept = "law-and-paralegal-studies";
+		break;
+	case "nursing":
+		$display_option_dept = "Nursing";
+		$option_value_dept = "nursing";
+		break;
+	case "radiologic-technology-and-medical-imaging":
+		$display_option_dept = "Radiologic Technology and Medical Imaging";
+		$option_value_dept = "radiologic-technology-and-medical-imaging";
+		break;
+	case "restorative-dentistry":
+		$display_option_dept = "Restorative Dentistry";
+		$option_value_dept = "restorative-dentistry";
+		break;
+	case "vision-care-technology":
+		$display_option_dept = "Vision Care Technology";
+		$option_value_dept = "vision-care-technology";
+		break;
+		//School of Arts and Sciences
+	case "african-american-studies":
+		$display_option_dept = "African-American Studies";
+		$option_value_dept = "african-american-studies";
+		break;
+	case "biological-sciences":
+		$display_option_dept = "Biological Sciences";
+		$option_value_dept = "biological-sciences";
+		break;
+	case "chemistry":
+		$display_option_dept = "Chemistry";
+		$option_value_dept = "chemistry";
+		break;
+	case "english":
+		$display_option_dept = "English";
+		$option_value_dept = "english";
+		break;
+	case "library":
+		$display_option_dept = "Humanities";
+		$option_value_dept = "humanities";
+		break;
+	case "library":
+		$display_option_dept = "Library";
+		$option_value_dept = "library";
+		break;
+	case "mathematics":
+		$display_option_dept = "Mathematics";
+		$option_value_dept = "mathematics";
+		break;
+	case "physics":
+		$display_option_dept = "Physics";
+		$option_value_dept = "physics";
+		break;
+	case "social-science":
+		$display_option_dept = "Social Science";
+		$option_value_dept = "social-science";
+		break;
 	default: 
-		$display_option_dept = "Select Desired Sequence";
+		$display_option_dept = "Select Department";
 		$option_value_dept = "";
 		break;
+}
+	//semesters
+if ( empty( $_GET['semester'] ) ) {
+	$_GET['semester'] = "active";
+}
+switch ($_GET['semester']) {
+	case "fall-2011":
+		$display_option_semester = "Fall 2011";
+		$option_value_semester = "fall-2011";
+		break;
+	case "winter-2011":
+		$display_option_semester = "Winter 2011";
+		$option_value_semester = "winter-2011";
+		break;
+	case "spring-2011":
+		$display_option_semester = "Spring 2011";
+		$option_value_semester = "spring-2011";
+		break;
+	case "summer-2011":
+		$display_option_semester = "Summer 2011";
+		$option_value_semester = "summer-2011";
+		break;
+	case "fall-2012":
+		$display_option_semester = "Fall 2012";
+		$option_value_semester = "fall-2012";
+		break;
+	default: 
+		$display_option_semester = "Select Semester";
+		$option_value_semester = "";
+		break;
 } 
-	
 	//sequence filter
 if ( empty( $_GET['group_sequence'] ) ) {
 	$_GET['group_sequence'] = "active";
@@ -202,7 +333,7 @@ switch ($_GET['group_sequence']) {
 		$option_value = "active";
 		break;
 	default: 
-		$display_option = "Select Desired Sequence";
+		$display_option = "Order By";
 		$option_value = "";
 		break;
 }
@@ -211,28 +342,26 @@ switch ($_GET['group_sequence']) {
 <div class="filter">
 <form id="group_seq_form" name="group_seq_form" action="#" method="get">
 <div class="red-square"></div>
-	<select name="school" class="last-select">
+	<select name="school" class="last-select" onchange="showDept(this.value);">
 		<option value="<?php echo $option_value_school; ?>"><?php echo $display_option_school; ?></option>
 		<option value='tech'>School of Technology & Design</option>
 		<option value='studies'>School of Professional Studies</option>
-		<option value='arts'>School of Arts & Sciencesoption</option>
+		<option value='arts'>School of Arts & Sciences</option>
 	</select>
-	<?php if ($option_value_school == "tech")
-	{?>
 	<div class="red-square"></div>
-	<select name="department" class="last-select">
+	<select name="department" class="last-select" id="dept-select">
 		<option value="<?php echo $option_value_dept; ?>"><?php echo $display_option_dept; ?></option>
-		<option value='advertising-design-and-graphic-arts'>Advertising Design and Graphic Arts</option>
-		<option value='architectural-technology'>Architectural Technology</option>
-		<option value='computer-engineering-technology'>Computer Engineering Technology</option>
-		<option value='computer-systems-technology'>Computer Systems Technology</option>
-		<option value='construction-management-and-civil-engineering-technology'>Construction Management and Civil Engineering Technology</option>
-		<option value='electrical-and-telecommunications-engineering-technology'>Electrical and Telecommunications Engineering Technology</option>
-		<option value='entertainment-technology'>Entertainment Technology</option>
-		<option value='environmental-control-technology'>Environmental Control Technology</option>
-		<option value='mechanical-engineering-technology'>Mechanical Engineering Technology</option>
+		<?php include '/wp-content/themes/citytech/includes/department_processing.php'; ?>
 	</select>
-	<?php } ?>
+	<div class="red-square"></div>
+	<select name="semester" class="last-select">
+		<option value="<?php echo $option_value_semester; ?>"><?php echo $display_option_semester; ?></option>
+		<option value='fall-2011'>Fall 2011</option>
+		<option value='winter-2011'>Winter 2011</option>
+		<option value='spring-2012'>Spring 2012</option>
+		<option value='summer-2012'>Summer 2012</option>
+		<option value='fall-2012'>Fall 2012</option>
+	</select>
 	<div class="red-square"></div>
 	<select name="group_sequence" class="last-select">
 		<option value="<?php echo $option_value; ?>"><?php echo $display_option; ?></option>
@@ -241,6 +370,7 @@ switch ($_GET['group_sequence']) {
 		<option value='active'>Last Active</option>
 	</select>
 	<input type="submit" onchange="document.forms['group_seq_form'].submit();" value="Submit">
+	<!--<input type="reset" onchange="clear_form" value="Reset">-->
 </form>
 <div class="clearfloat"></div>
 </div><!--filter-->
@@ -259,8 +389,37 @@ function slug_maker($full_string)
  $slug_val = str_replace(" ","-",$full_string);
  $slug_val = strtolower($slug_val);
  return $slug_val;
+} ?>
+<script type="text/javascript">
+function showDept(str)
+{
+if (str=="")
+  {
+  document.getElementById("dept-select").innerHTML="";
+  return;
+  }
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("dept-select").innerHTML=xmlhttp.responseText;
+    }
+  }
+xmlhttp.open("GET","http://openlabdev.org/wp-content/themes/citytech/includes/department_processing.php?q="+str,true);
+xmlhttp.send();
 }
-
+function clear_form(){
+	document.getElementById('group_seq_form').reset();
 }
+</script>
+<?php }
 
 genesis();
