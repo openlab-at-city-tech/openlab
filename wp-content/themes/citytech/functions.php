a16e4f1d42e1594c00fc3d4cd78611a3a6796cd6
Boone Gorges <boonebgorges@gmail.com>
2012-02-09 14:40:22 -0500

    Adds external link glyph next to privacy policy link in Help menu. Fixes #261 - pt 2

--- wp-content/themes/citytech/functions.php
+++ wp-content/themes/citytech/functions.php
@@ -485,7 +485,7 @@ function openlab_recent_account_activity_sidebar() {
  * Ensure that external links in the help menu get the external-link glyph
  */
 function openlab_help_menu_external_glyph( $items, $args ) {
-	if ( $args->theme_location == 'helpmenu' ) {
+	if ( false !== strpos( $args->theme_location, 'help' ) ) {
 		foreach( $items as $key => $item ) {
 			if ( false === strpos( $item->url, bp_get_root_domain() ) ) {
 				$items[$key]->classes[] = 'external-link';
