0b69c2c655c3b10dbbf580099ac46dd2a71cd646
OpenLab <openlab@citytech.cuny.edu>
2012-03-07 10:58:49 -0500

    Removes placeholder fallback for IE, which was causing login issues. See #345

--- wp-content/themes/citytech/js/utility.js
+++ wp-content/themes/citytech/js/utility.js
@@ -6,26 +6,4 @@ jQuery(document).ready(function ($) {
 			slideNum:false
 	});
 
-	jQuery.support.placeholder = false;
-	test = document.createElement('input');
-	if('placeholder' in test) jQuery.support.placeholder = true;
-
-	if(!jQuery.support.placeholder) {
-		var active = document.activeElement;
-		$(':text').focus(function () {
-			if ($(this).attr('placeholder') != '' && $(this).val() == $(this).attr('placeholder')) {
-				$(this).val('').removeClass('hasPlaceholder');
-			}
-		}).blur(function () {
-			if ($(this).attr('placeholder') != '' && ($(this).val() == '' || $(this).val() == $(this).attr('placeholder'))) {
-				$(this).val($(this).attr('placeholder')).addClass('hasPlaceholder');
-			}
-		});
-		$(':text').blur();
-		$(active).focus();
-		$('form').submit(function () {
-			$(this).find('.hasPlaceholder').each(function() { $(this).val(''); });
-		});
-	}
-
 },(jQuery));
