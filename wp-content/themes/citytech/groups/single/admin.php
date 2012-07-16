63db51ad7ccec8d7cc66fa7b20bfde470ac01c90
Boone Gorges <boonebgorges@gmail.com>
2012-07-16 19:47:06 -0400

    Implements Delete screen for Portfolio groups. See #152

--- wp-content/themes/citytech/groups/single/admin.php
+++ wp-content/themes/citytech/groups/single/admin.php
@@ -300,10 +300,10 @@ $group_type=groups_get_groupmeta($bp->groups->current_group->id, 'wds_group_type
 	<?php do_action( 'bp_before_group_delete_admin' ); ?>
 
 	<div id="message" class="info">
-		<p><?php _e( 'WARNING: Deleting this group will completely remove ALL content associated with it. There is no way back, please be careful with this option.', 'buddypress' ); ?></p>
+		<p><?php printf( 'WARNING: Deleting this %s will completely remove ALL content associated with it. There is no way back, please be careful with this option.', openlab_get_group_type() ); ?></p>
 	</div>
 
-	<input type="checkbox" name="delete-group-understand" id="delete-group-understand" value="1" onclick="if(this.checked) { document.getElementById('delete-group-button').disabled = ''; } else { document.getElementById('delete-group-button').disabled = 'disabled'; }" /> <?php _e( 'I understand the consequences of deleting this group.', 'buddypress' ); ?>
+	<input type="checkbox" name="delete-group-understand" id="delete-group-understand" value="1" onclick="if(this.checked) { document.getElementById('delete-group-button').disabled = ''; } else { document.getElementById('delete-group-button').disabled = 'disabled'; }" /> <?php printf( 'I understand the consequences of deleting this %s.', openlab_get_group_type() ); ?>
 
 	<?php do_action( 'bp_after_group_delete_admin' ); ?>
 
