ce90cb89f79479b1a9a74424f0608e21b8d9bc4d
Boone B Gorges <boonebgorges@gmail.com>
2012-12-06 10:02:42 -0500

    Adds plugin management mu-plugin, to blacklist certain plugins from appearing for normal users
    See #719

---
+++ wp-content/mu-plugins/openlab-plugin-management.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is responsible for determining which plugins are shown to users
+ * on the Plugins page of their individual sites
+ */
+
+function openlab_hide_plugins( $plugins ) {
+	$super_admin_only = $network_admin_only = array();
+
+	if ( ! is_network_admin() ) {
+		$network_admin_only = array(
+			'achievements/loader.php',
+			'bp-groupblog/loader.php',
+			'buddypress-group-documents/index.php',
+			'bp-include-non-member-comments/bp-include-non-member-comments.php',
+			'bp-mpo-activity-filter/bp-mpo-activity-filter.php',
+			'bp-system-report/bp-system-report.php',
+			'buddypress/bp-loader.php',
+			'buddypress-docs/loader.php',
+			'buddypress-group-email-subscription/bp-activity-subscription.php',
+			'bp-template-pack/loader.php',
+			'cac-group-announcements/cac-group-announcements.php',
+			'forum-attachments-for-buddypress/forum-attachments-for-buddypress.php',
+			'genesis-connect/genesis-connect.php',
+			'invite-anyone/invite-anyone.php',
+			'more-privacy-options/ds_wp3_private_blog.php',
+			'u-buddypress-forum-attachment/u-bp-forum-attachment.php',
+			'wds-buddypress-docs/loader.php',
+			'wds-citytech/wds-citytech.php',
+		);
+
+		foreach ( $plugins as $pkey => $plugin ) {
+			if ( in_array( $pkey, $network_admin_only ) ) {
+				unset( $plugins[ $pkey ] );
+			}
+		}
+	}
+
+	return $plugins;
+}
+add_filter( 'all_plugins', 'openlab_hide_plugins' );
