b952272f26eeaff5f327090205d5235d4717a7f1
Boone B Gorges <boonebgorges@gmail.com>
2012-12-11 13:41:05 -0500

    Remove Edit Group and Edit User menus from adminbar. See #752

--- wp-content/mu-plugins/openlab-toolbar.php
+++ wp-content/mu-plugins/openlab-toolbar.php
@@ -58,7 +58,8 @@ class OpenLab_Admin_Bar {
 			remove_action( 'admin_bar_menu', 'wp_admin_bar_my_sites_menu', 20 );
 
 			// Don't show the Edit Group or Edit Member menus
-			add_action( 'admin_bar_menu', array( $this, 'remove_item_admin_menus' ), 5000 );
+			remove_action( 'admin_bar_menu', 'bp_groups_group_admin_menu', 99 );
+			remove_action( 'admin_bar_menu', 'bp_members_admin_bar_user_admin_menu', 99 );
 
 			// Add the notification menus
 			add_action( 'admin_bar_menu', array( $this, 'add_invites_menu' ), 22 );
@@ -167,14 +168,6 @@ class OpenLab_Admin_Bar {
 			'title' => sprintf( "Hi, %s", $bp->loggedin_user->userdata->display_name ),
 			'meta'	=> array()
 		) );
-	}
-
-	/**
-	 * Remove the Edit Group and Edit Member dropdowns
-	 */
-	function remove_item_admin_menus( $wp_admin_bar ) {
-		remove_action( 'admin_bar_menu', 'bp_groups_group_admin_menu', 400 );
-		remove_action( 'admin_bar_menu', 'bp_members_admin_bar_user_admin_menu', 400 );
 	}
 
 	/**
