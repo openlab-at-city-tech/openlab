8e755008fc8b6fbd07f7b450989236a4820e691d
Joe Unander <joe@joeunander.com>
2013-03-27 20:08:24 -0400

    Ticket #822 - Missing Square Bullet Point on People/Projects/Clubs RH filter
    -minor fix in sidebar-group-archive.php to add this bullet point back in
    and correct the color of the text

--- wp-content/themes/citytech/sidebar-group-archive.php
+++ wp-content/themes/citytech/sidebar-group-archive.php
@@ -201,7 +201,7 @@ switch ($_GET['group_sequence']) {
 	?>
 	
     <?php if ($group_type == 'portfolio' || $group_type == 'course'): ?>
-      <div class="<?php echo $school_color; ?>-square"></div>
+      <div class="<?php echo $sort_color; ?>-square"></div>
 	<select name="group_sequence" class="last-select <?php echo $sort_color; ?>-text">
 		<option <?php selected( $option_value, 'alphabetical' ) ?> value='alphabetical'>Alphabetical</option>
 		<option <?php selected( $option_value, 'newest' ) ?>  value='newest'>Newest</option>
@@ -211,8 +211,8 @@ switch ($_GET['group_sequence']) {
       <input type="submit" onchange="document.forms['group_seq_form'].submit();" value="Submit">
     <?php else: ?>
        <form id="group_seq_form" name="group_seq_form" action="#" method="get">
-	<div class="<?php echo $square_class; ?>"></div>
-	<select name="group_sequence" onchange="document.forms['group_seq_form'].submit();" class="last-select <?php echo $select_class; ?>">
+        <div class="<?php echo $sort_color; ?>-square"></div>
+	<select name="group_sequence" onchange="document.forms['group_seq_form'].submit();" class="last-select <?php echo $select_class; ?> <?php echo $sort_color; ?>-text">
 		<option <?php selected( $option_value, 'alphabetical' ) ?> value='alphabetical'>Alphabetical</option>
 		<option <?php selected( $option_value, 'newest' ) ?>  value='newest'>Newest</option>
 		<option <?php selected( $option_value, 'active' ) ?> value='active'>Last Active</option>
