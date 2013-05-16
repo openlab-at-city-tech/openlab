44e6cc8d228d073278d7b3a94a3bc57318a769dc
Joe Unander <joe@joeunander.com>
2013-05-16 16:36:47 -0400

    Adding in global.js enqueue

--- wp-content/themes/citytech/functions.php
+++ wp-content/themes/citytech/functions.php
@@ -31,6 +31,7 @@ function my_init_method() {
         wp_enqueue_script( 'easyaccordion' );
         wp_register_script( 'utility', get_bloginfo('stylesheet_directory') . '/js/utility.js');
         wp_enqueue_script( 'utility' );
+        wp_enqueue_script( 'bp-js', BP_PLUGIN_URL . '/bp-themes/bp-default/_inc/global.js', array( 'jquery' ) );
     }
 }
 
