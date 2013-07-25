16120bd50e675e55e9afb952f3968a3297ebf33a
Boone B Gorges <boonebgorges@gmail.com>
2013-07-25 15:12:20 -0400

    Introduces theme-fixes.php file. See #852

---
+++ wp-content/mu-plugins/openlab-theme-fixes.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Loads theme fixes for OpenLab site themes
+ */
+
+function openlab_load_theme_fixes() {
+	$t = get_stylesheet();
+
+	switch ( $t ) {
+		case 'carrington-blog' :
+
+			echo '<link rel="stylesheet" id="' . $t . '-fixes" type="text/css" media="screen" href="' . get_home_url() . '/wp-content/mu-plugins/theme-fixes/' . $t . '.css" />
+';
+
+			break;
+	}
+}
+add_action( 'wp_print_styles', 'openlab_load_theme_fixes', 9999 );
