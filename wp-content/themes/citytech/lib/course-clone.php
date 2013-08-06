92dff1f001b9d0f3a3e544d265b4b6f9f3a03082
Boone B Gorges <boonebgorges@gmail.com>
2013-08-05 21:13:29 -0400

    Migrate forum topics on course clone

--- wp-content/themes/citytech/lib/course-clone.php
+++ wp-content/themes/citytech/lib/course-clone.php
@@ -287,7 +287,55 @@ class Openlab_Clone_Course_Group {
 	}
 
 	protected function migrate_topics() {
+		do_action( 'bbpress_init' );
 
+		$source_group_admins = $this->get_source_group_admins();
+
+		// Set up forum
+		$group = groups_get_group( array( 'group_id' => $this->group_id ) );
+		groups_new_group_forum( $this->group_id, $group->name, $group->description );
+		$forum_id = groups_get_groupmeta( $this->group_id, 'forum_id' );
+
+		// Get source topics
+		$source_forum_id = groups_get_groupmeta( $this->source_group_id, 'forum_id' );
+		$source_forum_topics = bp_forums_get_forum_topics( array(
+			'forum_id' => $source_forum_id,
+			'per_page' => 10000000,
+		) );
+
+		// rekey for better lookups
+		$source_forum_topics_keyed = array();
+		foreach ( $source_forum_topics as $sft ) {
+			if ( ! in_array( $sft->topic_poster, $source_group_admins ) ) {
+				continue;
+			}
+
+			$source_forum_topics_keyed[ $sft->topic_id ] = $sft;
+		}
+
+		// And their first posts
+		global $wpdb, $bp, $bbdb;
+		$source_forum_topic_ids = implode( ',', wp_list_pluck( $source_forum_topics, 'topic_id' ) );
+		$source_forum_posts = $wpdb->get_results( "SELECT topic_id, post_text FROM {$bbdb->posts} WHERE topic_id IN ({$source_forum_topic_ids}) AND post_position = 1" );
+		foreach ( $source_forum_posts as $sfp ) {
+			$source_forum_topics_keyed[ $sfp->topic_id ]->post_text = $sfp->post_text;
+		}
+
+		// Then post them
+		foreach ( $source_forum_topics_keyed as $sftk ) {
+			bp_forums_new_topic( array(
+				'forum_id' => $forum_id,
+				'topic_title' => $sftk->topic_title,
+				'topic_slug' => $sftk->topic_slug,
+				'topic_poster' => $sftk->topic_poster,
+				'topic_poster_name' => $sftk->topic_poster_name,
+				'topic_last_poster' => $sftk->topic_last_poster,
+				'topic_last_poster_name' => $sftk->topic_last_poster_name,
+				'topic_start_time' => $sftk->topic_start_time,
+				'topic_time' => $sftk->topic_time,
+				'topic_text' => $sftk->post_text,
+			) );
+		}
 	}
 
 	protected function get_source_group_admins() {
