391782fb11e70f3f44558322fabdd9bf8ec6905f
Boone B Gorges <boonebgorges@gmail.com>
2013-10-22 15:04:22 -0400

    Refactor directory filters to eliminate duplicated items
    Instead of dynamically prepending the currently selected option to the
    <select>, we now correctly use the "selected" attribute.
    
    See #812

--- wp-content/themes/citytech/lib/group-funcs.php
+++ wp-content/themes/citytech/lib/group-funcs.php
@@ -335,44 +335,24 @@ function openlab_delete_group() {
 /**
  * This function prints out the departments for the course archives (non ajax)
  *
+ * @param string $school The id of the school to return a course list for
+ * @param string $department The id of the deparment currently selected in
+ *        the dropdown.
  */
-function openlab_return_course_list($school) {
-    if ($school == "tech" || $option_value_school == "tech") {
-        $tech_depts = openlab_get_department_list('tech');
+function openlab_return_course_list( $school, $department ) {
 
-        $tech_list = '<option value="dept_all">All</option>';
+	// Sanitize and fall back on 'tech'
+	$school = in_array( $school, array( 'tech', 'studies', 'arts' ) ) ? $school : 'tech';
+	$depts = openlab_get_department_list( $school );
 
-        foreach ($tech_depts as $tech_dept) {
-            $display_option_dept = str_replace('And', '&amp;', $tech_dept);
-            $option_value_dept = strtolower(str_replace(' ', '-', $tech_dept));
-            $tech_list .= '<option value="' . $option_value_dept . '">' . $display_option_dept . '</option>';
-        }
-        return $tech_list;
-    } else if ($school == "studies" || $option_value_school == "studies") {
+        $list = '<option value="dept_all" ' . selected( '', $department ) . ' >All</option>';
+	foreach ( $depts as $dept ) {
+		$display_option_dept = str_replace( 'And', '&amp;', $dept );
+		$option_value_dept = strtolower( str_replace( ' ', '-', $dept ) );
+		$list .= '<option value="' . $option_value_dept . '" ' . selected( $department, $option_value_dept, false ) . '>' . $display_option_dept . '</option>';
+	}
 
-        $tech_depts = openlab_get_department_list('studies');
-
-        $tech_list = '<option value="dept_all">All</option>';
-
-        foreach ($tech_depts as $tech_dept) {
-            $display_option_dept = str_replace('And', '&amp;', $tech_dept);
-            $option_value_dept = strtolower(str_replace(' ', '-', $tech_dept));
-            $tech_list .= '<option value="' . $option_value_dept . '">' . $display_option_dept . '</option>';
-        }
-        return $tech_list;
-    } else if ($school == "arts" || $option_value_school == "arts") {
-
-        $tech_depts = openlab_get_department_list('arts');
-
-        $tech_list = '<option value="dept_all">All</option>';
-
-        foreach ($tech_depts as $tech_dept) {
-            $display_option_dept = str_replace('And', '&amp;', $tech_dept);
-            $option_value_dept = strtolower(str_replace(' ', '-', $tech_dept));
-            $tech_list .= '<option value="' . $option_value_dept . '">' . $display_option_dept . '</option>';
-        }
-        return $tech_list;
-    }
+	return $list;
 }
 
 function openlab_group_post_count($filters,$group_args) {
