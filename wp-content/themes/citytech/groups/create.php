9858fb5cebd5311b14ebb11023d112f574003b61
Joe Unander <joe@joeunander.com>
2013-10-29 16:10:44 -0400

    Adding in spacing on labels for "Portfolio Name" and "Portfolio Description" on Portfolio creation page, step one.
    Related to ticket #979 - Change Group creation page markup per design

--- wp-content/themes/citytech/groups/create.php
+++ wp-content/themes/citytech/groups/create.php
@@ -141,9 +141,9 @@ function cuny_create_group() {
                             <li>Jane Smith's <?php echo $group_label ?> (Example)</li>
                         </ul>
 
-                        <label for="group-name"><?php echo ucfirst($group_type); ?>Name <?php _e('(required)', 'buddypress') ?></label>
+                        <label for="group-name"><?php echo ucfirst($group_type); ?> Name <?php _e('(required)', 'buddypress') ?></label>
                         <input size="80" type="text" name="group-name" id="group-name" value="<?php bp_new_group_name() ?>" />
-                        <label for="group-desc"><?php echo ucfirst($group_type); ?>Description <?php _e('(required)', 'buddypress') ?></label>
+                        <label for="group-desc"><?php echo ucfirst($group_type); ?> Description <?php _e('(required)', 'buddypress') ?></label>
 
         <?php else : ?>
                         <label for="group-name"><?php echo ucfirst($group_type); ?> Name <?php _e('(required)', 'buddypress') ?></label>
