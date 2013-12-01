e74355ff126d68f508a61f6f5793bf1bd2238a8f
Boone B Gorges <boonebgorges@gmail.com>
2013-12-01 15:05:10 -0500

    Remove 'hidden' class from Leave Group wrapper in hidden groups
    A conflict with WP AJAX Edit Comments was causing the latter plugin to hide the
    button incorrectly on hidden groups, with the result that users could not
    leave hidden groups. This is a workaround for modifying the plugin.
    
    See #1004

--- wp-content/themes/citytech/functions.php
+++ wp-content/themes/citytech/functions.php
@@ -623,3 +623,14 @@ function cuny_buddypress_group_actions() {
 	<?php endwhile; endif;
 }
 
+/**
+ * Remove the 'hidden' class from hidden group leave buttons
+ *
+ * A crummy conflict with wp-ajax-edit-comments causes these items to be
+ * hidden by jQuery. See b208c80 and #1004
+ */
+function openlab_remove_hidden_class_from_leave_group_button( $button ) {
+	$button['wrapper_class'] = str_replace( ' hidden', '', $button['wrapper_class'] );
+	return $button;
+}
+add_action( 'bp_get_group_join_button', 'openlab_remove_hidden_class_from_leave_group_button', 20 );
