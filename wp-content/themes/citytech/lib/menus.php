42c91b7310cfd8f86dcdd7dc7b295dcc395609d1
Boone B Gorges <boonebgorges@gmail.com>
2014-01-06 16:35:46 -0500

    Remove Portfolio-specific Join button customizations
    See #933

--- wp-content/themes/citytech/lib/menus.php
+++ wp-content/themes/citytech/lib/menus.php
@@ -527,29 +527,6 @@ function openlab_group_submenu_nav() {
 
 add_action( 'bp_actions', 'openlab_group_submenu_nav', 1 );
 
-/*
- * Hide "Join Group/Leave Group" button on Portfolios and select group profile pages
- */
-function openlab_no_join_on_portfolios( $button ) {
-	global $bp;
-
-	if ( openlab_is_portfolio() ) {
-		if ( bp_group_has_requested_membership( groups_get_current_group() ) ) {
-			$button['link_text'] = $button['link_title'] = 'Requested';
-		} else {
-			$button['link_text'] = $button['link_title'] = 'Request Access';
-		}
-	}
-
-	// fix for files, docs, and membership pages in group profile - hiding join button
-	else if ( $bp->current_action == 'files' || $bp->current_action == 'docs' || $bp->current_action == 'invite-anyone' || $bp->current_action == 'notifications' ) {
-		$button = "";
-	}
-
-	return $button;
-}
-add_filter( 'bp_get_group_join_button', 'openlab_no_join_on_portfolios' );
-
 /**
  * Markup for group admin tabs
  */
