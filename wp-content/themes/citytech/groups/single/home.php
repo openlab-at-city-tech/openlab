247aaf20de029bb052eaa82d3b58e878a8b7f5bd
Boone B Gorges <boonebgorges@gmail.com>
2014-01-12 19:13:42 -0500

    Remove word "group" from visibility status in group header. See #1013
    Also normalize some whitespace

--- wp-content/themes/citytech/groups/single/home.php
+++ wp-content/themes/citytech/groups/single/home.php
@@ -20,73 +20,71 @@ get_header(); ?>
 
 function cuny_group_single() { ?>
 	<?php if ( bp_has_groups() ) : while ( bp_groups() ) : bp_the_group();
-		$group_type = openlab_get_group_type( bp_get_current_group_id() );
+		$group_type = openlab_get_group_type( bp_get_current_group_id() ); ?>
 
-		if ( $group_type == "cheese") {
-		//if ( $group_type = openlab_get_group_type( bp_get_current_group_id() )) {
-			locate_template( array( 'groups/' . $group_type . 's/single/home.php' ), true );
-		} else {
-			?>
-			<?php do_action( 'bp_before_group_home_content' ) ?>
+		<?php do_action( 'bp_before_group_home_content' ) ?>
 
-			<?php $group_slug = bp_get_group_slug(); ?>
+		<?php $group_slug = bp_get_group_slug(); ?>
 
-			<?php //group page vars
-				  global $bp, $wpdb;
-				  $group_id = $bp->groups->current_group->id;
-				  $group_name = $bp->groups->current_group->name;
-				  $group_description = $bp->groups->current_group->description;
-				  $faculty_id = $bp->groups->current_group->admins[0]->user_id;
-				  $first_name= ucfirst(xprofile_get_field_data( 'First Name', $faculty_id));
-				  $last_name= ucfirst(xprofile_get_field_data( 'Last Name', $faculty_id));
-				  $group_type = openlab_get_group_type( bp_get_current_group_id());
-				  $section = groups_get_groupmeta($group_id, 'wds_section_code');
-				  $html = groups_get_groupmeta($group_id, 'wds_course_html');?>
+		<?php
+		//group page vars
+		global $bp, $wpdb;
+		$group_id = $bp->groups->current_group->id;
+		$group_name = $bp->groups->current_group->name;
+		$group_description = $bp->groups->current_group->description;
+		$faculty_id = $bp->groups->current_group->admins[0]->user_id;
+		$first_name= ucfirst(xprofile_get_field_data( 'First Name', $faculty_id));
+		$last_name= ucfirst(xprofile_get_field_data( 'Last Name', $faculty_id));
+		$group_type = openlab_get_group_type( bp_get_current_group_id());
+		$section = groups_get_groupmeta($group_id, 'wds_section_code');
+		$html = groups_get_groupmeta($group_id, 'wds_course_html');
+		?>
 
-			<h1 class="entry-title group-title"><?php echo bp_group_name(); ?> Profile</h1>
-			<?php if ( bp_is_group_home() ): ?>
+		<h1 class="entry-title group-title"><?php echo bp_group_name(); ?> Profile</h1>
+		<?php if ( bp_is_group_home() ): ?>
 			<div id="<?php echo $group_type; ?>-header" class="group-header">
-            	<?php if ($group_type == 'portfolio'): ?>
-					<?php if (strpos(bp_get_group_name(),'ePortfolio'))
-                          {
-                              $profile = "ePortfolio";
-                          } else {
-                              $profile = "Portfolio";
-                          } ?>
-                    <h4 class="profile-header"><?php echo $profile; ?> Profile</h4>
-                <?php else: ?>
-            		<h4 class="profile-header"><?php echo ucfirst($group_type); ?> Profile</h4>
+				<?php if ( $group_type == 'portfolio' ) : ?>
+					<?php if ( strpos( bp_get_group_name(), 'ePortfolio' ) ) {
+						$profile = 'ePortfolio';
+					} else {
+						$profile = "Portfolio";
+					} ?>
+				<h4 class="profile-header"><?php echo $profile; ?> Profile</h4>
+		<?php else: ?>
+				<h4 class="profile-header"><?php echo ucfirst($group_type); ?> Profile</h4>
                 <?php endif; ?>
+
 				 <div id="<?php echo $group_type; ?>-header-avatar" class="alignleft group-header-avatar">
-                    <a href="<?php bp_group_permalink() ?>" title="<?php bp_group_name() ?>">
-                        <?php bp_group_avatar('type=full&width=225') ?>
-                    </a>
-                     <?php if (is_user_logged_in() && $bp->is_item_admin): ?>
-                     <div id="group-action-wrapper">
-                                <div id="action-edit-group"><a href="<?php echo bp_group_permalink(). 'admin/edit-details/'; ?>">Edit Profile</a></div>
-                                <div id="action-edit-avatar"><a href="<?php echo bp_group_permalink(). 'admin/group-avatar/'; ?>">Change Avatar</a></div>
-                     </div>
-                    <?php elseif (is_user_logged_in()): ?>
-                            <div id="group-action-wrapper">
-                                    <?php do_action( 'bp_group_header_actions' ); ?>
-                            </div>
-                    <?php endif; ?>
-                </div><!-- #<?php echo $group_type; ?>-header-avatar -->
+					<a href="<?php bp_group_permalink() ?>" title="<?php bp_group_name() ?>">
+						<?php bp_group_avatar('type=full&width=225') ?>
+					</a>
 
-				<div id="<?php echo $group_type; ?>-header-content" class="alignleft group-header-content group-<?php echo $group_id; ?>">
-                    <h2 class="<?php echo $group_type; ?>-title"><?php bp_group_name() ?>
-                        <?php if ($group_type != 'portfolio' && $group_type != 'club'): ?>
-                            <a href="<?php bp_group_permalink() ?>/feed" class="rss"><img src="<?php bloginfo('stylesheet_directory') ?>/images/icon-RSS.png" alt="Subscribe To <?php bp_group_name() ?>'s Feeds"></a>
-                        <?php endif; ?>
-                    </h2>
+					<?php if ( is_user_logged_in() && $bp->is_item_admin ): ?>
+						<div id="group-action-wrapper">
+						<div id="action-edit-group"><a href="<?php echo bp_group_permalink(). 'admin/edit-details/'; ?>">Edit Profile</a></div>
+						<div id="action-edit-avatar"><a href="<?php echo bp_group_permalink(). 'admin/group-avatar/'; ?>">Change Avatar</a></div>
+						</div>
+					<?php elseif ( is_user_logged_in() ) : ?>
+						<div id="group-action-wrapper">
+							<?php do_action( 'bp_group_header_actions' ); ?>
+						</div>
+					<?php endif; ?>
+			</div><!-- #<?php echo $group_type; ?>-header-avatar -->
 
-                        <?php if ($group_type == "portfolio"): ?>
-                    <div class="portfolio-displayname"><span class="highlight"><?php echo bp_core_get_userlink( openlab_get_user_id_from_portfolio_group_id( bp_get_group_id() ) ); ?></span></div>
-                        <?php else: ?>
-                            <div class="info-line"><span class="highlight"><?php bp_group_type() ?></span> <span class="activity"><?php printf( __( 'active %s', 'buddypress' ), bp_get_group_last_active() ) ?></span></div>
-                        <?php endif; ?>
+			<div id="<?php echo $group_type; ?>-header-content" class="alignleft group-header-content group-<?php echo $group_id; ?>">
+				<h2 class="<?php echo $group_type; ?>-title"><?php bp_group_name() ?>
+					<?php if ($group_type != 'portfolio' && $group_type != 'club'): ?>
+						<a href="<?php bp_group_permalink() ?>/feed" class="rss"><img src="<?php bloginfo('stylesheet_directory') ?>/images/icon-RSS.png" alt="Subscribe To <?php bp_group_name() ?>'s Feeds"></a>
+					<?php endif; ?>
+				</h2>
 
-              <?php do_action( 'bp_before_group_header_meta' ) ?>
+				<?php if ( $group_type == "portfolio" ) : ?>
+					<div class="portfolio-displayname"><span class="highlight"><?php echo bp_core_get_userlink( openlab_get_user_id_from_portfolio_group_id( bp_get_group_id() ) ); ?></span></div>
+				<?php else : ?>
+					<div class="info-line"><span class="highlight"><?php echo ucfirst( groups_get_current_group()->status ) ?></span>: <span class="activity"><?php printf( __( 'active %s', 'buddypress' ), bp_get_group_last_active() ) ?></span></div>
+				<?php endif; ?>
+
+				<?php do_action( 'bp_before_group_header_meta' ) ?>
 
               <?php if ($group_type == "course"): ?>
                   <div class="course-byline">
@@ -349,8 +347,7 @@ function cuny_group_single() { ?>
 
 <?php do_action( 'bp_after_group_home_content' ) ?>
 
-	<?php	}
-	endwhile; endif; ?>
+	<?php endwhile; endif; ?>
 <?php }
 
 add_filter( 'bp_get_options_nav_nav-invite-anyone', 'cuny_send_invite_fac_only');
