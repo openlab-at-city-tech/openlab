8c41a56b6136d9036bced704925ca30d7341fa3c
Boone B Gorges <boonebgorges@gmail.com>
2014-01-28 19:31:47 -0500

    Whitespace in groups-archive.php template

--- wp-content/themes/citytech/group-archive.php
+++ wp-content/themes/citytech/group-archive.php
@@ -2,17 +2,19 @@
 /**begin layout**/
 get_header(); ?>
 
-	<div id="content" class="hfeed">
-    	<div <?php post_class(); ?>>
-        	<h1 class="entry-title"><?php echo ucfirst(openlab_page_slug_to_grouptype()).'s'; ?> on the OpenLab</h1>
-            <div class="entry-content">
-				<?php openlab_group_archive(); ?>
-            </div><!--entry-content-->
-        </div><!--hentry-->
-    </div><!--content-->
-    <div id="sidebar" class="sidebar widget-area">
-		<?php get_sidebar('group-archive'); ?>
-    </div>
+<div id="content" class="hfeed">
+	<div <?php post_class(); ?>>
+		<h1 class="entry-title"><?php echo ucfirst(openlab_page_slug_to_grouptype()).'s'; ?> on the OpenLab</h1>
+
+		<div class="entry-content">
+			<?php openlab_group_archive(); ?>
+		</div><!--entry-content-->
+	</div><!--hentry-->
+</div><!--content-->
+
+<div id="sidebar" class="sidebar widget-area">
+	<?php get_sidebar( 'group-archive' ); ?>
+</div>
 
 <?php get_footer();
 /**end layout**/
