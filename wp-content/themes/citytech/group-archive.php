8a21566a84638827824c720f994fbb79a6350baa
Boone B Gorges <boonebgorges@gmail.com>
2014-04-14 15:16:46 -0400

    Revert "Responsive improvements on group directories"
    This reverts commit 19e06e4b9cb0b40b78451daa81a3ae37b19e5797.

--- wp-content/themes/citytech/group-archive.php
+++ wp-content/themes/citytech/group-archive.php
@@ -2,11 +2,6 @@
 /**begin layout**/
 get_header(); ?>
 
-<!-- Note: the sidebar comes first in the markup, but appears on the right. This makes responsive styling easier -->
-<div id="sidebar" class="sidebar widget-area">
-	<?php get_sidebar( 'group-archive' ); ?>
-</div>
-
 <div id="content" class="hfeed">
 	<div <?php post_class(); ?>>
 		<h1 class="entry-title"><?php echo ucfirst(openlab_page_slug_to_grouptype()).'s'; ?> on the OpenLab</h1>
@@ -16,6 +11,10 @@ get_header(); ?>
 		</div><!--entry-content-->
 	</div><!--hentry-->
 </div><!--content-->
+
+<div id="sidebar" class="sidebar widget-area">
+	<?php get_sidebar( 'group-archive' ); ?>
+</div>
 
 <?php get_footer();
 /**end layout**/
