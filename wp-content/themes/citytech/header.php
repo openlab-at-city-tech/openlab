bb4d0519732de2b45f8eabbab561cc516a059f53
User Openlabdev <openlabdev@qs2837.pair.com>
2014-05-05 09:54:27 -0400

    When printing, don't let #content wrapper have position: fixed
    This was causing multiple pages not to print.
    
    See #1065

--- wp-content/themes/citytech/header.php
+++ wp-content/themes/citytech/header.php
@@ -15,6 +15,8 @@
 <meta name="robots" content="noodp,noydir" />
 <link rel="Shortcut Icon" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicon.ico" type="image/x-icon" />
 
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/print.css" type="text/css" media="print" />
+
 <title><?php bloginfo( 'name' ); ?></title>
 
 <?php do_action( 'bp_head' ) ?>
