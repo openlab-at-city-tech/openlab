6aedb8728da660f80c6c127b30e6dd8dd0c00a28
Boone B Gorges <boonebgorges@gmail.com>
2014-07-23 18:00:56 -0400

    Display external blogs in portfolio list
    See #1075

--- wp-content/plugins/wds-citytech/includes/portfolios.php
+++ wp-content/plugins/wds-citytech/includes/portfolios.php
@@ -245,13 +245,22 @@ function openlab_get_group_member_portfolios( $group_id = false, $sort_by = 'dis
 			$portfolio_group = groups_get_group( array( 'group_id' => $portfolio_id ) );
 			$portfolio_blog_id = openlab_get_site_id_by_group_id( $portfolio_id );
 
-			if ( empty( $portfolio_id ) || empty( $portfolio_blog_id ) || empty( $portfolio_group ) ) {
+			if ( empty( $portfolio_id ) || empty( $portfolio_group ) ) {
 				continue;
 			}
 
 			// Don't add hidden portfolios, unless they've been requested
 			if ( 'all' !== $type && 'hidden' === $portfolio_group->status ) {
 				continue;
+			}
+
+			// If the portfolio_blog_id is empty, this may be an
+			// external portfolio
+			if ( empty( $portfolio_blog_id ) ) {
+				$external_url = openlab_get_external_site_url_by_group_id( $portfolio_id );
+				if ( empty( $external_url ) ) {
+					continue;
+				}
 			}
 
 			$portfolio = array(
