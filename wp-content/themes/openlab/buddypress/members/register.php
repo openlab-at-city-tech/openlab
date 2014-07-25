261d9c22737a583db3c8773baba80926bb2c1d91
Joe Unander <joe@joeunander.com>
2014-07-25 16:04:55 -0400

    Moving register and activate files to their new location under 'members'

---
+++ wp-content/themes/openlab/buddypress/members/register.php
@@ -0,0 +1,13 @@
+<?php /**
+ *  sign up form template
+ *
+ */
+?>
+
+<div id="content" class="hfeed col-sm-9">
+    <?php openlab_registration_page(); ?>
+</div><!--content-->
+
+<div id="sidebar" class="sidebar widget-area col-sm-3">
+    <?php openlab_buddypress_register_actions(); ?>
+</div>
