5105f8a77af48191e378f3363004e0e5c3a7d649
Joe Unander <joe@joeunander.com>
2014-07-25 15:00:20 -0400

    Bug #1101 - Fixing Invitations Received page

--- wp-content/themes/openlab/buddypress/members/single/groups.php
+++ wp-content/themes/openlab/buddypress/members/single/groups.php
@@ -1,12 +1,12 @@
 <?php if ('invites' == bp_current_action()) : ?>
     <div class="submenu"><div class="submenu-text">My Invitations: </div><?php echo openlab_my_invitations_submenu(); ?></div>
-    <?php bp_get_template_part('members/single/groups/invites.php'); ?>
+    <?php bp_get_template_part('members/single/groups/invites'); ?>
 
 <?php else : ?>
 
     <?php do_action('bp_before_member_groups_content') ?>
 
-    <?php bp_get_template_part('groups/groups-loop.php'); ?>
+    <?php bp_get_template_part('groups/groups-loop'); ?>
 
     <?php do_action('bp_after_member_groups_content') ?>
 
