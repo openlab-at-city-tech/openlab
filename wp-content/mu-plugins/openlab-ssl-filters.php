7b7453d53af427029592e0121c739b18d9ac4383
Boone B Gorges <boonebgorges@gmail.com>
2014-08-04 17:21:01 -0400

    SSL fixes:
    - manually entered nav menu URLs
    - attachment URLs
    
    See #1051

---
+++ wp-content/mu-plugins/openlab-ssl-filters.php
@@ -0,0 +1,40 @@
+<?php
+
+function openlab_ssl_fix( $url ) {
+	// Replacing the root domain will catch the specific blog domain
+	// because we aren't using subdomains
+	if ( is_ssl() ) {
+		$search  = set_url_scheme( bp_get_root_domain(), 'http' );
+		$replace = set_url_scheme( bp_get_root_domain(), 'https' );
+	} else {
+		$search  = set_url_scheme( bp_get_root_domain(), 'https' );
+		$replace = set_url_scheme( bp_get_root_domain(), 'http' );
+	}
+
+	return str_replace( $search, $replace, $url );
+}
+
+/**
+ * wp_nav_menu() fixes
+ *
+ * This has to be done in filters because people can enter their own links
+ * through the interface
+ */
+function openlab_ssl_filter_wp_nav_menu( $items ) {
+	foreach ( $items as $item ) {
+		$item->url = openlab_ssl_fix( $item->url );
+	}
+
+	return $items;
+}
+add_filter( 'wp_nav_menu_objects', 'openlab_ssl_filter_wp_nav_menu' );
+
+/**
+ * wp_upload_dir() - this fixes issues with attachment URLs throughout
+ */
+function openlab_ssl_upload_dir( $upload_dir ) {
+	$upload_dir['url']     = openlab_ssl_fix( $upload_dir['url'] );
+	$upload_dir['baseurl'] = openlab_ssl_fix( $upload_dir['baseurl'] );
+	return $upload_dir;
+}
+add_filter( 'upload_dir', 'openlab_ssl_upload_dir' );
