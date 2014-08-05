86cb2072783d7b0bc27e651c09b024ae4814eb82
Joe Unander <joe@joeunander.com>
2014-08-05 08:33:10 -0400

    Porting functionality from legacy functions.php to theme lib file content-processing.php
    1) Remove Filter get_the_excerpt removes wp_trim_excerpt()
    2) cuny_add_links_wp_trim_excerpt() hooks into get_the_excerpt

--- wp-content/themes/openlab/lib/content-processing.php
+++ wp-content/themes/openlab/lib/content-processing.php
@@ -70,3 +70,30 @@ function openlab_http_check($link)
 	return $link;
 }
 
+remove_filter( 'get_the_excerpt', 'wp_trim_excerpt' );
+add_filter( 'get_the_excerpt', 'cuny_add_links_wp_trim_excerpt' );
+function cuny_add_links_wp_trim_excerpt( $text ) {
+	$raw_excerpt = $text;
+	if ( '' == $text ) {
+		$text = get_the_content( '' );
+
+		$text = strip_shortcodes( $text );
+
+		$text = apply_filters( 'the_content', $text );
+		$text = str_replace( ']]>', ']]>', $text );
+		$text = strip_tags( $text, '<a>' );
+		$excerpt_length = apply_filters( 'excerpt_length', 55 );
+
+		$excerpt_more = apply_filters( 'excerpt_more', ' ' . '[...]' );
+		$words = preg_split( '/( <a.*?a> )|\n|\r|\t|\s/', $text, $excerpt_length + 1, PREG_SPLIT_NO_EMPTY|PREG_SPLIT_DELIM_CAPTURE );
+		if ( count( $words ) > $excerpt_length ) {
+			array_pop( $words );
+			$text = implode( ' ', $words );
+			$text = $text . $excerpt_more;
+		} else {
+			$text = implode( ' ', $words );
+		}
+	}
+	return apply_filters( 'new_wp_trim_excerpt', $text, $raw_excerpt );
+
+}
