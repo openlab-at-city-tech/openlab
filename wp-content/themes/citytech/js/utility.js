dad67f2c3e409144a94c6512cd0e9349c78ea6ac
OpenLab <openlab@citytech.cuny.edu>
2014-08-12 18:55:43 -0400

    Use ajaxurl for the dropdown filters on directories, rather than hardcoded URL
    This ensures SSL compliance
    
    See #1110

--- wp-content/themes/citytech/js/utility.js
+++ wp-content/themes/citytech/js/utility.js
@@ -97,7 +97,7 @@
 	  
 	  $.ajax({
 			 type: 'GET',
-			 url: 'http://' + document.domain + '/wp-admin/admin-ajax.php',
+			 url: ajaxurl,
 			 data:
 			  {
 				  action: 'openlab_ajax_return_course_list',
