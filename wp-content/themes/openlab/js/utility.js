ae0fc7d6e68b3777c545d1aab50054c0a01b23c2
Joe Unander <joe@joeunander.com>
2014-08-19 19:35:42 -0400

    Cleanup and some small changes to utlity.js related to homepage, primarily the auto height calculation done on the group boxes

--- wp-content/themes/openlab/js/utility.js
+++ wp-content/themes/openlab/js/utility.js
@@ -116,7 +116,7 @@
                 /*first we get the number of rows by finding the column with the greatest number of rows*/
                 var $row_num = 0;
                 $('.activity-list').each(function(){
-                        $row_check = $(this).find('.row').length;
+                        var $row_check = $(this).find('.activity-item').length;
 
                         if ($row_check > $row_num) {
                                 $row_num = $row_check;
@@ -124,14 +124,14 @@
                 });
 	
                 //build a loop to iterate through each row
-                $i = 1;
+                var $i = 1;
                 
                 while ($i <= $row_num) {
                         //check each cell in the row - find the one with the greatest height
                         var $greatest_height = 0;
                         
                         $('.row-'+$i).each(function(){
-                                $cell_height = $(this).height();
+                                var $cell_height = $(this).outerHeight();
 
                                 if ($cell_height > $greatest_height) {
                                         $greatest_height = $cell_height;
@@ -147,7 +147,7 @@
                 }
 	  
 	//there is an inline script that hides the lists from the user on load (just so the adjusment isn't jarring) - this will show the lists
-	$('.activity-list').css('visibility','visible');
+	$('.activity-list').css('visibility','visible').hide().fadeIn(700);
 		
 	}
 	
