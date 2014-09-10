9c12dea7ea30bf7970edf6fd7f533ac9082c3761
Joe Unander <joe@joeunander.com>
2014-09-10 15:44:58 -0400

    Adding viewport meta to header

--- wp-content/themes/openlab/header.php
+++ wp-content/themes/openlab/header.php
@@ -2,6 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes('xhtml'); ?>>
     <head profile="http://gmpg.org/xfn/11">
         <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="robots" content="noodp,noydir" />
         <link rel="Shortcut Icon" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicon.ico" type="image/x-icon" />
         <title><?php bloginfo('name'); ?></title>
