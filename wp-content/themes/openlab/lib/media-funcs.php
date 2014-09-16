41cd6f04fc4c03b4c0a32535d1ece83f0f61eeef
Joe Unander <joe@joeunander.com>
2014-09-16 18:37:04 -0400

    Fixing default avatar (going through gravatar was not working)

--- wp-content/themes/openlab/lib/media-funcs.php
+++ wp-content/themes/openlab/lib/media-funcs.php
@@ -1,10 +1,32 @@
 <?php
+
 /*
  * Media-oriented functionality
  */
 
-//custom default avatar
-function openlab_add_default_member_avatar( $url = false ) {
-	return get_stylesheet_directory_uri().'/images/default-avatar.jpg';
+/**
+ * Custom mysteryman
+ * @return type
+ */
+function openlab_new_mysteryman() {
+    return get_stylesheet_directory_uri() . '/images/default-avatar.jpg';
 }
-add_filter( 'bp_core_mysteryman_src', 'openlab_add_default_member_avatar' );
+
+add_filter('bp_core_mysteryman_src', 'openlab_new_mysteryman', 2, 7);
+
+/**
+ * Custom default avatar
+ * @param string $url
+ * @param type $params
+ * @return string
+ */
+function openlab_default_get_group_avatar($url, $params) {
+    
+    if(strstr($url,'default-avatar')){
+        $url = get_stylesheet_directory_uri() . '/images/default-avatar.jpg';
+    }
+    
+    return $url;
+}
+
+add_filter('bp_core_fetch_avatar_url', 'openlab_default_get_group_avatar', 10, 2);
