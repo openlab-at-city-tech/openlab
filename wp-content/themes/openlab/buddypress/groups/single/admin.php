355074ff174a16c12749bbf34c2212e200d8ed1b
Joe Unander <joe@joeunander.com>
2014-09-18 16:47:15 -0400

    Spacing commit

--- wp-content/themes/openlab/buddypress/groups/single/admin.php
+++ wp-content/themes/openlab/buddypress/groups/single/admin.php
@@ -14,38 +14,38 @@ $group_label_uc = openlab_get_group_type_label('case=upper');
         <div class="submenu">
 
             <?php if ($bp->action_variables[0] == 'membership-requests' || $bp->action_variables[0] == 'manage-members' || $bp->action_variables[0] == 'notifications'): ?>
-    <?php do_action('bp_before_group_members_content') ?>
+                <?php do_action('bp_before_group_members_content') ?>
 
                 <ul class="nav nav-inline">
-    <?php openlab_group_membership_tabs(); ?>
+                    <?php openlab_group_membership_tabs(); ?>
                 </ul>
 
-<?php else: ?>
+            <?php else: ?>
 
                 <div class="submenu-text pull-left bold"><?php echo $group_label_uc ?> Settings:</div>
                 <ul class="nav nav-inline">
-    <?php openlab_group_admin_tabs(); ?>
+                    <?php openlab_group_admin_tabs(); ?>
                 </ul>
 
-<?php endif; ?>
+            <?php endif; ?>
 
         </div><!-- .item-list-tabs -->
     </div></div>
 
 <form action="<?php bp_group_admin_form_action() ?>" name="group-settings-form" id="group-settings-form" class="standard-form form-panel" method="post" enctype="multipart/form-data">
 
-<?php do_action('bp_before_group_admin_content') ?>
+    <?php do_action('bp_before_group_admin_content') ?>
 
     <div class="item-body" id="group-create-body">
 
         <?php /* Edit Group Details */ ?>
-<?php if (bp_is_group_admin_screen('edit-details')) : ?>
+        <?php if (bp_is_group_admin_screen('edit-details')) : ?>
 
             <div class="panel panel-default">
                 <div class="panel-heading"><?php echo $group_label_uc; ?> Details</div>
                 <div class="panel-body">
 
-    <?php do_action('bp_before_group_details_admin'); ?>
+                    <?php do_action('bp_before_group_details_admin'); ?>
 
                     <label for="group-name"><?php echo $group_label_uc . ' Name' ?> (required)</label>
                     <input class="form-control" type="text" name="group-name" id="group-name" value="<?php bp_group_name() ?>" />
@@ -55,7 +55,7 @@ $group_label_uc = openlab_get_group_type_label('case=upper');
 
                     <?php do_action('groups_custom_group_fields_editable') ?>
 
-    <?php if (!openlab_is_portfolio()) : ?>
+                    <?php if (!openlab_is_portfolio()) : ?>
                         <div class="notify-settings">
                             <p class="ol-tooltip notify-members"><?php _e('Notify group members of changes via email', 'buddypress'); ?></p>
                             <div class="radio">
@@ -64,16 +64,16 @@ $group_label_uc = openlab_get_group_type_label('case=upper');
                             </div>
                         </div>
 
-    <?php else : ?>
+                    <?php else : ?>
 
                         <input type="hidden" name="group-notify-members" value="0" />
 
-    <?php endif ?>
+                    <?php endif ?>
                 </div>
             </div>
 
 
-    <?php do_action('bp_after_group_details_admin'); ?>
+            <?php do_action('bp_after_group_details_admin'); ?>
 
             <p><input class="btn btn-primary" type="submit" value="<?php _e('Save Changes', 'buddypress') ?> &rarr;" id="save" name="save" /></p>
             <?php wp_nonce_field('groups_edit_group_details') ?>
@@ -86,7 +86,7 @@ $group_label_uc = openlab_get_group_type_label('case=upper');
 
             <?php if (function_exists('bp_forums_is_installed_correctly') && !openlab_is_portfolio()) : ?>
 
-        <?php if (bp_forums_is_installed_correctly()) : ?>
+                <?php if (bp_forums_is_installed_correctly()) : ?>
 
                     <div class="checkbox">
                         <h4>Discussion Settings</h4>
@@ -94,18 +94,18 @@ $group_label_uc = openlab_get_group_type_label('case=upper');
                         <label><input type="checkbox" name="group-show-forum" id="group-show-forum" value="1"<?php bp_group_show_forum_setting() ?> /> <?php _e('Enable discussions forum', 'buddypress') ?></label>
                     </div>
 
-        <?php endif; ?>
+                <?php endif; ?>
 
                 <hr />
             <?php endif; ?>
 
-    <?php if (!openlab_is_portfolio()) : ?>
+            <?php if (!openlab_is_portfolio()) : ?>
                 <div class="checkbox">
                     <h4>Portfolio List Settings</h4>
                     <p id="portfolio-list-settings-tag">These settings enable or disable the member portfolio list display on your Course profile.</p>
 
                     <?php $portfolio_list_enabled = openlab_portfolio_list_enabled_for_group() ?>
-        <?php $portfolio_list_heading = openlab_portfolio_list_group_heading() ?>
+                    <?php $portfolio_list_heading = openlab_portfolio_list_group_heading() ?>
                     <label><input type="checkbox" name="group-show-portfolio-list" id="group-show-portfolio-list" value="1" <?php checked($portfolio_list_enabled) ?> /> Enable portfolio list</label>
 
                     <h5><label for="group-portfolio-list-heading">List Heading</label></h5>
@@ -122,7 +122,7 @@ $group_label_uc = openlab_get_group_type_label('case=upper');
         <?php /* Group Avatar Settings */ ?>
         <?php if (bp_is_group_admin_screen('group-avatar')) : ?>
 
-    <?php if ('upload-image' == bp_get_avatar_admin_step()) : ?>
+            <?php if ('upload-image' == bp_get_avatar_admin_step()) : ?>
                 <p id="upload-group-avatar-title">Upload New Avatar</p>
                 <p id="upload-group-avatar-text"><?php _e("Upload an image to use as an avatar for this group. The image will be shown on the main group page, and in search results.", 'buddypress') ?></p>
 
@@ -132,7 +132,7 @@ $group_label_uc = openlab_get_group_type_label('case=upper');
                     <input type="hidden" name="action" id="action" value="bp_avatar_upload" />
                 </p>
 
-        <?php if (bp_get_group_has_avatar()) : ?>
+                <?php if (bp_get_group_has_avatar()) : ?>
 
                     <p id="delete-group-avatar-title">Delete Avatar</p>
                     <p id="delete-group-avatar-text"><?php _e("If you'd like to remove the existing avatar but not upload a new one, please use the delete avatar button.", 'buddypress') ?></p>
@@ -145,7 +145,7 @@ $group_label_uc = openlab_get_group_type_label('case=upper');
 
             <?php endif; ?>
 
-    <?php if ('crop-image' == bp_get_avatar_admin_step()) : ?>
+            <?php if ('crop-image' == bp_get_avatar_admin_step()) : ?>
 
                 <h3><?php _e('Crop Avatar', 'buddypress') ?></h3>
 
@@ -172,16 +172,16 @@ $group_label_uc = openlab_get_group_type_label('case=upper');
         <?php /* Manage Group Members */ ?>
         <?php if (bp_is_group_admin_screen('manage-members')) : ?>
 
-    <?php do_action('bp_before_group_manage_members_admin'); ?>
+            <?php do_action('bp_before_group_manage_members_admin'); ?>
 
             <div class="bp-widget">
                 <h4><?php _e('Administrators', 'buddypress'); ?></h4>
 
-    <?php if (bp_has_members('&include=' . bp_group_admin_ids())) : ?>
+                <?php if (bp_has_members('&include=' . bp_group_admin_ids())) : ?>
 
                     <div id="group-list" class="item-list inline-element-list row group-manage-members">
 
-        <?php while (bp_members()) : bp_the_member(); ?>
+                        <?php while (bp_members()) : bp_the_member(); ?>
                             <div class="col-md-8 group-item">
                                 <div class="group-item-wrapper admins <?php echo (count(bp_group_admin_ids(false, 'array')) > 1 ? '' : 'no-btn'); ?>">
                                     <div class="row info-row">
@@ -194,31 +194,31 @@ $group_label_uc = openlab_get_group_type_label('case=upper');
                                             </h4>
                                         </div>
                                     </div>
-            <?php if (count(bp_group_admin_ids(false, 'array')) > 1) : ?>
+                                    <?php if (count(bp_group_admin_ids(false, 'array')) > 1) : ?>
                                         <div class="row">
                                             <div class="col-sm-24">
                                                 <a class="btn btn-primary btn-block link-btn confirm admin-demote-to-member admins" href="<?php bp_group_member_demote_link(bp_get_member_user_id()); ?>"><?php _e('Demote to Member', 'buddypress'); ?></a>
                                             </div>
                                         </div>
-            <?php endif; ?>
+                                    <?php endif; ?>
                                 </div>
                             </div>
-        <?php endwhile; ?>
+                        <?php endwhile; ?>
 
                     </div>
 
-    <?php endif; ?>
+                <?php endif; ?>
 
             </div>
 
-    <?php if (bp_group_has_moderators()) : ?>
+            <?php if (bp_group_has_moderators()) : ?>
                 <div class="bp-widget">
                     <h4><?php _e('Moderators', 'buddypress'); ?></h4>
 
-        <?php if (bp_has_members('&include=' . bp_group_mod_ids())) : ?>
+                    <?php if (bp_has_members('&include=' . bp_group_mod_ids())) : ?>
                         <div id="group-list" class="item-list single-line inline-element-list row group-manage-members">
 
-            <?php while (bp_members()) : bp_the_member(); ?>
+                            <?php while (bp_members()) : bp_the_member(); ?>
                                 <div class="col-md-8 group-item">
                                     <div class="group-item-wrapper moderators">
                                         <div class="row info-row">
@@ -239,13 +239,13 @@ $group_label_uc = openlab_get_group_type_label('case=upper');
                                         </div>
                                     </div>
                                 </div>
-            <?php endwhile; ?>
+                            <?php endwhile; ?>
 
                         </div>
 
-                <?php endif; ?>
+                    <?php endif; ?>
                 </div>
-    <?php endif ?>
+            <?php endif ?>
 
 
             <div class="bp-widget">
@@ -253,24 +253,24 @@ $group_label_uc = openlab_get_group_type_label('case=upper');
 
                 <?php if (bp_group_has_members('per_page=15&exclude_banned=false')) : ?>
 
-        <?php if (bp_group_member_needs_pagination()) : ?>
+                    <?php if (bp_group_member_needs_pagination()) : ?>
 
                         <div class="pagination no-ajax">
 
                             <div id="member-count" class="pag-count">
-            <?php bp_group_member_pagination_count(); ?>
+                                <?php bp_group_member_pagination_count(); ?>
                             </div>
 
                             <div id="member-admin-pagination" class="pagination-links">
-            <?php bp_group_member_admin_pagination(); ?>
+                                <?php bp_group_member_admin_pagination(); ?>
                             </div>
 
                         </div>
 
-        <?php endif; ?>
+                    <?php endif; ?>
 
                     <div id="group-list" class="item-list inline-element-list row group-manage-members">
-        <?php while (bp_group_members()) : bp_group_the_member(); ?>
+                        <?php while (bp_group_members()) : bp_group_the_member(); ?>
 
                             <div class="col-md-8 group-item">
                                 <div class="group-item-wrapper members">
@@ -287,36 +287,36 @@ $group_label_uc = openlab_get_group_type_label('case=upper');
                                     </div>
                                     <div class="row">
                                         <div class="col-sm-24">
-            <?php if (bp_get_group_member_is_banned()) : ?>
+                                            <?php if (bp_get_group_member_is_banned()) : ?>
 
                                                 <a href="<?php bp_group_member_unban_link(); ?>" class="btn btn-primary btn-xs link-btn confirm member-unban" title="<?php _e('Unban this member', 'buddypress'); ?>"><?php _e('Remove Ban', 'buddypress'); ?></a>
 
-            <?php else : ?>
+                                            <?php else : ?>
 
                                                 <a href="<?php bp_group_member_ban_link(); ?>" class="btn btn-primary btn-xs link-btn confirm member-ban" title="<?php _e('Kick and ban this member', 'buddypress'); ?>"><?php _e('Kick &amp; Ban', 'buddypress'); ?></a>
                                                 <a href="<?php bp_group_member_promote_mod_link(); ?>" class="btn btn-primary btn-xs link-btn confirm member-promote-to-mod" title="<?php _e('Promote to Mod', 'buddypress'); ?>"><?php _e('Promote to Mod', 'buddypress'); ?></a>
                                                 <a href="<?php bp_group_member_promote_admin_link(); ?>" class="btn btn-primary btn-xs link-btn confirm member-promote-to-admin" title="<?php _e('Promote to Admin', 'buddypress'); ?>"><?php _e('Promote to Admin', 'buddypress'); ?></a>
 
-            <?php endif; ?>
+                                            <?php endif; ?>
 
                                             <a href="<?php bp_group_member_remove_link(); ?>" class="btn btn-primary btn-xs link-btn confirm" title="<?php _e('Remove this member', 'buddypress'); ?>"><?php _e('Remove from group', 'buddypress'); ?></a>
 
-            <?php do_action('bp_group_manage_members_admin_item'); ?>
+                                            <?php do_action('bp_group_manage_members_admin_item'); ?>
                                         </div>
                                     </div>
                                 </div>
                             </div>
 
-        <?php endwhile; ?>
+                        <?php endwhile; ?>
                     </div>
 
-    <?php else: ?>
+                <?php else: ?>
 
                     <div id="message" class="info">
                         <p><?php _e('This group has no members.', 'buddypress'); ?></p>
                     </div>
 
-    <?php endif; ?>
+                <?php endif; ?>
 
             </div>
 
@@ -329,17 +329,17 @@ $group_label_uc = openlab_get_group_type_label('case=upper');
 
             <?php do_action('bp_before_group_membership_requests_admin'); ?>
 
-    <?php if (bp_group_has_membership_requests()) : ?>
+            <?php if (bp_group_has_membership_requests()) : ?>
 
                 <ul id="request-list" class="item-list">
-        <?php while (bp_group_membership_requests()) : bp_group_the_membership_request(); ?>
+                    <?php while (bp_group_membership_requests()) : bp_group_the_membership_request(); ?>
 
                         <li>
-            <?php bp_group_request_user_avatar_thumb() ?>
+                            <?php bp_group_request_user_avatar_thumb() ?>
                             <h4><?php bp_group_request_user_link() ?> <span class="comments"><?php bp_group_request_comment() ?></span></h4>
                             <span class="activity"><?php bp_group_request_time_since_requested() ?></span>
 
-            <?php do_action('bp_group_membership_requests_admin_item'); ?>
+                            <?php do_action('bp_group_membership_requests_admin_item'); ?>
 
                             <div class="action">
 
@@ -347,15 +347,15 @@ $group_label_uc = openlab_get_group_type_label('case=upper');
 
                                 <?php bp_button(array('id' => 'group_membership_reject', 'component' => 'groups', 'wrapper_class' => 'reject', 'link_href' => bp_get_group_request_reject_link(), 'link_title' => __('Reject', 'buddypress'), 'link_text' => __('Reject', 'buddypress'))); ?>
 
-            <?php do_action('bp_group_membership_requests_admin_item_action'); ?>
+                                <?php do_action('bp_group_membership_requests_admin_item_action'); ?>
 
                             </div>
                         </li>
 
-        <?php endwhile; ?>
+                    <?php endwhile; ?>
                 </ul>
 
-    <?php else: ?>
+            <?php else: ?>
 
                 <div id="message" class="info">
                     <p><?php _e('There are no pending membership requests.', 'buddypress'); ?></p>
@@ -367,22 +367,20 @@ $group_label_uc = openlab_get_group_type_label('case=upper');
 
         <?php endif; ?>
 
-        <?php do_action('groups_custom_edit_steps') // Allow plugins to add custom group edit screens  ?>
-
         <?php /* Delete Group Option */ ?>
         <?php if (bp_is_group_admin_screen('delete-group')) : ?>
 
-    <?php do_action('bp_before_group_delete_admin'); ?>
+            <?php do_action('bp_before_group_delete_admin'); ?>
 
             <div id="message" class="info">
                 <p><?php printf('WARNING: Deleting this %s will completely remove ALL content associated with it. There is no way back, please be careful with this option.', openlab_get_group_type()); ?></p>
             </div>
 
             <input type="checkbox" name="delete-group-understand" id="delete-group-understand" value="1" onclick="if (this.checked) {
-                        document.getElementById('delete-group-button').disabled = '';
-                    } else {
-                        document.getElementById('delete-group-button').disabled = 'disabled';
-                    }" /> <?php printf('I understand the consequences of deleting this %s.', openlab_get_group_type()); ?>
+                            document.getElementById('delete-group-button').disabled = '';
+                        } else {
+                            document.getElementById('delete-group-button').disabled = 'disabled';
+                        }" /> <?php printf('I understand the consequences of deleting this %s.', openlab_get_group_type()); ?>
 
             <?php do_action('bp_after_group_delete_admin'); ?>
 
--- wp-content/themes/openlab/buddypress/groups/single/home.php
+++ wp-content/themes/openlab/buddypress/groups/single/home.php
@@ -3,97 +3,101 @@
  * Group single page
  *
  */
+if (bp_has_groups()) : while (bp_groups()) : bp_the_group();
+        ?>
 
-if ( bp_has_groups() ) : while ( bp_groups() ) : bp_the_group(); ?>
+        <?php do_action('bp_before_group_home_content'); ?>
 
-	<?php do_action( 'bp_before_group_home_content' ); ?>
+        <div class="col-sm-18 groups-single-home">
 
-	<div class="col-sm-18 groups-single-home">
+            <?php
+            do_action('bp_before_group_body');
 
-		<?php do_action( 'bp_before_group_body' );
+            /**
+             * Does this next bit look familiar? If not, go check out WordPress's
+             * /wp-includes/template-loader.php file.
+             *
+             * @todo A real template hierarchy? Gasp!
+             */
+            // Group is visible
+            if (bp_get_group_status() == 'public' || bp_get_group_status() == 'private') :
 
-		/**
-		 * Does this next bit look familiar? If not, go check out WordPress's
-		 * /wp-includes/template-loader.php file.
-		 *
-		 * @todo A real template hierarchy? Gasp!
-		 */
-                
-		// Group is visible
-		if ( bp_get_group_status() == 'public' || bp_get_group_status() == 'private' ) : 
+                // Looking at home location
+                if (bp_is_group_home()) :
 
-			// Looking at home location
-			if ( bp_is_group_home() ) :
+                    // Use custom front if one exists
+                    $custom_front = bp_locate_template(array('groups/single/front.php'), false, true);
+                    if (!empty($custom_front)) : load_template($custom_front, true);
 
-				// Use custom front if one exists
-				$custom_front = bp_locate_template( array( 'groups/single/front.php' ), false, true );
-				if     ( ! empty( $custom_front   ) ) : load_template( $custom_front, true );
+                    // Default to activity
+                    elseif (bp_is_active('activity')) : cuny_group_single();
 
-				// Default to activity
-				elseif ( bp_is_active( 'activity' ) ) : cuny_group_single();
+                    // Otherwise show members
+                    elseif (bp_is_active('members')) : bp_get_template_part('groups/single/members');
 
-				// Otherwise show members
-				elseif ( bp_is_active( 'members'  ) ) : bp_get_template_part( 'groups/single/members'  );
+                    endif;
 
-				endif;
-				
-			// Not looking at home
-			else :
+                // Not looking at home
+                else :
 
-				// Group Admin
-				if     ( bp_is_group_admin_page() ) : bp_get_template_part( 'groups/single/admin'        );
+                    // Group Admin
+                    if (bp_is_group_admin_page()) : bp_get_template_part('groups/single/admin');
 
-				// Group Activity
-				elseif ( bp_is_group_activity()   ) : bp_get_template_part( 'groups/single/activity'     );
+                    // Group Activity
+                    elseif (bp_is_group_activity()) : bp_get_template_part('groups/single/activity');
 
-				// Group Members
-				elseif ( bp_is_group_members()    ) : bp_get_template_part( 'groups/single/members'      );
+                    // Group Members
+                    elseif (bp_is_group_members()) : bp_get_template_part('groups/single/members');
 
-				// Group Invitations
-				elseif ( bp_is_group_invites()    ) : bp_get_template_part( 'groups/single/send-invites' );
+                    // Group Invitations
+                    elseif (bp_is_group_invites()) : bp_get_template_part('groups/single/send-invites');
 
-				// Old group forums
-				elseif ( bp_is_group_forum()      ) : bp_get_template_part( 'groups/single/forum'        );
-                                
-                                // Membership request
-                                elseif ( bp_is_group_membership_request() ) : bp_get_template_part( 'groups/single/request-membership' );
-                                
-                                // Email subscription options
-                                elseif (bp_current_action() == 'notifications' ) : bp_get_template_part( 'groups/single/notifications' );
-                                
-				// Anything else (plugins mostly)
-				else                                : bp_get_template_part( 'groups/single/plugins'      );
+                    // Old group forums
+                    elseif (bp_is_group_forum()) : bp_get_template_part('groups/single/forum');
 
-				endif;
-			endif;
+                    // Membership request
+                    elseif (bp_is_group_membership_request()) : bp_get_template_part('groups/single/request-membership');
 
-		// Group is not visible
-		elseif ( ! bp_group_is_visible() ) :
+                    // Email subscription options
+                    elseif (bp_current_action() == 'notifications') : bp_get_template_part('groups/single/notifications');
 
-			// Membership request
-			if ( bp_is_group_membership_request() ) :
-				bp_get_template_part( 'groups/single/request-membership' );
+                    // Anything else (plugins mostly)
+                    else : bp_get_template_part('groups/single/plugins');
 
-			// The group is not visible, show the status message
-			else :
+                    endif;
+                endif;
 
-				do_action( 'bp_before_group_status_message' ); ?>
+            // Group is not visible
+            elseif (!bp_group_is_visible()) :
 
-				<div id="message" class="info">
-					<p><?php bp_group_status_message(); ?></p>
-				</div>
+                // Membership request
+                if (bp_is_group_membership_request()) :
+                    bp_get_template_part('groups/single/request-membership');
 
-				<?php do_action( 'bp_after_group_status_message' );
+                // The group is not visible, show the status message
+                else :
 
-			endif;
-		endif;
+                    do_action('bp_before_group_status_message');
+                    ?>
 
-		do_action( 'bp_after_group_body' ); ?>
+                    <div id="message" class="info">
+                        <p><?php bp_group_status_message(); ?></p>
+                    </div>
 
-	</div><!-- #item-body -->
+                    <?php
+                    do_action('bp_after_group_status_message');
 
-	<?php do_action( 'bp_after_group_home_content' ); ?>
-        
-	<?php endwhile; endif; ?>
-        
-        <?php openlab_bp_sidebar('actions'); ?>
+                endif;
+            endif;
+
+            do_action('bp_after_group_body');
+            ?>
+
+        </div><!-- #item-body -->
+
+        <?php do_action('bp_after_group_home_content'); ?>
+
+    <?php endwhile;
+endif; ?>
+
+<?php openlab_bp_sidebar('actions'); ?>
