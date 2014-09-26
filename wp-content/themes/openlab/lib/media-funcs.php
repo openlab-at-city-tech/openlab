e636a9308bd7c9395bb2a733d8a53efb221198fe
Joe Unander <joe@joeunander.com>
2014-09-26 17:29:49 -0400

    Bug #1133 - Help Posts - Photos should not exceed 100% of column width
    This was resolved by first eliminating width and height attributes from
    images embedded into WP posts, then adding a max-width: 100% to all img
    tags inside of entry content

--- wp-content/themes/openlab/lib/media-funcs.php
+++ wp-content/themes/openlab/lib/media-funcs.php
@@ -30,3 +30,19 @@ function openlab_default_get_group_avatar($url, $params) {
 }
 
 add_filter('bp_core_fetch_avatar_url', 'openlab_default_get_group_avatar', 10, 2);
+
+/**
+ * WordPress adds dimensions to embedded images; this is totally not responsive WordPress
+ * @param type $html
+ * @return type
+ */
+function openlab_remove_thumbnail_dimensions($html) {
+    
+    $html = preg_replace('/(width|height)=\"\d*\"\s/', "", $html);
+    
+    return $html;
+}
+
+add_filter('post_thumbnail_html', 'openlab_remove_thumbnail_dimensions', 10);
+add_filter('image_send_to_editor', 'openlab_remove_thumbnail_dimensions', 10);
+add_filter('the_content', 'openlab_remove_thumbnail_dimensions', 10);
