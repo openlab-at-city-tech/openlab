69069375e44a6a8567675f559dd3409f7bb34bad
Joe Unander <joe@joeunander.com>
2014-09-26 18:40:57 -0400

    Bug #1127 - MOL Person Profile Pagetype - Fine-Tune CSS
    Some additional markup and styling tweaks

--- wp-content/themes/openlab/buddypress/members/single/sidebar.php
+++ wp-content/themes/openlab/buddypress/members/single/sidebar.php
@@ -27,7 +27,8 @@ if (!$dud = bp_displayed_user_domain()) {
 
             <?php /* Get a friend request count */ ?>
             <?php if (bp_is_active('friends')) : ?>
-                <?php $request_ids = friends_get_friendship_request_user_ids(bp_loggedin_user_id());
+                <?php
+                $request_ids = friends_get_friendship_request_user_ids(bp_loggedin_user_id());
                 $request_count = intval(count((array) $request_ids));
                 ?>
 
@@ -43,7 +44,8 @@ if (!$dud = bp_displayed_user_domain()) {
 
             <?php /* Get an invitation count */ ?>
             <?php if (bp_is_active('groups')) : ?>
-                <?php $invites = groups_get_invites_for_user();
+                <?php
+                $invites = groups_get_invites_for_user();
                 $invite_count = isset($invites['total']) ? (int) $invites['total'] : 0;
                 ?>
 
@@ -90,27 +92,25 @@ if (!$dud = bp_displayed_user_domain()) {
 
     <div class="sidebar-widget mol-menu" id="portfolio-sidebar-widget">
         <h2 class="sidebar-header">
-            <a href="<?php openlab_user_portfolio_url() ?>"><?php openlab_portfolio_label('user_id=' . $displayed_user_id . '&case=upper') ?> Site</a>
+    <?php openlab_portfolio_label('user_id=' . $displayed_user_id . '&case=upper') ?> Site
         </h2>
-        
+
         <div class="sidebar-block">
 
-        <ul class="sidebar-sublinks portfolio-sublinks">
+            <ul class="sidebar-sublinks portfolio-sublinks inline-element-list">
 
-            <li class="portfolio-profile-link">
-                <a href="<?php openlab_user_portfolio_profile_url() ?>">Profile</a>
-            </li>
-
-            <li class="portfolio-site-link">
-                <a href="<?php openlab_user_portfolio_url() ?>">Site</a>
-            </li>
-
-    <?php if (openlab_is_my_profile() && openlab_user_portfolio_site_is_local()) : ?>
-                <li class="portfolio-dashboard-link">
-                    <a href="<?php openlab_user_portfolio_url() ?>/wp-admin">Dashboard</a>
+                <li class="portfolio-profile-link bold">
+                    <span class="fa fa-external-link"></span><a class="bold" href="<?php openlab_user_portfolio_profile_url() ?>">Profile</a>
                 </li>
+
+                <li class="portfolio-site-link">
+                    <a href="<?php openlab_user_portfolio_url() ?>">Site</a>
+                    <?php if (openlab_is_my_profile() && openlab_user_portfolio_site_is_local()) : ?>
+                        | <a class="portfolio-dashboard-link" href="<?php openlab_user_portfolio_url() ?>/wp-admin">Dashboard</a>
     <?php endif ?>
-        </ul>
+                </li>
+
+            </ul>
         </div>
     </div>
 
@@ -119,7 +119,7 @@ if (!$dud = bp_displayed_user_domain()) {
     <div class="sidebar-widget" id="portfolio-sidebar-widget">
         <h2 class="sidebar-header">
             <div class="sidebar-block">
-            <a href="<?php openlab_portfolio_creation_url() ?>">+ Create <?php openlab_portfolio_label('leading_a=1&case=upper&user_id=' . $displayed_user_id) ?></a>
+                <a href="<?php openlab_portfolio_creation_url() ?>">+ Create <?php openlab_portfolio_label('leading_a=1&case=upper&user_id=' . $displayed_user_id) ?></a>
             </div>
         </h2>
     </div>
@@ -151,46 +151,46 @@ $activity_args = array(
 <?php endif ?>
 
 <?php if (bp_has_activities($activity_args)) : ?>
-<div class="sidebar-block">
-    <ul id="activity-stream" class="activity-list item-list">
-        <div>
-            <?php while (bp_activities()) : bp_the_activity(); ?>
+    <div class="sidebar-block">
+        <ul id="activity-stream" class="activity-list item-list inline-element-list sidebar-sublinks">
+            <div class="row">
+    <?php while (bp_activities()) : bp_the_activity(); ?>
 
-                <div class="activity-avatar">
-                    <a href="<?php bp_activity_user_link() ?>">
-                        <?php bp_activity_avatar('type=full&width=100&height=100') ?>
-                    </a>
-                </div>
-
-                <div class="activity-content">
-
-                    <div class="activity-header">
-                        <?php bp_activity_action() ?>
+                    <div class="activity-avatar col-sm-7">
+                        <a href="<?php bp_activity_user_link() ?>">
+                            <?php echo openlab_activity_user_avatar(); ?>
+                        </a>
                     </div>
 
-                    <?php if (bp_activity_has_content()) : ?>
-                        <div class="activity-inner">
-                            <?php bp_activity_content_body() ?>
+                    <div class="activity-content col-sm-17">
+
+                        <div class="activity-header">
+                        <?php bp_activity_action() ?>
                         </div>
-                    <?php endif; ?>
 
-                    <?php do_action('bp_activity_entry_content') ?>
+                        <?php if (bp_activity_has_content()) : ?>
+                            <div class="activity-inner">
+                            <?php bp_activity_content_body() ?>
+                            </div>
+        <?php endif; ?>
 
-                </div>
-                <hr style="clear:both" />
+        <?php do_action('bp_activity_entry_content') ?>
 
-            <?php endwhile; ?>
-        </div>
-    </ul>
-</div>
+                    </div>
+                    <hr style="clear:both" />
+
+    <?php endwhile; ?>
+            </div>
+        </ul>
+    </div>
 <?php else : ?>
     <div class="sidebar-block">
-    <ul id="activity-stream" class="activity-list item-list">
-        <div>
-            <div id="message" class="info">
-                <p><?php _e('No recent activity.', 'buddypress') ?></p>
+        <ul id="activity-stream" class="activity-list item-list">
+            <div>
+                <div id="message" class="info">
+                    <p><?php _e('No recent activity.', 'buddypress') ?></p>
+                </div>
             </div>
-        </div>
-    </ul>
+        </ul>
     </div>
 <?php endif; ?>
