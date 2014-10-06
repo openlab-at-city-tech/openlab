76e9c64ac058f703163e66023a8a752a78d97e7e
Joe Unander <joe@joeunander.com>
2014-10-06 17:01:48 -0400

    Admin bar: adding the OpenLab network menu to group sites

--- wp-content/mu-plugins/openlab-toolbar.php
+++ wp-content/mu-plugins/openlab-toolbar.php
@@ -66,8 +66,10 @@ class OpenLab_Admin_Bar {
 		// Removes the Search menu item
 		remove_action( 'admin_bar_menu', 'wp_admin_bar_search_menu', 4 );
                 
-                //taking out the admin network menu for now
-		//add_action( 'admin_bar_menu', array( $this, 'add_network_menu' ), 1 );
+                //restricting network menu to group sites only
+                if(get_current_blog_id() !== 1){
+                    add_action( 'admin_bar_menu', array( $this, 'add_network_menu' ), 1 );
+                }
                 
                 //adjust the padding at the top of the page
                 add_action('wp_head',array($this,'admin_bar_html_update'),99999);
@@ -134,7 +136,8 @@ class OpenLab_Admin_Bar {
 			'title'  => '<span class="openlab-open">Open</span>Lab', // Span is here in case you want to bold 'OPEN'
 			'href'   => bp_get_root_domain(),
 			'meta'	 => array(
-				'tabindex' => 90
+				'tabindex' => 90,
+                                'class' => 'admin-bar-menu',
 			)
  		) );
 
@@ -1050,6 +1053,14 @@ class OpenLab_Admin_Bar {
 	}
 
 	function enqueue_styles() {
+            global $wpdb;
+            
+            //getting the theme folder for the main site
+            $main_site_theme = $wpdb->get_var('SELECT option_value FROM wp_options WHERE option_name = "template"');
+            
+            //registering font-awesome here so it can be used on the admin bar and on the main site
+            wp_register_style('font-awesome', get_site_url(1,'wp-content/themes/'). $main_site_theme . '/css/font-awesome.min.css', array(), '20130604', 'all');
+            wp_enqueue_style('font-awesome');
                 //custom admin bar styles
                 wp_enqueue_style( 'admin-bar-custom', WP_CONTENT_URL . '/mu-plugins/css/admin-bar-custom.css',array('font-awesome') );
 		wp_enqueue_style( 'openlab-toolbar', WP_CONTENT_URL . '/mu-plugins/css/openlab-toolbar.css',array('font-awesome') );
--- wp-content/themes/openlab/functions.php
+++ wp-content/themes/openlab/functions.php
@@ -39,14 +39,12 @@ function openlab_load_scripts() {
     /**
      * scripts, additional functionality
      */
-    if (!is_admin()) {
 
+    if (!is_admin()) {
 
         //google fonts
         wp_register_style('google-open-sans', 'http://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600,600italic,700,700italic', array(), '2014', 'all');
         wp_enqueue_style('google-open-sans');
-        wp_register_style('font-awesome', get_stylesheet_directory_uri() . '/css/font-awesome.min.css', array(), '20130604', 'all');
-        wp_enqueue_style('font-awesome');
 
         wp_register_style('camera-js-styles', get_stylesheet_directory_uri() . '/css/camera.css', array(), '20130604', 'all');
         wp_enqueue_style('camera-js-styles');
