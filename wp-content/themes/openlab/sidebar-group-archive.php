89f89b8e5b70edaa6521ef712da066c49c9da6a8
Joe Unander <joe@joeunander.com>
2014-10-07 03:15:13 -0400

    Bug #1138 - People List Pagetype - fine-tune CSS
    Additional styling and markup tweaks for the People list page

--- wp-content/themes/openlab/sidebar-group-archive.php
+++ wp-content/themes/openlab/sidebar-group-archive.php
@@ -202,7 +202,7 @@ if ($group_type == 'not-archive' && $post_obj->post_title == "People") {
         </form>
 
         <div class="archive-search">
-            <h5 class="bold">Search</h5>
+            <h5 class="bold font-size font-14">Search</h5>
             <form method="get" class="form-inline btn-combo" role="form">
                 <div class="form-group">
                     <input id="search-terms" class="form-control" type="text" name="search" placeholder="Search" /><button class="btn btn-primary top-align" id="search-submit" type="submit"><i class="fa fa-search"></i></button>
