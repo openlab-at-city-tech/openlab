e3f7d45cf23bbfb59f8c96eb2daf3e5ab6a4bd50
Joe Unander <joe@joeunander.com>
2014-10-09 09:42:22 -0400

    Bug #1203 - Style activation page for sign up form

--- wp-content/themes/openlab/buddypress/members/activate.php
+++ wp-content/themes/openlab/buddypress/members/activate.php
@@ -5,14 +5,18 @@
 
 		<?php do_action( 'bp_before_activation_page' ) ?>
 
-		<div class="page" id="activate-page">
+		<div class="page row" id="activate-page">
+                    
+                    <div class="col-md-12">
+                    
+                    <div class="panel panel-default">
 
 			<?php do_action( 'template_notices' ) ?>
 
 			<?php if ( bp_account_was_activated() ) : ?>
 
-				<h2 class="widgettitle"><?php _e( 'Account Activated', 'buddypress' ) ?></h2>
-
+				<div class="panel-heading"><?php _e( 'Account Activated', 'buddypress' ) ?></div>
+                                <div class="panel-body">
 				<?php do_action( 'bp_before_activate_content' ) ?>
 
 				<?php if ( isset( $_GET['e'] ) ) : ?>
@@ -20,29 +24,35 @@
 				<?php else : ?>
 					<p><?php _e( 'Your account was activated successfully! You can now log in with the username and password you provided when you signed up.', 'buddypress' ) ?></p>
 				<?php endif; ?>
+                                </div>
 
 			<?php else : ?>
 
-				<h3><?php _e( 'Activate your Account', 'buddypress' ) ?></h3>
-
+				<div class="panel-heading"><?php _e( 'Activate your Account', 'buddypress' ) ?></div>
+                                <div class="panel-body">
 				<?php do_action( 'bp_before_activate_content' ) ?>
 
 				<p><?php _e( 'Please provide a valid activation key.', 'buddypress' ) ?></p>
 
-				<form action="" method="get" class="standard-form" id="activation-form">
+				<form action="" method="get" class="standard-form form" id="activation-form">
 
 					<label for="key"><?php _e( 'Activation Key:', 'buddypress' ) ?></label>
-					<input type="text" name="key" id="key" value="" />
+					<input class="form-control" type="text" name="key" id="key" value="" />
 
 					<p class="submit">
-						<input type="submit" name="submit" value="<?php _e( 'Activate', 'buddypress' ) ?> &rarr;" />
+						<input class="btn btn-primary" type="submit" name="submit" value="<?php _e( 'Activate', 'buddypress' ) ?> &#xf138;" />
 					</p>
 
 				</form>
+                                </div>
 
 			<?php endif; ?>
 
 			<?php do_action( 'bp_after_activate_content' ) ?>
+                                
+                    </div>
+                        
+                    </div>
 
 		</div><!-- .page -->
 
