3f8accd6c316ce0ddf29570940ab7f7b4dc05f58
Joe Unander <joe@joeunander.com>
2014-10-10 02:46:02 -0400

    Bug #1160 - on 2.1.4 My Invitations, boxes should be bigger

--- wp-content/themes/openlab/buddypress/members/single/groups/invites.php
+++ wp-content/themes/openlab/buddypress/members/single/groups/invites.php
@@ -10,17 +10,46 @@
                 <div class="group-item col-md-12">
                             <div class="group-item-wrapper">
                                 <div class="row info-row">
-                                    <div class="item-avatar alignleft col-sm-9">
+                                    <div class="item-avatar alignleft col-sm-7">
                                         <a href="<?php bp_group_permalink() ?>"><img class="img-responsive" src ="<?php echo bp_core_fetch_avatar(array('item_id' => bp_get_group_id(), 'object' => 'group', 'type' => 'full', 'html' => false)) ?>" alt="<?php echo $group->name; ?>"/></a>
                                     </div>
-                                    <div class="item col-sm-15">
-                                        <h4><a href="<?php bp_group_permalink() ?>"><?php openlab_shortened_text(bp_get_group_name(), 15); ?></a></h4>
-                                        <p class="members">
-                                            <span class="small"> ( <?php printf(__('%s members', 'buddypress'), bp_group_total_members(false)) ?> )</span>
-                                        </p>
-                                        <p class="desc">
-                                            <?php bp_group_description_excerpt() ?>
-                                        </p>
+                                    <div class="item col-sm-16">
+                                        <h2 class="item-title"><a href="<?php bp_group_permalink() ?>" title="<?php bp_group_name() ?>"><?php bp_group_name() ?></a></h2>
+                                        <?php
+                                        $group_type = openlab_get_group_type();
+                                        $group_id = bp_get_group_id();
+                                        //course group type
+                                        if ($group_type == 'course'):
+                                            ?>
+
+                                            <?php
+                                            $admins = groups_get_group_admins($group_id);
+                                            $faculty_id = $admins[0]->user_id;
+                                            $first_name = ucfirst(xprofile_get_field_data('First Name', $faculty_id));
+                                            $last_name = ucfirst(xprofile_get_field_data('Last Name', $faculty_id));
+                                            $wds_faculty = $first_name . " " . $last_name;
+                                            $wds_course_code = groups_get_groupmeta($group_id, 'wds_course_code');
+                                            $wds_semester = groups_get_groupmeta($group_id, 'wds_semester');
+                                            $wds_year = groups_get_groupmeta($group_id, 'wds_year');
+                                            $wds_departments = groups_get_groupmeta($group_id, 'wds_departments');
+                                            ?>
+                                            <div class="info-line uppercase"><?php echo $wds_faculty; ?> | <?php echo openlab_shortened_text($wds_departments, 20); ?> | <?php echo $wds_course_code; ?> | <span class="bold"><?php echo $wds_semester; ?> <?php echo $wds_year; ?></span></div>
+                                        <?php elseif ($group_type == 'portfolio'): ?>
+
+                                            <div class="info-line"><?php echo bp_core_get_userlink(openlab_get_user_id_from_portfolio_group_id(bp_get_group_id())); ?></div>
+
+                                        <?php endif; ?>
+
+                                        <?php
+                                        $len = strlen(bp_get_group_description());
+                                        if ($len > 135) {
+                                            $this_description = substr(bp_get_group_description(), 0, 135);
+                                            $this_description = str_replace("</p>", "", $this_description);
+                                            echo $this_description . '&hellip; <a href="' . bp_get_group_permalink() . '">See&nbsp;More</a></p>';
+                                        } else {
+                                            bp_group_description();
+                                        }
+                                        ?>
 
                                         <?php do_action('bp_group_invites_item') ?>
 
