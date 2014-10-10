3316ca1ad017c391635c3bf0ef21aae20bfc3e54
Boone B Gorges <boonebgorges@gmail.com>
2014-10-10 10:37:09 -0400

    Update plugin: Invite Anyone.
    This should improve serving of assets over SSL.

--- wp-content/plugins/invite-anyone/group-invites/group-invites.php
+++ wp-content/plugins/invite-anyone/group-invites/group-invites.php
@@ -7,10 +7,22 @@ function invite_anyone_add_js() {
 
 	if ( $bp->current_action == BP_INVITE_ANYONE_SLUG || ( isset( $bp->action_variables[1] ) && $bp->action_variables[1] == BP_INVITE_ANYONE_SLUG ) ) {
 
-		wp_enqueue_script( 'invite-anyone-autocomplete-js', plugins_url() . '/invite-anyone/group-invites/jquery.autocomplete/jquery.autocomplete-min.js', array( 'jquery' ) );
+		$min = '-min';
+		if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
+			$min = '';
+		}
+
+		wp_enqueue_script( 'invite-anyone-autocomplete-js', plugins_url() . "/invite-anyone/group-invites/jquery.autocomplete/jquery.autocomplete$min.js", array( 'jquery' ) );
 
 		wp_register_script( 'invite-anyone-js', plugins_url() . '/invite-anyone/group-invites/group-invites-js.js', array( 'invite-anyone-autocomplete-js' ) );
 		wp_enqueue_script( 'invite-anyone-js' );
+
+		// Add words that we need to use in JS to the end of the page
+		// so they can be translated and still used.
+		$params = apply_filters( 'ia_get_js_strings', array(
+			'unsent_invites'     => __( 'Click &ldquo;Send Invites&rdquo; to finish sending your new invitations.', 'invite-anyone' ),
+		) );
+		wp_localize_script( 'invite-anyone-js', 'IA_js_strings', $params );
 
 	}
 }
