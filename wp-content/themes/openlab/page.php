d1788dd1df0887314013890725589eb51d04821e
Joe Unander <joe@early-adopter.com>
2014-10-24 18:17:24 -0400

    Bug #1190 - 3.2 My Courses - Very Bottom of page displays extra page title

--- wp-content/themes/openlab/page.php
+++ wp-content/themes/openlab/page.php
@@ -2,25 +2,27 @@
 
 <div id="content" class="hfeed row">
     <?php
-    
     global $wp_query;
     $post = $wp_query->post;
     $postID = $post->ID;
     $parent = $post->post_parent;
-    
+
     if ($postID == "49" || $parent == "49") {
         openlab_bp_mobile_sidebar('about');
     }
     ?>
 
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-            <div <?php post_class('col-sm-18 col-xs-24'); ?>>
-                <div class="content-wrapper">
-                    <h1 class="entry-title"><?php the_title(); ?></h1>
-                    <div class="entry-content"><?php the_content(); ?></div>
-                </div>
-            </div><!--hentry-->
+    
+            <?php //my-<group> pages should not be displaying this ?>
+            <?php if (!strstr(get_the_title(),'My')): ?>
+                <div <?php post_class('col-sm-18 col-xs-24'); ?>>
+                    <div class="content-wrapper">
+                        <h1 class="entry-title"><?php the_title(); ?></h1>
+                        <div class="entry-content"><?php the_content(); ?></div>
+                    </div>
+                </div><!--hentry-->
+            <?php endif; ?>
 
             <?php
         endwhile;
@@ -28,7 +30,6 @@
     ?>
 
     <?php
-
 //add the about-page sidebar to just the about page and any child about page
     if ($postID == "49" || $parent == "49") {
         openlab_bp_sidebar('about');
