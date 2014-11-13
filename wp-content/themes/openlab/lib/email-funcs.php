e9424c1407f9bf2c9cd4318d8d328b5c359ce881
Joe Unander <joe@joeunander.com>
2014-11-13 17:45:06 -0500

    Feature #1278 - New Design for Confirmation/error messages: Group Silos
    Rolling back parsed messages on email notifications page - returned page
    takes user to top of the page, so multi-panel forms will always have their
    confirmation message reside there

--- wp-content/themes/openlab/lib/email-funcs.php
+++ wp-content/themes/openlab/lib/email-funcs.php
@@ -9,6 +9,8 @@
  */
 function openlab_ass_admin_notice_form() {
     global $bp;
+    
+    do_action('template_notices');
 
     if (groups_is_user_admin(bp_loggedin_user_id(), bp_get_current_group_id()) || is_super_admin()) {
         $submit_link = bp_get_groups_action_link('notifications');
@@ -21,8 +23,6 @@ function openlab_ass_admin_notice_form() {
                     <div class="panel-heading semibold"><?php _e('Send an email notice to everyone in the group', 'bp-ass'); ?></div>
                     <div class="panel-body">
                         
-                        <?php openlab_email_confirmation_messages('sent'); ?>
-
                         <p><?php _e('You can use the form below to send an email notice to all group members.', 'bp-ass'); ?> <br>
                             <b><?php _e('Everyone in the group will receive the email -- regardless of their email settings -- so use with caution', 'bp-ass'); ?></b>.</p>
 
@@ -50,8 +50,6 @@ function openlab_ass_admin_notice_form() {
                     <div class="panel-heading semibold"><?php _e('Welcome Email', 'bp-ass'); ?></div>
                     <div class="panel-body">
                         
-                        <?php openlab_email_confirmation_messages('welcome email'); ?>
-
                         <p><?php _e('Send an email when a new member join the group.', 'bp-ass'); ?></p>
 
                         <p class="checkbox">
@@ -147,12 +145,4 @@ function openlab_ass_group_subscribe_settings() {
         </form>
     </div><!-- end ass-email-subscriptions-options-page -->
     <?php
-}
-
-function openlab_email_confirmation_messages($search_str){
-    global $bp;
-    
-    if(strpos($bp->template_message,$search_str) > -1){
-        do_action('template_notices');
-    }
 }
