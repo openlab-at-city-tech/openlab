637d3647443def5f73e1c00722da118e976e6d4a
Joe Unander <joe@joeunander.com>
2014-11-21 17:22:15 -0500

    Merge:  SSL fixes:
    - manually entered nav menu URLs
    - attachment URLs
    
    See #1051
    
    Original: https://github.com/livinglab/openlab/commit/7b7453d53af427029592e0121c739b18d9ac4383

---
+++ wp-content/mu-plugins/openlab-ssl-filters.php
@@ -0,0 +1,63 @@
+<?php
+
+function openlab_ssl_fix( $text ) {
+	// Replacing the root domain will catch the specific blog domain
+	// because we aren't using subdomains
+	if ( is_ssl() ) {
+		$search  = set_url_scheme( bp_get_root_domain(), 'http' );
+		$replace = set_url_scheme( bp_get_root_domain(), 'https' );
+	} else {
+		$search  = set_url_scheme( bp_get_root_domain(), 'https' );
+		$replace = set_url_scheme( bp_get_root_domain(), 'http' );
+	}
+
+	return str_replace( $search, $replace, $text );
+}
+
+/**
+ * wp_nav_menu() fixes
+ *
+ * This has to be done in filters because people can enter their own links
+ * through the interface
+ */
+function openlab_ssl_filter_wp_nav_menu( $items ) {
+	foreach ( $items as $item ) {
+		$item->url = openlab_ssl_fix( $item->url );
+	}
+
+	return $items;
+}
+add_filter( 'wp_nav_menu_objects', 'openlab_ssl_filter_wp_nav_menu' );
+
+/**
+ * wp_upload_dir() - this fixes issues with attachment URLs throughout
+ */
+function openlab_ssl_upload_dir( $upload_dir ) {
+	$upload_dir['url']     = openlab_ssl_fix( $upload_dir['url'] );
+	$upload_dir['baseurl'] = openlab_ssl_fix( $upload_dir['baseurl'] );
+	return $upload_dir;
+}
+add_filter( 'upload_dir', 'openlab_ssl_upload_dir' );
+
+/**
+ * Widget callbacks
+ *
+ * This is needed for cac-featured-content (and maybe others)
+ */
+function openlab_ssl_widget_display_callback( $instance, $widget ) {
+	// A bit ham-handed, but whatevs
+	if ( ! empty( $instance ) ) {
+		$instance = array_map( 'openlab_ssl_fix', $instance );
+	}
+
+	return $instance;
+}
+add_filter( 'widget_display_callback', 'openlab_ssl_widget_display_callback', 10, 2 );
+
+/**
+ * Content
+ *
+ * This is very heavy-handed, but should be harmless, and is faster than
+ * preg_replace() to catch things like inline images.
+ */
+add_filter( 'the_content', 'openlab_ssl_fix' );
