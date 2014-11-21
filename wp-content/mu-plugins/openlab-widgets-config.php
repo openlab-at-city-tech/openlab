5b36092389599fd584375f7b306ad5db0d46995a
Joe Unander <joe@joeunander.com>
2014-11-21 15:05:10 -0500

    Merge:  Don't register a number of widgets on non-root blogs:
    - Invite Anyone
    - BuddyPress Friends
    - BuddyPress Login
    - BuddyPress Members
    - BuddyPress Sitewide Notices
    
    See #1082
    
    Original:
    https://github.com/livinglab/openlab/commit/a8afa896116731f4ec7be4f29439cdbe6d840847

---
+++ wp-content/mu-plugins/openlab-widgets-config.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Disable some widgets on non-root blog.
+ *
+ * Hooked at 99 because this is as late as we can go before WP loads them up.
+ */
+function openlab_disable_widgets_on_non_root_blogs() {
+    if (bp_is_root_blog()) {
+        return;
+    }
+    unregister_widget('InviteAnyoneWidget');
+    unregister_widget('BP_Core_Friends_Widget');
+    unregister_widget('BP_Core_Login_Widget');
+    unregister_widget('BP_Core_Members_Widget');
+    unregister_widget('BP_Messages_Sitewide_Notices_Widget');
+}
+
+add_action('widgets_init', 'openlab_disable_widgets_on_non_root_blogs', 99);
+
