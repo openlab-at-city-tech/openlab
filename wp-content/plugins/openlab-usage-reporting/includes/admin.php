9d2cb0df69399ec026e8c1b379d826d5d4a43b83
Joe Unander <joe@joeunander.com>
2014-11-25 15:23:00 -0500

    Merge: 1)  First pass at usage reporting tool.
    See #1008.
    
    Original: https://github.com/livinglab/openlab/commit/5939d3731e07f0e12c737cb3d7378ce5333f5a26
    
    2)  Disable unicode setting for usage-reporting plugin.
    
    Was breaking headers for some reason.
    
    Original: https://github.com/livinglab/openlab/commit/1cf69476feabeb630a54932dcb737c1995329125

---
+++ wp-content/plugins/openlab-usage-reporting/includes/admin.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * Create the menu item in the Network Admin.
+ */
+function olur_add_menu_item() {
+	$plugin_page = add_menu_page(
+		__( 'OpenLab Usage Reporting', 'openlab-usage-reporting' ),
+		__( 'OpenLab Usage', 'openlab-usage-reporting' ),
+		'manage_network_options',
+		'openlab-usage-reporting',
+		'olur_admin_panel'
+	);
+
+	add_action( 'admin_print_scripts-' . $plugin_page, 'olur_admin_assets' );
+}
+add_action( 'network_admin_menu', 'olur_add_menu_item' );
+
+/**
+ * Enqueue CSS and JS assets.
+ */
+function olur_admin_assets() {
+	wp_enqueue_style( 'openlab-usage-reporting', OLUR_PLUGIN_URL . 'assets/css/openlab-usage-reporting.css' );
+	wp_enqueue_script( 'openlab-usage-reporting', OLUR_PLUGIN_URL . 'assets/js/openlab-usage-reporting.js', array( 'jquery-ui-datepicker' ) );
+}
+
+/**
+ * Markup for the admin panel.
+ */
+function olur_admin_panel() {
+
+	?>
+	<div class="wrap">
+		<h2><?php _e( 'OpenLab Usage Reporting', 'openlab-usage-reporting' ) ?></h2>
+
+		<p><?php _e( 'Select a date range to generate a usage report.', 'openlab-usage-reporting' ) ?></p>
+
+		<div class="olur-dates">
+
+		<form method="post" action="">
+			<label for="olur-start"><?php _e( 'Start: ', 'openlab-usage-reporting' ) ?></label> <input type="text" class="olur-datepicker" name="olur-start" id="olur-start"><br />
+			<label for="olur-end"><?php _e( 'End: ', 'openlab-usage-reporting' ) ?></label> <input type="text" class="olur-datepicker" name="olur-end" id="olur-end">
+
+			<?php wp_nonce_field( 'olur-generate' ) ?>
+			<?php submit_button( 'Generate Report' ) ?>
+		</form>
+
+		</div>
+
+		<h3><?php _e( 'Information', 'openlab-usage-reporting' ) ?></h3>
+
+		<p><?php _e( 'Reports contain the following data.', 'openlab-usage-reporting' ) ?></p>
+
+		<h4><?php _e( 'For each user type (student, faculty, staff, alumni, other):', 'openlab-usage-reporting' ) ?></h4>
+		<ul>
+			<li><?php _e( 'Total counts for the start and end date.', 'openlab-usage-reporting' ) ?></li>
+			<li><?php _e( 'Number of users created during the period', 'openlab-usage-reporting' ) ?></li>
+			<li><?php _e( 'Number of users deleted during the period', 'openlab-usage-reporting' ) ?></li>
+		</ul>
+
+		<p><?php _e( 'Note that it is possible for users to change their own user type in some cases (eg, Student to Alumni). The report reflects <strong>current data</strong>.', 'openlab-usage-reporting' ) ?></p>
+
+
+		<h4><?php _e( 'For each group type (course, project, club, student ePortfolio, faculty Portfolio, staff Portfolio, each subdivided by privacy level):', 'openlab-usage-reporting' ) ?></h4>
+		<ul>
+			<li><?php _e( 'Total counts for the start and end date.', 'openlab-usage-reporting' ) ?></li>
+			<li><?php _e( 'Number of users created during the period', 'openlab-usage-reporting' ) ?></li>
+			<li><?php _e( 'Number of users deleted during the period', 'openlab-usage-reporting' ) ?></li>
+		</ul>
+	</div>
+	<?php
+}
+
+/**
+ * Catch submit requests and process.
+ */
+function olur_catch_generate_requests() {
+	if ( empty( $_POST['submit'] ) ) {
+		return;
+	}
+
+	if ( ! is_super_admin() ) {
+		return;
+	}
+
+	if ( empty( $_POST['olur-start'] ) || empty( $_POST['olur-end'] ) || empty( $_POST['_wpnonce'] ) ) {
+		return;
+	}
+
+	check_admin_referer( 'olur-generate' );
+
+	$start = stripslashes( $_POST['olur-start'] );
+	$end   = stripslashes( $_POST['olur-end'] );
+
+	$start = date( 'Y-m-d H:i:s', strtotime( $start ) );
+	$end   = date( 'Y-m-d H:i:s', strtotime( $end ) + ( 60 * 60 * 24 ) - 1 ); // Bump to remain inclusive
+
+	olur_generate_report( $start, $end );
+}
+add_action( 'admin_init', 'olur_catch_generate_requests', 0 );
---
+++ wp-content/plugins/openlab-usage-reporting/includes/report.php
@@ -0,0 +1,479 @@
+<?php
+
+/**
+ * Registered report callbacks.
+ *
+ * @return array
+ */
+function olur_report_callbacks() {
+	$callbacks = array(
+		// Users.
+		'user_student_counts' => 'olur_user_student_counts',
+		'user_faculty_counts' => 'olur_user_faculty_counts',
+		'user_staff_counts'   => 'olur_user_staff_counts',
+		'user_alumni_counts'  => 'olur_user_alumni_counts',
+		'user_other_counts'   => 'olur_user_other_counts',
+		'user_total_counts'   => 'olur_user_total_counts',
+
+		// Groups.
+		'group_course_public_counts' => 'olur_group_course_public_counts',
+		'group_course_private_counts' => 'olur_group_course_private_counts',
+		'group_course_hidden_counts' => 'olur_group_course_hidden_counts',
+
+		'group_club_public_counts' => 'olur_group_club_public_counts',
+		'group_club_private_counts' => 'olur_group_club_private_counts',
+		'group_club_hidden_counts' => 'olur_group_club_hidden_counts',
+
+		'group_project_public_counts' => 'olur_group_project_public_counts',
+		'group_project_private_counts' => 'olur_group_project_private_counts',
+		'group_project_hidden_counts' => 'olur_group_project_hidden_counts',
+
+		'group_student_eportfolio_public_counts' => 'olur_group_student_eportfolio_public_counts',
+		'group_student_eportfolio_private_counts' => 'olur_group_student_eportfolio_private_counts',
+		'group_student_eportfolio_hidden_counts' => 'olur_group_student_eportfolio_hidden_counts',
+
+		'group_faculty_portfolio_public_counts' => 'olur_group_faculty_portfolio_public_counts',
+		'group_faculty_portfolio_private_counts' => 'olur_group_faculty_portfolio_private_counts',
+		'group_faculty_portfolio_hidden_counts' => 'olur_group_faculty_portfolio_hidden_counts',
+
+
+		'group_staff_portfolio_public_counts' => 'olur_group_staff_portfolio_public_counts',
+		'group_staff_portfolio_private_counts' => 'olur_group_staff_portfolio_private_counts',
+		'group_staff_portfolio_hidden_counts' => 'olur_group_staff_portfolio_hidden_counts',
+	);
+
+	return $callbacks;
+}
+
+/**
+ * Generate a report and serve as a CSV.
+ *
+ * @param string $start MySQL-formatted start date.
+ * @param string $end MySQL-formatted end date.
+ */
+function olur_generate_report( $start, $end ) {
+	$data = olur_generate_report_data( $start, $end );
+
+	$filename = 'openlab-usage-' . date( 'Y-m-d', strtotime( $start ) ) . '-through-' . date( 'Y-m-d', strtotime( $end ) ) . '.csv';
+
+	$header_row = array(
+		0 => '',
+		1 => 'Start #',
+		2 => '# Created',
+		3 => '# Deleted',
+		4 => 'End #',
+	);
+
+	$data_rows = array();
+
+	$data_rows[] = array_merge(
+		array( 'Students' ),
+		array_values( $data['user_student_counts'] )
+	);
+
+	$data_rows[] = array_merge(
+		array( 'Faculty' ),
+		array_values( $data['user_faculty_counts'] )
+	);
+
+	$data_rows[] = array_merge(
+		array( 'Staff' ),
+		array_values( $data['user_staff_counts'] )
+	);
+
+	$data_rows[] = array_merge(
+		array( 'Alumni' ),
+		array_values( $data['user_alumni_counts'] )
+	);
+
+	$data_rows[] = array_merge(
+		array( 'Other' ),
+		array_values( $data['user_other_counts'] )
+	);
+
+	$data_rows[] = array_merge(
+		array( 'Total' ),
+		array_values( $data['user_total_counts'] )
+	);
+
+	// Blank
+	$data_rows[] = array();
+
+	// Courses
+	$data_rows[] = array_merge(
+		array( 'Courses (Public)' ),
+		array_values( $data['group_course_public_counts'] )
+	);
+
+	$data_rows[] = array_merge(
+		array( 'Courses (Private)' ),
+		array_values( $data['group_course_private_counts'] )
+	);
+
+	$data_rows[] = array_merge(
+		array( 'Courses (Hidden)' ),
+		array_values( $data['group_course_hidden_counts'] )
+	);
+
+	// Blank
+	$data_rows[] = array();
+
+	// Clubs.
+	$data_rows[] = array_merge(
+		array( 'Clubs (Public)' ),
+		array_values( $data['group_club_public_counts'] )
+	);
+
+	$data_rows[] = array_merge(
+		array( 'Clubs (Private)' ),
+		array_values( $data['group_club_private_counts'] )
+	);
+
+	$data_rows[] = array_merge(
+		array( 'Clubs (Hidden)' ),
+		array_values( $data['group_club_hidden_counts'] )
+	);
+
+	// Blank
+	$data_rows[] = array();
+
+	// Projects.
+	$data_rows[] = array_merge(
+		array( 'Projects (Public)' ),
+		array_values( $data['group_project_public_counts'] )
+	);
+
+	$data_rows[] = array_merge(
+		array( 'Projects (Private)' ),
+		array_values( $data['group_project_private_counts'] )
+	);
+
+	$data_rows[] = array_merge(
+		array( 'Projects (Hidden)' ),
+		array_values( $data['group_project_hidden_counts'] )
+	);
+
+	// Blank
+	$data_rows[] = array();
+
+	// Student ePortfolios.
+	$data_rows[] = array_merge(
+		array( 'Student ePortfolios (Public)' ),
+		array_values( $data['group_student_eportfolio_public_counts'] )
+	);
+
+	$data_rows[] = array_merge(
+		array( 'Student ePortfolios (Private)' ),
+		array_values( $data['group_student_eportfolio_private_counts'] )
+	);
+
+	$data_rows[] = array_merge(
+		array( 'Student ePortfolios (Hidden)' ),
+		array_values( $data['group_student_eportfolio_hidden_counts'] )
+	);
+
+	// Blank
+	$data_rows[] = array();
+
+	// Faculty Portfolio.
+	$data_rows[] = array_merge(
+		array( 'Faculty Portfolios (Public)' ),
+		array_values( $data['group_faculty_portfolio_public_counts'] )
+	);
+
+	$data_rows[] = array_merge(
+		array( 'Faculty Portfolios (Private)' ),
+		array_values( $data['group_faculty_portfolio_private_counts'] )
+	);
+
+	$data_rows[] = array_merge(
+		array( 'Faculty Portfolios (Hidden)' ),
+		array_values( $data['group_faculty_portfolio_hidden_counts'] )
+	);
+
+	// Blank
+	$data_rows[] = array();
+
+	// Staff Portfolio.
+	$data_rows[] = array_merge(
+		array( 'Staff Portfolios (Public)' ),
+		array_values( $data['group_staff_portfolio_public_counts'] )
+	);
+
+	$data_rows[] = array_merge(
+		array( 'Staff Portfolios (Private)' ),
+		array_values( $data['group_staff_portfolio_private_counts'] )
+	);
+
+	$data_rows[] = array_merge(
+		array( 'Staff Portfolios (Hidden)' ),
+		array_values( $data['group_staff_portfolio_hidden_counts'] )
+	);
+
+	$fh = @fopen( 'php://output', 'w' );
+
+	//fprintf( $fh, chr(0xEF) . chr(0xBB) . chr(0xBF) );
+
+	header( 'Cache-Control: must-revalidate, post-check=0, pre-check=0' );
+	header( 'Content-Description: File Transfer' );
+	header( 'Content-type: text/csv' );
+	header( "Content-Disposition: attachment; filename={$filename}" );
+	header( 'Expires: 0' );
+	header( 'Pragma: public' );
+
+	fputcsv( $fh, $header_row );
+
+	foreach ( $data_rows as $data_row ) {
+		fputcsv( $fh, $data_row );
+	}
+
+	fclose( $fh );
+	die();
+}
+
+/**
+ * Generate report data.
+ *
+ * @param string $start MySQL-formatted start date.
+ * @param string $end MySQL-formatted end date.
+ * @return array
+ */
+function olur_generate_report_data( $start, $end ) {
+	$data = array();
+	$callbacks = olur_report_callbacks();
+
+	foreach ( $callbacks as $cb_label => $cb ) {
+		$data[ $cb_label ] = call_user_func( $cb, $start, $end );
+	}
+
+	return $data;
+}
+
+/** Callbacks ****************************************************************/
+
+function olur_user_counts( $start, $end, $user_type ) {
+	global $wpdb;
+
+	$counts = array(
+		'start'   => '',
+		'created' => '',
+		'deleted' => '',
+		'end'     => '',
+	);
+
+	$bp = buddypress();
+	$ut_subquery = $wpdb->prepare( "SELECT user_id FROM {$bp->profile->table_name_data} WHERE field_id = 7 AND value = %s", $user_type );
+
+	// Start
+	$counts['start'] = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM {$wpdb->users} WHERE deleted != 1 AND spam != 1 AND ID IN ({$ut_subquery}) AND user_registered < %s", $start ) );
+
+	// End
+	$counts['end'] = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM {$wpdb->users} WHERE deleted != 1 AND spam != 1 AND ID IN ({$ut_subquery}) AND user_registered < %s", $end ) );
+
+	// Created
+	$counts['created'] = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM {$wpdb->users} WHERE deleted != 1 AND spam != 1 AND ID IN ({$ut_subquery}) AND user_registered >= %s AND user_registered < %s", $start, $end ) );
+
+	return array_map( 'intval', $counts );
+}
+
+function olur_user_student_counts( $start, $end ) {
+	return olur_user_counts( $start, $end, 'Student' );
+}
+
+function olur_user_faculty_counts( $start, $end ) {
+	return olur_user_counts( $start, $end, 'Faculty' );
+}
+
+function olur_user_staff_counts( $start, $end ) {
+	return olur_user_counts( $start, $end, 'Staff' );
+}
+
+function olur_user_alumni_counts( $start, $end ) {
+	return olur_user_counts( $start, $end, 'Alumni' );
+}
+
+function olur_user_other_counts( $start, $end ) {
+	global $wpdb;
+
+	$counts = array(
+		'start'   => '',
+		'created' => '',
+		'deleted' => '',
+		'end'     => '',
+	);
+
+	$bp = buddypress();
+
+	// Note that this is for a NOT IN.
+	$student_subquery = "SELECT user_id FROM {$bp->profile->table_name_data} WHERE field_id = 7 AND value IN ('Student','Faculty','Staff','Alumni')";
+
+	// Start
+	$counts['start'] = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM {$wpdb->users} WHERE deleted != 1 AND spam != 1 AND ID NOT IN ({$student_subquery}) AND user_registered < %s", $start ) );
+
+	// End
+	$counts['end'] = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM {$wpdb->users} WHERE deleted != 1 AND spam != 1 AND ID NOT IN ({$student_subquery}) AND user_registered < %s", $end ) );
+
+	// Created
+	$counts['created'] = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM {$wpdb->users} WHERE deleted != 1 AND spam != 1 AND ID NOT IN ({$student_subquery}) AND user_registered >= %s AND user_registered < %s", $start, $end ) );
+
+	return array_map( 'intval', $counts );
+}
+
+function olur_user_total_counts( $start, $end ) {
+	global $wpdb;
+
+	$counts = array(
+		'start'   => '',
+		'created' => '',
+		'deleted' => '',
+		'end'     => '',
+	);
+
+	$bp = buddypress();
+
+	// Start
+	$counts['start'] = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM {$wpdb->users} WHERE deleted != 1 AND spam != 1 AND user_registered < %s", $start ) );
+
+	// End
+	$counts['end'] = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM {$wpdb->users} WHERE deleted != 1 AND spam != 1 AND user_registered < %s", $end ) );
+
+	// Created
+	$counts['created'] = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM {$wpdb->users} WHERE deleted != 1 AND spam != 1 AND user_registered >= %s AND user_registered < %s", $start, $end ) );
+
+	return array_map( 'intval', $counts );
+}
+
+function olur_group_course_public_counts( $start, $end ) {
+	return olur_group_counts( $start, $end, 'course', 'public' );
+}
+
+function olur_group_course_private_counts( $start, $end ) {
+	return olur_group_counts( $start, $end, 'course', 'private' );
+}
+
+function olur_group_course_hidden_counts( $start, $end ) {
+	return olur_group_counts( $start, $end, 'course', 'hidden' );
+}
+
+function olur_group_club_public_counts( $start, $end ) {
+	return olur_group_counts( $start, $end, 'club', 'public' );
+}
+
+function olur_group_club_private_counts( $start, $end ) {
+	return olur_group_counts( $start, $end, 'club', 'private' );
+}
+
+function olur_group_club_hidden_counts( $start, $end ) {
+	return olur_group_counts( $start, $end, 'club', 'hidden' );
+}
+
+function olur_group_project_public_counts( $start, $end ) {
+	return olur_group_counts( $start, $end, 'project', 'public' );
+}
+
+function olur_group_project_private_counts( $start, $end ) {
+	return olur_group_counts( $start, $end, 'project', 'private' );
+}
+
+function olur_group_project_hidden_counts( $start, $end ) {
+	return olur_group_counts( $start, $end, 'project', 'hidden' );
+}
+
+function olur_group_student_eportfolio_public_counts( $start, $end ) {
+	return olur_portfolio_counts( $start, $end, 'public', 'student' );
+}
+
+function olur_group_student_eportfolio_private_counts( $start, $end ) {
+	return olur_portfolio_counts( $start, $end, 'private', 'student' );
+}
+
+function olur_group_student_eportfolio_hidden_counts( $start, $end ) {
+	return olur_portfolio_counts( $start, $end, 'hidden', 'student' );
+}
+
+function olur_group_faculty_portfolio_public_counts( $start, $end ) {
+	return olur_portfolio_counts( $start, $end, 'public', 'faculty' );
+}
+
+function olur_group_faculty_portfolio_private_counts( $start, $end ) {
+	return olur_portfolio_counts( $start, $end, 'private', 'faculty' );
+}
+
+function olur_group_faculty_portfolio_hidden_counts( $start, $end ) {
+	return olur_portfolio_counts( $start, $end, 'hidden', 'faculty' );
+}
+
+function olur_group_staff_portfolio_public_counts( $start, $end ) {
+	return olur_portfolio_counts( $start, $end, 'public', 'staff' );
+}
+
+function olur_group_staff_portfolio_private_counts( $start, $end ) {
+	return olur_portfolio_counts( $start, $end, 'private', 'staff' );
+}
+
+function olur_group_staff_portfolio_hidden_counts( $start, $end ) {
+	return olur_portfolio_counts( $start, $end, 'hidden', 'staff' );
+}
+
+function olur_group_counts( $start, $end, $group_type, $group_status ) {
+	global $wpdb;
+
+	$counts = array(
+		'start'   => '',
+		'created' => '',
+		'deleted' => '',
+		'end'     => '',
+	);
+
+	$bp = buddypress();
+	$gt_subquery = $wpdb->prepare( "SELECT group_id FROM {$bp->groups->table_name_groupmeta} WHERE meta_key = 'wds_group_type' AND meta_value = %s", $group_type );
+
+	$statuses = array();
+	foreach ( (array) $group_status as $status ) {
+		$statuses[] = $wpdb->prepare( '%s', $status );
+	}
+	$status_sql = implode( ', ', $statuses );
+
+	// Start
+	$counts['start'] = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM {$bp->groups->table_name} WHERE id IN ({$gt_subquery}) AND status IN ({$status_sql}) AND date_created < %s", $start ) );
+
+	// End
+	$counts['end'] = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM {$bp->groups->table_name} WHERE id IN ({$gt_subquery}) AND status IN ({$status_sql}) AND date_created < %s", $end ) );
+
+	// Created
+	$counts['created'] = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM {$bp->groups->table_name} WHERE id IN ({$gt_subquery}) AND status IN ({$status_sql}) AND date_created >= %s AND date_created < %s", $start, $end ) );
+
+	return array_map( 'intval', $counts );
+}
+
+function olur_portfolio_counts( $start, $end, $group_status, $user_type ) {
+	global $wpdb;
+
+	$counts = array(
+		'start'   => '',
+		'created' => '',
+		'deleted' => '',
+		'end'     => '',
+	);
+
+	$bp = buddypress();
+	$gt_subquery = "SELECT group_id FROM {$bp->groups->table_name_groupmeta} WHERE meta_key = 'wds_group_type' AND meta_value = 'portfolio'";
+	$ut_subquery = $wpdb->prepare( "SELECT p.user_id FROM {$bp->profile->table_name_data} p WHERE p.field_id = 7 AND p.value = %s", ucwords( $user_type ) );
+
+	$statuses = array();
+	foreach ( (array) $group_status as $status ) {
+		$statuses[] = $wpdb->prepare( '%s', $status );
+	}
+	$status_sql = implode( ', ', $statuses );
+
+	// Start
+	$counts['start'] = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM {$bp->groups->table_name} g INNER JOIN {$wpdb->usermeta} um ON (g.id = um.meta_value AND um.meta_key = 'portfolio_group_id') WHERE g.id IN ({$gt_subquery}) AND um.user_id IN ({$ut_subquery}) AND g.status IN ({$status_sql}) AND g.date_created < %s", $start ) );
+
+	// End
+	$counts['end'] = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM {$bp->groups->table_name} g INNER JOIN {$wpdb->usermeta} um ON (g.id = um.user_id AND um.meta_key = 'portfolio_group_id') WHERE g.id IN ({$gt_subquery}) AND um.user_id IN ({$ut_subquery}) AND g.status IN ({$status_sql}) AND g.date_created < %s", $end ) );
+
+	// Created
+	$counts['created'] = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM {$bp->groups->table_name} g INNER JOIN {$wpdb->usermeta} um ON (g.id = um.user_id AND um.meta_key = 'portfolio_group_id') WHERE g.id IN ({$gt_subquery}) AND um.user_id IN ({$ut_subquery}) AND g.status IN ({$status_sql}) AND g.date_created >= %s AND g.date_created < %s", $start, $end ) );
+
+	return array_map( 'intval', $counts );
+}
