e6eacf992b64dc47a2f932c5b5da3244cc23b977
Joe Unander <joe@joeunander.com>
2014-11-25 13:12:47 -0500

    Merge:  Ensure that forums are enable for all groups at creation, without requiring extra creation step.
    See #1212.
    
    Original: https://github.com/livinglab/openlab/commit/38e6c371ac00a0d0853bcbf996d2b042c9eba06d

--- wp-content/plugins/wds-citytech/wds-citytech-bp.php
+++ wp-content/plugins/wds-citytech/wds-citytech-bp.php
@@ -286,6 +286,62 @@ function openlab_group_creation_redirect( $redirect ) {
 add_filter( 'wp_redirect', 'openlab_group_creation_redirect' );
 
 /**
+ * Don't show a bbPress step during group creation.
+ */
+function openlab_remove_forum_step_from_group_creation() {
+    $gcs = buddypress()->groups->group_creation_steps;
+    if (isset($gcs['forum'])) {
+        unset($gcs['forum']);
+    }
+    buddypress()->groups->group_creation_steps = $gcs;
+}
+
+add_action('bp_actions', 'openlab_remove_forum_step_from_group_creation', 9);
+
+/**
+ * Create bbPress 2.x forum for newly created groups.
+ */
+function openlab_create_forum_on_group_creation($group_id, $member, $group) {
+// Set the default forum status
+    switch ($group->status) {
+        case 'hidden' :
+            $status = bbp_get_hidden_status_id();
+            break;
+        case 'private' :
+            $status = bbp_get_private_status_id();
+            break;
+        case 'public' :
+        default :
+            $status = bbp_get_public_status_id();
+            break;
+    }
+// Create the initial forum
+    $forum_id = bbp_insert_forum(array(
+        'post_parent' => bbp_get_group_forums_root_id(),
+        'post_title' => $group->name,
+        'post_content' => $group->description,
+        'post_status' => $status
+            ));
+    bbp_add_forum_id_to_group($group_id, $forum_id);
+    bbp_add_group_id_to_forum($forum_id, $group_id);
+// Update forum active
+    groups_update_groupmeta($group_id, '_bbp_forum_enabled_' . $forum_id, true);
+// Set forum enabled status
+    $group->enable_forum = 1;
+// Save the group
+    $group->save();
+    bbp_repair_forum_visibility();
+}
+
+add_action('groups_create_group', 'openlab_create_forum_on_group_creation', 10, 3);
+/**
+ * Force group forums to be active.
+ *
+ * This is redundant but for some reason bbPress requires it.
+ */
+add_filter('bp_get_new_group_enable_forum', '__return_true');
+
+/**
  * When getting a blog avatar in the context of the Featured Content widget, test to see whether
  * the blog is associated with a group. If so, fetch the group avatar instead
  */
