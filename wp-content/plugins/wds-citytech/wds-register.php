openlab-at-city-tech/openlab

commit c2d28a964814c0b86861aa0e5d910baaa9dec108

Non-City Tech signup, first pass.
See #1237.

wp-content/plugins/wds-citytech/wds-register.php
@@ -81,32 +81,38 @@ function wds_email_validate() {
 	if ( !defined( 'OPENLAB_SKIP_EMAIL_CHECK' ) || !OPENLAB_SKIP_EMAIL_CHECK ) {
 
 		$email = $_POST['signup_email'];
-		/*$email2 = $email;
-		$email = explode ( '.', $email );
-		$email2 = explode ( '@', $email[0] );
-		$domains = array_merge ($email, $email2);*/
-		//$error = 1;
+		$email_parts = explode( '@', $email );
+		$domain = isset( $email_parts[1] ) ? stripslashes( $email_parts[1] ) : '';
 
-		//print_r ($domains);
-		//Students
-		if($_POST['field_7']=="Student"){
-			//if ( in_array('mail', $domains) && in_array('citytech', $domains) && in_array('cuny', $domains) && in_array('edu', $domains) && $_POST['field_7']=="Student") {
-			//}else{
-				$pos = strrpos($email, "@mail.citytech.cuny.edu");
-				if ($pos === false) {
-				$bp->signup->errors['signup_email'] = 'Students must register with an @mail.citytech.cuny.edu e-mail address!';
+		$account_type = isset( $_POST['field_7'] ) ? stripslashes( $_POST['field_7'] ) : 'Student';
+
+		switch ( $account_type ) {
+			case 'Student' :
+				if ( 'mail.citytech.cuny.edu' !== $domain ) {
+					$bp->signup->errors['signup_email'] = 'Students must register with an @mail.citytech.cuny.edu e-mail address!';
+				}
+				break;
+
+			case 'Faculty' :
+			case 'Staff' :
+				if ( 'citytech.cuny.edu' !== $domain ) {
+					$bp->signup->errors['signup_email'] = 'You must register with an @citytech.cuny.edu e-mail address!';
 				}
 
-			//}
-		//}elseif ( in_array('citytech', $domains) && in_array('cuny', $domains) && in_array('edu', $domains) ) {
+				break;
 
-		}else{
-			$pos = strrpos($email, "@citytech.cuny.edu");
-			if ($pos === false) {
-				$bp->signup->errors['signup_email'] = 'You must register with an @citytech.cuny.edu e-mail address!';
-			}
+			case 'Non-City Tech' :
+				$code = '';
+				if ( isset( $_POST['signup_validation_code'] ) ) {
+					$code = stripslashes( $_POST['signup_validation_code'] );
+				}
+
+				if ( ! cac_ncs_validate_code( $code ) ) {
+					$bp->signup->errors['signup_email'] = 'Non-City Tech addresses need a valid registration code to sign up for the OpenLab.';
+
+				}
+				break;
 		}
-
 	}
 
 	// Check that the email addresses match

wp-content/plugins/cac-non-cuny-signup/cac-non-cuny-signup.php
@@ -1,0 +1,355 @@
+<?php
+/**
+ * AJAX handler for registration email check
+ *
+ * Return values:
+ *   1: success
+ *   2: no email provided
+ *   3: not a valid email address
+ *   4: unsafe
+ *   5: not in domain whitelist
+ *   6: email exists
+ *   7: a known undergraduate student address
+ */
+function cac_ajax_email_check() {
+	$email = isset( $_POST['email'] ) ? $_POST['email'] : false;
+
+	$retval = '1';
+
+	if ( !$email ) {
+		$retval = '2'; // no email
+	} else {
+		if ( !is_email( $email ) ) {
+			$retval = '3'; // Not an email address
+		} else if ( function_exists( 'is_email_address_unsafe' ) && is_email_address_unsafe( $email ) ) {
+			$retval = '4'; // Unsafe
+		} else if ( ! cac_ncs_email_domain_is_in_whitelist( $email ) ) {
+			$retval = '5';
+		} else if ( email_exists( $email ) ) {
+			$retval = '6';
+		}
+	}
+
+	die( $retval );
+}
+add_action( 'wp_ajax_cac_ajax_email_check', 'cac_ajax_email_check' );
+add_action( 'wp_ajax_nopriv_cac_ajax_email_check', 'cac_ajax_email_check' );
+
+/**
+ * AJAX handler for registration code check
+ *
+ * Return values:
+ *   1: success
+ *   0: failure
+ */
+function cac_ajax_vcode_check() {
+	$vcode = isset( $_POST['code'] ) ? $_POST['code'] : '';
+
+	$retval = cac_ncs_validate_code( $vcode ) ? '1' : '0';
+
+	echo $retval;
+	die();
+}
+add_action( 'wp_ajax_cac_ajax_vcode_check', 'cac_ajax_vcode_check' );
+add_action( 'wp_ajax_nopriv_cac_ajax_vcode_check', 'cac_ajax_vcode_check' );
+
+/**
+ * Verify that an email address is from a whitelisted domain.
+ */
+function cac_ncs_email_domain_is_in_whitelist( $email ) {
+	$domains = array(
+		'mail.citytech.cuny.edu',
+		'citytech.cuny.edu',
+	);
+
+	$email = explode( '@', trim( $email ) );
+
+	if ( ! isset( $email[1] ) || ! in_array( $email[1], $domains, true ) ) {
+		return false;
+	}
+
+	return true;
+}
+
+/**
+ * Checks non-CUNY validation codes
+ */
+function cac_check_signup_validation_code( $result ) {
+
+	foreach( $result['errors']->errors as $error_key => $error ) {
+		if ( in_array( 'Sorry, that email address is not allowed!', $error ) ) {
+			// Check for a validation code
+			if ( isset( $_POST['signup_validation_code'] ) ) {
+				$vcode = $_POST['signup_validation_code'];
+
+				if ( cac_ncs_validate_code( $vcode ) ) {
+					unset( $result['errors']->errors['user_email'] );
+				} else {
+
+					// Otherwise we will have to add a new error and hook something into
+
+					// the registration template. See how BP does this.
+
+					$result['errors']->errors['user_email'][0] = 'Non-CUNY registrations are only permitted with a specially provided signup code.';
+				}
+			}
+		}
+	}
+
+	return $result;
+}
+add_filter( 'bp_core_validate_user_signup', 'cac_check_signup_validation_code', 5 );
+
+/**
+ * At user signup, grab the code details and stash in wp_signup usermeta
+ *
+ * This will then have to be loaded at activation time and recorded in wp_usermeta
+ */
+function cac_ncs_signup_meta( $usermeta ) {
+	if ( isset( $_POST['signup_validation_code'] ) ) {
+		$data = CAC_NCS_Schema::get_code_data( $_POST['signup_validation_code'] );
+		$usermeta['cac_signup_code'] = $data;
+	}
+
+	return $usermeta;
+}
+add_action( 'bp_signup_usermeta', 'cac_ncs_signup_meta' );
+
+/**
+ * At user activation, grabs CAC signup code data and moves to usermeta
+ */
+function cac_ncs_activation_meta( $user_id, $key, $user ) {
+
+	if ( isset( $user['meta']['cac_signup_code'] ) ) {
+		update_user_meta( $user_id, 'cac_signup_code_data', $user['meta']['cac_signup_code'] );
+		update_user_meta( $user_id, 'cac_signup_code', $user['meta']['cac_signup_code']->ID );
+
+		if ( isset( $user['meta']['cac_signup_code']->groups ) ) {
+			foreach ( (array)$user['meta']['cac_signup_code']->groups as $group_id ) {
+				groups_join_group( $group_id, $user_id );
+			}
+		}
+	}
+}
+add_action( 'bp_core_activated_user', 'cac_ncs_activation_meta', 10, 3 );
+
+/**
+ * Validates a code
+ */
+function cac_ncs_validate_code( $code = false ) {
+	return CAC_NCS_Schema::validate_code( $code );
+}
+
+/**
+ * Data schema class
+ */
+class CAC_NCS_Schema {
+	var $post_type_name;
+	var $search_terms;
+
+	function __construct() {
+		$this->post_type_name = 'cac_ncs_code';
+
+		add_action( 'init', array( $this, 'register_post_type' ), 99 );
+		add_action( 'admin_init', array( $this, 'add_meta_boxes' ) );
+		add_action( 'save_post', array( $this, 'save_meta' ) );
+
+		add_action( 'admin_print_scripts', array( $this, 'admin_scripts' ) );
+		add_action( 'admin_print_styles', array( $this, 'admin_styles' ) );
+		add_action( 'wp_ajax_cac_ncs_groups_query', array( $this, 'cac_ncs_groups_query' ) );
+	}
+
+	function register_post_type() {
+		$labels = array(
+			'name'		=> 'Non-CUNY Signup Codes',
+			'singular_name' => 'Signup Code',
+			'add_new'	=> 'Add New',
+			'all_items'	=> 'All Signup Codes',
+			'add_new_item'	=> 'Add New Signup Code',
+			'edit_item'	=> 'Edit Signup Code',
+			'new_item'	=> 'New Signup Code',
+			'view_item'	=> 'View Signup Code',
+			'search_items'	=> 'Search Signup Codes',
+			'not_found'	=> 'No Signup Codes found',
+			'not_found_in_trash' => 'No Signup Codes found in trash'
+		);
+
+		register_post_type( $this->post_type_name, array(
+			'labels' => $labels,
+			'public' => false,
+			'show_in_menu' => current_user_can( 'delete_users' ),
+			'show_ui' => true,
+			'supports' => array( 'title' )
+		) );
+	}
+
+	function add_meta_boxes() {
+		add_meta_box( 'cac_ncs_vcode', "Code", array( $this, 'code_meta_box_render' ), $this->post_type_name, "normal", "high" );
+		add_meta_box( 'cac_ncs_groups', "Add to Groups", array( $this, 'group_meta_box_render' ), $this->post_type_name, "normal" );
+	}
+
+	function code_meta_box_render() {
+		global $post;
+
+		$vcode = isset( $post->ID ) ? get_post_meta( $post->ID, 'cac_ncs_vcode', true ) : '';
+
+		echo '<input type="text" name="cac_ncs_vcode" value="' . esc_attr( $vcode ) . '" />';
+	}
+
+	function group_meta_box_render() {
+		global $post;
+
+		$groups = isset( $post->ID ) ? get_post_meta( $post->ID, 'cac_ncs_groups', true ) : array();
+
+		if ( !$groups )
+			$groups = array();
+
+		echo '<input type="text" id="cac_ncs_groups" name="cac_ncs_groups" />';
+
+		echo '<ul class="cac-ncs-groups-results">';
+		foreach( $groups as $group_id ) {
+			$group_obj = new BP_Groups_Group( $group_id );
+
+			if ( empty( $group_obj->name ) )
+				continue;
+
+			echo '<li id="cac-nsc-add-to-group-' . $group_id . '">' . $group_obj->name . ' <span class="cac-nsc-remove-group"><a href="#">x</a></span></li>';
+		}
+		echo '</ul>';
+
+		echo '<input type="hidden" id="cac_nsc_group_ids" name="cac_ncs_group_ids" value="' . implode( ',', $groups ) . '" />';
+	}
+
+	function save_meta( $post_id ) {
+		global $post;
+
+		// WP's autosave javascript will wipe out our custom fields
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return $post_id;
+		}
+
+		$vcode = isset( $_POST['cac_ncs_vcode'] ) ? $_POST['cac_ncs_vcode'] : '';
+		$groups = isset( $_POST['cac_ncs_group_ids'] ) ? explode( ',', $_POST['cac_ncs_group_ids'] ) : '';
+
+		if ( isset( $_POST['post_type'] ) ) {
+			$post_type = $_POST['post_type'];
+		} else if ( isset( $post->post_type ) ) {
+			$post_type = $post->post_type;
+		} else {
+			$post_type = '';
+		}
+
+		if ( $post_type == $this->post_type_name ) {
+			update_post_meta( $post_id, 'cac_ncs_vcode', $vcode );
+			update_post_meta( $post_id, 'cac_ncs_groups', explode( ',', $_POST['cac_ncs_group_ids'] ) );
+		}
+
+		return $post_id;
+	}
+
+	/**
+	 * STATIC METHODS
+	 */
+
+	function get_wp_query_args( $code = false ) {
+		$args = array(
+			'post_type'  => 'cac_ncs_code',
+			'meta_query' => array(
+				array(
+					'key' => 'cac_ncs_vcode',
+					'value' => $code,
+					'compare' => '='
+				)
+			)
+		);
+
+		return $args;
+	}
+
+	function validate_code( $code = false ) {
+		$validate = false;
+
+		if ( $code ) {
+			$args = self::get_wp_query_args( $code );
+
+			$vcodes = new WP_Query( $args );
+
+			$validate = $vcodes->have_posts();
+		}
+
+		return $validate;
+	}
+
+	function get_code_data( $code = false ) {
+		$data = false;
+
+		if ( $code ) {
+			$args = self::get_wp_query_args( $code );
+
+			$vcodes = new WP_Query( $args );
+
+			// Avoid the BS and just take the first one
+			$data = isset( $vcodes->posts[0] ) ? $vcodes->posts[0] : false;
+
+			// Pull up the postmeta
+			$data->vcode = get_post_meta( $data->ID, 'cac_ncs_vcode', true );
+			$data->groups = get_post_meta( $data->ID, 'cac_ncs_groups', true );
+		}
+
+		return $data;
+	}
+
+	function admin_scripts() {
+		global $post;
+
+		if ( isset( $post->post_type ) && $this->post_type_name == $post->post_type ) {
+			wp_enqueue_script( 'suggest' );
+			wp_enqueue_script( 'cac_ncs_js', WP_CONTENT_URL . '/plugins/cac-non-cuny-signup/js/admin.js', array( 'jquery', 'suggest' ) );
+		}
+	}
+
+	function admin_styles() {
+		global $post;
+
+		if ( isset( $post->post_type ) && $this->post_type_name == $post->post_type ) {
+			wp_enqueue_style( 'cac_ncs_css', WP_CONTENT_URL . '/plugins/cac-non-cuny-signup/css/admin.css' );
+		}
+	}
+
+	function cac_ncs_groups_query() {
+		$q = isset( $_REQUEST['query'] ) ? urldecode( $_REQUEST['query'] ) : false;
+		$retval = array(
+			'query' 	=> $q,
+			'suggestions' 	=> array(),
+			'data' 		=> array()
+		);
+
+		if ( $q ) {
+			$this->search_terms = $q;
+
+			// Shitty but necessary. 'search_terms' is too generous
+			add_filter( 'bp_groups_get_paged_groups_sql', array( $this, 'filter_group_sql' ) );
+			if ( bp_has_groups() ) {
+				while ( bp_groups() ) {
+					bp_the_group();
+					$retval['suggestions'][] = bp_get_group_name();
+					$retval['data'][]	 = bp_get_group_id();
+				}
+			}
+		}
+
+		die( json_encode( $retval ) );
+	}
+
+	function filter_group_sql( $sql ) {
+		global $bp;
+
+		$sqla = explode( 'WHERE', $sql );
+
+		$new_sql = $sqla[0] . ' WHERE g.name LIKE "%' . $this->search_terms . '%" AND ' . $sqla[1];
+
+		return $new_sql;
+	}
+
+}
+$cac_ncs_schema = new CAC_NCS_Schema;

wp-content/plugins/cac-non-cuny-signup/css/admin.css
@@ -1,0 +1,9 @@
+.autocomplete-w1 { background:#fff; position:absolute; top:0px; left:0px; margin:8px 0 0 6px; /* IE6 fix: */ _background:none; _margin:0; }
+.autocomplete { border:1px solid #999; background:#FFF; cursor:default; text-align:left; max-height:350px; overflow:auto; margin:-6px 6px 6px -6px; /* IE6 specific: */ _height:350px;  _margin:0; _overflow-x:hidden; }
+.autocomplete .selected { background:#F0F0F0; }
+.autocomplete div { padding:2px 5px; white-space:nowrap; }
+.autocomplete strong { font-weight:normal; color:#3399FF; }
+
+input#cac_ncs_groups {
+	width: 90%;
+}

wp-content/plugins/cac-non-cuny-signup/js/admin.js
@@ -1,0 +1,447 @@
+jQuery(document).ready(function($) {
+	$('input#cac_ncs_groups').autocomplete( { 
+		serviceUrl: ajaxurl,
+		params: {
+			'action' : 'cac_ncs_groups_query'
+		},
+		deferRequestBy: 200,
+		onSelect: function( value, data ) {
+			var newitem = '<li id="cac-nsc-add-to-group-' + data + '">' + value + ' <span class="cac-nsc-remove-group"><a href="#">x</a></span></li>';
+			$('ul.cac-ncs-groups-results').append(newitem);
+			
+			/* Add id to the list */
+			var groupids = $('#cac_nsc_group_ids').val();
+
+			if ( !groupids ) {
+				var newgroupids = [ data ];
+			} else {
+				var newgroupids = groupids.split( ',' );
+				newgroupids.push( data );
+			}
+			
+			$('#cac_nsc_group_ids').val(newgroupids);
+			
+			$('input#cac_ncs_groups').val('');
+			
+			$('.cac-nsc-remove-group a').bind( 'click', function() { remove_item(this); } );
+		}
+	} );
+	
+	$('.cac-nsc-remove-group a').bind( 'click', function() { remove_item(this); } );
+	
+},(jQuery));
+
+function remove_item( item ){
+	var j = jQuery;
+	
+	var groupid = j(item).parent().parent().attr('id').split('cac-nsc-add-to-group-').pop();
+	
+	/* Remove from group ids list */
+	var cgroupids = j('#cac_nsc_group_ids').val().split(',');
+	
+	j(cgroupids).each(function(index, value){
+		if ( groupid == value ) {
+			cgroupids.splice(index,1);
+		}
+	});
+	
+	j('#cac_nsc_group_ids').val(cgroupids);
+	
+	/* Remove the list item itself */
+	j('li#cac-nsc-add-to-group-' + groupid).remove();
+				
+				
+}
+
+/**
+*  Ajax Autocomplete for jQuery, version 1.1.3
+*  (c) 2010 Tomas Kirda
+*
+*  Ajax Autocomplete for jQuery is freely distributable under the terms of an MIT-style license.
+*  For details, see the web site: http://www.devbridge.com/projects/autocomplete/jquery/
+*
+*  Last Review: 04/19/2010
+*/
+
+/*jslint onevar: true, evil: true, nomen: true, eqeqeq: true, bitwise: true, regexp: true, newcap: true, immed: true */
+/*global window: true, document: true, clearInterval: true, setInterval: true, jQuery: true */
+
+(function($) {
+
+  var reEscape = new RegExp('(\\' + ['/', '.', '*', '+', '?', '|', '(', ')', '[', ']', '{', '}', '\\'].join('|\\') + ')', 'g');
+
+  function fnFormatResult(value, data, currentValue) {
+    var pattern = '(' + currentValue.replace(reEscape, '\\$1') + ')';
+    return value.replace(new RegExp(pattern, 'gi'), '<strong>$1<\/strong>');
+  }
+
+  function Autocomplete(el, options) {
+    this.el = $(el);
+    this.el.attr('autocomplete', 'off');
+    this.suggestions = [];
+    this.data = [];
+    this.badQueries = [];
+    this.selectedIndex = -1;
+    this.currentValue = this.el.val();
+    this.intervalId = 0;
+    this.cachedResponse = [];
+    this.onChangeInterval = null;
+    this.ignoreValueChange = false;
+    this.serviceUrl = options.serviceUrl;
+    this.isLocal = false;
+    this.options = {
+      autoSubmit: false,
+      minChars: 1,
+      maxHeight: 300,
+      deferRequestBy: 0,
+      width: 0,
+      highlight: true,
+      params: {},
+      fnFormatResult: fnFormatResult,
+      delimiter: null,
+      zIndex: 9999
+    };
+    this.initialize();
+    this.setOptions(options);
+  }
+  
+  $.fn.autocomplete = function(options) {
+    return new Autocomplete(this.get(0)||$('<input />'), options);
+  };
+
+
+  Autocomplete.prototype = {
+
+    killerFn: null,
+
+    initialize: function() {
+
+      var me, uid, autocompleteElId;
+      me = this;
+      uid = Math.floor(Math.random()*0x100000).toString(16);
+      autocompleteElId = 'Autocomplete_' + uid;
+
+      this.killerFn = function(e) {
+        if ($(e.target).parents('.autocomplete').size() === 0) {
+          me.killSuggestions();
+          me.disableKillerFn();
+        }
+      };
+
+      if (!this.options.width) { this.options.width = this.el.width(); }
+      this.mainContainerId = 'AutocompleteContainter_' + uid;
+
+      $('<div id="' + this.mainContainerId + '" style="position:absolute;z-index:9999;"><div class="autocomplete-w1"><div class="autocomplete" id="' + autocompleteElId + '" style="display:none; width:300px;"></div></div></div>').appendTo('body');
+
+      this.container = $('#' + autocompleteElId);
+      this.fixPosition();
+      if (window.opera) {
+        this.el.keypress(function(e) { me.onKeyPress(e); });
+      } else {
+        this.el.keydown(function(e) { me.onKeyPress(e); });
+      }
+      this.el.keyup(function(e) { me.onKeyUp(e); });
+      this.el.blur(function() { me.enableKillerFn(); });
+      this.el.focus(function() { me.fixPosition(); });
+    },
+    
+    setOptions: function(options){
+      var o = this.options;
+      $.extend(o, options);
+      if(o.lookup){
+        this.isLocal = true;
+        if($.isArray(o.lookup)){ o.lookup = { suggestions:o.lookup, data:[] }; }
+      }
+      $('#'+this.mainContainerId).css({ zIndex:o.zIndex });
+      this.container.css({ maxHeight: o.maxHeight + 'px', width:o.width });
+    },
+    
+    clearCache: function(){
+      this.cachedResponse = [];
+      this.badQueries = [];
+    },
+    
+    disable: function(){
+      this.disabled = true;
+    },
+    
+    enable: function(){
+      this.disabled = false;
+    },
+
+    fixPosition: function() {
+      var offset = this.el.offset();
+      $('#' + this.mainContainerId).css({ top: (offset.top + this.el.innerHeight()) + 'px', left: offset.left + 'px' });
+    },
+
+    enableKillerFn: function() {
+      var me = this;
+      $(document).bind('click', me.killerFn);
+    },
+
+    disableKillerFn: function() {
+      var me = this;
+      $(document).unbind('click', me.killerFn);
+    },
+
+    killSuggestions: function() {
+      var me = this;
+      this.stopKillSuggestions();
+      this.intervalId = window.setInterval(function() { me.hide(); me.stopKillSuggestions(); }, 300);
+    },
+
+    stopKillSuggestions: function() {
+      window.clearInterval(this.intervalId);
+    },
+
+    onKeyPress: function(e) {
+      if (this.disabled || !this.enabled) { return; }
+      // return will exit the function
+      // and event will not be prevented
+      switch (e.keyCode) {
+        case 27: //KEY_ESC:
+          this.el.val(this.currentValue);
+          this.hide();
+          break;
+        case 9: //KEY_TAB:
+        case 13: //KEY_RETURN:
+          if (this.selectedIndex === -1) {
+            this.hide();
+            return;
+          }
+          this.select(this.selectedIndex);
+          if(e.keyCode === 9){ return; }
+          break;
+        case 38: //KEY_UP:
+          this.moveUp();
+          break;
+        case 40: //KEY_DOWN:
+          this.moveDown();
+          break;
+        default:
+          return;
+      }
+      e.stopImmediatePropagation();
+      e.preventDefault();
+    },
+
+    onKeyUp: function(e) {
+      if(this.disabled){ return; }
+      switch (e.keyCode) {
+        case 38: //KEY_UP:
+        case 40: //KEY_DOWN:
+          return;
+      }
+      clearInterval(this.onChangeInterval);
+      if (this.currentValue !== this.el.val()) {
+        if (this.options.deferRequestBy > 0) {
+          // Defer lookup in case when value changes very quickly:
+          var me = this;
+          this.onChangeInterval = setInterval(function() { me.onValueChange(); }, this.options.deferRequestBy);
+        } else {
+          this.onValueChange();
+        }
+      }
+    },
+
+    onValueChange: function() {
+      clearInterval(this.onChangeInterval);
+      this.currentValue = this.el.val();
+      var q = this.getQuery(this.currentValue);
+      this.selectedIndex = -1;
+      if (this.ignoreValueChange) {
+        this.ignoreValueChange = false;
+        return;
+      }
+      if (q === '' || q.length < this.options.minChars) {
+        this.hide();
+      } else {
+        this.getSuggestions(q);
+      }
+    },
+
+    getQuery: function(val) {
+      var d, arr;
+      d = this.options.delimiter;
+      if (!d) { return $.trim(val); }
+      arr = val.split(d);
+      return $.trim(arr[arr.length - 1]);
+    },
+
+    getSuggestionsLocal: function(q) {
+      var ret, arr, len, val, i;
+      arr = this.options.lookup;
+      len = arr.suggestions.length;
+      ret = { suggestions:[], data:[] };
+      q = q.toLowerCase();
+      for(i=0; i< len; i++){
+        val = arr.suggestions[i];
+        if(val.toLowerCase().indexOf(q) === 0){
+          ret.suggestions.push(val);
+          ret.data.push(arr.data[i]);
+        }
+      }
+      return ret;
+    },
+    
+    getSuggestions: function(q) {
+      var cr, me;
+      cr = this.isLocal ? this.getSuggestionsLocal(q) : this.cachedResponse[q];
+      
+      if (cr && $.isArray(cr.suggestions)) {
+        this.suggestions = cr.suggestions;
+        this.data = cr.data;
+        this.suggest();
+      } else if (!this.isBadQuery(q)) {
+        me = this;
+        me.options.params.query = q;
+        $.get(this.serviceUrl, me.options.params, function(txt) { me.processResponse(txt); }, 'text');
+      }
+    },
+
+    isBadQuery: function(q) {
+      var i = this.badQueries.length;
+      while (i--) {
+        if (q.indexOf(this.badQueries[i]) === 0) { return true; }
+      }
+      return false;
+    },
+
+    hide: function() {
+      this.enabled = false;
+      this.selectedIndex = -1;
+      this.container.hide();
+    },
+
+    suggest: function() {
+      if (this.suggestions.length === 0) {
+        this.hide();
+        return;
+      }
+
+      var me, len, div, f, v, i, s, mOver, mClick;
+      me = this;
+      len = this.suggestions.length;
+      f = this.options.fnFormatResult;
+      v = this.getQuery(this.currentValue);
+      mOver = function(xi) { return function() { me.activate(xi); }; };
+      mClick = function(xi) { return function() { me.select(xi); }; };
+      this.container.hide().empty();
+      for (i = 0; i < len; i++) {
+        s = this.suggestions[i];
+        div = $((me.selectedIndex === i ? '<div class="selected"' : '<div') + ' title="' + s + '">' + f(s, this.data[i], v) + '</div>');
+        div.mouseover(mOver(i));
+        div.click(mClick(i));
+        this.container.append(div);
+      }
+      this.enabled = true;
+      this.container.show();
+    },
+
+    processResponse: function(text) {
+      var response;
+      try {
+        response = eval('(' + text + ')');
+      } catch (err) { return; }
+      if (!$.isArray(response.data)) { response.data = []; }
+      if(!this.options.noCache){
+        this.cachedResponse[response.query] = response;
+        if (response.suggestions.length === 0) { this.badQueries.push(response.query); }
+      }
+      if (response.query === this.getQuery(this.currentValue)) {
+       
+       this.suggestions = response.suggestions;
+        this.data = response.data;
+        this.suggest(); 
+      }
+    },
+
+    activate: function(index) {
+      var divs, activeItem;
+      divs = this.container.children();
+      // Clear previous selection:
+      if (this.selectedIndex !== -1 && divs.length > this.selectedIndex) {
+        $(divs.get(this.selectedIndex)).removeClass();
+      }
+      this.selectedIndex = index;
+      if (this.selectedIndex !== -1 && divs.length > this.selectedIndex) {
+        activeItem = divs.get(this.selectedIndex);
+        $(activeItem).addClass('selected');
+      }
+      return activeItem;
+    },
+
+    deactivate: function(div, index) {
+      div.className = '';
+      if (this.selectedIndex === index) { this.selectedIndex = -1; }
+    },
+
+    select: function(i) {
+      var selectedValue, f;
+      selectedValue = this.suggestions[i];
+      if (selectedValue) {
+        this.el.val(selectedValue);
+        if (this.options.autoSubmit) {
+          f = this.el.parents('form');
+          if (f.length > 0) { f.get(0).submit(); }
+        }
+        this.ignoreValueChange = true;
+        this.hide();
+        this.onSelect(i);
+      }
+    },
+
+    moveUp: function() {
+      if (this.selectedIndex === -1) { return; }
+      if (this.selectedIndex === 0) {
+        this.container.children().get(0).className = '';
+        this.selectedIndex = -1;
+        this.el.val(this.currentValue);
+        return;
+      }
+      this.adjustScroll(this.selectedIndex - 1);
+    },
+
+    moveDown: function() {
+      if (this.selectedIndex === (this.suggestions.length - 1)) { return; }
+      this.adjustScroll(this.selectedIndex + 1);
+    },
+
+    adjustScroll: function(i) {
+      var activeItem, offsetTop, upperBound, lowerBound;
+      activeItem = this.activate(i);
+      offsetTop = activeItem.offsetTop;
+      upperBound = this.container.scrollTop();
+      lowerBound = upperBound + this.options.maxHeight - 25;
+      if (offsetTop < upperBound) {
+        this.container.scrollTop(offsetTop);
+      } else if (offsetTop > lowerBound) {
+        this.container.scrollTop(offsetTop - this.options.maxHeight + 25);
+      }
+      this.el.val(this.getValue(this.suggestions[i]));
+    },
+
+    onSelect: function(i) {
+      var me, fn, s, d;
+      me = this;
+      fn = me.options.onSelect;
+      s = me.suggestions[i];
+      d = me.data[i];
+      me.el.val(me.getValue(s));
+      if ($.isFunction(fn)) { fn(s, d, me.el); }
+    },
+    
+    getValue: function(value){
+        var del, currVal, arr, me;
+        me = this;
+        del = me.options.delimiter;
+        if (!del) { return value; }
+        currVal = me.currentValue;
+        arr = currVal.split(del);
+        if (arr.length === 1) { return value; }
+        return currVal.substr(0, currVal.length - arr[arr.length - 1].length) + value;
+    }
+
+  };
+
+}(jQuery));

wp-content/plugins/cac-non-cuny-signup/js/register.js
@@ -1,0 +1,2 @@
+( function( $ ) {
+}( jQuery ) );

wp-content/plugins/cac-non-cuny-signup/loader.php
@@ -1,0 +1,15 @@
+<?php
+
+/*
+Plugin Name: CAC Non-CUNY Signup
+Description: Allows admin to enter validation codes for non-CUNY email addresses to register
+Version: 1.0
+Author: Boone Gorges
+*/
+
+function cac_non_cuny_signup_loader() {
+	require( dirname( __FILE__ ) . '/cac-non-cuny-signup.php' );
+}
+add_action( 'bp_include', 'cac_non_cuny_signup_loader' );
+
+?>

commit 67df31b8b6a82ac730dd9ba12217fc18c2f82b6a

Enforce 'citytech.cuny.edu' email whitelist for Alumni.
The refactor of the signup process in c2d28a964814c0b86861aa0e5d910baaa9dec108
inadvertently made the restriction optional in the case of Alumni, something
that was discovered to be vulnerable when POSTing directly to the register page.

See #1337.

wp-content/plugins/wds-citytech/wds-register.php
@@ -88,6 +88,7 @@ function wds_email_validate() {
 
 		switch ( $account_type ) {
 			case 'Student' :
+			case 'Alumni' :
 				if ( 'mail.citytech.cuny.edu' !== $domain ) {
 					$bp->signup->errors['signup_email'] = 'Students must register with an @mail.citytech.cuny.edu e-mail address!';
 				}