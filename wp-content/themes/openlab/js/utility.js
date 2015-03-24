9f1256fc2ccdfc6db7dafd274ba96eafaaf20038
Joe Unander <joe@joeunander.com>
2015-03-24 15:53:12 -0400

    Bug #1426 - Mobile: Hover is creating a tappable area - trying more things

--- wp-content/themes/openlab/js/utility.js
+++ wp-content/themes/openlab/js/utility.js
@@ -5,10 +5,12 @@
             $cloned_related_link_fields;
 
     $(document).ready(function () {
-        
-        //resolving issue with hover states on mobile
-        if(isBreakpoint('xs') || isBreakpoint('sm')){
-            hoverTouchUnstick();
+
+        //fixing hover issues on mobile
+        if (isBreakpoint('xs') || isBreakpoint('sm')) {
+            $('.mobile-no-hover').bind('touchend', function () {
+                fixHoverOnMobile($(this));
+            })
         }
 
         // Workshop fields on Contact Us
@@ -189,12 +191,14 @@
     });//end document.ready
 
     $(window).resize(function () {
-        
-        //resolving issue with hover states on mobile
-        if(isBreakpoint('xs') || isBreakpoint('sm')){
-            hoverTouchUnstick();
+
+        //fixing hover issues on mobile
+        if (isBreakpoint('xs') || isBreakpoint('sm')) {
+            $('.mobile-no-hover').bind('touchend', function () {
+                fixHoverOnMobile($(this));
+            })
         }
-        
+
         //resetting the search on resize
         if ($('.search-trigger-wrapper').length) {
             var adminBar = $('#wpadminbar');
@@ -382,36 +386,15 @@
         return $('.device-' + alias).is(':visible');
     }
 
-    /**
-     * Converts :hover CSS to :active CSS on mobile devices.
-     * Otherwise, when tapping a button on a mobile device, the button stays in
-     * the :hover state until the button is pressed.
-     *
-     * Inspired by: https://gist.github.com/rcmachado/7303143
-     * @author Michael Vartan <michael@mvartan.com>
-     * @version 1.0
-     * @date 2014-12-20
-     */
-    function hoverTouchUnstick() {
-        // Check if the device supports touch events
-        if ('ontouchstart' in document.documentElement) {
-            // Loop through each stylesheet
-            for (var sheetI = document.styleSheets.length - 1; sheetI >= 0; sheetI--) {
-                var sheet = document.styleSheets[sheetI];
-                // Verify if cssRules exists in sheet
-                if (sheet.cssRules) {
-                    // Loop through each rule in sheet
-                    for (var ruleI = sheet.cssRules.length - 1; ruleI >= 0; ruleI--) {
-                        var rule = sheet.cssRules[ruleI];
-                        // Verify rule has selector text
-                        if (rule.selectorText) {
-                        // Replace hover psuedo-class with active psuedo-class
-                            rule.selectorText = rule.selectorText.replace(":hover", ":active");
-                        }
-                    }
-                }
-            }
-        }
+    //fixing hover on mobile
+    function fixHoverOnMobile(el)
+    {
+        var par = el.parentNode;
+        var next = el.nextSibling;
+        par.removeChild(el);
+        setTimeout(function () {
+            par.insertBefore(el, next);
+        }, 0)
     }
 
 })(jQuery);
