048a37c69fa7f9f2fa877df40abf92322932c93e
junander <joe@joeunander.com>
2015-04-28 14:29:14 -0400

    Feature #1439 - Mobile: Add Special menu items to the mobile 'sidebar' menu for groups - fix for conditional use of 'Portfolio' link

--- wp-content/themes/openlab/lib/menus.php
+++ wp-content/themes/openlab/lib/menus.php
@@ -956,7 +956,7 @@ function openlab_get_group_profile_mobile_anchor_links(){
     }
     
     $portfolio_data = openlab_get_group_member_portfolios( $group_id );
-    if(!empty($portfolio_data)){
+    if(!empty($portfolio_data) && openlab_portfolio_list_enabled_for_group()){
         
         $links .= '<li id="portfolios-groups-li" class="visible-xs mobile-anchor-link"><a href="#group-member-portfolio-sidebar-widget" id="portfolios">Portfolios</a></li>';
         
