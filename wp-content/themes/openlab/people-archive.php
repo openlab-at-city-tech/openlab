959d9f39183760529dc1fb98421cc8c43ab6bc66
junander <joe@joeunander.com>
2015-04-28 13:47:23 -0400

    Bug #1448 - Mobile: sidebar content is missing from all page types - additional tweaks for people

--- wp-content/themes/openlab/people-archive.php
+++ wp-content/themes/openlab/people-archive.php
@@ -8,10 +8,10 @@ global $wp_query;
 $post_obj = $wp_query->get_queried_object();
 ?>
 <div id="content" class="hfeed row">
-    <?php openlab_bp_sidebar('groups'); ?>
+    <?php openlab_bp_sidebar('groups',true); ?>
     <div <?php post_class('col-sm-18 col-xs-24'); ?>>
         <div class="content-wrapper">
-            <h1 class="entry-title"><?php echo $post_obj->post_title; ?> on the OpenLab <button data-target="#sidebar" data-plusheight="67" class="mobile-toggle direct-toggle pull-right visible-xs" type="button"><span class="fa fa-binoculars"></span></button></h1>
+            <h1 class="entry-title"><?php echo $post_obj->post_title; ?> on the OpenLab <button data-target="#sidebar" data-plusheight="47" class="mobile-toggle direct-toggle pull-right visible-xs" type="button"><span class="fa fa-binoculars"></span></button></h1>
             <div class="entry-content">
                 <div id="people-listing">
                     <?php openlab_list_members('more'); ?>
