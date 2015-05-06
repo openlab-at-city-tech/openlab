31941cbb7fde7d3eb92e34d033271ec82429d5dc
junander <joe@joeunander.com>
2015-05-06 17:26:32 -0400

    Adding query monitor plugin to help diagnose load issues on EA dev site

---
+++ wp-content/plugins/query-monitor/classes/Dispatcher.php
@@ -0,0 +1,72 @@
+<?php
+/*
+Copyright 2009-2015 John Blackbourn
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+*/
+
+if ( ! class_exists( 'QM_Dispatcher' ) ) {
+abstract class QM_Dispatcher {
+
+	public function __construct( QM_Plugin $qm ) {
+		$this->qm = $qm;
+
+		if ( !defined( 'QM_COOKIE' ) ) {
+			define( 'QM_COOKIE', 'query_monitor_' . COOKIEHASH );
+		}
+
+	}
+
+	abstract public function is_active();
+
+	public function init() {
+		// nothing
+	}
+
+	public function before_output() {
+		// nothing
+	}
+
+	public function after_output() {
+		// nothing
+	}
+
+	public function user_can_view() {
+
+		if ( !did_action( 'plugins_loaded' ) ) {
+			return false;
+		}
+
+		if ( current_user_can( 'view_query_monitor' ) ) {
+			return true;
+		}
+
+		return $this->user_verified();
+
+	}
+
+	public function user_verified() {
+		if ( isset( $_COOKIE[QM_COOKIE] ) ) {
+			return $this->verify_cookie( stripslashes( $_COOKIE[QM_COOKIE] ) );
+		}
+		return true;
+	}
+
+	public static function verify_cookie( $value ) {
+		if ( $old_user_id = wp_validate_auth_cookie( $value, 'logged_in' ) ) {
+			return user_can( $old_user_id, 'view_query_monitor' );
+		}
+		return false;
+	}
+
+}
+}
---
+++ wp-content/plugins/query-monitor/collectors/php_errors.php
@@ -0,0 +1,123 @@
+<?php
+/*
+Copyright 2009-2015 John Blackbourn
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+*/
+
+# E_DEPRECATED and E_USER_DEPRECATED were introduced in PHP 5.3 so we need to use back-compat constants that work on 5.2.
+if ( defined( 'E_DEPRECATED' ) ) {
+	define( 'QM_E_DEPRECATED', E_DEPRECATED );
+} else {
+	define( 'QM_E_DEPRECATED', 0 );
+}
+
+if ( defined( 'E_USER_DEPRECATED' ) ) {
+	define( 'QM_E_USER_DEPRECATED', E_USER_DEPRECATED );
+} else {
+	define( 'QM_E_USER_DEPRECATED', 0 );
+}
+
+class QM_Collector_PHP_Errors extends QM_Collector {
+
+	public $id = 'php_errors';
+	private $display_errors = null;
+
+	public function name() {
+		return __( 'PHP Errors', 'query-monitor' );
+	}
+
+	public function __construct() {
+
+		parent::__construct();
+		set_error_handler( array( $this, 'error_handler' ) );
+
+		$this->display_errors = ini_get( 'display_errors' );
+		ini_set( 'display_errors', 0 );
+
+	}
+
+	public function error_handler( $errno, $message, $file = null, $line = null ) {
+
+		#if ( !( error_reporting() & $errno ) )
+		#	return false;
+
+		switch ( $errno ) {
+
+			case E_WARNING:
+			case E_USER_WARNING:
+				$type = 'warning';
+				break;
+
+			case E_NOTICE:
+			case E_USER_NOTICE:
+				$type = 'notice';
+				break;
+
+			case E_STRICT:
+				$type = 'strict';
+				break;
+
+			case QM_E_DEPRECATED:
+			case QM_E_USER_DEPRECATED:
+				$type = 'deprecated';
+				break;
+
+			default:
+				return false;
+				break;
+
+		}
+
+		if ( error_reporting() > 0 ) {
+
+			if ( ! class_exists( 'QM_Backtrace' ) ) {
+				return false;
+			}
+
+			$trace  = new QM_Backtrace;
+			$caller = $trace->get_caller();
+			$key    = md5( $message . $file . $line . $caller['id'] );
+
+			$filename = QM_Util::standard_dir( $file, '' );
+
+			if ( isset( $this->data['errors'][$type][$key] ) ) {
+				$this->data['errors'][$type][$key]->calls++;
+			} else {
+				$this->data['errors'][$type][$key] = (object) array(
+					'errno'    => $errno,
+					'type'     => $type,
+					'message'  => $message,
+					'file'     => $file,
+					'filename' => $filename,
+					'line'     => $line,
+					'trace'    => $trace,
+					'calls'    => 1
+				);
+			}
+
+		}
+
+		return apply_filters( 'qm/collect/php_errors_return_value', false );
+
+	}
+
+	public function tear_down() {
+		parent::tear_down();
+		ini_set( 'display_errors', $this->display_errors );
+		restore_error_handler();
+	}
+
+}
+
+# Load early to catch early errors
+QM_Collectors::add( new QM_Collector_PHP_Errors );
