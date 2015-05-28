6e6f11fb36f6992649733d2b680fc753113caa62
junander <joe@joeunander.com>
2015-05-28 12:00:04 -0400

    OpenLab assets: using a PHP minify library to combine and minify assets
    Involves two sets of minify/concatination: in the OpenLab them folder, and
    in the mu-plugins folder, via mu-enqueues

--- wp-content/mu-plugins/mu-enqueues.php
+++ wp-content/mu-plugins/mu-enqueues.php
@@ -4,15 +4,54 @@
  * MU Plugins enqueues
  * Keeping this all in once place
  */
+/* * minify library* */
+require_once( WPMU_PLUGIN_DIR . '/minify/src/Converter.php' );
+require_once( WPMU_PLUGIN_DIR . '/minify/src/Minify.php' );
+require_once( WPMU_PLUGIN_DIR . '/minify/src/Exception.php' );
+require_once( WPMU_PLUGIN_DIR . '/minify/src/JS.php' );
+require_once( WPMU_PLUGIN_DIR . '/minify/src/CSS.php' );
+
 function openlab_mu_enqueue() {
+
+    //google plus one
+    wp_register_script('google-plus-one', 'https://apis.google.com/js/plusone.js');
+    wp_enqueue_script('google-plus-one');
+
     //adding smooth scroll
-    wp_register_script('smoothscroll-js', plugins_url('js', __FILE__) . '/jquery-smooth-scroll/jquery.smooth-scroll.min.js', array('jquery'));
+    /*wp_register_script('smoothscroll-js', plugins_url('js', __FILE__) . '/jquery-smooth-scroll/jquery.smooth-scroll.min.js', array('jquery'));
     wp_enqueue_script('smoothscroll-js');
     wp_register_script('select-js', plugins_url('js', __FILE__) . '/jquery-custom-select/jquery.customSelect.min.js', array('jquery'));
     wp_enqueue_script('select-js');
     wp_register_script('openlab-search-js', plugins_url('js', __FILE__) . '/openlab/openlab.search.js', array('jquery'));
     wp_enqueue_script('openlab-search-js');
+    wp_register_script('openlab-nav-js', plugins_url('js', __FILE__) . '/openlab/openlab.nav.js', array('jquery'));
+    wp_enqueue_script('openlab-nav-js');*/
+
+    $result_path = WPMU_PLUGIN_DIR . '/js/openlab/openlab.nav.min.js';
+
+    $smoothscroll = WPMU_PLUGIN_DIR . '/js/jquery-smooth-scroll/jquery.smooth-scroll.min.js';
+    $select = WPMU_PLUGIN_DIR . '/js/jquery-custom-select/jquery.customSelect.min.js';
+    $openlab_search = WPMU_PLUGIN_DIR . '/js/openlab/openlab.search.js';
+    $openlab_nav = WPMU_PLUGIN_DIR . '/js/openlab/openlab.nav.js';
+
+    $minifier = new MatthiasMullie\Minify\JS($smoothscroll, $select, $openlab_search, $openlab_nav);
+    $minifier->minify($result_path);
+
+    wp_register_script('openlab-nav-js', plugins_url('js', __FILE__) . '/openlab/openlab.nav.min.js', array('jquery'));
+    wp_enqueue_script('openlab-nav-js');
 }
 
-add_action('wp_enqueue_scripts', 'openlab_mu_enqueue');
+add_action('wp_enqueue_scripts', 'openlab_mu_enqueue',9);
 add_action('admin_enqueue_scripts', 'openlab_mu_enqueue');
+
+function openlab_script_additional_attributes($good_protocol_url, $original_url, $_context) {
+
+    if (false !== strpos($original_url, 'plusone.js')) {
+        remove_filter('clean_url', 'openlab_script_additional_attributes', 10, 3);
+        $url_parts = parse_url($good_protocol_url);
+        return $url_parts['scheme'] . '://' . $url_parts['host'] . $url_parts['path'] . "' async defer='defer";
+    }
+    return $good_protocol_url;
+}
+
+add_filter('clean_url', 'openlab_script_additional_attributes', 10, 3);
--- wp-content/themes/openlab/functions.php
+++ wp-content/themes/openlab/functions.php
@@ -38,6 +38,13 @@ require_once( STYLESHEETPATH . '/lib/sidebar-funcs.php' );
 require_once( STYLESHEETPATH . '/lib/plugin-hooks.php' );
 require_once( STYLESHEETPATH . '/lib/theme-hooks.php' );
 
+/* * minify library* */
+require_once( WPMU_PLUGIN_DIR . '/minify/src/Converter.php' );
+require_once( WPMU_PLUGIN_DIR . '/minify/src/Minify.php' );
+require_once( WPMU_PLUGIN_DIR . '/minify/src/Exception.php' );
+require_once( WPMU_PLUGIN_DIR . '/minify/src/JS.php' );
+require_once( WPMU_PLUGIN_DIR . '/minify/src/CSS.php' );
+
 function openlab_load_scripts() {
     $stylesheet_dir_uri = get_stylesheet_directory_uri();
 
@@ -61,23 +68,39 @@ function openlab_load_scripts() {
             wp_enqueue_script('less-js');
         }
 
-        wp_register_script('bootstrap-js', $stylesheet_dir_uri . '/js/bootstrap.min.js', array('jquery'));
-        wp_enqueue_script('bootstrap-js');
-        wp_register_script('jcarousellite', $stylesheet_dir_uri . '/js/jcarousellite.js');
-        wp_enqueue_script('jcarousellite');
-        wp_register_script('easyaccordion', $stylesheet_dir_uri . '/js/easyaccordion.js');
-        wp_enqueue_script('easyaccordion');
-        wp_register_script('easing-js', $stylesheet_dir_uri . '/js/jquery.easing.1.3.js', array('jquery'));
-        wp_enqueue_script('easing-js');
-        wp_register_script('mobile-custom-js', $stylesheet_dir_uri . '/js/jquery.mobile.customized.min.js', array('jquery'));
-        wp_enqueue_script('mobile-custom-js');
-        wp_register_script('camera-js', $stylesheet_dir_uri . '/js/camera.min.js', array('jquery'));
-        wp_enqueue_script('camera-js');
-        wp_register_script('hyphenator-js', $stylesheet_dir_uri . '/js/hyphenator.js', array('jquery'));
-        wp_enqueue_script('hyphenator-js');
-        wp_register_script('succinct-js', $stylesheet_dir_uri . '/js/jQuery.succinct.min.js', array('jquery'));
-        wp_enqueue_script('succinct-js');
-        wp_register_script('utility', $stylesheet_dir_uri . '/js/utility.js');
+        /* wp_register_script('bootstrap-js', $stylesheet_dir_uri . '/js/bootstrap.min.js', array('jquery'));
+          wp_enqueue_script('bootstrap-js');
+          wp_register_script('jcarousellite', $stylesheet_dir_uri . '/js/jcarousellite.js');
+          wp_enqueue_script('jcarousellite');
+          wp_register_script('easyaccordion', $stylesheet_dir_uri . '/js/easyaccordion.js');
+          wp_enqueue_script('easyaccordion');
+          wp_register_script('easing-js', $stylesheet_dir_uri . '/js/jquery.easing.1.3.js', array('jquery'));
+          wp_enqueue_script('easing-js');
+          wp_register_script('mobile-custom-js', $stylesheet_dir_uri . '/js/jquery.mobile.customized.min.js', array('jquery'));
+          wp_enqueue_script('mobile-custom-js');
+          wp_register_script('camera-js', $stylesheet_dir_uri . '/js/camera.min.js', array('jquery'));
+          wp_enqueue_script('camera-js');
+          wp_register_script('hyphenator-js', $stylesheet_dir_uri . '/js/hyphenator.js', array('jquery'));
+          wp_enqueue_script('hyphenator-js');
+          wp_register_script('succinct-js', $stylesheet_dir_uri . '/js/jQuery.succinct.min.js', array('jquery'));
+          wp_enqueue_script('succinct-js'); */
+
+        $result_path = STYLESHEETPATH . '/js/utility.min.js';
+
+        $bootstrap = STYLESHEETPATH . '/js/bootstrap.min.js';
+        $jcarousellite = STYLESHEETPATH . '/js/jcarousellite.js';
+        $easyaccordion = STYLESHEETPATH . '/js/easyaccordion.js';
+        $easing = STYLESHEETPATH . '/js/jquery.easing.1.3.js';
+        $mobile_custom = STYLESHEETPATH . '/js/jquery.mobile.customized.min.js';
+        $camera = STYLESHEETPATH . '/js/camera.min.js';
+        $hyphenator = STYLESHEETPATH . '/js/hyphenator.js';
+        $succinct = STYLESHEETPATH . '/js/jQuery.succinct.min.js';
+        $utility = STYLESHEETPATH . '/js/utility.js';
+
+        $minifier = new MatthiasMullie\Minify\JS($bootstrap, $jcarousellite, $easyaccordion, $easing, $mobile_custom, $camera, $hyphenator, $succinct, $utility);
+        $minifier->minify($result_path);
+
+        wp_register_script('utility', $stylesheet_dir_uri . '/js/utility.min.js', array('jquery'));
         wp_enqueue_script('utility');
     }
 }
@@ -97,12 +120,15 @@ function openlab_load_scripts_high_priority() {
         wp_register_style('main-styles', $stylesheet_dir_uri . '/style.less', array(), '20130604', 'all');
         wp_enqueue_style('main-styles');
     } else {
-        wp_register_style('main-styles', get_stylesheet_uri(), array(), '20130604', 'all');
+
+        $source_path = STYLESHEETPATH . '/style.css';
+        $result_path = STYLESHEETPATH . '/style.min.css';
+        $minifier = new MatthiasMullie\Minify\CSS($source_path);
+        $minifier->minify($result_path);
+
+        wp_register_style('main-styles', $stylesheet_dir_uri . '/style.min.css', array(), '20130604', 'all');
         wp_enqueue_style('main-styles');
     }
-
-    wp_register_style('main-styles', get_stylesheet_uri(), array(), '20130604', 'all');
-    wp_enqueue_style('main-styles');
 
     if ($post->post_type == 'help') {
         wp_register_style('print-styles', $stylesheet_dir_uri . '/css/print.css', array(), '2015', 'print');
