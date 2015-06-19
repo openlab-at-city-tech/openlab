03a7a013eefb3cbe5590f0bc9f243c1650619131
junander <joe@joeunander.com>
2015-06-19 10:14:35 -0400

    My Friends page: cleaning up wp_debug notice

--- wp-content/themes/openlab/lib/member-funcs.php
+++ wp-content/themes/openlab/lib/member-funcs.php
@@ -976,7 +976,7 @@ function openlab_trim_member_name($name) {
 
     if ($post->post_name == 'people' || $bp->current_action == 'members') {
         $trim_switch = true;
-    } else if ($bp->action_variables[0] == 'manage-members' || ($bp->current_action == 'members' && $bp->current_component == 'groups')) {
+    } else if ((isset($bp->action_variables[0]) && $bp->action_variables[0] == 'manage-members') || ($bp->current_action == 'members' && $bp->current_component == 'groups')) {
         $trim_switch = true;
     }
 
