0fd90d3dded84bac07588fe604994b045b66e905
junander <joe@joeunander.com>
2015-07-24 00:30:16 -0400

    Bug #1611 - Mobile projects name covered by right hand nav stack

--- wp-content/themes/openlab/lib/group-funcs.php
+++ wp-content/themes/openlab/lib/group-funcs.php
@@ -525,7 +525,7 @@ function openlab_group_profile_header() {
     global $bp;
     $group_type = groups_get_groupmeta($bp->groups->current_group->id, 'wds_group_type');
     ?>
-    <h1 class="entry-title group-title clearfix"><span class="profile-name"><?php echo bp_group_name(); ?></span>
+    <h1 class="entry-title group-title clearfix"><span class="profile-name hyphenate"><?php echo bp_group_name(); ?></span>
         <button data-target="#sidebar-mobile" class="mobile-toggle direct-toggle pull-right visible-xs" type="button">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
