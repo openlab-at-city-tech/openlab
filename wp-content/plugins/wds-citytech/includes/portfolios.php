d4b1c3a5aeaf9f2682e831c8d32d96ebcf0444b4
Boone B Gorges <boonebgorges@gmail.com>
2015-08-28 16:58:55 -0500

    On portfolio list site widget, use portfolio group name for external sites.
    Otherwise the name of the current site is used in its place.
    
    See #1692.

--- wp-content/plugins/wds-citytech/includes/portfolios.php
+++ wp-content/plugins/wds-citytech/includes/portfolios.php
@@ -256,19 +256,27 @@ function openlab_get_group_member_portfolios( $group_id = false, $sort_by = 'dis
 
                         // If the portfolio_blog_id is empty, this may be an external portfolio.
                         if ( empty( $portfolio_blog_id ) ) {
-				$external_url = openlab_get_external_site_url_by_group_id( $portfolio_id );
-				if ( empty( $external_url ) ) {
+				$portfolio_url = openlab_get_external_site_url_by_group_id( $portfolio_id );
+
+				// No URL found? There's no portfolio to link to.
+				if ( empty( $portfolio_url ) ) {
 					continue;
 				}
-                        }
+
+				// Use the group title for the link text.
+				$portfolio_title = $portfolio_group->name;
+                        } else {
+				$portfolio_url = openlab_get_user_portfolio_url( $member->ID );
+				$portfolio_title = get_blog_option( $portfolio_blog_id, 'blogname' );
+			}
 
                         $portfolio = array(
 				'user_id' => $member->ID,
 				'user_display_name' => $member->display_name,
 				'user_type' => xprofile_get_field_data( 'Account Type', $member->ID ),
 				'portfolio_id' => $portfolio_id,
-				'portfolio_url' => openlab_get_user_portfolio_url( $member->ID ),
-				'portfolio_title' => get_blog_option( $portfolio_blog_id, 'blogname' ),
+				'portfolio_url' => $portfolio_url,
+				'portfolio_title' => $portfolio_title,
 			);
 
 			$portfolios[] = $portfolio;
