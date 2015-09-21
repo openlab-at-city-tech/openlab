a87b355233dc2003d808e6e471725d5b180ceb3a
junander <joe@joeunander.com>
2015-09-21 18:14:09 -0400

    Feature #1641 - Profile > Settings > email notifications layout

--- wp-content/themes/openlab/buddypress/members/single/settings/notifications.php
+++ wp-content/themes/openlab/buddypress/members/single/settings/notifications.php
@@ -3,33 +3,37 @@
  * Members settings - email notifications settings
  *
  * */
-    do_action('bp_before_member_settings_template');
-    ?>
-    <?php echo openlab_submenu_markup(); ?>
+do_action('bp_before_member_settings_template');
+?>
+<?php echo openlab_submenu_markup(); ?>
 
-    <div id="item-body" role="main">
+<div id="item-body" role="main">
 
-        <?php do_action('bp_template_content') ?>
+    <?php do_action('bp_template_content') ?>
+
+    <form action="<?php echo bp_displayed_user_domain() . bp_get_settings_slug() . '/notifications'; ?>" method="post" class="standard-form form-panel" id="settings-form">
 
         <div class="panel panel-default">
+            <div class="panel-heading">Email Notifications</div>
             <div class="panel-body">
-        <form action="<?php echo bp_displayed_user_domain() . bp_get_settings_slug() . '/notifications'; ?>" method="post" class="standard-form panel-form" id="settings-form">
-            <p><?php _e('Send a notification by email when:', 'buddypress'); ?></p>
 
-            <?php do_action('bp_notification_settings');
-            do_action('bp_members_notification_settings_before_submit'); ?>
+                <p><?php _e('Send a notification by email when:', 'buddypress'); ?></p>
+
+                <?php do_action('bp_notification_settings'); ?>
+                <?php do_action('bp_members_notification_settings_before_submit'); ?>
             </div>
         </div>
 
-            <div class="submit">
-                <input type="submit" name="submit" value="<?php _e('Save Changes', 'buddypress'); ?>" id="submit" class="auto btn btn-primary" />
-            </div>
+        <div class="submit">
+            <input type="submit" name="submit" value="<?php _e('Save Changes', 'buddypress'); ?>" id="submit" class="auto btn btn-primary btn-margin btn-margin-top" />
+        </div>
 
-    <?php do_action('bp_members_notification_settings_after_submit');
-    wp_nonce_field('bp_settings_notifications'); ?>
+        <?php do_action('bp_members_notification_settings_after_submit');
+        wp_nonce_field('bp_settings_notifications');
+        ?>
 
-        </form>
-    <?php do_action('bp_after_member_body'); ?>
-    </div><!-- #item-body -->
-    <?php
-    do_action('bp_after_member_settings_template');
+    </form>
+<?php do_action('bp_after_member_body'); ?>
+</div><!-- #item-body -->
+<?php
+do_action('bp_after_member_settings_template');
