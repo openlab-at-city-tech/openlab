4a71ca3e916b5cd46e0f78aff1d845bc95183b8f
Boone B Gorges <boonebgorges@gmail.com>
2015-09-22 16:36:35 -0500

    Add plugin: bp-reply-by-email.
    See #1711.

---
+++ wp-content/plugins/bp-reply-by-email/loader.php
@@ -0,0 +1,129 @@
+<?php
+/*
+Plugin Name: BuddyPress Reply By Email
+Description: Reply to BuddyPress items from the comfort of your email inbox.
+Author: r-a-y
+Author URI: http://profiles.wordpress.org/r-a-y
+Version: 1.0-RC3
+License: GPLv2 or later
+*/
+
+/**
+ * BuddyPress Reply By Email
+ *
+ * @package BP_Reply_By_Email
+ * @subpackage Loader
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// pertinent constants
+define( 'BP_RBE_DIR', dirname( __FILE__ ) );
+define( 'BP_RBE_URL', plugin_dir_url( __FILE__ ) );
+
+/**
+ * Loads BP Reply By Email only if BuddyPress is activated
+ */
+function bp_rbe_init() {
+	global $bp_rbe;
+
+	require( BP_RBE_DIR . '/bp-rbe-core.php' );
+
+	// initialize!
+	$bp_rbe = new BP_Reply_By_Email;
+	$bp_rbe->init();
+
+	// admin area
+	if ( is_admin() ) {
+		require( BP_RBE_DIR . '/includes/bp-rbe-admin.php' );
+		new BP_Reply_By_Email_Admin;
+	}
+}
+add_action( 'bp_include', 'bp_rbe_init' );
+
+/**
+ * Adds default settings when plugin is activated
+ */
+function bp_rbe_activate() {
+	// Load the bp-rbe functions file
+	require( BP_RBE_DIR . '/includes/bp-rbe-functions.php' );
+
+	if ( ! $settings = bp_get_option( 'bp-rbe' ) ) {
+		$settings = array();
+	}
+
+	// set default mode to IMAP if no mode exists
+	// might default to 'inbound' in the future
+	if ( ! isset( $settings['mode'] ) ) {
+		$settings['mode'] = 'imap';
+	}
+
+	// generate a unique key if one doesn't exist
+	if ( ! isset( $settings['key'] ) ) {
+		$settings['key'] = uniqid( '' );
+	}
+
+	// set a default value for the keepalive value
+	if ( ! isset( $settings['keepalive'] ) ) {
+		$settings['keepalive'] = bp_rbe_get_execution_time( 'minutes' );
+	}
+
+	bp_update_option( 'bp-rbe', $settings );
+
+	// remove remnants from any previous failed attempts to stop the inbox
+	bp_rbe_cleanup();
+}
+add_action( 'activate_' . basename( BP_RBE_DIR ) . '/loader.php', 'bp_rbe_activate' );
+
+/**
+ * Remove our scheduled function from WP and stop the IMAP loop.
+ */
+function bp_rbe_deactivate() {
+	// stop IMAP connection if active
+	if ( bp_rbe_is_connected() ) {
+		bp_rbe_stop_imap();
+
+		// give plugin a chance to stop IMAP connection as it could be sleeping
+		sleep( 10 );
+
+		bp_rbe_log( 'Daisy, Daisy, give me your answer, do...' );
+	}
+
+	// remove remnants from any previous failed attempts to stop the inbox
+	bp_rbe_cleanup();
+
+	bp_rbe_log( 'Plugin deactivated!' );
+}
+add_action( 'deactivate_' . basename( BP_RBE_DIR ) . '/loader.php', 'bp_rbe_deactivate' );
+
+/**
+ * BP Reply By Email default extensions.
+ *
+ * Currently supports BuddyPress Docs and bbPress
+ * More to come in the future?
+ *
+ * @since 1.0-RC1
+ */
+function bp_rbe_default_extensions() {
+	// if RBE requirements aren't fulfilled, stop now!
+	if ( ! bp_rbe_is_required_completed() )
+		return;
+
+	// BuddyPress Docs
+	if ( defined( 'BP_DOCS_VERSION' ) ) {
+		require( BP_RBE_DIR . '/includes/bp-rbe-extend-bpdocs.php' );
+
+		// initialize the BP Docs RBE extension!
+		new BP_Docs_Comment_RBE_Extension;
+	}
+
+	// bbPress
+	if ( function_exists( 'bbpress' ) ) {
+		require( BP_RBE_DIR . '/includes/bp-rbe-extend-bbpress.php' );
+
+		// initialize the bbPress RBE extension!
+		new BBP_RBE_Extension;
+	}
+}
+add_action( 'bp_include', 'bp_rbe_default_extensions', 20 );
---
+++ wp-content/plugins/bp-reply-by-email/readme.md
@@ -0,0 +1,31 @@
+# BuddyPress Reply By Email #
+
+BuddyPress Reply By Email is a plugin for [BuddyPress](http://buddypress.org) that allows you to reply to various email notifications from the comfort of your email inbox.
+
+You can reply to the following items from your inbox:
+
+* @mentions
+* Activity replies
+* Private messages
+* Group forum topics / posts (requires [BP Group Email Subscription Plugin](http://wordpress.org/extend/plugins/buddypress-group-email-subscription/))
+
+You can also create new group forum topics from your email inbox as well.
+
+This plugin was developed for the [CUNY Academic Commons](http://commons.gc.cuny.edu).  Licensed under the GPLv2 or later.
+
+#### Get Help
+
+Check out the [wiki](https://github.com/r-a-y/bp-reply-by-email/wiki/) for up-to-date information.
+
+***
+
+#### Translations
+
+Italian - [htrex](https://github.com/htrex)
+
+#### Thanks
+
+* Jim Wigginton - for his `Crypt_AES` class from the [PHP Secure Communications Library](http://phpseclib.sourceforge.net/). Licensed under the [MIT License](http://www.opensource.org/licenses/mit-license.html).
+* Jevon Wright - for his [html2text](https://code.google.com/p/iaml/source/browse/trunk/org.openiaml.model.runtime/src/include/html2text/html2text.php) functions from the [IAML Modelling Platform](http://openiaml.org/).  Licensed under the [Eclipse Public License v1.0](http://www.eclipse.org/legal/epl-v10.html).
+* Dan Roscoe - for his [PHP-Tail](https://github.com/ruscoe/PHP-Tail) library. Licensed under the [MIT License](http://www.opensource.org/licenses/mit-license.html).
+* Janis Elsts - for his [Plugin Update Checker](https://github.com/YahnisElsts/plugin-update-checker) library. Licensed under the [GPL License](http://www.gnu.org/licenses/gpl.html).
