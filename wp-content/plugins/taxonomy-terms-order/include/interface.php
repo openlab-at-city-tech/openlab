9c10b039bfd99631b9e01ac258b2b1c666626301
Boone B Gorges <boonebgorges@gmail.com>
2015-09-28 22:10:32 -0500

    Update plugin: taxonomy-terms-order.
    Name: Category Order and Taxonomy Terms Order
    New version: 1.4.6.1
    Previous version: 1.3.6

--- wp-content/plugins/taxonomy-terms-order/include/interface.php
+++ wp-content/plugins/taxonomy-terms-order/include/interface.php
@@ -5,8 +5,27 @@
         {
             global $wpdb, $wp_locale;
             
-            $taxonomy = isset($_GET['taxonomy']) ? $_GET['taxonomy'] : '';
-            $post_type = isset($_GET['post_type']) ? $_GET['post_type'] : 'post';
+            $taxonomy = isset($_GET['taxonomy']) ? sanitize_key($_GET['taxonomy']) : '';
+            $post_type = isset($_GET['post_type']) ? sanitize_key($_GET['post_type']) : '';
+            if(empty($post_type))
+                {
+                    $screen = get_current_screen();
+                    
+                    if(isset($screen->post_type)    && !empty($screen->post_type))
+                        $post_type  =   $screen->post_type;
+                        else
+                        {
+                            switch($screen->parent_file)
+                                {
+                                    case "upload.php" :
+                                                        $post_type  =   'attachment';
+                                                        break;
+                                                
+                                    default:
+                                                        $post_type  =   'post';   
+                                }
+                        }       
+                } 
                                             
             $post_type_data = get_post_type_object($post_type);
             
@@ -16,7 +35,7 @@
             ?>
             <div class="wrap">
                 <div class="icon32" id="icon-edit"><br></div>
-                <h2><?php _e( "Taxonomy Order", 'to' ) ?></h2>
+                <h2><?php _e( "Taxonomy Order", 'tto' ) ?></h2>
 
                 <?php tto_info_box() ?>
                 
@@ -24,18 +43,36 @@
                 
                 <noscript>
                     <div class="error message">
-                        <p><?php _e( "This plugin can't work without javascript, because it's use drag and drop and AJAX.", 'to' ) ?></p>
+                        <p><?php _e( "This plugin can't work without javascript, because it's use drag and drop and AJAX.", 'tto' ) ?></p>
                     </div>
                 </noscript>
 
                 <div class="clear"></div>
                 
-                <form action="edit.php" method="get" id="to_form">
-                    <input type="hidden" name="page" value="to-interface-<?php echo $post_type ?>" />
+                <?php
+                
+                    $current_section_parent_file    =   '';
+                    switch($post_type)
+                        {
+                            
+                            case "attachment" :
+                                            $current_section_parent_file    =   "upload.php";
+                                            break;
+                                            
+                            default :
+                                            $current_section_parent_file    =    "edit.php";
+                                            break;
+                        }
+                
+                
+                ?>
+                
+                <form action="<?php echo $current_section_parent_file ?>" method="get" id="to_form">
+                    <input type="hidden" name="page" value="to-interface-<?php echo esc_attr($post_type) ?>" />
                     <?php
                 
-                     if ($post_type != 'post')
-                        echo '<input type="hidden" name="post_type" value="'. $post_type .'" />';
+                     if (!in_array($post_type, array('post', 'attachment'))) 
+                        echo '<input type="hidden" name="post_type" value="'. esc_attr($post_type) .'" />';
 
                     //output all available taxonomies for this post type
                     
@@ -60,11 +97,11 @@
                 
                             ?>
                             
-                            <h2 class="subtitle"><?php echo ucfirst($post_type_data->labels->name) ?> <?php _e( "Taxonomies", 'to' ) ?></h2>
+                            <h2 class="subtitle"><?php echo ucfirst($post_type_data->labels->name) ?> <?php _e( "Taxonomies", 'tto' ) ?></h2>
                             <table cellspacing="0" class="wp-list-taxonomy">
                                 <thead>
                                 <tr>
-                                    <th style="" class="column-cb check-column" id="cb" scope="col">&nbsp;</th><th style="" class="" id="author" scope="col"><?php _e( "Taxonomy Title", 'to' ) ?></th><th style="" class="manage-column" id="categories" scope="col"><?php _e( "Total  Posts", 'to' ) ?></th>    </tr>
+                                    <th style="" class="column-cb check-column" id="cb" scope="col">&nbsp;</th><th style="" class="" id="author" scope="col"><?php _e( "Taxonomy Title", 'tto' ) ?></th><th style="" class="manage-column" id="categories" scope="col"><?php _e( "Total  Posts", 'tto' ) ?></th>    </tr>
                                 </thead>
 
    
@@ -81,7 +118,7 @@
                                             $taxonomy_terms = get_terms($key);
                                                              
                                             ?>
-                                                <tr valign="top" class="<?php if ($alternate === TRUE) {echo 'alternate ';} ?>" id="taxonomy-<?php echo $taxonomy  ?>">
+                                                <tr valign="top" class="<?php if ($alternate === TRUE) {echo 'alternate ';} ?>" id="taxonomy-<?php echo esc_attr($taxonomy)  ?>">
                                                         <th class="check-column" scope="row"><input type="radio" onclick="to_change_taxonomy(this)" value="<?php echo $post_type_taxonomy ?>" <?php if ($post_type_taxonomy == $taxonomy) {echo 'checked="checked"';} ?> name="taxonomy">&nbsp;</th>
                                                         <td class="categories column-categories"><b><?php echo $taxonomy_info->label ?></b> (<?php echo  $taxonomy_info->labels->singular_name; ?>)</td>
                                                         <td class="categories column-categories"><?php echo count($taxonomy_terms) ?></td>
@@ -92,7 +129,7 @@
                                 ?>
                                 </tbody>
                             </table>
-                            <br /><br /> 
+                            <br />
                             <?php
                         }
                             ?>
@@ -114,7 +151,7 @@
                     
                     <div class="alignleft actions">
                         <p class="submit">
-                            <a href="javascript:;" class="save-order button-primary"><?php _e( "Update", 'to' ) ?></a>
+                            <a href="javascript:;" class="save-order button-primary"><?php _e( "Update", 'tto' ) ?></a>
                         </p>
                     </div>
                     
@@ -163,7 +200,7 @@
                                 var serialize_data = serialize(mySortable);
                                                                                             
                                 jQuery.post( ajaxurl, { action:'update-taxonomy-order', order: serialize_data, taxonomy : '<?php echo  $taxonomy ?>' }, function() {
-                                    jQuery("#ajax-response").html('<div class="message updated fade"><p><?php _e( "Items Order Updates", 'to' ) ?></p></div>');
+                                    jQuery("#ajax-response").html('<div class="message updated fade"><p><?php _e( "Items Order Updated", 'tto' ) ?></p></div>');
                                     jQuery("#ajax-response div").delay(3000).hide("slow");
                                 });
                             });
--- wp-content/plugins/taxonomy-terms-order/include/options.php
+++ wp-content/plugins/taxonomy-terms-order/include/options.php
@@ -8,12 +8,12 @@ function to_plugin_options()
         if (isset($_POST['form_submit']))
             {
                     
-                $options['level'] = $_POST['level'];
+                $options['capability'] = sanitize_key($_POST['capability']);
                 
-                $options['autosort']    = isset($_POST['autosort'])     ? $_POST['autosort']    : '';
-                $options['adminsort']   = isset($_POST['adminsort'])    ? $_POST['adminsort']   : '';
+                $options['autosort']    = isset($_POST['autosort'])     ? intval($_POST['autosort'])    : '';
+                $options['adminsort']   = isset($_POST['adminsort'])    ? intval($_POST['adminsort'])   : '';
                     
-                ?><div class="updated fade"><p><?php _e('Settings Saved', 'to') ?></p></div><?php
+                ?><div class="updated fade"><p><?php _e('Settings Saved', 'tto') ?></p></div><?php
 
                 update_option('tto_options', $options);   
             }
@@ -21,76 +21,56 @@ function to_plugin_options()
                     ?>
                       <div class="wrap"> 
                         <div id="icon-settings" class="icon32"></div>
-                            <h2><?php _e( "General Settings", 'to' ) ?></h2>
+                            <h2><?php _e( "General Settings", 'tto' ) ?></h2>
                             
                             <?php tto_info_box() ?>
                            
                             <form id="form_data" name="form" method="post">   
                                 <br />
-                                <h2 class="subtitle"><?php _e( "General", 'to' ) ?></h2>                              
+                                <h2 class="subtitle"><?php _e( "General", 'tto' ) ?></h2>                              
                                 <table class="form-table">
                                     <tbody>
                             
                                         <tr valign="top">
-                                            <th scope="row" style="text-align: right;"><label><?php _e( "Minimum Level to use this plugin", 'to' ) ?></label></th>
+                                            <th scope="row" style="text-align: right;"><label><?php _e( "Minimum Level to use this plugin", 'tto' ) ?></label></th>
                                             <td>
-                                                <select id="role" name="level">
-                                                    <option value="0" <?php if ($options['level'] == "0") echo 'selected="selected"'?>><?php _e('Subscriber', 'tto') ?></option>
-                                                    <option value="1" <?php if ($options['level'] == "1") echo 'selected="selected"'?>><?php _e('Contributor', 'tto') ?></option>
-                                                    <option value="2" <?php if ($options['level'] == "2") echo 'selected="selected"'?>><?php _e('Author', 'tto') ?></option>
-                                                    <option value="5" <?php if ($options['level'] == "5") echo 'selected="selected"'?>><?php _e('Editor', 'tto') ?></option>
-                                                    <option value="8" <?php if ($options['level'] == "8") echo 'selected="selected"'?>><?php _e('Administrator', 'tto') ?></option>
+                                                <select id="role" name="capability">
+                                                    <option value="read" <?php if (isset($options['capability']) && $options['capability'] == "read") echo 'selected="selected"'?>><?php _e('Subscriber', 'cpt') ?></option>
+                                                    <option value="edit_posts" <?php if (isset($options['capability']) && $options['capability'] == "edit_posts") echo 'selected="selected"'?>><?php _e('Contributor', 'cpt') ?></option>
+                                                    <option value="publish_posts" <?php if (isset($options['capability']) && $options['capability'] == "publish_posts") echo 'selected="selected"'?>><?php _e('Author', 'cpt') ?></option>
+                                                    <option value="publish_pages" <?php if (isset($options['capability']) && $options['capability'] == "publish_pages") echo 'selected="selected"'?>><?php _e('Editor', 'cpt') ?></option>
+                                                    <option value="install_plugins" <?php if (!isset($options['capability']) || empty($options['capability']) || (isset($options['capability']) && $options['capability'] == "install_plugins")) echo 'selected="selected"'?>><?php _e('Administrator', 'cpt') ?></option>
                                                 </select>
                                             </td>
                                         </tr>
                                         
                                         
                                         <tr valign="top">
-                                            <th scope="row" style="text-align: right;"><label><?php _e( "Auto Sort", 'to' ) ?></label></th>
+                                            <th scope="row" style="text-align: right;"><label><?php _e( "Auto Sort", 'tto' ) ?></label></th>
                                             <td>
                                                 <select id="role" name="autosort">
                                                     <option value="0" <?php if ($options['autosort'] == "0") echo 'selected="selected"'?>><?php _e('OFF', 'tto') ?></option>
                                                     <option value="1" <?php if ($options['autosort'] == "1") echo 'selected="selected"'?>><?php _e('ON', 'tto') ?></option>
-                                                </select> *(<?php _e( "global setting", 'to' ) ?>)
+                                                </select> *(<?php _e( "global setting", 'tto' ) ?>) <?php _e( "Additional description and details at ", 'tto' ) ?><a target="_blank" href="http://www.nsp-code.com/taxonomy-terms-order-and-auto-sort-admin-sort-description-an-usage/"><?php _e( "Auto Sort Description", 'tto' ) ?></a>
                                             </td>
                                         </tr>
                                         
                                         <tr valign="top">
-                                            <th scope="row" style="text-align: right;"><label><?php _e( "Admin Sort", 'to' ) ?></label></th>
+                                            <th scope="row" style="text-align: right;"><label><?php _e( "Admin Sort", 'tto' ) ?></label></th>
                                             <td>
-                                                <label for="users_can_register">
-                                                <input type="checkbox" <?php if ($options['adminsort'] == "1") {echo ' checked="checked"';} ?> value="1" name="adminsort">
-                                                <?php _e("This will change the order of terms within the admin interface", 'to') ?>.</label>
+                                                <input id="adminsort" type="checkbox" <?php if ($options['adminsort'] == "1") {echo ' checked="checked"';} ?> value="1" name="adminsort">
+                                                <label for="adminsort"><?php _e("This will change the order of terms within the admin interface", 'tto') ?>. <?php _e( "Additional description and details at ", 'tto' ) ?><a target="_blank" href="http://www.nsp-code.com/taxonomy-terms-order-and-auto-sort-admin-sort-description-an-usage/"><?php _e( "Auto Sort Description", 'tto' ) ?></a></label>
                                             </td>
                                         </tr>
                                         
-                                        <tr valign="top">
-                                            <th scope="row" style="text-align: right;"></th>
-                                            <td>
-                                                <br /><br /><br />
-                                <p><b><u><?php _e( "Autosort OFF", 'to' ) ?></u></b></p>                                                
-                                <p class="example"><?php _e('No query will be changed, the terms will appear in the original order. To retrieve the terms in the required order you must use the term_order on the orderby parameter', 'to') ?>:</p>
-                                <pre class="example">
-$argv = array(
-                'orderby'       =>  'term_order',
-                'hide_empty'    => false
-                );
-get_terms('category', $argv);
-</pre>
-                                <p><?php _e( "See more info on the get_terms usage", 'to' ) ?> <a href="http://codex.wordpress.org/Function_Reference/get_terms" target="_blank"><?php _e( "here", 'to' ) ?></a></p>
-
-                                <p><b><u><?php _e( "Autosort ON", 'to' ) ?></u></b></p> 
-                                <p class="example"><?php _e('The queries will be updated, all terms will appear in the order you manually defined. This is recommended if you don\'t want to change any theme code to apply the terms order', 'to') ?></p>
-                                                   
-                                            </td>
-                                        </tr>
+                                        
                                         
                                     </tbody>
                                 </table>
                                 
 
                                 <p class="submit">
-                                    <input type="submit" name="Submit" class="button-primary" value="<?php _e('Save Settings', 'to') ?>">
+                                    <input type="submit" name="Submit" class="button-primary" value="<?php _e('Save Settings', 'tto') ?>">
                                </p>
                             
                                 <input type="hidden" name="form_submit" value="true" />
