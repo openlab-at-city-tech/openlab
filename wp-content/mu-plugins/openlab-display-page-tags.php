961779e62d1fae373061f0d1705a38f69c714301
Boone B Gorges <boonebgorges@gmail.com>
2016-02-03 23:40:16 -0600

    Only show page tags on 'page' posts.
    See #1731.

--- wp-content/mu-plugins/openlab-display-page-tags.php
+++ wp-content/mu-plugins/openlab-display-page-tags.php
@@ -5,6 +5,10 @@ function openlab_display_page_tags( $content ) {
 		return $content;
 	}
 
+	if ( 'page' !== get_post_type( get_the_ID() ) ) {
+		return $content;
+	}
+
 	$tag_list = get_the_tag_list( '', ', ' );
 	if ( $tag_list ) {
 		$content .= '<br />Tags: ' . $tag_list;
