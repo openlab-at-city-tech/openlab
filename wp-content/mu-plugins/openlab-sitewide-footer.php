fd097fd70437aa7647f29a0074296070a7928911
junander <joe@joeunander.com>
2016-05-02 23:56:50 -0400

    Feature #1803 - accessibility> heading hierarchy > Footer

--- wp-content/mu-plugins/openlab-sitewide-footer.php
+++ wp-content/mu-plugins/openlab-sitewide-footer.php
@@ -111,142 +111,16 @@ function cuny_site_wide_footer() {
  * @param type $placholder
  */
 function openlab_footer_markup($placeholder = NULL) {
+    $footer_out = '';
+
     $site = bp_get_root_domain();
     $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-    ?>
-    <div id="openlab-footer" class="oplb-bs<?php echo ($placeholder ? ' placeholder' : ''); ?>">
-        <div class="oplb-bs">
-            <!--footer: desktop-->
-            <div class="container-fluid footer-desktop hidden-xs">
-                <div class="row row-footer">
-                    <div class="col-sm-12 col-logos">
-                        <h2>OPENLAB at City Tech: A place to learn, work, and share</h2>
-                        <p class="semibold"><a class="pull-left" href="http://www.citytech.cuny.edu/" target="_blank"><img src="<?php echo bp_root_domain(); ?>/wp-content/mu-plugins/css/images/ctnyc_seal.png" alt="New York City College of Technology" border="0" /></a>
-                            <a class="pull-left" href="http://www.cuny.edu/" target="_blank"><img class="cuny-logo" src="<?php echo bp_root_domain(); ?>/wp-content/mu-plugins/css/images/cuny_logo.png" alt="City University of New York" border="0" /></a>OpenLab is an open-source, digital platform designed to support teaching and learning at New York City College of Technology (NYCCT), and to promote student and faculty engagement in the intellectual and social life of the college community.</p>
-                    </div>
-                    <div class="horiz-bar-wrapper"><div class="horiz-bar"></div></div>
-                    <div class="col-sm-8 col-links semibold">
-                        <h2>Support</h2>
-                        <a class="no-deco roll-over-color" href="<?php echo $site; ?>/blog/help/openlab-help/">Help</a> | <a class="no-deco roll-over-color" href="<?php echo $site; ?>/about/contact-us/">Contact Us</a> | <a class="no-deco roll-over-color" href="http://cuny.edu/website/privacy.html" target="_blank">Privacy Policy</a> | <a class="no-deco roll-over-color" href="<?php echo $site; ?>/about/terms-of-service/">Terms of Use</a> | <a class="no-deco roll-over-color" href="<?php echo $site; ?>/about/credits/">Credits</a>
-                    </div>
-                    <div class="horiz-bar-wrapper"><div class="horiz-bar"></div></div>
-                    <div class="col-sm-2 col-share">
-                        <h2>Share</h2>
-                        <a class="rss-link" href="<?php echo $site . "/activity/feed/" ?>">RSS</a>
-                        <a class="google-plus-link" href="https://plus.google.com/share?url=<?= $url ?>" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
-                                    return false;">Google +</a>
-                    </div>
-                </div>
-                <div class="row row-copyright">
-                    <div class="col-sm-24">
-                        <p><span>&copy; <a class="no-deco roll-over-color" href="http://www.citytech.cuny.edu/" target="_blank">New York City College of Technology</a></span> | <span><a class="no-deco roll-over-color" href="http://www.cuny.edu" target="_blank">City University of New York</a></span></p>
-                    </div>
-                </div>
-            </div>
-            <!--end footer: desktop-->
-            <!--footer: mobile-->
-            <div class="container-fluid footer-mobile visible-xs">
-                <div class="footer-wrapper">
-                    <div class="row row-footer">
-                        <div class="col-sm-12 col-logos">
-                            <h2>OPENLAB at City Tech:<br /><span>A place to learn, work, and share</span></h2>
-                            <p><span class="semibold">OpenLab</span> is an open-source, digital platform designed to support teaching and learning at New York City College of Technology (NYCCT), and to promote student and faculty engagement in the intellectual and social life of the college community.</p>
-                            <p class="clearfix mobile-logos"><a class="pull-left" href="http://www.citytech.cuny.edu/" target="_blank"><img class="img-responsive" src="<?php echo bp_root_domain(); ?>/wp-content/mu-plugins/css/images/ctnyc_seal.png" alt="New York City College of Technology" border="0" /></a>
-                                <a class="pull-left" href="http://www.cuny.edu/" target="_blank"><img class="cuny-logo img-responsive" src="<?php echo bp_root_domain(); ?>/wp-content/mu-plugins/css/images/cuny_logo.png" alt="City University of New York" border="0" /></a></p>
-                        </div>
-                        <div class="row-copyright">
-                            <div class="col-sm-24">
-                                <p><span>&copy; <a class="no-deco roll-over-color" href="http://www.citytech.cuny.edu/" target="_blank">New York City College of Technology</a></span> | <span><a class="no-deco roll-over-color" href="http://www.cuny.edu" target="_blank">City University of New York</a></span></p>
-                            </div>
-                        </div>
-                        <div class="col-sm-8 col-links semibold">
-                            <h2>Support</h2>
-                            <ul class="support-list">
-                                <li><a class="no-deco roll-over-color" href="<?php echo $site; ?>/blog/help/openlab-help/">Help</a></li>
-                                <li><a class="no-deco roll-over-color" href="<?php echo $site; ?>/about/contact-us/">Contact Us</a></li>
-                                <li><a class="no-deco roll-over-color" href="http://cuny.edu/website/privacy.html" target="_blank">Privacy Policy</a></li>
-                                <li><a class="no-deco roll-over-color" href="<?php echo $site; ?>/about/terms-of-service/">Terms of Use</a></li>
-                                <li><a class="no-deco roll-over-color" href="<?php echo $site; ?>/about/credits/">Credits</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-sm-2 col-share">
-                            <h2>Share</h2>
-                            <a class="rss-link" href="<?php echo $site . "/activity/feed/" ?>">RSS</a>
-                            <a class="google-plus-link" href="https://plus.google.com/share?url={URL}" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
-                                        return false;">Google +</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!--end footer: mobile-->
-        </div>
-        <a class="visible-xs" id="go-to-top" href="#"><span class="fa fa-chevron-circle-up"></span><br />top</a>
-    </div>
-    <?php /**
-     * Adds divs that can be used for client-side detection of bootstrap breakpoints
-     */ ?>
-    <div class="device-xs visible-xs"></div>
-    <div class="device-sm visible-sm"></div>
-    <div class="device-md visible-md"></div>
-    <div class="device-lg visible-lg"></div>
 
-    <?php if (!$placeholder): ?>
-        <script type="text/javascript">
+    ob_start();
+    include(WPMU_PLUGIN_DIR . '/parts/persistent/footer.php');
+    $footer_out = ob_get_clean();
 
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', 'UA-24214531-1']);
-            _gaq.push(['_setDomainName', 'openlab.citytech.cuny.edu']);
-            _gaq.push(['_trackPageview']);
-
-            (function () {
-                var ga = document.createElement('script');
-                ga.type = 'text/javascript';
-                ga.async = true;
-                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0];
-                s.parentNode.insertBefore(ga, s);
-            })();
-
-            jQuery(document).ready(function ($) {
-                getCurrentScroll();
-
-                //go to top functionality
-                $('#go-to-top').on('click', function (e) {
-                    e.preventDefault();
-
-                    var offsetHeight = $('#wpadminbar').height() + $('.navbar').height();
-
-                    $.smoothScroll({
-                        offset: -offsetHeight
-                    });
-
-                });
-
-            });
-            jQuery(window).scroll(function ($) {
-                getCurrentScroll();
-            });
-
-            function getCurrentScroll() {
-                //go to top button functionality
-                var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-
-                if (currentScroll > 250) {
-                    jQuery('#go-to-top').css('display', 'block');
-                } else {
-                    jQuery('#go-to-top').css('display', 'none');
-                }
-
-            }
-
-            //detection of bootstrap breakpoints
-            function isBreakpoint(alias) {
-                return jQuery('.device-' + alias).is(':visible');
-            }
-
-        </script>
-    <?php endif; ?>
-    <?php
+    echo $footer_out;
 }
 
 remove_action('init', 'maybe_add_existing_user_to_blog');
