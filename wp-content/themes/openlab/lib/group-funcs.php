d22577bbebd274cd28aeda34903b8df7138bdefe
junander <joe@joeunander.com>
2016-05-03 14:10:15 -0400

    Feature #1808 - Accessibility> heading hierarchy > Course Search page

--- wp-content/themes/openlab/lib/group-funcs.php
+++ wp-content/themes/openlab/lib/group-funcs.php
@@ -255,10 +255,10 @@ function openlab_group_archive() {
                             </div>
                             <div class="item col-xs-18">
 
-                                <h2 class="item-title">
+                                <p class="item-title h2">
                                     <a class="no-deco truncate-on-the-fly hyphenate" href="<?php bp_group_permalink() ?>" title="<?php bp_group_name() ?>" data-basevalue="<?php echo ($group_type == 'course' ? 50 : 65 ) ?>" data-minvalue="20" data-basewidth="290"><?php bp_group_name() ?></a>
                                     <span class="original-copy hidden"><?php bp_group_name() ?></span>
-                                </h2>
+                                </p>
                                 <?php
                                 //course group type
                                 if ($group_type == 'course'):
