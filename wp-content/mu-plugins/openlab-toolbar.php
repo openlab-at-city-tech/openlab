1eea150c220b7a78a340e91c266158ab66d8d4eb
junander <joe@joeunander.com>
2016-05-30 13:38:37 -0400

    Bug #1820 - rollover state of admin bar drop-down for “See all messages/activity/friends” links is teal on teal

--- wp-content/mu-plugins/openlab-toolbar.php
+++ wp-content/mu-plugins/openlab-toolbar.php
@@ -1371,8 +1371,8 @@ HTML;
             $openlab_toolbar_url = WP_CONTENT_URL . '/mu-plugins/css/openlab-toolbar.css';
             $openlab_toolbar_url = set_url_scheme( $openlab_toolbar_url );
 
-            wp_enqueue_style( 'admin-bar-custom', $adminbar_custom_url,array('font-awesome'), '1.6.8' );
-            wp_enqueue_style( 'openlab-toolbar', $openlab_toolbar_url,array('font-awesome'), '1.6.8' );
+            wp_enqueue_style( 'admin-bar-custom', $adminbar_custom_url,array('font-awesome'), '1.6.9' );
+            wp_enqueue_style( 'openlab-toolbar', $openlab_toolbar_url,array('font-awesome'), '1.6.9' );
         }
 
         function adminbar_special_body_class($classes){
