ac9860e5e63ae7f9181c7ee84882273147a86cc6
junander <joe@joeunander.com>
2016-06-10 16:44:41 -0400

    Feature #1860 - Calendar: Site-wide calendar on about/calendar
    Temporarily walking back auto page creation, something is off

--- wp-content/themes/openlab/lib/core/page-control.php
+++ wp-content/themes/openlab/lib/core/page-control.php
@@ -48,10 +48,10 @@ function openlab_custom_page($slug, $title, $parent_obj = NULL) {
 function openlab_custom_pages() {
 
     $about_page_obj = get_page_by_path('about');
-    openlab_custom_page('calendar', 'OpenLab Calendar', $about_page_obj);
+    //openlab_custom_page('calendar', 'OpenLab Calendar', $about_page_obj);
     
     $calendar_page_obj = get_page_by_path('about/calendar');
-    openlab_custom_page('upcoming', 'OpenLab Calendar: Upcoming', $calendar_page_obj);
+    //openlab_custom_page('upcoming', 'OpenLab Calendar: Upcoming', $calendar_page_obj);
 }
 
 add_filter('after_setup_theme', 'openlab_custom_pages');
