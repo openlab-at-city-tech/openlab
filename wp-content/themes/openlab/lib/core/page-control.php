37b05e8f9a861028ed10c4c52124e2bb2050c636
junander <joe@joeunander.com>
2016-06-27 00:48:07 -0400

    Event Organiser: slight tweak to functions that auto set up pages

--- wp-content/themes/openlab/lib/core/page-control.php
+++ wp-content/themes/openlab/lib/core/page-control.php
@@ -16,11 +16,17 @@ function openlab_custom_page($slug, $title, $parent_obj = NULL) {
     $author_id = 1;
     $check_path = $slug;
 
-    if ($parent_obj) {
-        $parent_id = $parent_obj->ID;
-        $check_path = $parent_obj->post_name . '/' . $slug;
+    if ($parent_obj && !empty($parent_obj)) {
+
+        $ancestor_path = array();
+        foreach ($parent_obj as $parent) {
+            $ancestor_path[] = $parent->post_name;
+            $parent_id = $parent->ID;
+        }
+
+        $check_path = implode('/', $ancestor_path) . '/' . $slug;
     }
-    
+
     if (NULL === get_page_by_path($check_path)) {
 
         $post_id = wp_insert_post(
@@ -48,10 +54,10 @@ function openlab_custom_page($slug, $title, $parent_obj = NULL) {
 function openlab_custom_pages() {
 
     $about_page_obj = get_page_by_path('about');
-    //openlab_custom_page('calendar', 'OpenLab Calendar', $about_page_obj);
-    
+    openlab_custom_page('calendar', 'OpenLab Calendar', array($about_page_obj));
+
     $calendar_page_obj = get_page_by_path('about/calendar');
-    //openlab_custom_page('upcoming', 'OpenLab Calendar: Upcoming', $calendar_page_obj);
+    openlab_custom_page('upcoming', 'OpenLab Calendar: Upcoming', array($about_page_obj, $calendar_page_obj));
 }
 
 add_filter('after_setup_theme', 'openlab_custom_pages');
