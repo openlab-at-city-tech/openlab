a4eb22b8f2e59d557df2129f91efcd197c6fe343
junander <joe@joeunander.com>
2016-07-07 19:02:47 -0400

    Bug #1898 - Calendar: Upcoming Events sidebar widget > add a space before the event time

--- wp-content/themes/openlab/parts/source/sidebar/activity-events-feed.php
+++ wp-content/themes/openlab/parts/source/sidebar/activity-events-feed.php
@@ -11,7 +11,7 @@
                     ?>
                     <li class="bpeo-upcoming-event-<?php echo esc_attr($post->ID) ?>">
                         <a class="bpeo-upcoming-event-title" href="<?php echo esc_url(apply_filters('eventorganiser_calendar_event_link', get_permalink($post->ID), $post->ID)) ?>"><?php echo esc_html($post->post_title) ?></a><br />
-                        <span class="bpeo-upcoming-event-date"><?php echo date('M j, Y', strtotime($post->StartDate)) ?></span> <span class="bpeo-upcoming-event-time"><?php echo date('g:ia', strtotime($post->StartTime)) ?></span>
+                        <span class="bpeo-upcoming-event-date"><?php echo date('M j, Y', strtotime($post->StartDate)) ?></span>&nbsp;<span class="bpeo-upcoming-event-time"><?php echo date('g:ia', strtotime($post->StartTime)) ?></span>
                     </li>
                     <?php
                 }
