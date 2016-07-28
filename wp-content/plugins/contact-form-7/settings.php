0ff3492e995e716c8e89cc46f0a3f3822707ce90
Boone B Gorges <boonebgorges@gmail.com>
2016-07-28 10:50:44 -0500

    Update plugin: contact-form-7.
    Name: Contact Form 7
    New version: 4.4.2
    Previous version: 4.3

--- wp-content/plugins/contact-form-7/settings.php
+++ wp-content/plugins/contact-form-7/settings.php
@@ -12,6 +12,7 @@ require_once WPCF7_PLUGIN_DIR . '/includes/mail.php';
 require_once WPCF7_PLUGIN_DIR . '/includes/submission.php';
 require_once WPCF7_PLUGIN_DIR . '/includes/upgrade.php';
 require_once WPCF7_PLUGIN_DIR . '/includes/integration.php';
+require_once WPCF7_PLUGIN_DIR . '/includes/config-validator.php';
 
 if ( is_admin() ) {
 	require_once WPCF7_PLUGIN_DIR . '/admin/admin.php';
@@ -29,7 +30,6 @@ class WPCF7 {
 		self::load_module( 'date' );
 		self::load_module( 'file' );
 		self::load_module( 'flamingo' );
-		self::load_module( 'jetpack' );
 		self::load_module( 'listo' );
 		self::load_module( 'number' );
 		self::load_module( 'quiz' );
@@ -118,18 +118,26 @@ function wpcf7_upgrade() {
 add_action( 'activate_' . WPCF7_PLUGIN_BASENAME, 'wpcf7_install' );
 
 function wpcf7_install() {
-	if ( $opt = get_option( 'wpcf7' ) )
+	if ( $opt = get_option( 'wpcf7' ) ) {
 		return;
+	}
 
 	wpcf7_load_textdomain();
 	wpcf7_register_post_types();
 	wpcf7_upgrade();
 
-	if ( get_posts( array( 'post_type' => 'wpcf7_contact_form' ) ) )
+	if ( get_posts( array( 'post_type' => 'wpcf7_contact_form' ) ) ) {
 		return;
+	}
 
 	$contact_form = WPCF7_ContactForm::get_template( array(
 		'title' => sprintf( __( 'Contact form %d', 'contact-form-7' ), 1 ) ) );
 
 	$contact_form->save();
+
+	WPCF7::update_option( 'bulk_validate', array(
+		'timestamp' => current_time( 'timestamp' ),
+		'version' => WPCF7_VERSION,
+		'count_valid' => 1,
+		'count_invalid' => 0 ) );
 }
