openlab-at-city-tech/openlab

commit 96edbf105c5c6848dc1e4658f049f3e610b70151

Hemingway theme: support for Profile and Home links on fallback nav.
Hemingway uses `wp_list_pages()` rather than a proper function from the
Menu API to build its fallback.

See #1932.

wp-content/mu-plugins/openlab-theme-fixes.php
@@ -60,3 +60,61 @@ function openlab_reorder_theme_selections( $themes ) {
 	return array_merge( $t1, $t2 );
 }
 add_filter( 'wp_prepare_themes_for_js', 'openlab_reorder_theme_selections' );
+
+/**
+ * Hemingway: When there's no nav menu, ensure that Course Profile and Home links appear.
+ *
+ * This theme uses wp_list_pages() rather than a normal WP function for building
+ * the default menu.
+ */
+function openlab_fix_fallback_menu_for_hemingway( $output, $r, $pages ) {
+	if ( 'hemingway' !== get_template() ) {
+		return $pages;
+	}
+
+	$dbs = debug_backtrace();
+	$gp_key = null;
+	foreach ( $dbs as $key => $db ) {
+		if ( 'wp_list_pages' === $db['function'] ) {
+			$lp_key = $key;
+			break;
+		}
+	}
+
+	if ( null === $lp_key ) {
+		return $output;
+	}
+
+	// It really doesn't get any worse than this.
+	if ( ! isset( $dbs[ $lp_key + 4 ] ) || 'get_header' !== $dbs[ $lp_key + 4 ]['function'] ) {
+		return $output;
+	}
+
+	// Fake pages.
+	$group_id = openlab_get_group_id_by_blog_id( get_current_blog_id() );
+	if ( ! $group_id ) {
+		return $output;
+	}
+
+	$home_link = sprintf(
+		'<li><a title="Site Home" href="%s">Home</a></li>',
+		esc_url( trailingslashit( get_option( 'home' ) ) )
+	);
+
+	$group_type_label = openlab_get_group_type_label( array(
+		'group_id' => $group_id,
+		'case' => 'upper',
+	) );
+	$group_link = bp_get_group_permalink( groups_get_group( array( 'group_id' => $group_id ) ) );
+
+	$profile_link = sprintf(
+		'<li id="menu-item-group-profile-link" class="group-profile-link"><a href="%s">%s</a>',
+		esc_url( $group_link ),
+		sprintf( '%s Profile', $group_type_label )
+	);
+
+	$output = $profile_link . "\n" . $home_link . "\n" . $output;
+
+	return $output;
+}
+add_filter( 'wp_list_pages', 'openlab_fix_fallback_menu_for_hemingway', 10, 3 );

commit 9c53fed6a3daa814cfe38b8330e7caffe9ef9268

Fix incorrect variable name introduced in 96edbf105c5c6848dc1e4658f049f3e610b70151.
See #1932.

wp-content/mu-plugins/openlab-theme-fixes.php
@@ -70,7 +70,7 @@ add_filter( 'wp_prepare_themes_for_js', 'openlab_reorder_theme_selections' );
  */
 function openlab_fix_fallback_menu_for_hemingway( $output, $r, $pages ) {
 	if ( 'hemingway' !== get_template() ) {
-		return $pages;
+		return $output;
 	}
 
 	$dbs = debug_backtrace();