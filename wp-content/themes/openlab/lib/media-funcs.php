35b3e8bfe5c1d744dd77321de0a7f96b539aeeb2
Boone B Gorges <boonebgorges@gmail.com>
2016-08-30 16:45:44 -0500

    Add caching for What's Happening widget.
    See #1943.

--- wp-content/themes/openlab/lib/media-funcs.php
+++ wp-content/themes/openlab/lib/media-funcs.php
@@ -98,9 +98,12 @@ function openlab_activity_user_avatar() {
     return '<img class="img-responsive" src ="' . bp_core_fetch_avatar(array('item_id' => $item_id, 'object' => 'user', 'type' => 'full', 'html' => false)) . '" alt="' . bp_get_displayed_user_fullname() . '"/>';
 }
 
-function openlab_activity_group_avatar() {
+function openlab_activity_group_avatar( $current_activity_item = null ) {
     global $activities_template;
-    $current_activity_item = isset($activities_template->activity->current_comment) ? $activities_template->activity->current_comment : $activities_template->activity;
+
+    if ( null === $current_activity_item ) {
+	    $current_activity_item = isset($activities_template->activity->current_comment) ? $activities_template->activity->current_comment : $activities_template->activity;
+    }
 
     $item_id = $current_activity_item->item_id;
 
@@ -109,9 +112,12 @@ function openlab_activity_group_avatar() {
     return '<img class="img-responsive" src ="' . bp_core_fetch_avatar(array('item_id' => $item_id, 'object' => 'group', 'type' => 'full', 'html' => false)) . '" alt="' . $group->name . '"/>';
 }
 
-function openlab_activity_group_link() {
+function openlab_activity_group_link( $current_activity_item = null ) {
     global $bp, $activities_template;
-    $current_activity_item = isset($activities_template->activity->current_comment) ? $activities_template->activity->current_comment : $activities_template->activity;
+
+    if ( null === $current_activity_item ) {
+	    $current_activity_item = isset($activities_template->activity->current_comment) ? $activities_template->activity->current_comment : $activities_template->activity;
+    }
 
     $item_id = $current_activity_item->item_id;
 
@@ -121,23 +127,58 @@ function openlab_activity_group_link() {
 }
 
 /**
+ * Get the list of activity types that should appear in the What's Happening feed.
+ *
+ * @return array
+ */
+function openlab_whats_happening_activity_types() {
+	return array( 'created_group', 'added_group_document', 'bbp_reply_create', 'bbp_topic_create', 'bpeo_create_event', 'bpeo_edit_event', 'bp_doc_comment', 'bp_doc_created', 'bp_doc_edited', 'deleted_group_document', 'joined_group', 'new_blog', 'new_blog_comment', 'new_blog_post', 'new_forum_post', 'new_forum_topic', 'group_details_updated' );
+}
+/**
+ * Get activity items for the What's Happening feed.
+ *
+ * @return array
+ */
+function openlab_whats_happening_activity_items() {
+	$cached = wp_cache_get( 'whats_happening_items', 'openlab' );
+	if ( ! $cached ) {
+		$now = new DateTime();
+		$activity_args = array(
+			'per_page' => 10,
+			'filter' => array(
+				'action' => openlab_whats_happening_activity_types(),
+			),
+			'update_meta_cache' => false, //we'll be hitting this alot
+			'date_query' => array(
+				'before' => $now->format( 'Y-m-d H:i:s' ),
+			),
+		);
+
+		$a = bp_activity_get( $activity_args );
+		$cached = $a['activities'];
+		wp_cache_set( 'whats_happening_items', $cached, 'openlab' );
+	}
+
+	return $cached;
+}
+
+/**
+ * Invalidate whats_happening cache when a new activity item is posted.
+ */
+function openlab_invalidate_whats_happening_cache( $args ) {
+	if ( in_array( $args['type'], openlab_whats_happening_activity_types(), true ) ) {
+		wp_cache_delete( 'whats_happening_items', 'openlab' );
+	}
+}
+add_action( 'bp_activity_add', 'openlab_invalidate_whats_happening_cache' );
+
+/**
  * Generates activity feed
  * Filters by group-centric actions
  * @return type
  */
 function openlab_whats_happening() {
     $whats_happening_out = '';
-
-    $now = new DateTime();
-
-    $activity_args = array(
-        'per_page' => 10,
-        'action' => array('created_group', 'added_group_document', 'bbp_reply_create', 'bbp_topic_create', 'bpeo_create_event', 'bpeo_edit_event', 'bp_doc_comment', 'bp_doc_created', 'bp_doc_edited', 'deleted_group_document', 'joined_group', 'new_blog', 'new_blog_comment', 'new_blog_post', 'new_forum_post', 'new_forum_topic', 'group_details_updated'),
-        'update_meta_cache' => false, //we'll be hitting this alot
-        'date_query' => array(
-            'before' => $now->format('Y-m-d H:i:s'),
-        ),
-    );
 
     ob_start();
     include(locate_template('parts/home/whats-happening.php'));
