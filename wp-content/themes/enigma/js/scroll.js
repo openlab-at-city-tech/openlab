36c662a075eacb05d9147639e96ae08ef353132c
Boone B Gorges <boonebgorges@gmail.com>
2016-10-17 10:35:05 -0500

    Remove theme: enigma.
    See #1954.

--- wp-content/themes/enigma/js/scroll.js
+++
@@ -1,6 +0,0 @@
-/*============================================
-	Scrolling Animations
-	==============================================*/
-	jQuery('.scrollimation').waypoint(function(){
-		jQuery(this).addClass('in');
-	},{offset:'100%'});
