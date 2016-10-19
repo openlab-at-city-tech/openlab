85e17c30492c47ff8e24c0748fbc75b79658e069
OpenLab <openlab@citytech.cuny.edu>
2016-10-18 21:31:51 -0400

    webwork config

--- wp-content/mu-plugins/webwork-config.php
+++ wp-content/mu-plugins/webwork-config.php
@@ -2,10 +2,10 @@
 
 add_filter( 'webwork_client_site_base', function() {
 	$base = get_blog_option( 1, 'home' );
-	return trailingslashit( $base ) . 'webwork-playground/';
+	return trailingslashit( $base ) . 'ol-webwork';
 } );
 
 add_filter( 'webwork_server_site_base', function() {
 	$base = get_blog_option( 1, 'home' );
-	return trailingslashit( $base );
+	return trailingslashit( $base ) . 'ol-webwork';
 } );
