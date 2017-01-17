94472fe8ff6c32c4461e36bc84a9d03d0c6e9acc
Boone B Gorges <boonebgorges@gmail.com>
2017-01-17 14:04:12 -0600

    Fix broken filepath in concat task.
    See #2007.

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function (grunt) {
 			'wp-content/plugins/buddypress/bp-core/js/vendor/jquery-cookie.min.js',
 			'wp-content/plugins/buddypress/bp-core/js/vendor/jquery-scroll-to.min.js',
 			'wp-content/plugins/buddypress-group-documents/js/general.js',
-			'wp-content/plugins/buddypress-group-email-subscription/bp-activity-subscription.js',
+			'wp-content/plugins/buddypress-group-email-subscription/bp-activity-subscription-js.js',
 			'wp-content/plugins/buddypress/bp-core/js/vendor/jquery.caret.min.js',
 			'wp-content/plugins/buddypress/bp-core/js/vendor/jquery.atwho.min.js',
 			'wp-content/plugins/buddypress/bp-activity/js/mentions.min.js'
