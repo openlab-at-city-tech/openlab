cf37492e0a50d511f1995fb9296ecbaa92f9e8ab
Boone B Gorges <boonebgorges@gmail.com>
2017-02-07 14:46:39 -0600

    'Join OpenLab' -> 'Join the OpenLab' in front-page login box.
    See #2021.

--- wp-content/themes/openlab/lib/page-funcs.php
+++ wp-content/themes/openlab/lib/page-funcs.php
@@ -43,7 +43,7 @@ function cuny_home_login() {
 
     <?php else : ?>
         <?php echo '<div id="open-lab-join" class="log-box links-lighter-hover">'; ?>
-        <?php echo '<h1 class="title"><span class="fa fa-plus-circle flush-left"></span> Join OpenLab</h1>'; ?>
+        <?php echo '<h1 class="title"><span class="fa fa-plus-circle flush-left"></span> Join the OpenLab</h1>'; ?>
         <?php _e('<p><a class="btn btn-default btn-primary link-btn pull-right semibold" href="' . site_url() . '/register/">Sign up</a> <span class="font-size font-14">Need an account?<br />Sign Up to become a member!</span></p>', 'buddypress') ?>
         <?php echo '</div>'; ?>
 
