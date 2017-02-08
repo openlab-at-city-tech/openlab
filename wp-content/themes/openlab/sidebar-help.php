a883c401495bfde704dded09a95d8ed65f5f06b2
Boone B Gorges <boonebgorges@gmail.com>
2017-02-08 11:20:13 -0600

    s/Our Support Team/Community Team
    See #2022.

--- wp-content/themes/openlab/sidebar-help.php
+++ wp-content/themes/openlab/sidebar-help.php
@@ -31,10 +31,10 @@ wp_nav_menu($args);
 </div>
 </div>
 
-<h2 class="sidebar-help-title support-team-title">Our Support Team</h2>
+<h2 class="sidebar-help-title support-team-title">Community Team</h2>
 <div class="sidebar-block padded-block">
 <div id="support-team">
-    <div class="help-tags-copy"><p>The Support Team is here to answer all your OpenLab questions.</p></div>
+    <div class="help-tags-copy"><p>The Community Team is here to answer all your OpenLab questions.</p></div>
     <?php
 	$args = array(
 		'name' => 'contact-us',
