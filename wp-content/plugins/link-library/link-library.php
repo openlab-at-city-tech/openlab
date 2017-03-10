8251e15cec38626e1ae9f32d9033e5040183a8f8
Boone B Gorges <boonebgorges@gmail.com>
2017-03-10 11:18:14 -0600

    Update plugin: link-library.
    Name: Link Library
    New version: 5.9.13.20
    Previous version: 5.9.13.19

--- wp-content/plugins/link-library/link-library.php
+++ wp-content/plugins/link-library/link-library.php
@@ -3,7 +3,7 @@
 Plugin Name: Link Library
 Plugin URI: http://wordpress.org/extend/plugins/link-library/
 Description: Display links on pages with a variety of options
-Version: 5.9.13.19
+Version: 5.9.13.20
 Author: Yannick Lefebvre
 Author URI: http://ylefebvre.ca/
 Text Domain: link-library
