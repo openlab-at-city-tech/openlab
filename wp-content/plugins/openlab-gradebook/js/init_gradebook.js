ac474a2576569782039b51d6d311fe798e246230
junander <joe@joeunander.com>
2017-09-04 23:25:31 -0400

    OpenLab Gradebook
    Small fix to address JS error

--- wp-content/plugins/openlab-gradebook/js/init_gradebook.js
+++ wp-content/plugins/openlab-gradebook/js/init_gradebook.js
@@ -1,10 +1,14 @@
-(function($){
-		$(document).ready(function(){
-		var _x = document.querySelector('a.toplevel_page_oplb_gradebook');	
-		_x.setAttribute('href',_x.getAttribute('href') + '#courses');
-		var _x = document.querySelector('[href="admin.php?page=oplb_gradebook"]:not(.toplevel_page_oplb_gradebook)');
-		_x.setAttribute('href',_x.getAttribute('href') + '#courses');
-		var _x = document.querySelector('[href$="oplb_gradebook_settings"]');
-		_x.setAttribute('href',_x.getAttribute('href') + '#settings');	
-	});
+(function ($) {
+    $(document).ready(function () {
+        var _x = document.querySelector('a.toplevel_page_oplb_gradebook');
+        _x.setAttribute('href', _x.getAttribute('href') + '#courses');
+        var _x = document.querySelector('[href="admin.php?page=oplb_gradebook"]:not(.toplevel_page_oplb_gradebook)');
+
+        if (typeof x !== 'undefined') {
+            _x.setAttribute('href', _x.getAttribute('href') + '#courses');
+        }
+
+        var _x = document.querySelector('[href$="oplb_gradebook_settings"]');
+        _x.setAttribute('href', _x.getAttribute('href') + '#settings');
+    });
 })(jQuery);
