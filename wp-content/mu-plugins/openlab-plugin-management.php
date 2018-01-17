d8f54dceb9e58935afebc0cc1c06522e883f78a0
Boone B Gorges <boonebgorges@gmail.com>
2018-01-17 12:59:03 -0600

    Make HD5 plugin available to openstax-bio.
    See #2088.

--- wp-content/mu-plugins/openlab-plugin-management.php
+++ wp-content/mu-plugins/openlab-plugin-management.php
@@ -83,6 +83,7 @@ function openlab_hide_plugins( $plugins ) {
 	$blog_specific_whitelist = array(
 		'h5p/h5p.php' => array(
 			11188, // bio-oer - https://redmine.citytech.cuny.edu/issues/2088
+			11261, // openstax-bio - https://redmine.citytech.cuny.edu/issues/2088
 		),
 	);
 
