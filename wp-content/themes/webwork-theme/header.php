9819cf4af3b38d20e329bf1c6a045dd928700905
Boone B Gorges <boonebgorges@gmail.com>
2018-05-01 17:05:00 -0500

    Theme updates for a11y in webwork-theme.
    See livinglab/webwork-for-wordpress#109.

--- wp-content/themes/webwork-theme/header.php
+++ wp-content/themes/webwork-theme/header.php
@@ -5,7 +5,7 @@
 	<head>
 
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" >
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 
 		<?php if ( is_singular() ) wp_enqueue_script( "comment-reply" ); ?>
 
@@ -17,7 +17,7 @@
 
 		<div class="big-wrapper">
 
-			<div class="header-cover section bg-dark-light no-padding">
+			<div class="header-cover section bg-dark-light no-padding" role="banner">
 
 				<div class="header section">
 					<img class="banner-img" src="<?php echo get_stylesheet_directory_uri() . '/images/OLWW_BANNER.png'; ?>" alt="OpenLab WeBWorK logo" />
@@ -25,7 +25,7 @@
 
 			</div> <!-- /bg-dark -->
 
-			<div class="navigation section no-padding bg-dark">
+			<div class="navigation section no-padding bg-dark" role="navigation">
 
 				<div class="navigation-inner section-inner">
 
@@ -74,8 +74,6 @@
 								'title_li' => '',
 							) ) ?>
 						<?php endif; ?>
-
-						<div class="clear"></div>
 
 					 </ul>
 
