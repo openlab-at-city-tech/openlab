cc6de7506d28b94689ad2b075b54d3860f27514c
junander <joe@joeunander.com>
2018-08-14 12:29:52 -0400

    Bug #2397 - Gradebook: Averages do not account for grades of zero
    Making sure null values properly display in student view - see
    http://redmine.citytech.cuny.edu/issues/2397#note-3

--- wp-content/plugins/openlab-gradebook/js/app/templates/edit-cell-template.php
+++ wp-content/plugins/openlab-gradebook/js/app/templates/edit-cell-template.php
@@ -5,8 +5,13 @@
     <% } else { %>
         <%= cell.get('assign_points_earned') %>
     <% } %>
-
     </span>
 <% } else { %>
-    <span class="grade-numeric" style="cursor: default;"><%= cell.get('assign_points_earned') %></span>
+    <span class="grade-numeric" style="cursor: default;">
+    <% if(cell.get('is_null')) { %>
+        --
+    <% } else { %>
+        <%= cell.get('assign_points_earned') %>
+    <% } %>
+    </span>
 <% } %>
