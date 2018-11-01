3aa54ac7514e3ccb28e937bafebbed4db85911e9
Boone B Gorges <boonebgorges@gmail.com>
2018-11-01 13:38:53 -0500

    Update env-sample.php.

--- env-sample.php
+++ env-sample.php
@@ -9,16 +9,6 @@ define( 'DB_PASSWORD', '' );
 define( 'DB_HOST', '' );
 
 /**
- * bbPress (BP version)
- *
- * These should be the same as your WP info
- */
-define( 'BBDB_NAME', '' );
-define( 'BBDB_USER', '' );
-define( 'BBDB_PASSWORD', '' );
-define( 'BBDB_HOST', '' );
-
-/**
  * Other environment specific constants
  */
 define( 'IS_LOCAL_ENV', true ); // Leave this as true, except on staging and production environments
@@ -27,7 +17,3 @@ define( 'WP_DEBUG', false );
 define( 'DOMAIN_CURRENT_SITE', 'openlabdev.org' );
 define( 'PATH_CURRENT_SITE', '/' );
 define( 'DO_SHARDB', false );
-
-@ini_set('log_errors','On');
-@ini_set('display_errors','Off');
-@ini_set('error_log','/usr/home/openlab/public_html/php_error.log');
