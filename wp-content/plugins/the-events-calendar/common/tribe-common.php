b6b18a8ced0a0438a7012ec7f55e4db79a72181e
Boone B Gorges <boonebgorges@gmail.com>
2018-11-02 18:07:38 +0000

    Update plugin: the-events-calendar.
    Name: The Events Calendar
    New version: 4.6.25
    Previous version: 4.6.24
    
    See #2453.

--- wp-content/plugins/the-events-calendar/common/tribe-common.php
+++ wp-content/plugins/the-events-calendar/common/tribe-common.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Description: An event settings framework for managing shared options
-Version: 4.7.21
+Version: 4.7.22
 Author: Modern Tribe, Inc.
 Author URI: http://m.tri.be/1x
 Text Domain: tribe-common
