0f123482f32306d66ad8a1a948fbb96a2eee57a4
Boone B Gorges <boonebgorges@gmail.com>
2019-01-13 20:28:00 -0600

    Don't show Site Dashboard link to users without the ability to edit posts.
    See #1392, #2483.

--- wp-content/themes/openlab/lib/menus.php
+++ wp-content/themes/openlab/lib/menus.php
@@ -681,8 +681,15 @@ function openlab_filter_subnav_home($subnav_item) {
 
     if (!empty($group_site_settings['site_url']) && $group_site_settings['is_visible']) {
         $site_link = '<li id="site-groups-li" class="visible-xs"><a href="' . trailingslashit(esc_attr($group_site_settings['site_url'])) . '" id="site">' . $group_label . ' Site</a></li>';
-
-        if ($group_site_settings['is_local'] && ((openlab_is_portfolio() && openlab_is_my_portfolio()) || (!openlab_is_portfolio() && groups_is_user_member(bp_loggedin_user_id(), bp_get_current_group_id())) || $bp->is_item_admin || is_super_admin())) {
+        if (
+            $group_site_settings['is_local'] &&
+            (
+                (openlab_is_portfolio() && openlab_is_my_portfolio())
+                || ( !openlab_is_portfolio() && groups_is_user_member( bp_loggedin_user_id(), bp_get_current_group_id() ) && current_user_can_for_blog( $group_site_settings['site_id'], 'edit_posts' ) )
+                || $bp->is_item_admin
+                || is_super_admin()
+            )
+        ) {
 
             $site_link .= '<li id="site-admin-groups-li" class="visible-xs"><a href="' . trailingslashit(esc_attr($group_site_settings['site_url'])) . 'wp-admin/" id="site-admin">Site Dashboard</a></li>';
         }
