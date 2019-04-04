f3e531387e010e3de16cf543591183eeff6c1921
Boone B Gorges <boonebgorges@gmail.com>
2019-04-04 12:23:26 -0500

    Update theme: customify.
    Name: Customify
    New version: 0.2.9
    Previous version: 0.2.8

--- wp-content/themes/customify/inc/panel-builder/class-layout-builder-frontend.php
+++ wp-content/themes/customify/inc/panel-builder/class-layout-builder-frontend.php
@@ -1,13 +1,17 @@
 <?php
-
-class Customify_Customize_Layout_Builder_Frontend {
+/**
+ * Builder frontemd class
+ *
+ * @since 0.2.9
+ */
+class Customify_Layout_Builder_Frontend extends Customify_Abstract_Layout_Frontend {
 	public static $_instance;
-	private $control_id = 'header_builder_panel';
+	protected $control_id = 'header_builder_panel';
 	public $id = 'header';
-	private $render_items = array();
-	private $rows = array();
-	private $data = false;
-	private $config_items = false;
+	protected $render_items = array();
+	protected $rows = array();
+	protected $data = false;
+	protected $config_items = false;
 
 	public function __construct() {
 
@@ -19,6 +23,10 @@ class Customify_Customize_Layout_Builder_Frontend {
 		}
 
 		return self::$_instance;
+	}
+
+	public function get_id() {
+		return $this->id;
 	}
 
 	public function set_id( $id ) {
@@ -89,7 +97,6 @@ class Customify_Customize_Layout_Builder_Frontend {
 
 		return false;
 	}
-
 	/**
 	 * Render items to HTML
 	 *
@@ -247,31 +254,6 @@ class Customify_Customize_Layout_Builder_Frontend {
 		array_multisort( $ordered_items, SORT_ASC, $items );
 
 		return $items;
-	}
-
-	/**
-	 * Setup Item content
-	 *
-	 * @todo Ensure item have not duplicate id
-	 *
-	 * @param string $content Content.
-	 * @param string $id      ID.
-	 * @param string $device  Device ID.
-	 *
-	 * @return mixed
-	 */
-	public function setup_item_content( $content, $id, $device ) {
-		$content = str_replace( '__id__', $id, $content );
-		$content = str_replace( '__device__', $device, $content );
-		/**
-		 *
-		 * Ensure only one H! tag for the site title
-		 *
-		 * @since 0.2.3
-		 */
-		$content = str_replace( '__site_device_tag__', 'desktop' == $device ? 'h1' : 'h2', $content );
-
-		return $content;
 	}
 
 	public function render_row( $items, $id = '', $device = 'desktop' ) {
@@ -640,24 +622,36 @@ class Customify_Customize_Layout_Builder_Frontend {
 		}
 	}
 
-	/**
-	 * Close item HTML markup
-	 *
-	 * @param string $class Icon class.
-	 *
-	 * @return string
-	 */
-	public function close_icon( $class = '' ) {
-		$menu_sidebar_skin = Customify()->get_setting( 'header_sidebar_text_mode' );
-		$close             = '<a class="close is-size-medium ' . $menu_sidebar_skin . esc_attr( $class ) . '" href="#">
-        <span class="hamburger hamburger--squeeze is-active">
-            <span class="hamburger-box">
-              <span class="hamburger-inner"><span class="screen-reader-text">' . __( 'Menu', 'customify' ) . '</span></span>
-            </span>
-        </span>
-        <span class="screen-reader-text">' . __( 'Close', 'customify' ) . '</span>
-        </a>';
+}
 
-		return $close;
-	}
+
+/**
+ * Deprecated class name
+ *
+ * @deprecated 0.2.7
+ */
+class Customify_Customize_Layout_Builder_Frontend extends Customify_Layout_Builder_Frontend {}
+
+/**
+ * Alias of class Customify_Layout_Builder_Frontend
+ *
+ * @see Customify_Layout_Builder_Frontend
+ *
+ * @return Customify_Layout_Builder_Frontend
+ */
+function Customify_Layout_Builder_Frontend() {
+	return Customify_Layout_Builder_Frontend::get_instance();
+}
+
+
+/**
+ * Alias of class Customify_Layout_Builder_Frontend
+ *
+ * @see Customify_Layout_Builder_Frontend
+ * @deprecated 2.0.7
+ *
+ * @return Customify_Layout_Builder_Frontend
+ */
+function Customify_Customize_Layout_Builder_Frontend() {
+	return Customify_Layout_Builder_Frontend();
 }
