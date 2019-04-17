df08649ffa8045e713843f03dd1f2b18765ce414
Boone B Gorges <boonebgorges@gmail.com>
2019-04-17 11:42:43 -0500

    Add theme: charm.
    See #2537.

---
+++ wp-content/themes/charm/footer.php
@@ -0,0 +1,11 @@
+	</main>
+
+	<footer class="site-footer">
+		<div class="site-copyright">
+			<?php echo wpautop( get_theme_mod( 'rain_copyright', '&copy; ' . date( 'Y ' ) . get_bloginfo( 'name' ) ) ); ?>
+		</div>
+	</footer>
+
+	<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/charm/sidebar.php
@@ -0,0 +1,5 @@
+<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+	<div class="page-sidebar">
+		<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	</div>
+<?php endif; ?>
