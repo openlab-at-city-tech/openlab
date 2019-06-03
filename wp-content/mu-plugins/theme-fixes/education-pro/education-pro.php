daa3909b736f923d274f31c2fb831a82ebfd7dcd
Boone B Gorges <boonebgorges@gmail.com>
2019-06-03 13:29:54 -0500

    education-pro: Remove Google AdSense and Header/Footer Scripts metaboxes.
    See #2548.

--- wp-content/mu-plugins/theme-fixes/education-pro/education-pro.php
+++ wp-content/mu-plugins/theme-fixes/education-pro/education-pro.php
@@ -7,3 +7,15 @@
  * on the theme's Settings panel.
  */
 remove_theme_support( 'genesis-auto-updates' );
+
+/**
+ * Remove unused Settings metaboxes.
+ */
+add_action(
+	'load-toplevel_page_genesis',
+	function() {
+		remove_meta_box( 'genesis-theme-settings-adsense', get_current_screen(), 'main'  );
+		remove_meta_box( 'genesis-theme-settings-scripts', get_current_screen(), 'main'  );
+	},
+	50
+);
