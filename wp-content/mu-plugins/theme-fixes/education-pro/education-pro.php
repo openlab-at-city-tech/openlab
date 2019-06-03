d48247734326437036622b3f54f5042ff40700bb
Boone B Gorges <boonebgorges@gmail.com>
2019-06-03 13:24:18 -0500

    Remove auto-update support from education-pro.
    This prevents the Updates section from appearing on the Settings panel.
    
    See #2548.

--- wp-content/mu-plugins/theme-fixes/education-pro/education-pro.php
+++ wp-content/mu-plugins/theme-fixes/education-pro/education-pro.php
@@ -1,2 +1,9 @@
 <?php
 
+/**
+ * Disable auto-update support for the theme.
+ *
+ * We manage the theme independently. This also prevents 'Updates' section from appearing
+ * on the theme's Settings panel.
+ */
+remove_theme_support( 'genesis-auto-updates' );
