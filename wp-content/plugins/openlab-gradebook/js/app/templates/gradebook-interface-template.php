90154161240dd41290eb471fb0df4717ea0d0287
junander <joe@joeunander.com>
2019-06-05 23:55:38 -0400

    OpenLab Gradebook: adding arrow icons to column sorting

--- wp-content/plugins/openlab-gradebook/js/app/templates/gradebook-interface-template.php
+++ wp-content/plugins/openlab-gradebook/js/app/templates/gradebook-interface-template.php
@@ -48,23 +48,74 @@ if (count($z)) {
             <div
                 class="table-wrapper <% if(assign_length === 0) { print('no-assignments') } else { print('assignments') } %>">
                 <div class="pinned hidden-xs">
-                    <table id="an-gradebook-container-pinned" class="table table-bordered table-striped">  
+                    <table id="an-gradebook-container-pinned" class="table table-bordered table-striped">
                         <thead id="students-header-pinned" class="students-header">
                             <tr>
-                                <th class="gradebook-student-column-interactive student-tools download-csv adjust-widths" data-targetwidth="50"></th>
-                                <th class="gradebook-student-column-first_name"><span data-toggle="tooltip" data-placement="top" title='<?php esc_html_e('First Name', 'openlab-gradebook') ?>'><?php esc_html_e('First Name', 'openlab-gradebook') ?></span></th>
-                                <th class="gradebook-student-column-last_name"><span data-toggle="tooltip" data-placement="top" title='<?php esc_html_e('Last Name', 'openlab-gradebook') ?>'><?php esc_html_e('Last Name', 'openlab-gradebook') ?></span></th>
-                                <th class="gradebook-student-column-user_login"><span data-toggle="tooltip" data-placement="top" title='<?php esc_html_e('Username', 'openlab-gradebook') ?>'><?php esc_html_e('Username', 'openlab-gradebook') ?></span></th>
-                                <th class="gradebook-student-column-average adjust-widths" data-targetwidth="65"><span data-toggle="tooltip" data-placement="top" title='<?php esc_html_e('Current Average Grade', 'openlab-gradebook') ?>'><?php esc_html_e('Avg.', 'openlab-gradebook') ?></span></th>
+                                <th class="gradebook-student-column-interactive student-tools download-csv adjust-widths"
+                                    data-targetwidth="50"></th>
+                                <th class="gradebook-student-column-first_name">
+                                    <span class="header-wrapper">
+                                        <span class="tooltip-wrapper" data-toggle="tooltip"
+                                            data-placement="top"
+                                            title='<?php esc_html_e('First Name', 'openlab-gradebook')?>'><?php esc_html_e('First Name', 'openlab-gradebook')?></span>
+                                            <span class="arrow-placement">
+                                                <span class="arrow-wrapper">
+                                                    <span class="glyphicon glyphicon-triangle-top" aria-hidden="true"></span>
+                                                    <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true"></span>
+                                                </span>
+                                            </span>
+                                    </span>
+                                </th>
+                                <th class="gradebook-student-column-last_name">
+                                    <span class="header-wrapper sort-up">
+                                        <span class="tooltip-wrapper" data-toggle="tooltip"
+                                        data-placement="top"
+                                        title='<?php esc_html_e('Last Name', 'openlab-gradebook')?>'><?php esc_html_e('Last Name', 'openlab-gradebook')?></span>
+                                        <span class="arrow-placement">
+                                            <span class="arrow-wrapper">
+                                                <span class="glyphicon glyphicon-triangle-top" aria-hidden="true"></span>
+                                                <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true"></span>
+                                            </span>
+                                        </span>
+                                    </span>
+                                </th>
+                                <th class="gradebook-student-column-user_login">
+                                    <span class="header-wrapper">    
+                                        <span class="tooltip-wrapper" data-toggle="tooltip"
+                                        data-placement="top"
+                                        title='<?php esc_html_e('Username', 'openlab-gradebook')?>'><?php esc_html_e('Username', 'openlab-gradebook')?></span>
+                                        <span class="arrow-placement">
+                                            <span class="arrow-wrapper">
+                                                <span class="glyphicon glyphicon-triangle-top" aria-hidden="true"></span>
+                                                <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true"></span>
+                                            </span>
+                                        </span>
+                                    </span>
+                                </th>
+                                <th class="gradebook-student-column-average adjust-widths" data-targetwidth="65">
+                                    <span class="header-wrapper">
+                                        <span class="tooltip-wrapper"
+                                        data-toggle="tooltip" data-placement="top"
+                                        title='<?php esc_html_e('Current Average Grade', 'openlab-gradebook')?>'><?php esc_html_e('Avg.', 'openlab-gradebook')?></span>
+                                        <span class="arrow-placement">
+                                            <span class="arrow-wrapper">
+                                                <span class="glyphicon glyphicon-triangle-top" aria-hidden="true"></span>
+                                                <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true"></span>
+                                            </span>
+                                        </span>
+                                    </span>
+                                </th>
                             </tr>
-                        </thead>		    	
+                        </thead>
                         <tbody id="students-pinned" class="students"></tbody>
                     </table>
                 </div>
                 <div class="scroll-control">
                     <div class="scrollable">
-                        <table id="an-gradebook-container" class="table table-bordered table-striped">  
-                            <thead id="students-header" class="students-header"><tr></tr></thead>		    	
+                        <table id="an-gradebook-container" class="table table-bordered table-striped">
+                            <thead id="students-header" class="students-header">
+                                <tr></tr>
+                            </thead>
                             <tbody id="students" class="students"></tbody>
                         </table>
                     </div>
