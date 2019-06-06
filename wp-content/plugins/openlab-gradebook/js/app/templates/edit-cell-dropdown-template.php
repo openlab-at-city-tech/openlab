150b6a7eb580987aec111b0d81ca3e1156b0a2d6
junander <joe@joeunander.com>
2019-06-06 00:02:03 -0400

    OpenLab Gradebook: styling updates to grades in Student View

--- wp-content/plugins/openlab-gradebook/js/app/templates/edit-cell-dropdown-template.php
+++ wp-content/plugins/openlab-gradebook/js/app/templates/edit-cell-dropdown-template.php
@@ -1,9 +1,18 @@
 <div class="cell-wrapper">
-    <select class="grade-selector" <%= role === 'instructor' ? '' : 'disabled="disabled"' %>>
+    <% if (role === 'instructor') { %>
+    <select class="grade-selector">
         <% grades.each(function(grade) { %>
         <option value="<%= grade.get('value') %>"
             <% if(cell.get('assign_points_earned') >= grade.get('range_low') && cell.get('assign_points_earned') < grade.get('range_high')) { %>selected<% } %>>
             <%= grade.get('label') %></option>
         <% }); %>
     </select>
+    <% } else { %>
+    <span class="grade-numeric" style="cursor: default;">
+        <% grades.each(function(grade) { %>
+        <% if(cell.get('assign_points_earned') >= grade.get('range_low') && cell.get('assign_points_earned') < grade.get('range_high')) { %>
+        <%= grade.get('label') %> <% } %>
+        <% }); %>
+    </span>
+    <% } %>
 </div>
--- wp-content/plugins/openlab-gradebook/js/app/templates/student-view-template.php
+++ wp-content/plugins/openlab-gradebook/js/app/templates/student-view-template.php
@@ -28,19 +28,35 @@
 <td class="gradebook-student-column-average student<%= mobile_styles %>"><span id="average<%= student.get('id') %>" data-toggle="tooltip" data-placement="top" title='<%= student.get("current_grade_average") %>'><%= student.get("current_grade_average") %></span></td>
 <td class="student student-grades mid-semester-grade">
 <span>
-<select class="grade-selector mid" data-type="mid" data-uid="<%= student.get('id') %>" <%= role === 'instructor' ? '' : 'disabled="disabled"' %>>
+<% if(role === 'instructor') { %>
+    <select class="grade-selector mid" data-type="mid" data-uid="<%= student.get('id') %>">
         <% midGrades.each(function(grade) { %>
             <option <% if(grade.get('type') === 'display_value') { print('class="display-value"') } %> value="<%= grade.get('value') %>" <% if(student.get('mid_semester_grade') === grade.get('value')) { %>selected<% } %> ><%= grade.get('label') %></option>
         <% }); %>
-</select>
+    </select>
+<% } else { %>
+    <span class="grade-numeric" style="cursor: default;">
+        <% midGrades.each(function(grade) { %>
+            <% if(student.get('mid_semester_grade') === grade.get('value')) { %><%= grade.get('label') %><% } %>
+        <% }); %>
+    </span>
+<% } %>
 </span>
 </td>
 <td class="student student-grades final-grade">
 <span>
-<select class="grade-selector final" data-type="final" data-uid="<%= student.get('id') %>" <%= role === 'instructor' ? '' : 'disabled="disabled"' %>>
+<% if(role === 'instructor') { %>
+    <select class="grade-selector final" data-type="final" data-uid="<%= student.get('id') %>" <%= role === 'instructor' ? '' : 'disabled="disabled"' %>>
+            <% finalGrades.each(function(grade) { %>
+                <option <% if(grade.get('type') === 'display_value') { print('class="display-value"') } %> value="<%= grade.get('value') %>" <% if(student.get('final_grade') === grade.get('value')) { %>selected<% } %> ><%= grade.get('label') %></option>
+            <% }); %>
+    </select>
+<% } else { %>
+    <span class="grade-numeric" style="cursor: default;">
         <% finalGrades.each(function(grade) { %>
-            <option <% if(grade.get('type') === 'display_value') { print('class="display-value"') } %> value="<%= grade.get('value') %>" <% if(student.get('final_grade') === grade.get('value')) { %>selected<% } %> ><%= grade.get('label') %></option>
+            <% if(student.get('final_grade') === grade.get('value')) { %><%= grade.get('label') %><% } %>
         <% }); %>
-</select>
+    </span>
+<% } %>
 </span>
 </td>
