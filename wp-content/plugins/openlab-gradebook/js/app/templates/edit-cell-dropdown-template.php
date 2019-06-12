ecaab4b5ce0a726e601811438c28b395f1647f2a
junander <joe@joeunander.com>
2019-06-12 15:40:24 -0400

    Spacing

--- wp-content/plugins/openlab-gradebook/js/app/templates/edit-cell-dropdown-template.php
+++ wp-content/plugins/openlab-gradebook/js/app/templates/edit-cell-dropdown-template.php
@@ -1,5 +1,6 @@
 <div class="cell-wrapper">
-    <select class="grade-selector" <%= role === 'instructor' ? '' : 'disabled="disabled"' %>>
+    <% if (role === 'instructor') { %>
+    <select class="grade-selector">
         <% grades.each(function(grade) { %>
         <option value="<%= grade.get('value') %>"
             <% if(cell.get('assign_points_earned') >= grade.get('range_low') && cell.get('assign_points_earned') < grade.get('range_high')) { %>selected<% } %>>
