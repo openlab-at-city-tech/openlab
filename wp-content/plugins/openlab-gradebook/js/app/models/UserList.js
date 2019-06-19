a980bda69c8a4f7816581c0fa8378e9fe8db4e34
junander <joe@joeunander.com>
2019-06-18 21:23:02 -0400

    OpenLab Gradebook: sorting issues
    Working out functional issues with column sorting, particularly on mobile

--- wp-content/plugins/openlab-gradebook/js/app/models/UserList.js
+++ wp-content/plugins/openlab-gradebook/js/app/models/UserList.js
@@ -1,22 +1,23 @@
-define(['backbone', 'models/User'], function (Backbone, User) {
-    var UserCourseList = Backbone.Collection.extend({
-        model: User,
-        search: '',
-        sort_direction: 'asc',
-        sort_key: 'last_name',
-        comparator: function (model1, model2) {
-            var _str1 = model1.get(this.sort_key);
-            var _str2 = model2.get(this.sort_key);
+define(["backbone", "models/User"], function(Backbone, User) {
+	var UserCourseList = Backbone.Collection.extend({
+		model: User,
+		search: "",
+		sort_direction: "asc",
+		sort_key: "last_name",
+		comparator: function(model1, model2) {
 
-            if (this.sort_direction === 'asc') {
-                return _str2.localeCompare(_str1);
-            } else {
-                return _str1.localeCompare(_str2);
-            }
-        },
-        url: function () {
-            return ajaxurl + '?action=oplb_user_list';
-        }
-    });
-    return UserCourseList;
+			var _str1 = model1.get(this.sort_key);
+			var _str2 = model2.get(this.sort_key);
+
+			if (this.sort_direction === "asc") {
+				return _str2.localeCompare(_str1);
+			} else {
+				return _str1.localeCompare(_str2);
+			}
+		},
+		url: function() {
+			return ajaxurl + "?action=oplb_user_list";
+		}
+	});
+	return UserCourseList;
 });
--- wp-content/plugins/openlab-gradebook/js/app/templates/gradebook-interface-template.php
+++ wp-content/plugins/openlab-gradebook/js/app/templates/gradebook-interface-template.php
@@ -59,52 +59,63 @@ if (count($z)) {
                                     <span class="header-wrapper">
                                         <span class="tooltip-wrapper" data-toggle="tooltip"
                                             data-placement="top"
+                                            data-target="first_name"
                                             title='<?php esc_html_e('First Name', 'openlab-gradebook')?>'><?php esc_html_e('First Name', 'openlab-gradebook')?></span>
-                                            <span class="arrow-placement">
-                                                <span class="arrow-wrapper">
-                                                    <span class="glyphicon glyphicon-triangle-top" aria-hidden="true"></span>
-                                                    <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true"></span>
+                                            <% if(role === 'instructor') { %>
+                                                <span class="arrow-placement">
+                                                    <span class="arrow-wrapper">
+                                                        <span class="glyphicon glyphicon-triangle-top" aria-hidden="true"></span>
+                                                        <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true"></span>
+                                                    </span>
                                                 </span>
-                                            </span>
+                                            <% } %>
                                     </span>
                                 </th>
                                 <th class="gradebook-student-column-last_name">
                                     <span class="header-wrapper sort-up">
                                         <span class="tooltip-wrapper" data-toggle="tooltip"
                                         data-placement="top"
+                                        data-target="last_name"
                                         title='<?php esc_html_e('Last Name', 'openlab-gradebook')?>'><?php esc_html_e('Last Name', 'openlab-gradebook')?></span>
-                                        <span class="arrow-placement">
-                                            <span class="arrow-wrapper">
-                                                <span class="glyphicon glyphicon-triangle-top" aria-hidden="true"></span>
-                                                <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true"></span>
+                                        <% if(role === 'instructor') { %>
+                                            <span class="arrow-placement">
+                                                <span class="arrow-wrapper">
+                                                    <span class="glyphicon glyphicon-triangle-top" aria-hidden="true"></span>
+                                                    <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true"></span>
+                                                </span>
                                             </span>
-                                        </span>
+                                        <% } %>
                                     </span>
                                 </th>
                                 <th class="gradebook-student-column-user_login">
                                     <span class="header-wrapper">    
                                         <span class="tooltip-wrapper" data-toggle="tooltip"
                                         data-placement="top"
+                                        data-target="user_login"
                                         title='<?php esc_html_e('Username', 'openlab-gradebook')?>'><?php esc_html_e('Username', 'openlab-gradebook')?></span>
-                                        <span class="arrow-placement">
-                                            <span class="arrow-wrapper">
-                                                <span class="glyphicon glyphicon-triangle-top" aria-hidden="true"></span>
-                                                <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true"></span>
+                                        <% if(role === 'instructor') { %>
+                                            <span class="arrow-placement">
+                                                <span class="arrow-wrapper">
+                                                    <span class="glyphicon glyphicon-triangle-top" aria-hidden="true"></span>
+                                                    <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true"></span>
+                                                </span>
                                             </span>
-                                        </span>
+                                        <% } %>
                                     </span>
                                 </th>
                                 <th class="gradebook-student-column-average adjust-widths" data-targetwidth="65">
                                     <span class="header-wrapper">
                                         <span class="tooltip-wrapper"
-                                        data-toggle="tooltip" data-placement="top"
+                                        data-toggle="tooltip" data-placement="top" data-target="average"
                                         title='<?php esc_html_e('Current Average Grade', 'openlab-gradebook')?>'><?php esc_html_e('Avg.', 'openlab-gradebook')?></span>
-                                        <span class="arrow-placement">
-                                            <span class="arrow-wrapper">
-                                                <span class="glyphicon glyphicon-triangle-top" aria-hidden="true"></span>
-                                                <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true"></span>
+                                        <% if(role === 'instructor') { %>
+                                            <span class="arrow-placement">
+                                                <span class="arrow-wrapper">
+                                                    <span class="glyphicon glyphicon-triangle-top" aria-hidden="true"></span>
+                                                    <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true"></span>
+                                                </span>
                                             </span>
-                                        </span>
+                                        <% } %>
                                     </span>
                                 </th>
                             </tr>
