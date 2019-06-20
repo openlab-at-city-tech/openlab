11e219fd75c8bef8ad46cb872fa6edb75168708b
junander <joe@joeunander.com>
2019-06-20 18:15:16 -0400

    OpenLab Gradebook: removing tooltips from Mid-Semester and Final Grade titles

--- wp-content/plugins/openlab-gradebook/js/app/templates/gradebook-interface-template-student-header.php
+++ wp-content/plugins/openlab-gradebook/js/app/templates/gradebook-interface-template-student-header.php
@@ -56,10 +56,7 @@
         <% } %>
     </span>
 </th>
-<th class="gradebook-student-mid-semester-grade student-grades adjust-widths" data-targetwidth="65"><span
-        data-toggle="tooltip" data-placement="top"
-        title='<?php esc_html_e(' Mid-semester Grade ', 'openlab-gradebook ')?>'>
+<th class="gradebook-student-mid-semester-grade student-grades adjust-widths" data-targetwidth="65"><span>
         <?php esc_html_e('Mid-semester Grade', 'openlab-gradebook')?></span></th>
-<th class="gradebook-student-final-grade student-grades adjust-widths" data-targetwidth="65"><span data-toggle="tooltip"
-        data-placement="top" title='<?php esc_html_e(' Final Grade ', 'openlab-gradebook ')?>'>
+<th class="gradebook-student-final-grade student-grades adjust-widths" data-targetwidth="65"><span>
         <?php esc_html_e('Final Grade', 'openlab-gradebook')?></span></th>
