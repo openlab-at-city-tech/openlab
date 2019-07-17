95e08c9b53c4ecbf78d2c9eb80f8a90e54b3a4d1
Boone B Gorges <boonebgorges@gmail.com>
2019-07-17 16:27:42 -0500

    Update theme: twentyfifteen.
    Name: Twenty Fifteen
    New version: 2.5
    Previous version: 2.3

--- wp-content/themes/twentyfifteen/inc/template-tags.php
+++ wp-content/themes/twentyfifteen/inc/template-tags.php
@@ -209,7 +209,7 @@ if ( ! function_exists( 'twentyfifteen_post_thumbnail' ) ) :
 		?>
 	</a>
 
-	<?php
+		<?php
 	endif; // End is_singular()
 	}
 endif;
@@ -265,5 +265,23 @@ if ( ! function_exists( 'twentyfifteen_the_custom_logo' ) ) :
 		if ( function_exists( 'the_custom_logo' ) ) {
 			the_custom_logo();
 		}
+	}
+endif;
+
+if ( ! function_exists( 'wp_body_open' ) ) :
+	/**
+	 * Fire the wp_body_open action.
+	 *
+	 * Added for backwards compatibility to support pre 5.2.0 WordPress versions.
+	 *
+	 * @since Twenty Fifteen 2.5
+	 */
+	function wp_body_open() {
+		/**
+		 * Triggered after the opening <body> tag.
+		 *
+		 * @since Twenty Fifteen 2.5
+		 */
+		do_action( 'wp_body_open' );
 	}
 endif;
