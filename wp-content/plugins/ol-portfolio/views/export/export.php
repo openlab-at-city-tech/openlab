8cdde3ea2ec0ddd22b9f7848f5740da5ebd94c91
George Mamadashvili <georgemamadashvili@gmail.com>
2019-07-23 14:21:44 +0400

    Minor CS fixes

--- wp-content/plugins/ol-portfolio/views/export/export.php
+++ wp-content/plugins/ol-portfolio/views/export/export.php
@@ -6,9 +6,9 @@
 	<p>Use this tool to export your OpenLab Portfolio.</p>
 	<p>A Portfolio Archive file (.zip) will be downloaded to your computer and can be used with Import Portfolio tool.</p>
 
-	<form method="post" id="export-portfolio" action="<?php echo admin_url('admin-post.php'); ?>">
+	<form method="post" id="export-portfolio" action="<?php echo admin_url( 'admin-post.php' ); ?>">
 		<input type="hidden" name="action" value="export-portfolio" />
-		<?php wp_nonce_field('ol-export-portfolio'); ?>
-		<?php submit_button('Download Archive File'); ?>
+		<?php wp_nonce_field( 'ol-export-portfolio' ); ?>
+		<?php submit_button( 'Download Archive File' ); ?>
 	</form>
 </div>
--- wp-content/plugins/ol-portfolio/views/import/settings.php
+++ wp-content/plugins/ol-portfolio/views/import/settings.php
@@ -6,7 +6,7 @@
 
 	<form method="post" action="<?php echo esc_url( $this->get_url(2) ); ?>">
 		<input type="hidden" name="import_id" value="<?php echo esc_attr( $this->id ); ?>" />
-		<?php wp_nonce_field (sprintf( 'portfolio.import:%d', $this->id ) ); ?>
+		<?php wp_nonce_field( sprintf( 'portfolio.import:%d', $this->id ) ); ?>
 		<?php submit_button( 'Start Importing' ); ?>
 	</form>
 </div>
