0754c8186ded21e419d46fc1c6ddfb508336d379
Boone B Gorges <boonebgorges@gmail.com>
2019-07-24 20:14:49 -0500

    education-pro: Header customizer help text.
    See #2548.

--- wp-content/mu-plugins/theme-fixes/education-pro/customize.js
+++ wp-content/mu-plugins/theme-fixes/education-pro/customize.js
@@ -1,6 +1,8 @@
-(function($){
+(function(wp, $){
 	$(document).ready(function(){
-		var c = $('#customize-control-header_image .customizer-section-intro');
-		c.html('To upload a header image from your computer, click “Add new image.” Your theme works best with a seamlessly tiled image or one sized to at least 2000 × 130 pixels. You will have the option to crop your image during upload.');
+		wp.customize.bind('ready',function() {
+			var c = $('#customize-control-header_image .customizer-section-intro');
+			c.html('To upload a header image from your computer, click “Add new image.” Your theme works best with one sized to at least <strong>2000 × 130</strong> pixels. You will have the option to crop your image during upload.');
+		});
 	});
-}(jQuery));
+}(wp, jQuery));
