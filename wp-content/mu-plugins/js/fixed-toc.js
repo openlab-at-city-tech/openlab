c6f7c60b45707e61b9607c872a936fc64e25c065
Boone B Gorges <boonebgorges@gmail.com>
2019-07-25 14:37:30 -0500

    JS-based WAVE fixes for fixed-toc.
    See #2562.

---
+++ wp-content/mu-plugins/js/fixed-toc.js
@@ -0,0 +1,7 @@
+(function($){
+	$(document).ready(function(){
+		$('#ftwp-trigger').append('<span class="screen-reader-text">Click to trigger table of contents</span>');
+		$('#ftwp-header-minimize').append('<span class="screen-reader-text">Click to minimize table of contents</span>');
+		$('.ftwp-icon-expand').append('<span class="screen-reader-text">Click to expand table of contents</span>');
+	});
+}(jQuery));
