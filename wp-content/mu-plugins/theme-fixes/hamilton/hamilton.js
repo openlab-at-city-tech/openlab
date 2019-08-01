87c0de142dca7b6df9c883cd5a8de7a954612b52
Boone B Gorges <boonebgorges@gmail.com>
2019-08-01 16:38:04 +0000

    hamilton: Menu accordions.
    See #2549.

--- wp-content/mu-plugins/theme-fixes/hamilton/hamilton.js
+++ wp-content/mu-plugins/theme-fixes/hamilton/hamilton.js
@@ -6,6 +6,31 @@
 	$(window).ready(function(){
 		$header = $('.site-header');
 		$siteNav = $('.site-nav');
+
+		var $menusWithChildren = $('.menu-item-has-children');
+		var counter = 0;
+		$menusWithChildren.each(function(){
+			counter++;
+
+			var $theMenu = $(this);
+			var subMenuId = 'sub-menu-' + counter;
+			var toggleId = 'menu-toggle-' + counter;
+
+			$theMenu.find('.sub-menu').attr( 'id', subMenuId ).attr('aria-labelledby', toggleId);
+			$theMenu.append('<button class="menu-toggle" id="' + toggleId + '" aria-controls="' + subMenuId + ' aria-expanded="false""><span class="screen-reader-text">Toggle Submenu</span></button>');
+			$theMenu.find('.menu-toggle').click(function(){
+				toggleMenu($theMenu);
+			});
+
+			toggleMenu($theMenu);
+		});
+
+		$menusWithChildren.click(function(e){
+			e.stopPropagation();
+
+			var $el = $(e.target);
+			toggleMenu($el);
+		});
 	});
 
 	$(window).on('load resize orientationchange', function(){
@@ -18,4 +43,15 @@
 			},
 		250);
 	});
+
+	var toggleMenu = function( $menuItem ) {
+		if ( $menuItem.hasClass( 'menu-collapsed' ) ) {
+			$menuItem.removeClass( 'menu-collapsed' );
+			$menuItem.find( '.sub-menu' ).attr( 'aria-hidden', 'false' ).attr( 'aria-expanded', 'true' );
+
+		} else {
+			$menuItem.addClass( 'menu-collapsed' );
+			$menuItem.find( '.sub-menu' ).attr( 'aria-hidden', 'true' ).attr( 'aria-expanded', 'false' );
+		}
+	}
 }(jQuery));
