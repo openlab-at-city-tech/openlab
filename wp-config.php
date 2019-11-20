778cd4fda92441ed033c3e455c492aa3cdd09906
Boone B Gorges <boonebgorges@gmail.com>
2019-11-20 15:21:23 +0000

    OL_VERSION 1.7.37.

--- wp-config.php
+++ wp-config.php
@@ -23,7 +23,7 @@ if ( !defined( 'DB_NAME' ) ) {
 }
 
 // Version of the OpenLab. Used for asset versioning and cache busting.
-define( 'OL_VERSION', '1.7.36' );
+define( 'OL_VERSION', '1.7.37' );
 
 if ( defined( 'DO_SHARDB' ) && DO_SHARDB ) {
 	require __DIR__ . '/db-settings.php';
