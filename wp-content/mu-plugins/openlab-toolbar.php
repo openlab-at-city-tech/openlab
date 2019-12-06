1e8fdc6e8d3f967b7cb9a62be077a55f14f6b240
junander <joe@joeunander.com>
2019-12-05 22:19:14 -0500

    WP Adminbar styles: versioning

--- wp-content/mu-plugins/openlab-toolbar.php
+++ wp-content/mu-plugins/openlab-toolbar.php
@@ -1660,7 +1660,7 @@ HTML;
         $openlab_toolbar_url = set_url_scheme($openlab_toolbar_url);
 
         wp_enqueue_style('admin-bar-custom', $adminbar_custom_url, array('font-awesome'), '1.6.9');
-        wp_enqueue_style('openlab-toolbar', $openlab_toolbar_url, array('font-awesome'), '1.7.0.6');
+        wp_enqueue_style('openlab-toolbar', $openlab_toolbar_url, array('font-awesome'), '1.7.0.7');
     }
 
     public function adminbar_special_body_class($classes)
