b9d697eca738fc9828996a29926d80a36b1735ba
junander <joe@joeunander.com>
2019-12-11 10:58:26 -0500

    Feature #2647 - Group sites admin bar: add a 1 px line between the icons and the username

--- wp-content/mu-plugins/openlab-toolbar.php
+++ wp-content/mu-plugins/openlab-toolbar.php
@@ -1625,7 +1625,7 @@ HTML;
 		$openlab_toolbar_url = set_url_scheme( $openlab_toolbar_url );
 
 		wp_enqueue_style( 'admin-bar-custom', $adminbar_custom_url, array( 'font-awesome' ), '1.6.9' );
-		wp_enqueue_style( 'openlab-toolbar', $openlab_toolbar_url, array( 'font-awesome' ), '1.7.0.8' );
+		wp_enqueue_style( 'openlab-toolbar', $openlab_toolbar_url, array( 'font-awesome' ), '1.7.0.9' );
 	}
 
 	public function adminbar_special_body_class( $classes ) {
