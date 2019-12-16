09a53dae5b56c4fa8ada18aa6b69e15471346296
Boone B Gorges <boonebgorges@gmail.com>
2019-12-16 15:45:02 +0000

    Update plugin: taxonomy-terms-order.
    Name: Category Order and Taxonomy Terms Order
    New version: 1.5.7.1
    Previous version: 1.5.7

--- wp-content/plugins/taxonomy-terms-order/taxonomy-terms-order.php
+++ wp-content/plugins/taxonomy-terms-order/taxonomy-terms-order.php
@@ -3,7 +3,7 @@
 Plugin Name: Category Order and Taxonomy Terms Order
 Plugin URI: http://www.nsp-code.com
 Description: Order Categories and all custom taxonomies terms (hierarchically) and child terms using a Drag and Drop Sortable javascript capability. 
-Version: 1.5.7
+Version: 1.5.7.1
 Author: Nsp-Code
 Author URI: http://www.nsp-code.com
 Author Email: electronice_delphi@yahoo.com
