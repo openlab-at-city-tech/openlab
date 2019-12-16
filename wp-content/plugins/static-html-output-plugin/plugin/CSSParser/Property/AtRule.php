9683f3236356394ceebeb75ae55a3459a2b4741e
Boone B Gorges <boonebgorges@gmail.com>
2019-12-16 15:52:05 +0000

    Update plugin: static-html-output-plugin.
    Name: WP2Static
    New version: 6.6.7
    Previous version: 1.9

---
+++ wp-content/plugins/static-html-output-plugin/plugin/CSSParser/Property/AtRule.php
@@ -0,0 +1,2 @@
+<?php
+ namespace Sabberworm\CSS\Property; use Sabberworm\CSS\Renderable; use Sabberworm\CSS\Comment\Commentable; interface AtRule extends Renderable, Commentable { const BLOCK_RULES = 'media/document/supports/region-style/font-feature-values'; const SET_RULES = 'font-face/counter-style/page/swash/styleset/annotation'; public function atRuleName(); public function atRuleArgs(); }
---
+++ wp-content/plugins/static-html-output-plugin/plugin/CSSParser/Value/Value.php
@@ -0,0 +1,2 @@
+<?php
+ namespace Sabberworm\CSS\Value; use Sabberworm\CSS\Renderable; abstract class Value implements Renderable { protected $iLineNo; public function __construct($iLineNo = 0) { $this->iLineNo = $iLineNo; } public function getLineNo() { return $this->iLineNo; } } 
