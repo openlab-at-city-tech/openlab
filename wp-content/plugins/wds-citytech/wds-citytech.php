3b8870e04f62dad178154fa3c6a64dfdbc8adbdd
Boone B Gorges <boonebgorges@gmail.com>
2020-01-07 12:52:16 -0600

    Staff should see 'required' flag when creating portfolio.
    See #2661.

--- wp-content/plugins/wds-citytech/wds-citytech.php
+++ wp-content/plugins/wds-citytech/wds-citytech.php
@@ -959,7 +959,7 @@ function wds_load_group_type( $group_type ) {
 		$selector_args['entities'] = [ 'school' ];
 	}
 
-	$selector_args['required'] = openlab_is_school_required_for_group_type( $group_type ) && 'staff' != strtolower( $account_type );
+	$selector_args['required'] = openlab_is_school_required_for_group_type( $group_type );
 	$selector_args['checked']  = openlab_get_group_academic_units( bp_get_current_group_id() );
 
 	$return .= '<tr><td class="school-inputs" colspan="2">';
