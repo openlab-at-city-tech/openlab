4996bf3b50865c69c080e6e077d619b4ea07db40
Boone B Gorges <boonebgorges@gmail.com>
2020-04-27 14:16:11 +0000

    Hide media-cleaner from non-super-admins.
    See #2709.

--- wp-content/mu-plugins/openlab-plugin-management.php
+++ wp-content/mu-plugins/openlab-plugin-management.php
@@ -31,6 +31,7 @@ function openlab_hide_plugins( $plugins ) {
 		'grader/grader.php',
 		'highlighter-pro/highlighter.php',
 		'kb-gradebook/kb-gradebook.php',
+		'media-cleaner/media-cleaner.php',
 		'p3-profiler/p3-profiler.php',
 		'pdf-embedder/pdf_embedder.php',
 		'pdfembedder-premium/mobile_pdf_embedder.php',
