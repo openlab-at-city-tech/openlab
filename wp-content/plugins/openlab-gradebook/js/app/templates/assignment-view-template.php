d6455e7c77f5442d4f29a0b3f5820a8e83b6d46a
junander <joe@joeunander.com>
2020-05-11 17:49:54 -0400

    OpenLab Gradebook: spacing commit

--- wp-content/plugins/openlab-gradebook/js/app/templates/assignment-view-template.php
+++ wp-content/plugins/openlab-gradebook/js/app/templates/assignment-view-template.php
@@ -1,15 +1,16 @@
-<div class="btn-group">
-    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-        <span class="name"><%= assignment.get('assign_name') %><span class="caret"></span></span>
-    </button>
-    <ul class="dropdown-menu" role="menu">	
-        <li class='assign-submenu-stats'><a href='#'><?php esc_html_e('Statistics', 'openlab-gradebook') ?></a></li>
-        <li class='assign-submenu-details'><a href='#'><?php esc_html_e('Details', 'openlab-gradebook') ?></a></li>  					
-        <?php
-        global $oplb_gradebook_api;
-        if ($oplb_gradebook_api->oplb_is_gb_administrator()) {
+<div class="btn-group-wrapper">
+    <div class="btn-group">
+        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+            <span class="name"><%= assignment.get('assign_name') %><span class="caret"></span></span>
+        </button>
+        <ul class="dropdown-menu" role="menu">
+            <li class='assign-submenu-stats'><a href='#'><?php esc_html_e('Statistics', 'openlab-gradebook') ?></a></li>
+            <li class='assign-submenu-details'><a href='#'><?php esc_html_e('Details', 'openlab-gradebook') ?></a></li>
+            <?php
+            global $oplb_gradebook_api;
+            if ($oplb_gradebook_api->oplb_is_gb_administrator()) {
             ?>
-            <%
+                <%
             if (min.get('assign_order') != max.get('assign_order')){
             if( assignment.get('assign_order') === min.get('assign_order') ) { 
             print("<li class='assign-submenu-right'><a href='#'>Shift Right</a></li>");				
@@ -20,11 +21,12 @@
             print("<li class='assign-submenu-right'><a href='#'>Shift Right</a></li>");	
             }
             }
-            %>	
+            %>
 
-            <li class='assign-submenu-sort'><a href='#'><?php esc_html_e('Sort', 'openlab-gradebook') ?></a></li>  					
-            <li class='assign-submenu-edit'><a href='#'><?php esc_html_e('Edit', 'openlab-gradebook') ?></a></li>
-            <li class='assign-submenu-delete'><a href='#'><span class='text-danger'><?php esc_html_e('Delete', 'openlab-gradebook') ?></span></a></li>	
-            <?php } ?>			
-    </ul>
-</div>    
+                <li class='assign-submenu-sort'><a href='#'><?php esc_html_e('Sort', 'openlab-gradebook') ?></a></li>
+                <li class='assign-submenu-edit'><a href='#'><?php esc_html_e('Edit', 'openlab-gradebook') ?></a></li>
+                <li class='assign-submenu-delete'><a href='#'><span class='text-danger'><?php esc_html_e('Delete', 'openlab-gradebook') ?></span></a></li>
+            <?php } ?>
+        </ul>
+    </div>
+</div>
