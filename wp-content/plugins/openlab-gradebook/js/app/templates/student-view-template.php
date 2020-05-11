a3e4eea71a6ae486a5f6972a7e05ae0a804e90cd
junander <joe@joeunander.com>
2020-05-11 17:46:13 -0400

    Bug #2726 - Gradebook Alignment is off for tables of more than 5 students
    - two part resolution: 1) moved cell styling to flexbox in order to removing inconsistent padding and replace with more dynamic flex alignments
    - 2) moving the css-element-queries "remote-sensor" container: this container was originally sitting in the thead of the table element and causing a 1px misalignment; it is now moved to the main table element, and is not causing any issues as a result

--- wp-content/plugins/openlab-gradebook/js/app/templates/student-view-template.php
+++ wp-content/plugins/openlab-gradebook/js/app/templates/student-view-template.php
@@ -4,27 +4,29 @@
             <span class="caret"></span>
         </button>
         <ul class="dropdown-menu" role="menu">
-            <li class='student-submenu-stats'><a href='#'><?php esc_html_e('Statistics', 'openlab-gradebook') ?></a></li>	
+            <li class='student-submenu-stats'><a href='#'><?php esc_html_e('Statistics', 'openlab-gradebook') ?></a></li>
             <?php
             global $current_user;
             $x = $current_user->roles;
             $y = array_keys(get_option('oplb_gradebook_settings'), true);
             $z = array_intersect($x, $y);
             if (count($z)) {
-                ?>						
+            ?>
                 <li class='student-submenu-delete'><a href='#'><span class="text-danger"><?php esc_html_e('Delete', 'openlab-gradebook') ?></span></a></li>
-            <?php 
-        } ?>        									
+            <?php
+            } ?>
         </ul>
     </div>
 </td>
 <td class="student<%= mobile_styles %>">
-    <div class="column-frame">
-        <span id="student-<%= student.get('last_name') %>" data-toggle="tooltip" data-placement="top" title='<%= student.get("first_name") %>'><%= student.get("first_name") %></span> 				
+    <span class="column-frame-wrapper">
+        <div class="column-frame">
+            <span id="student-<%= student.get('last_name') %>" data-toggle="tooltip" data-placement="top" title='<%= student.get("first_name") %>'><%= student.get("first_name") %></span>
+    </span>
     </div>
 </td>
-<td class="student<%= mobile_styles %>"><span data-toggle="tooltip" data-placement="top" title='<%= student.get("last_name") %>'><%= student.get("last_name") %></span></td>
-<td class="student<%= mobile_styles %>"><span data-toggle="tooltip" data-placement="top" title='<%= student.get("user_login") %>'><%= student.get("user_login") %></span></td>
+<td class="student<%= mobile_styles %>"><span class="header-span-wrapper"><span data-toggle="tooltip" data-placement="top" title='<%= student.get("last_name") %>'><%= student.get("last_name") %></span></span></td>
+<td class="student<%= mobile_styles %>"><span class="header-span-wrapper"><span data-toggle="tooltip" data-placement="top" title='<%= student.get("user_login") %>'><%= student.get("user_login") %></span></span></td>
 <td class="gradebook-student-column-average student<%= mobile_styles %>"><span id="average<%= student.get('id') %>" data-toggle="tooltip" data-placement="top" title='<%= student.get("current_grade_average") %>'><%= student.get("current_grade_average") %></span></td>
 <td class="student student-grades mid-semester-grade"></td>
 <td class="student student-grades final-grade"></td>
