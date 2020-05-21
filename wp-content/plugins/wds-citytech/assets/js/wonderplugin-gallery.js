ef83eb23de015cdfd434f22e396345dca416fc51
Boone B Gorges <boonebgorges@gmail.com>
2020-05-21 11:17:50 -0500

    wonderplugin-gallery: Remove Upload buttons from 'Add Video' dialog.
    See #2700.

--- wp-content/plugins/wds-citytech/assets/js/wonderplugin-gallery.js
+++ wp-content/plugins/wds-citytech/assets/js/wonderplugin-gallery.js
@@ -1,5 +1,25 @@
 (function($){
 	$(document).ready(function(){
 		$('#wonderplugin-add-folder').remove();
+
+		// Cool plugin
+		$('#wonderplugin-add-video').click(function(){
+			setTimeout(
+				function() {
+					$('.wonderplugin-dialog-form td').each(function(){
+						var $td = $(this);
+						var $button = $td.find('input[type="button"]');
+						if ( 0 !== $button.length ) {
+							$button.remove();
+
+							var regExp = / or ?$/
+							var tdContents = $td.html().replace( regExp, '' )
+							$td.html( tdContents );
+						}
+					});
+				},
+				50
+			);
+		});
 	});
 }(jQuery))
