5be9609143cc8ad353177968ffa1ccf1bcbe923e
Boone B Gorges <boonebgorges@gmail.com>
2020-05-21 16:14:27 -0500

    wonderplugin-gallery: Specify upload buttons to remove.
    See #2700.

--- wp-content/plugins/wds-citytech/assets/js/wonderplugin-gallery.js
+++ wp-content/plugins/wds-citytech/assets/js/wonderplugin-gallery.js
@@ -4,17 +4,27 @@
 
 		// Cool plugin
 		$('#wonderplugin-add-video').click(function(){
+			var buttonsToRemove = [
+				'wonderplugin-dialog-select-mp4',
+				'wonderplugin-dialog-select-hdmp4',
+				'wonderplugin-dialog-select-webm',
+				'wonderplugin-dialog-select-hdwebm',
+			];
+
 			setTimeout(
 				function() {
 					$('.wonderplugin-dialog-form td').each(function(){
 						var $td = $(this);
 						var $button = $td.find('input[type="button"]');
 						if ( 0 !== $button.length ) {
-							$button.remove();
+							console.log($button.attr('id'));
+							if ( -1 !== buttonsToRemove.indexOf( $button.attr( 'id' ) ) ) {
+								$button.remove();
 
-							var regExp = / or ?$/
-							var tdContents = $td.html().replace( regExp, '' )
-							$td.html( tdContents );
+								var regExp = / or ?$/
+								var tdContents = $td.html().replace( regExp, '' )
+								$td.html( tdContents );
+							}
 						}
 					});
 				},
