34c1726122b460027f8065f9fa7f3d08d7857d0a
Boone B Gorges <boonebgorges@gmail.com>
2020-06-03 16:25:26 -0500

    Truncate group names on directory to two lines(ish).
    See #2738.

--- wp-content/themes/openlab/lib/group-funcs.php
+++ wp-content/themes/openlab/lib/group-funcs.php
@@ -260,7 +260,7 @@ function openlab_group_archive() {
                             <div class="item col-xs-18">
 
                                 <p class="item-title h2">
-                                    <a class="no-deco truncate-on-the-fly hyphenate" href="<?php bp_group_permalink() ?>" data-basevalue="<?php echo ($group_type == 'course' ? 50 : 65 ) ?>" data-minvalue="20" data-basewidth="290"><?php bp_group_name() ?></a>
+                                    <a class="no-deco truncate-on-the-fly hyphenate" href="<?php bp_group_permalink() ?>" data-basevalue="55" data-minvalue="20" data-basewidth="290"><?php bp_group_name() ?></a>
                                     <span class="original-copy hidden"><?php bp_group_name() ?></span>
                                 </p>
                                 <?php
