ec627f0c0d38653974d34dd8befeb720bb65a6ff
Boone B Gorges <boonebgorges@gmail.com>
2020-07-07 14:51:27 +0000

    Update plugin: elasticpress.
    Name: ElasticPress
    New version: 3.4.2
    Previous version: 3.3

--- wp-content/plugins/elasticpress/includes/classes/Feature/Facets/Facets.php
+++ wp-content/plugins/elasticpress/includes/classes/Feature/Facets/Facets.php
@@ -9,6 +9,7 @@
 namespace ElasticPress\Feature\Facets;
 
 use ElasticPress\Feature as Feature;
+use ElasticPress\Features as Features;
 use ElasticPress\Utils as Utils;
 use ElasticPress\FeatureRequirementsStatus as FeatureRequirementsStatus;
 use ElasticPress\Indexables as Indexables;
@@ -159,7 +160,7 @@ class Facets extends Feature {
 	 * @since 2.5
 	 */
 	public function front_scripts() {
-		wp_enqueue_script(
+		wp_register_script(
 			'elasticpress-facets',
 			EP_URL . 'dist/js/facets-script.min.js',
 			[ 'jquery', 'underscore' ],
@@ -167,7 +168,7 @@ class Facets extends Feature {
 			true
 		);
 
-		wp_enqueue_style(
+		wp_register_style(
 			'elasticpress-facets',
 			EP_URL . 'dist/css/facets-styles.min.css',
 			[],
@@ -198,6 +199,12 @@ class Facets extends Feature {
 		$ep_integrate = $query->get( 'ep_integrate', null );
 
 		if ( false === $ep_integrate ) {
+			return false;
+		}
+
+		$woocommerce = Features::factory()->get_registered_feature( 'woocommerce' );
+
+		if ( ! $woocommerce->is_active() && ( function_exists( 'is_product_category' ) && is_product_category() ) ) {
 			return false;
 		}
 
