a9e46a05d61c0705127c63215d608006b40f4fc9
Boone B Gorges <boonebgorges@gmail.com>
2020-07-18 08:42:23 -0500

    Ensure that filter toggle (binoculars) appears on mobile Search Results page.

--- wp-content/themes/openlab/search-results.php
+++ wp-content/themes/openlab/search-results.php
@@ -10,7 +10,7 @@
     <?php openlab_bp_sidebar( 'groups', true ); ?>
 	<div <?php post_class( 'col-sm-18 col-xs-24' ); ?>>
 		<div id="openlab-main-content"  class="content-wrapper openlab-search-results">
-			<h1 class="entry-title">OpenLab Search Results</h1>
+			<h1 class="entry-title">OpenLab Search Results<button data-target="#sidebar" data-backgroundonly="true" class="mobile-toggle direct-toggle pull-right visible-xs" type="button"><span class="fa fa-binoculars"></span><span class="sr-only">Search</span></button></h1>
 
 			<div class="entry-content">
 				<?php bp_get_template_part( 'groups/groups-loop' ); ?>
