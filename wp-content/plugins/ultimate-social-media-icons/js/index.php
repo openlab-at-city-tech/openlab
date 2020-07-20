75cfa6635ebda085551733a9b76c8290de8a20c9
Boone B Gorges <boonebgorges@gmail.com>
2020-07-20 08:31:53 -0500

    Remove plugin: ultimate-social-media-icons.
    See #2765.

--- wp-content/plugins/ultimate-social-media-icons/js/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo _('Access Denied');
-?>
--- wp-content/plugins/ultimate-social-media-icons/views/sfsi_aboutus.php
+++
@@ -1,2 +0,0 @@
-<?php echo ""; ?><h1 class="abt_titl">Please visit us at <a href="http://ultimatelysocial.com">Ultimatelysocial.com</a> or write to us at support@ultimatelysocial.com</h1>
-
