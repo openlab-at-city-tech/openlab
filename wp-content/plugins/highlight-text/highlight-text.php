8b96151496547badd855d051b0b66c2acd76e9b6
Boone B Gorges <boonebgorges@gmail.com>
2020-07-23 13:44:19 -0500

    Install plugin: highlight-text.
    Name: Highlight Text
    Version: 1.0.1
    
    See #2796.

---
+++ wp-content/plugins/highlight-text/highlight-text.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Plugin Name:       Highlight Text
+ * Plugin URI:        https://github.com/Mamaduka/highlight
+ * Description:       Text highligher for the Block Editor
+ * Version:           1.0.1
+ * Requires at least: 5.3
+ * Requires PHP:      5.6
+ * Author:            George Mamadashvili
+ * Author URI:        https://mamaduka.com/
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * @package Mamaduka_Highlight
+ */
+
+namespace Mamaduka\Highlight;
+
+/**
+ * Register and enqueue block editor assets.
+ *
+ * @return void
+ */
+function enqueue_editor_assets() {
+	$asset_filepath = __DIR__ . '/build/index.asset.php';
+	$asset_file     = file_exists( $asset_filepath ) ? include $asset_filepath : [
+		'dependencies' => [],
+		'version'      => false,
+	];
+
+	wp_enqueue_script(
+		'mamaduka-highlight',
+		plugins_url( 'build/index.js', __FILE__ ),
+		$asset_file['dependencies'],
+		$asset_file['version'],
+		true
+	);
+
+	wp_enqueue_style(
+		'mamaduka-highlight-style',
+		plugins_url( 'build/editor.css', __FILE__ ),
+		[],
+		$asset_file['version']
+	);
+}
+add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\\enqueue_editor_assets' );
---
+++ wp-content/plugins/highlight-text/readme.txt
@@ -0,0 +1,26 @@
+=== Highlight Text ===
+Contributors: mamaduka
+Tags: highlight, format, block editor, gutenberg
+Requires at least: 5.3
+Tested up to: 5.4
+Stable tag: 1.0.1
+Requires PHP: 5.6
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Text highligher for the WordPress Block Editor (Gutenberg).
+
+== Description ==
+
+Spice up your text by giving it a color highlight.
+
+Report issues, or collaborate, on [Github](https://github.com/Mamaduka/highlight/issues).
+
+== Screensht ==
+
+Clickety. Click. Highlight.
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release.
