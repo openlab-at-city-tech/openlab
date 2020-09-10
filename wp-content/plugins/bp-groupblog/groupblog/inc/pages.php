406901093be54e628b4546c59b3b6513b2d9a32a
Boone B Gorges <boonebgorges@gmail.com>
2020-09-10 06:31:38 -0500

    Update plugin: bp-groupblog.
    Name: BP Groupblog
    New version: 1.9.2
    Previous version: 1.9.1

--- wp-content/plugins/bp-groupblog/groupblog/inc/pages.php
+++ wp-content/plugins/bp-groupblog/groupblog/inc/pages.php
@@ -1,5 +1,24 @@
-<?php $current = $post->ID; ?> 
-<?php $nav_query = new WP_Query( 'post_type=page&showposts=-1&order=ASC' );	?>
+<?php $current = $post->ID; ?>
+
+<?php
+/**
+ * Filters the arguments for the page 'button' query on the Blog tab.
+ *
+ * @since 1.9.2
+ *
+ * @param array $nav_query_args WP_Query args.
+ */
+$nav_query_args = apply_filters(
+	'bp_groupblog_page_tabs_query_args',
+	array(
+		'post_type' => 'page',
+		'showposts' => -1,
+		'order'     => 'ASC',
+	)
+);
+
+$nav_query = new WP_Query( $nav_query_args );
+?>
 
 <?php if ( $nav_query->have_posts() ) :?>
 	
