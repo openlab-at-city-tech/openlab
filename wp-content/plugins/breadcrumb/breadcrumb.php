5973a5065b7bc347162f10017c3b9f33005d52f0
Boone B Gorges <boonebgorges@gmail.com>
2020-11-09 15:48:08 +0000

    Install plugin: breadcrumb.
    Name: Breadcrumb
    Version: 1.5.17
    
    See #2890.

---
+++ wp-content/plugins/breadcrumb/breadcrumb.php
@@ -0,0 +1,91 @@
+<?php
+/*
+Plugin Name: Breadcrumb
+Plugin URI: https://www.pickplugins.com/item/breadcrumb-awesome-breadcrumbs-style-navigation-for-wordpress/
+Description: Awesome Breadcrumb for wordpress.
+Version: 1.5.17
+WC requires at least: 3.0.0
+WC tested up to: 3.6
+Author: PickPlugins
+Author URI: http://pickplugins.com
+Text Domain: breadcrumb
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+if ( ! defined('ABSPATH')) exit;  // if direct access 
+
+
+class BreadcrumbMain{
+	
+	public function __construct(){
+		
+		define('breadcrumb_plugin_url', plugins_url('/', __FILE__)  );
+		define('breadcrumb_plugin_dir', plugin_dir_path( __FILE__ ) );
+		define('breadcrumb_plugin_name', 'Breadcrumb' );
+		define('breadcrumb_plugin_version', '1.5.17' );
+
+
+        require_once( breadcrumb_plugin_dir . 'includes/class-settings-tabs.php');
+		require_once( breadcrumb_plugin_dir . 'includes/functions.php');
+        require_once( breadcrumb_plugin_dir . 'includes/functions-settings.php');
+
+		require_once( breadcrumb_plugin_dir . 'includes/themes-css.php');
+		
+		require_once( breadcrumb_plugin_dir . 'includes/class-shortcodes.php');
+		require_once( breadcrumb_plugin_dir . 'includes/class-settings.php');
+        require_once( breadcrumb_plugin_dir . 'includes/functions-hooks.php');
+
+
+        add_action( 'wp_enqueue_scripts', array( $this, '_front_scripts' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, '_admin_scripts' ) );
+		add_filter('widget_text', 'do_shortcode');
+		add_action( 'plugins_loaded', array( $this, 'breadcrumb_load_textdomain' ));
+		
+		}
+	
+	public function breadcrumb_load_textdomain() {
+
+        $locale = apply_filters( 'plugin_locale', get_locale(), 'breadcrumb' );
+        load_textdomain('breadcrumb', WP_LANG_DIR .'/breadcrumb/breadcrumb-'. $locale .'.mo' );
+
+        load_plugin_textdomain( 'breadcrumb', false, plugin_basename( dirname( __FILE__ ) ) . '/languages/' );
+
+
+
+
+		}
+		
+	
+	public function _front_scripts(){
+
+
+	
+	}
+	
+	
+	public function _admin_scripts(){
+
+        $screen = get_current_screen();
+
+        wp_register_script('settings-tabs', plugins_url( 'assets/admin/js/settings-tabs.js' , __FILE__ ) , array( 'jquery' ));
+        wp_register_style('settings-tabs', breadcrumb_plugin_url.'assets/admin/css/settings-tabs.css');
+        wp_register_style('font-awesome-5', breadcrumb_plugin_url.'assets/admin/css/fontawesome.css');
+
+        if($screen->id =='toplevel_page_breadcrumb_settings'){
+            $settings_tabs_field = new settings_tabs_field();
+            $settings_tabs_field->admin_scripts();
+        }
+
+
+
+
+	}
+	
+	
+	
+
+}
+
+new BreadcrumbMain();
+
---
+++ wp-content/plugins/breadcrumb/includes/menu/settings.php
@@ -0,0 +1,168 @@
+<?php
+if ( ! defined('ABSPATH')) exit; // if direct access 
+
+$current_tab = isset($_POST['tab']) ? $_POST['tab'] : 'options';
+
+
+
+
+$breadcrumb_settings_tab = array();
+
+
+$breadcrumb_settings_tab[] = array(
+    'id' => 'options',
+    'title' => sprintf(__('%s Options','breadcrumb'),'<i class="fas fa-laptop-code"></i>'),
+    'priority' => 1,
+    'active' => ($current_tab == 'options') ? true : false,
+);
+
+$breadcrumb_settings_tab[] = array(
+    'id' => 'builder',
+    'title' => sprintf(__('%s Builder','breadcrumb'),'<i class="fas fa-pencil-ruler"></i>'),
+    'priority' => 2,
+    'active' => ($current_tab == 'builder') ? true : false,
+);
+
+
+$breadcrumb_settings_tab[] = array(
+    'id' => 'style',
+    'title' => sprintf(__('%s Style','breadcrumb'),'<i class="fas fa-palette"></i>'),
+    'priority' => 3,
+    'active' => ($current_tab == 'style') ? true : false,
+);
+
+$breadcrumb_settings_tab[] = array(
+    'id' => 'custom_scripts',
+    'title' => sprintf(__('%s Custom Scripts','breadcrumb'),'<i class="fas fa-code"></i>'),
+    'priority' => 4,
+    'active' => ($current_tab == 'custom_scripts') ? true : false,
+);
+
+$breadcrumb_settings_tab[] = array(
+    'id' => 'help_support',
+    'title' => sprintf(__('%s Help & Support','breadcrumb'),'<i class="fas fa-hands-helping"></i>'),
+    'priority' => 5,
+    'active' => ($current_tab == 'help_support') ? true : false,
+);
+
+
+
+$breadcrumb_settings_tab[] = array(
+    'id' => 'buy_pro',
+    'title' => sprintf(__('%s Buy Pro','breadcrumb'),'<i class="fas fa-store"></i>'),
+    'priority' => 6,
+    'active' => ($current_tab == 'buy_pro') ? true : false,
+);
+
+
+$breadcrumb_settings_tabs = apply_filters('breadcrumb_settings_tabs', $breadcrumb_settings_tab);
+
+
+$tabs_sorted = array();
+foreach ($breadcrumb_settings_tabs as $page_key => $tab) $tabs_sorted[$page_key] = isset( $tab['priority'] ) ? $tab['priority'] : 0;
+array_multisort($tabs_sorted, SORT_ASC, $breadcrumb_settings_tabs);
+
+
+
+
+
+?>
+
+
+
+
+
+<div class="wrap">
+
+	<div id="icon-tools" class="icon32"><br></div><?php echo "<h2>".sprintf(__('%s Settings'), breadcrumb_plugin_name )."</h2>";?>
+		<form  method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+	        <input type="hidden" name="breadcrumb_hidden" value="Y">
+            <input type="hidden" name="tab" value="<?php echo $current_tab; ?>">
+
+            <?php
+            if(!empty($_POST['breadcrumb_hidden'])){
+
+                $nonce = sanitize_text_field($_POST['_wpnonce']);
+
+                if(wp_verify_nonce( $nonce, 'breadcrumb_nonce' ) && $_POST['breadcrumb_hidden'] == 'Y') {
+
+
+                    do_action('breadcrumb_settings_save');
+
+                    ?>
+                    <div class="updated notice  is-dismissible"><p><strong><?php _e('Changes Saved.', 'breadcrumb' ); ?></strong></p></div>
+
+                    <?php
+                }
+            }
+            ?>
+
+            <div class="settings-tabs-loading" style=""><?php _e('Loading...', 'breadcrumb' ); ?></div>
+            <div class="settings-tabs vertical has-right-panel" style="display: none">
+                <ul class="tab-navs">
+                    <?php
+                    foreach ($breadcrumb_settings_tabs as $tab){
+                        $id = $tab['id'];
+                        $title = $tab['title'];
+                        $active = $tab['active'];
+                        $data_visible = isset($tab['data_visible']) ? $tab['data_visible'] : '';
+                        $hidden = isset($tab['hidden']) ? $tab['hidden'] : false;
+                        ?>
+                        <li <?php if(!empty($data_visible)):  ?> data_visible="<?php echo $data_visible; ?>" <?php endif; ?> class="tab-nav <?php if($hidden) echo 'hidden';?> <?php if($active) echo 'active';?>" data-id="<?php echo $id; ?>"><?php echo $title; ?></li>
+                        <?php
+                    }
+                    ?>
+                </ul>
+
+                <div class="settings-tabs-right-panel">
+                    <?php
+                    foreach ($breadcrumb_settings_tabs as $tab) {
+                        $id = $tab['id'];
+                        $active = $tab['active'];
+
+                        ?>
+                        <div class="right-panel-content <?php if($active) echo 'active';?> right-panel-content-<?php echo $id; ?>">
+                            <?php
+
+                            do_action('breadcrumb_settings_tabs_right_panel_'.$id);
+                            ?>
+
+                        </div>
+                        <?php
+
+                    }
+                    ?>
+                </div>
+
+                <?php
+                foreach ($breadcrumb_settings_tabs as $tab){
+                    $id = $tab['id'];
+                    $title = $tab['title'];
+                    $active = $tab['active'];
+
+
+                    ?>
+
+                    <div class="tab-content <?php if($active) echo 'active';?>" id="<?php echo $id; ?>">
+                        <?php
+                        do_action('breadcrumb_settings_tabs_content_'.$id, $tab);
+                        ?>
+                    </div>
+                    <?php
+                }
+                ?>
+
+                <div class="clear clearfix"></div>
+                <p class="submit">
+                    <?php wp_nonce_field( 'breadcrumb_nonce' ); ?>
+                    <input class="button button-primary" type="submit" name="Submit" value="<?php echo __('Save Changes', 'breadcrumb' ) ?>" />
+                </p>
+
+            </div>
+
+
+
+		</form>
+
+
+</div> <!-- end wrap -->
