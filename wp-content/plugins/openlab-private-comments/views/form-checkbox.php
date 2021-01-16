76272fdf4734771839da3db9d527ea2bf6ca5794
George Mamadashvili <georgemamadashvili@gmail.com>
2021-01-16 15:50:17 +0400

    Set tabindex -1 for private comments checkbox

--- wp-content/plugins/openlab-private-comments/views/form-checkbox.php
+++ wp-content/plugins/openlab-private-comments/views/form-checkbox.php
@@ -1,3 +1,3 @@
 <div class="openlab-private-comments">
-	<label for="ol-private-comment"><?php esc_html_e( 'Make this comment private.', 'openlab-private-comments' ) ?></label> <input type="checkbox" name="ol-private-comment" id="ol-private-comment" value="1" />
+	<label for="ol-private-comment"><?php esc_html_e( 'Make this comment private.', 'openlab-private-comments' ) ?></label> <input type="checkbox" name="ol-private-comment" id="ol-private-comment" value="1" tabindex="-1" />
 </div>
