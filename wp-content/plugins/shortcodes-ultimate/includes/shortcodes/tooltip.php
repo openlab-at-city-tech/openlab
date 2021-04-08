aaf406a5d4cb5b6babc880848628907d20646d4a
Boone B Gorges <boonebgorges@gmail.com>
2021-04-08 14:47:57 +0000

    Update plugin: shortcodes-ultimate.
    Name: Shortcodes Ultimate
    New version: 5.9.8
    Previous version: 5.9.7

--- wp-content/plugins/shortcodes-ultimate/includes/shortcodes/tooltip.php
+++ wp-content/plugins/shortcodes-ultimate/includes/shortcodes/tooltip.php
@@ -1,125 +1,131 @@
 <?php
 
-su_add_shortcode( array(
-		'id' => 'tooltip',
+su_add_shortcode(
+	array(
+		'id'       => 'tooltip',
 		'callback' => 'su_shortcode_tooltip',
-		'image' => su_get_plugin_url() . 'admin/images/shortcodes/tooltip.svg',
-		'name' => __( 'Tooltip', 'shortcodes-ultimate' ),
-		'type' => 'wrap',
-		'group' => 'other',
-		'atts' => array(
-			'style' => array(
-				'type' => 'select',
-				'values' => array(
-					'light' => __( 'Basic: Light', 'shortcodes-ultimate' ),
-					'dark' => __( 'Basic: Dark', 'shortcodes-ultimate' ),
-					'yellow' => __( 'Basic: Yellow', 'shortcodes-ultimate' ),
-					'green' => __( 'Basic: Green', 'shortcodes-ultimate' ),
-					'red' => __( 'Basic: Red', 'shortcodes-ultimate' ),
-					'blue' => __( 'Basic: Blue', 'shortcodes-ultimate' ),
-					'youtube' => __( 'Youtube', 'shortcodes-ultimate' ),
-					'tipsy' => __( 'Tipsy', 'shortcodes-ultimate' ),
+		'image'    => su_get_plugin_url() . 'admin/images/shortcodes/tooltip.svg',
+		'name'     => __( 'Tooltip', 'shortcodes-ultimate' ),
+		'type'     => 'wrap',
+		'group'    => 'other',
+		'atts'     => array(
+			'style'    => array(
+				'type'    => 'select',
+				'values'  => array(
+					'light'     => __( 'Basic: Light', 'shortcodes-ultimate' ),
+					'dark'      => __( 'Basic: Dark', 'shortcodes-ultimate' ),
+					'yellow'    => __( 'Basic: Yellow', 'shortcodes-ultimate' ),
+					'green'     => __( 'Basic: Green', 'shortcodes-ultimate' ),
+					'red'       => __( 'Basic: Red', 'shortcodes-ultimate' ),
+					'blue'      => __( 'Basic: Blue', 'shortcodes-ultimate' ),
+					'youtube'   => __( 'Youtube', 'shortcodes-ultimate' ),
+					'tipsy'     => __( 'Tipsy', 'shortcodes-ultimate' ),
 					'bootstrap' => __( 'Bootstrap', 'shortcodes-ultimate' ),
-					'jtools' => __( 'jTools', 'shortcodes-ultimate' ),
-					'tipped' => __( 'Tipped', 'shortcodes-ultimate' ),
-					'cluetip' => __( 'Cluetip', 'shortcodes-ultimate' ),
+					'jtools'    => __( 'jTools', 'shortcodes-ultimate' ),
+					'tipped'    => __( 'Tipped', 'shortcodes-ultimate' ),
+					'cluetip'   => __( 'Cluetip', 'shortcodes-ultimate' ),
 				),
 				'default' => 'yellow',
-				'name' => __( 'Style', 'shortcodes-ultimate' ),
-				'desc' => __( 'Tooltip window style', 'shortcodes-ultimate' )
+				'name'    => __( 'Style', 'shortcodes-ultimate' ),
+				'desc'    => __( 'Tooltip window style', 'shortcodes-ultimate' ),
 			),
 			'position' => array(
-				'type' => 'select',
-				'values' => array(
+				'type'    => 'select',
+				'values'  => array(
 					'north' => __( 'Top', 'shortcodes-ultimate' ),
 					'south' => __( 'Bottom', 'shortcodes-ultimate' ),
-					'west' => __( 'Left', 'shortcodes-ultimate' ),
-					'east' => __( 'Right', 'shortcodes-ultimate' )
+					'west'  => __( 'Left', 'shortcodes-ultimate' ),
+					'east'  => __( 'Right', 'shortcodes-ultimate' ),
 				),
 				'default' => 'top',
-				'name' => __( 'Position', 'shortcodes-ultimate' ),
-				'desc' => __( 'Tooltip position', 'shortcodes-ultimate' )
+				'name'    => __( 'Position', 'shortcodes-ultimate' ),
+				'desc'    => __( 'Tooltip position', 'shortcodes-ultimate' ),
 			),
-			'shadow' => array(
-				'type' => 'bool',
+			'shadow'   => array(
+				'type'    => 'bool',
 				'default' => 'no',
-				'name' => __( 'Shadow', 'shortcodes-ultimate' ),
-				'desc' => __( 'Add shadow to tooltip. This option is only works with basic styes, e.g. blue, green etc.', 'shortcodes-ultimate' )
+				'name'    => __( 'Shadow', 'shortcodes-ultimate' ),
+				'desc'    => __( 'Add shadow to tooltip. This option is only works with basic styes, e.g. blue, green etc.', 'shortcodes-ultimate' ),
 			),
-			'rounded' => array(
-				'type' => 'bool',
+			'rounded'  => array(
+				'type'    => 'bool',
 				'default' => 'no',
-				'name' => __( 'Rounded corners', 'shortcodes-ultimate' ),
-				'desc' => __( 'Use rounded for tooltip. This option is only works with basic styes, e.g. blue, green etc.', 'shortcodes-ultimate' )
+				'name'    => __( 'Rounded corners', 'shortcodes-ultimate' ),
+				'desc'    => __( 'Use rounded for tooltip. This option is only works with basic styes, e.g. blue, green etc.', 'shortcodes-ultimate' ),
 			),
-			'size' => array(
-				'type' => 'select',
-				'values' => array(
+			'size'     => array(
+				'type'    => 'select',
+				'values'  => array(
 					'default' => __( 'Default', 'shortcodes-ultimate' ),
-					'1' => 1,
-					'2' => 2,
-					'3' => 3,
-					'4' => 4,
-					'5' => 5,
-					'6' => 6,
+					'1'       => 1,
+					'2'       => 2,
+					'3'       => 3,
+					'4'       => 4,
+					'5'       => 5,
+					'6'       => 6,
 				),
 				'default' => 'default',
-				'name' => __( 'Font size', 'shortcodes-ultimate' ),
-				'desc' => __( 'Tooltip font size', 'shortcodes-ultimate' )
+				'name'    => __( 'Font size', 'shortcodes-ultimate' ),
+				'desc'    => __( 'Tooltip font size', 'shortcodes-ultimate' ),
 			),
-			'title' => array(
+			'title'    => array(
 				'default' => '',
-				'name' => __( 'Tooltip title', 'shortcodes-ultimate' ),
-				'desc' => __( 'Enter title for tooltip window. Leave this field empty to hide the title', 'shortcodes-ultimate' )
+				'name'    => __( 'Tooltip title', 'shortcodes-ultimate' ),
+				'desc'    => __( 'Enter title for tooltip window. Leave this field empty to hide the title', 'shortcodes-ultimate' ),
 			),
-			'content' => array(
+			'content'  => array(
 				'default' => __( 'Tooltip text', 'shortcodes-ultimate' ),
-				'name' => __( 'Tooltip content', 'shortcodes-ultimate' ),
-				'desc' => __( 'Enter tooltip content here', 'shortcodes-ultimate' )
+				'name'    => __( 'Tooltip content', 'shortcodes-ultimate' ),
+				'desc'    => __( 'Enter tooltip content here', 'shortcodes-ultimate' ),
 			),
 			'behavior' => array(
-				'type' => 'select',
-				'values' => array(
-					'hover' => __( 'Show and hide on mouse hover', 'shortcodes-ultimate' ),
-					'click' => __( 'Show and hide by mouse click', 'shortcodes-ultimate' ),
-					'always' => __( 'Always visible', 'shortcodes-ultimate' )
+				'type'    => 'select',
+				'values'  => array(
+					'hover'  => __( 'Show and hide on mouse hover', 'shortcodes-ultimate' ),
+					'click'  => __( 'Show and hide by mouse click', 'shortcodes-ultimate' ),
+					'always' => __( 'Always visible', 'shortcodes-ultimate' ),
 				),
 				'default' => 'hover',
-				'name' => __( 'Behavior', 'shortcodes-ultimate' ),
-				'desc' => __( 'Select tooltip behavior', 'shortcodes-ultimate' )
+				'name'    => __( 'Behavior', 'shortcodes-ultimate' ),
+				'desc'    => __( 'Select tooltip behavior', 'shortcodes-ultimate' ),
 			),
-			'close' => array(
-				'type' => 'bool',
+			'close'    => array(
+				'type'    => 'bool',
 				'default' => 'no',
-				'name' => __( 'Close button', 'shortcodes-ultimate' ),
-				'desc' => __( 'Show close button', 'shortcodes-ultimate' )
+				'name'    => __( 'Close button', 'shortcodes-ultimate' ),
+				'desc'    => __( 'Show close button', 'shortcodes-ultimate' ),
 			),
-			'class' => array(
-				'type' => 'extra_css_class',
-				'name' => __( 'Extra CSS class', 'shortcodes-ultimate' ),
-				'desc' => __( 'Additional CSS class name(s) separated by space(s)', 'shortcodes-ultimate' ),
+			'class'    => array(
+				'type'    => 'extra_css_class',
+				'name'    => __( 'Extra CSS class', 'shortcodes-ultimate' ),
+				'desc'    => __( 'Additional CSS class name(s) separated by space(s)', 'shortcodes-ultimate' ),
 				'default' => '',
 			),
 		),
-		'content' => __( 'Hover me to open tooltip', 'shortcodes-ultimate' ),
-		'desc' => __( 'Tooltip window with custom content', 'shortcodes-ultimate' ),
-		'icon' => 'comment-o',
-	) );
+		'content'  => __( 'Hover me to open tooltip', 'shortcodes-ultimate' ),
+		'desc'     => __( 'Tooltip window with custom content', 'shortcodes-ultimate' ),
+		'icon'     => 'comment-o',
+	)
+);
 
 function su_shortcode_tooltip( $atts = null, $content = null ) {
 
-	$atts = shortcode_atts( array(
-			'style'        => 'yellow',
-			'position'     => 'north',
-			'shadow'       => 'no',
-			'rounded'      => 'no',
-			'size'         => 'default',
-			'title'        => '',
-			'content'      => __( 'Tooltip text', 'shortcodes-ultimate' ),
-			'behavior'     => 'hover',
-			'close'        => 'no',
-			'class'        => ''
-		), $atts, 'tooltip' );
+	$atts = shortcode_atts(
+		array(
+			'style'    => 'yellow',
+			'position' => 'north',
+			'shadow'   => 'no',
+			'rounded'  => 'no',
+			'size'     => 'default',
+			'title'    => '',
+			'content'  => __( 'Tooltip text', 'shortcodes-ultimate' ),
+			'behavior' => 'hover',
+			'close'    => 'no',
+			'class'    => '',
+		),
+		$atts,
+		'tooltip'
+	);
 
 	// Prepare style
 	$atts['style'] = in_array( $atts['style'], array( 'light', 'dark', 'green', 'red', 'blue', 'youtube', 'tipsy', 'bootstrap', 'jtools', 'tipped', 'cluetip' ) )
@@ -128,13 +134,13 @@ function su_shortcode_tooltip( $atts = null, $content = null ) {
 
 	// Position
 	$atts['position'] = str_replace( array( 'top', 'right', 'bottom', 'left' ), array( 'north', 'east', 'south', 'west' ), $atts['position'] );
-	$position = array(
+	$position         = array(
 		'my' => str_replace( array( 'north', 'east', 'south', 'west' ), array( 'bottom center', 'center left', 'top center', 'center right' ), $atts['position'] ),
-		'at' => str_replace( array( 'north', 'east', 'south', 'west' ), array( 'top center', 'center right', 'bottom center', 'center left' ), $atts['position'] )
+		'at' => str_replace( array( 'north', 'east', 'south', 'west' ), array( 'top center', 'center right', 'bottom center', 'center left' ), $atts['position'] ),
 	);
 
 	// Prepare classes
-	$classes = array( 'su-qtip qtip-' . $atts['style'] );
+	$classes   = array( 'su-qtip qtip-' . $atts['style'] );
 	$classes[] = 'su-qtip-size-' . $atts['size'];
 
 	if ( $atts['shadow'] === 'yes' ) {
