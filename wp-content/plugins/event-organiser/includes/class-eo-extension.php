88d608ce5f70549b9fc5e70c56dacb7340ead572
Boone B Gorges <boonebgorges@gmail.com>
2021-07-01 10:55:18 -0500

    PHP compat for event-organiser.

--- wp-content/plugins/event-organiser/includes/class-eo-extension.php
+++ wp-content/plugins/event-organiser/includes/class-eo-extension.php
@@ -280,7 +280,7 @@ if ( ! class_exists( 'EO_Extension' ) ) {
 			do_settings_sections( 'settings-network' );
 		}
 
-		static function save_ntw_settings() {
+		public function save_ntw_settings() {
 
 			if ( ! current_user_can( 'manage_network_options' ) ) {
 				return false;
@@ -289,7 +289,7 @@ if ( ! class_exists( 'EO_Extension' ) ) {
 				return false;
 			}
 			$value = wp_unslash($_POST[$this->id . '_license']);
-			
+
 			if (!empty($value)) {
 				update_site_option( $this->id . '_license', $value );
 			}
