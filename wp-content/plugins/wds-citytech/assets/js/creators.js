07ac773f67a956cecfb20971416706effc2fa618
Boone B Gorges <boonebgorges@gmail.com>
2021-07-22 09:12:20 -0500

    Trigger blur on Enter in non-member Creator input.
    Enter acts like an 'I'm done' button in this context.
    
    See #2945.

--- wp-content/plugins/wds-citytech/assets/js/creators.js
+++ wp-content/plugins/wds-citytech/assets/js/creators.js
@@ -98,7 +98,7 @@
 			}
 		);
 
-		$entryEl.find( '.non-member-name, .non-member-url' ).on(
+		$entryEl.find( '.non-member-name' ).on(
 			'blur',
 			function( e ) {
 				setTimeout(
@@ -109,6 +109,18 @@
 					},
 					100
 				);
+			}
+		);
+
+		// When pressing Enter in a non-member name field, trigger 'blur' to go to Preview mode.
+		$entryEl.find( '.non-member-name' ).on(
+			'keypress',
+			function( e ) {
+				var code = (e.keyCode ? e.keyCode : e.which);
+				if ( code === 13 ) {
+					$( e.target ).blur();
+					return false;
+				}
 			}
 		);
 	}
