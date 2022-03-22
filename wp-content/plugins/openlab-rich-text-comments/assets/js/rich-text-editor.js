5f29cf9517ff17735c70c7023a3aa66a49bc982d
boris.kuzmanov <boris.kuzmanov@haselt.com>
2022-03-22 16:03:30 +0100

    WIP Implement QuillJS for post/page comments editor

---
+++ wp-content/plugins/openlab-rich-text-comments/assets/js/rich-text-editor.js
@@ -0,0 +1,30 @@
+'use strict';
+(function($) {
+    
+    $(document).ready( function() {
+        /**
+         * Initialize Quill Editor
+         */
+        var quillEditor = new Quill('#ol-rich-editor', {
+            modules: {
+                toolbar: [
+                    [ 'bold', 'italic', 'underline', 'link' ],
+                    [ { 'list': 'ordered'}, { 'list': 'bullet' } ]
+                ]
+            },
+            theme: 'snow'
+        });
+
+        /**
+         * Get content of the Quill editor and put it's content
+         * in the comment text field.
+         * 
+         */
+        quillEditor.on( 'text-change', function( delta, oldDelta, source ) {
+            let contentHtml = quillEditor.root.innerHTML;
+            $('textarea#comment').val(contentHtml);
+        });
+
+    } );
+
+})(jQuery);
