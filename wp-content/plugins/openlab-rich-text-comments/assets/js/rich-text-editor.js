76edbb6486c8db7b484d1246fefcc10e0ead1cde
boris.kuzmanov <boris.kuzmanov@haselt.com>
2022-03-24 14:18:09 +0100

    Add accessibility attributes to the editor toolbar buttons

--- wp-content/plugins/openlab-rich-text-comments/assets/js/rich-text-editor.js
+++ wp-content/plugins/openlab-rich-text-comments/assets/js/rich-text-editor.js
@@ -30,6 +30,33 @@
             }
         });
 
+        /**
+         * Add aria-label to the buttons in the toolbar;
+         * Add aria-pressed on the buttons in the toolbar;
+         * Add aria-hidden on the SVG icons in the buttons;
+         * Add role and aria-multiline attribute to the editor textarea;
+         */
+        $('button.ql-bold').attr( 'aria-label', 'Toggle bold text' );
+        $('button.ql-italic').attr( 'aria-label', 'Toggle italic text' );
+        $('button.ql-underline').attr( 'aria-label', 'Toggle underline text' );
+        $('button.ql-link').attr( 'aria-label', 'Toggle link modal' );
+        $('button.ql-list[value="ordered"]').attr('aria-label', 'Toggle ordered list');
+        $('button.ql-list[value="bullet"]').attr('aria-label', 'Toggle bulleted list');
+        $('.ql-formats button').attr('aria-pressed', false );
+        $('.ql-formats button > svg').attr('aria-hidden', true );
+        $('#ol-rich-editor .ql-editor').attr( {
+            'role': 'textbox',
+            'aria-multiline': true
+        } );
+
+        /**
+         * Toggle aria-pressed attribute on clicking the buttons
+         * in the editor's toolbar.
+         */
+        $(document).on( 'click', '.ql-formats button', function(e) {
+            $(this).attr( 'aria-pressed', $(this).hasClass('ql-active') );
+        });
+
     } );
 
 })(jQuery);
