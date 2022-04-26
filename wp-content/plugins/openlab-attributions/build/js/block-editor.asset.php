b65a554976a33139339ff8941b06f47315a5fab9
boris.kuzmanov <boris.kuzmanov@haselt.com>
2022-04-26 10:41:42 +0200

    Update react-modal-resizable-draggable to 0.1.6 and it's css

--- wp-content/plugins/openlab-attributions/build/js/block-editor.asset.php
+++ wp-content/plugins/openlab-attributions/build/js/block-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-rich-text'), 'version' => 'ce5958696c73be005c6f1e4cbecf2941');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-rich-text'), 'version' => 'a17216e076359df94efd0afcae95b43b');
--- wp-content/plugins/openlab-attributions/build/js/classic-editor.asset.php
+++ wp-content/plugins/openlab-attributions/build/js/classic-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '32f102a926151f3b89fc3d6b6e92df28');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '33802ede43e96572ea933dfd4eb6851b');
