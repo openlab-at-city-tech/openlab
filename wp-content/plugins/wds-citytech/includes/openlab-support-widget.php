92e09ff8101d37902fec35f101cc4721ce2217b5
Boone B Gorges <boonebgorges@gmail.com>
2022-05-02 11:58:56 -0500

    Add block/widget infrastructure, and add OpenLab Support widget.
    New widgets can be added as Blocks. Markup is loaded from shared PHP
    templates in the 'shared' directory. A placeholder WP_Widget class
    then loads from the same template.
    
    See #3047, #3044.

---
+++ wp-content/plugins/wds-citytech/includes/openlab-support-widget.php
@@ -0,0 +1,57 @@
+<?php
+
+add_action(
+	'widgets_init',
+	function() {
+		register_widget( 'OpenLab_Support_Widget' );
+	}
+);
+
+class OpenLab_Support_Widget extends WP_Widget {
+	public function __construct() {
+		parent::__construct(
+			'openlab_support',
+			'OpenLab Support',
+			array(
+				'description' => '',
+			)
+		);
+	}
+
+	public function widget( $args, $instance ) {
+		echo $args['before_widget'];
+
+		echo $args['before_title'];
+		echo esc_html( $instance['title'] );
+		echo $args['after_title'];
+
+		echo openlab_render_block( 'openlab-support', $atts );
+
+		echo $args['after_widget'];
+	}
+
+	public function form( $instance ) {
+		$r = array_merge(
+			[
+				'title' => 'OpenLab Support',
+			],
+			$instance
+		);
+
+		?>
+
+		<p>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>">Title:</label>
+			<input type="text" class="widefat" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" value="<?php echo esc_attr( $r['title'] ); ?>" />
+		</p>
+		<?php
+	}
+
+	public function update( $new_instance, $old_instance ) {
+		$instance = [
+			'title' => isset( $new_instance['title'] ) ? $new_instance['title'] : '',
+		];
+
+		return $instance;
+	}
+}
---
+++ wp-content/plugins/wds-citytech/src/blocks/openlab-support/openlab-support.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+	'api_version'     => 2,
+	'render_callback' => function( $atts ) {
+		$markup  = '<div class="openlab-block-openlab-support widget">';
+		$markup .= '<h2 class="widget-title">OpenLab Support</h2>';
+		$markup .= openlab_render_block( 'openlab-support', $atts );
+		$markup .= '</div>';
+
+		return $markup;
+	},
+];
+
