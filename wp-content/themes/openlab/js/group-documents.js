6a051879a646ecb2552ad3d3cc147af8c90c22b5
boris.kuzmanov <boris.kuzmanov@haselt.com>
2022-05-13 12:36:06 +0200

    Override group documents form submit event

--- wp-content/themes/openlab/js/group-documents.js
+++ wp-content/themes/openlab/js/group-documents.js
@@ -12,6 +12,27 @@
             }
         });
 
+        // Remove the original 'submit' event for the group documents form
+        $('form#bp-group-documents-form').off('submit');
+
+        // Attach new submit event on the group documents form
+        $(document).on( 'submit', 'form#bp-group-documents-form', function() {
+            
+            //check for pre-filled values, and remove before sumitting
+            if( $('input.bp-group-documents-new-category').val() == 'New Category...' ) {
+                $('input.bp-group-documents-new-category').val('');
+            }
+
+            if( $('input[name=bp_group_documents_operation]').val() == 'add' && $('input[name=bp_group_documents_file_type]:checked').val() == 'upload' ) {
+                if($('input.bp-group-documents-file').val()) {
+                    return true;
+                }
+
+                alert('You must select a file to upload!');
+                return false;
+            }
+        });
+
 	});
 
 }(jQuery));
