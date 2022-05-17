22828a33b7da0121f46b1ef7159318e804c633b7
Boone B Gorges <boonebgorges@gmail.com>
2022-05-17 08:54:00 -0500

    Specify form action for forum search form.
    This ensures that it will work on single topics.
    
    See #3060.

--- wp-content/themes/openlab/bbpress/form-search.php
+++ wp-content/themes/openlab/bbpress/form-search.php
@@ -13,7 +13,7 @@ defined( 'ABSPATH' ) || exit;
 if ( bbp_allow_search() ) : ?>
 
 	<div class="bbp-search-form">
-		<form role="search" method="get" id="bbp-search-form">
+		<form role="search" method="get" id="bbp-search-form" action="<?php echo esc_url( bp_get_group_permalink( groups_get_current_group() ) ); ?>forum/">
 			<div>
 				<label class="screen-reader-text hidden" for="bbp_search"><?php esc_html_e( 'Search for:', 'bbpress' ); ?></label>
 				<input type="text" value="<?php bbp_search_terms(); ?>" name="bbp_search" id="bbp_search" />
