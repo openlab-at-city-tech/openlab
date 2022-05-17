356ad7f367599ca1b80db6a6cbca5c922c4d7022
Boone B Gorges <boonebgorges@gmail.com>
2022-05-17 14:03:54 -0500

    openlab-attributions build

--- wp-content/plugins/openlab-attributions/build/js/block-editor.asset.php
+++ wp-content/plugins/openlab-attributions/build/js/block-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-rich-text'), 'version' => '52b5a3c2abe21491ee204874b680b90f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-rich-text'), 'version' => '9c709cb7f4587a92f49b19671f6ff714');
--- wp-content/plugins/openlab-attributions/build/js/classic-editor.asset.php
+++ wp-content/plugins/openlab-attributions/build/js/classic-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '9200256d082fa352ca134baaa0444a57');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'df171f2a41b71ec3eaaa3cdaed14d607');
