6e752c3a19d67ff25c34e04f7a5a7c69f88ce7b5
Boone B Gorges <boonebgorges@gmail.com>
2022-05-20 11:57:41 -0500

    Improve 'In reply to' language on discussion forum search results.
    See #3060.

--- wp-content/themes/openlab/bbpress/loop-search-reply.php
+++ wp-content/themes/openlab/bbpress/loop-search-reply.php
@@ -14,8 +14,7 @@ defined( 'ABSPATH' ) || exit;
 
 <div class="bbp-reply-header">
 	<div class="bbp-reply-title">
-		<h3><?php esc_html_e( 'In reply to: ', 'bbpress' ); ?>
-		<a class="bbp-topic-permalink" href="<?php bbp_topic_permalink( bbp_get_reply_topic_id() ); ?>"><?php bbp_topic_title( bbp_get_reply_topic_id() ); ?></a></h3>
+		<h3>In reply to the topic: <a class="bbp-topic-permalink" href="<?php bbp_topic_permalink( bbp_get_reply_topic_id() ); ?>"><?php bbp_topic_title( bbp_get_reply_topic_id() ); ?></a></h3>
 	</div><!-- .bbp-reply-title -->
 
 	<div class="bbp-meta">
