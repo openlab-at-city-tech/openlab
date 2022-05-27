06fac8f2037591d1533a3718684fd85da3a03f5b
boris.kuzmanov <boris.kuzmanov@haselt.com>
2022-05-27 15:05:36 +0200

    Show activity filter if there are no results

--- wp-content/themes/openlab/buddypress/members/single/my-activity.php
+++ wp-content/themes/openlab/buddypress/members/single/my-activity.php
@@ -24,20 +24,20 @@ add_filter( 'bp_activity_time_since', 'openlab_change_activity_date_format' );
 <div id="item-body" role="main">
 	<?php do_action( 'bp_before_activity_loop' ); ?>
 
-	<?php if ( bp_has_activities( $args ) ) : ?>
-		<div class="activity-filter">
-			<form action="" id="activity-filter-form" class="activity-filter-form" method="GET">
-				<?php if( ! empty( $type ) ) { ?>
-				<input type="hidden" name="type" value="<?php echo $type; ?>" />
+	<div class="activity-filter">
+		<form action="" id="activity-filter-form" class="activity-filter-form" method="GET">
+			<?php if( ! empty( $type ) ) { ?>
+			<input type="hidden" name="type" value="<?php echo $type; ?>" />
+			<?php } ?>
+			<select id="activity-filter-by" name="filter" class="form-control">
+				<?php foreach( $filter_options as $key => $label ) { ?>
+				<option value="<?php echo $key; ?>" <?php echo ( $filter === $key ) ? 'selected' : ''; ?>><?php echo $label; ?></option>
 				<?php } ?>
-				<select id="activity-filter-by" name="filter" class="form-control">
-					<?php foreach( $filter_options as $key => $label ) { ?>
-					<option value="<?php echo $key; ?>" <?php echo ( $filter === $key ) ? 'selected' : ''; ?>><?php echo $label; ?></option>
-					<?php } ?>
-				</select>
-			</form>
-		</div>
-		
+			</select>
+		</form>
+	</div>
+
+	<?php if ( bp_has_activities( $args ) ) : ?>
 		<?php if ( empty( $_POST['page'] ) ) : ?>
 			<div id="activity-stream" class="activity-list item-list group-list">
 		<?php endif; ?>
