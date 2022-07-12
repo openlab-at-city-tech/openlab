d525354750dd89f71c43bb36b12aef134d452b1a
Boone B Gorges <boonebgorges@gmail.com>
2022-07-12 09:23:19 -0500

    Updated logo for bp-ges-single email.
    See #3085.

--- wp-content/themes/openlab/buddypress/assets/emails/single-bp-email-bp-ges-single.php
+++ wp-content/themes/openlab/buddypress/assets/emails/single-bp-email-bp-ges-single.php
@@ -142,7 +142,7 @@ $settings = bp_email_get_appearance_settings();
 			<!-- Email Header : BEGIN -->
 			<table cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="max-width: 600px; border-top: 7px solid <?php echo esc_attr( $settings['highlight_color'] ); ?>" bgcolor="<?php echo esc_attr( $settings['header_bg'] ); ?>" class="header_bg">
 				<tr>
-					<td style="text-align: center; padding: 15px 0; font-family: sans-serif; mso-height-rule: exactly; font-weight: bold; color: <?php echo esc_attr( $settings['header_text_color'] ); ?>; font-size: <?php echo esc_attr( $settings['header_text_size'] . 'px' ); ?>" class="header_text_color header_text_size">
+					<td style="text-align: center; padding: 0 0 15px 0; font-family: sans-serif; mso-height-rule: exactly; font-weight: bold; color: <?php echo esc_attr( $settings['header_text_color'] ); ?>; font-size: <?php echo esc_attr( $settings['header_text_size'] . 'px' ); ?>" class="header_text_color header_text_size">
 						<?php
 						/**
 						 * Fires before the display of the email template header.
@@ -151,7 +151,7 @@ $settings = bp_email_get_appearance_settings();
 						 */
 						do_action( 'bp_before_email_header' );
 						?>
-						<img src="https://openlab.citytech.cuny.edu/wp-content/themes/openlab/images/openlab-logo.jpg" alt="<?php echo bp_get_option( 'blogname' ); ?>" />
+						<img style="max-width: 400px;" src="<?php echo esc_attr( home_url( 'wp-content/themes/openlab/images/OpenLab_logo.svg' ) ); ?>" alt="<?php echo esc_attr( bp_get_option( 'blogname' ) ); ?>" width="100%" />
 
 						<?php
 						/**
