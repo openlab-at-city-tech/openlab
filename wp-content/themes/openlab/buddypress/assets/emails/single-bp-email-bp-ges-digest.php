d4038c6789e8f8cbd652171e33ef3f3ac9a3f851
Boone B Gorges <boonebgorges@gmail.com>
2022-07-14 10:41:23 -0500

    Use shared template parts in bp-ges-digest emails.

--- wp-content/themes/openlab/buddypress/assets/emails/single-bp-email-bp-ges-digest.php
+++ wp-content/themes/openlab/buddypress/assets/emails/single-bp-email-bp-ges-digest.php
@@ -140,32 +140,7 @@ $settings = bp_email_get_appearance_settings();
 			<![endif]-->
 
 			<!-- Email Header : BEGIN -->
-			<table cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="max-width: 600px; border-top: 7px solid <?php echo esc_attr( $settings['highlight_color'] ); ?>" bgcolor="<?php echo esc_attr( $settings['header_bg'] ); ?>" class="header_bg">
-				<tr>
-					<td style="text-align: center; padding: 0 0 15px 0; font-family: sans-serif; mso-height-rule: exactly; font-weight: bold; color: <?php echo esc_attr( $settings['header_text_color'] ); ?>; font-size: <?php echo esc_attr( $settings['header_text_size'] . 'px' ); ?>" class="header_text_color header_text_size">
-						<?php
-						/**
-						 * Fires before the display of the email template header.
-						 *
-						 * @since 2.5.0
-						 */
-						do_action( 'bp_before_email_header' );
-						?>
-
-						<?php /* Note that image path is hardcoded to production site to avoid HTTP auth issues */ ?>
-						<img style="max-width: 400px;" src="<?php echo esc_attr( 'https://openlab.citytech.cuny.edu/wp-content/themes/openlab/images/openlab-logo-full.png' ); ?>" alt="<?php echo esc_attr( bp_get_option( 'blogname' ) ); ?>" width="100%" />
-
-						<?php
-						/**
-						 * Fires after the display of the email template header.
-						 *
-						 * @since 2.5.0
-						 */
-						do_action( 'bp_after_email_header' );
-						?>
-					</td>
-				</tr>
-			</table>
+			<?php bp_get_template_part( 'buddypress/assets/emails/parts/logo-header', '', $settings ); ?>
 			<!-- Email Header : END -->
 
 			<!-- Email Body : BEGIN -->
@@ -204,7 +179,7 @@ $settings = bp_email_get_appearance_settings();
 			<!-- Email Body : END -->
 
 			<!-- Email Footer : BEGIN -->
-			<?php bp_get_template_part( 'buddypress/assets/emails/parts/footer', '', $settings ); ?>
+			<?php bp_get_template_part( 'buddypress/assets/emails/parts/footer', 'ges', $settings ); ?>
 			<!-- Email Footer : END -->
 
 			<!--[if (gte mso 9)|(IE)]>
