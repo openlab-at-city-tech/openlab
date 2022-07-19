decc37caafa0e5eec6f6c74b430dbd087faee219
Boone B Gorges <boonebgorges@gmail.com>
2022-07-19 10:46:12 -0500

    Update theme: hamilton.
    Name: Hamilton
    New version: 2.1.2
    Previous version: 2.0.7

--- wp-content/themes/hamilton/footer.php
+++ wp-content/themes/hamilton/footer.php
@@ -3,7 +3,7 @@
 		<footer class="site-footer section-inner">
 
 			<p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>" class="site-name"><?php bloginfo( 'name' ); ?></a></p>
-			<p class="theme-by"><?php _e( 'Theme by', 'hamilton' ); ?> <a href="https://www.andersnoren.se">Anders Nor&eacute;n</a></p>
+			<p class="theme-by"><?php _e( 'Theme by', 'hamilton' ); ?> <a href="https://andersnoren.se">Anders Nor&eacute;n</a></p>
 
 		</footer><!-- footer -->
 	    
