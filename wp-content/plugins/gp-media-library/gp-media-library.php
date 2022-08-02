b300f1e8d68c1aef4ce8b909886af63f6b12ec0e
Boone B Gorges <boonebgorges@gmail.com>
2022-08-02 11:02:37 -0500

    Update plugin: gp-media-library.
    Name: GP Media Library
    New version: 1.2.25
    Previous version: 1.2.18

--- wp-content/plugins/gp-media-library/gp-media-library.php
+++ wp-content/plugins/gp-media-library/gp-media-library.php
@@ -3,16 +3,17 @@
  * Plugin Name: GP Media Library
  * Description: Upload files from a Gravity Forms File Upload field to the WordPress media library.
  * Plugin URI: https://gravitywiz.com/documentation/gravity-forms-media-library/
- * Version: 1.2.18
+ * Version: 1.2.25
  * Author: Gravity Wiz
  * Author URI: http://gravitywiz.com/
  * License: GPL2
  * Perk: True
+ * Update URI: https://gravitywiz.com/updates/gp-media-library
  * Text Domain: gp-media-library
  * Domain Path: /languages
  */
 
-define( 'GP_MEDIA_LIBRARY_VERSION', '1.2.18' );
+define( 'GP_MEDIA_LIBRARY_VERSION', '1.2.25' );
 
 require 'includes/class-gp-bootstrap.php';
 
