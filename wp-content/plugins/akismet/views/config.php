108ee4e2ecfa37e212cf65e9468aaeeebbaed5a2
Boone B Gorges <boonebgorges@gmail.com>
2022-10-03 11:16:42 -0500

    Update plugin: akismet.
    Name: Akismet Anti-Spam
    New version: 5.0.1
    Previous version: 5.0

--- wp-content/plugins/akismet/views/config.php
+++ wp-content/plugins/akismet/views/config.php
@@ -21,19 +21,21 @@
 				<?php Akismet::view( 'notice', $notice ); ?>
 			<?php } ?>
 		<?php } ?>
-		<?php if ( $stat_totals && isset( $stat_totals['all'] ) && (int) $stat_totals['all']->spam > 0 ) : ?>
-			<div class="akismet-card">
-				<div class="akismet-section-header">
-					<div class="akismet-section-header__label">
-						<span><?php esc_html_e( 'Statistics' , 'akismet'); ?></span>
-					</div>
-					<div class="akismet-section-header__actions">
-						<a href="<?php echo esc_url( Akismet_Admin::get_page_url( 'stats' ) ); ?>">
-							<?php esc_html_e( 'Detailed Stats' , 'akismet');?>
-						</a>
-					</div>
+
+		<div class="akismet-card">
+			<div class="akismet-section-header">
+				<div class="akismet-section-header__label">
+					<span><?php esc_html_e( 'Statistics', 'akismet' ); ?></span>
 				</div>
-				
+
+			<?php if ( $stat_totals && isset( $stat_totals['all'] ) && (int) $stat_totals['all']->spam > 0 ) : ?>
+				<div class="akismet-section-header__actions">
+					<a href="<?php echo esc_url( Akismet_Admin::get_page_url( 'stats' ) ); ?>">
+						<?php esc_html_e( 'Detailed Stats', 'akismet' ); ?>
+					</a>
+				</div>
+			</div> <!-- close akismet-section-header -->
+
 				<div class="akismet-new-snapshot">
 					<iframe allowtransparency="true" scrolling="no" frameborder="0" style="width: 100%; height: 220px; overflow: hidden;" src="<?php echo esc_url( sprintf( 'https://tools.akismet.com/1.0/snapshot.php?blog=%s&api_key=%s&height=200&locale=%s', urlencode( get_option( 'home' ) ), Akismet::get_api_key(), get_locale() ) ); ?>"></iframe>
 					<ul>
@@ -55,9 +57,16 @@
 							<?php printf( _n( '%s false positive', '%s false positives', $stat_totals['all']->false_positives, 'akismet' ), number_format( $stat_totals['all']->false_positives ) ); ?>
 						</li>
 					</ul>
-				</div>
+				</div> <!-- close akismet-new-snapshot -->
+
+			<?php else : ?>
+			</div> <!-- close akismet-section-header -->
+			<div class="inside">
+				<p>Akismet is active and ready to stop spam. Your site's spam statistics will be displayed here.</p>
 			</div>
-		<?php endif;?>
+			<?php endif; ?>
+
+		</div> <!-- close akismet-card -->
 
 		<?php if ( $akismet_user ) : ?>
 			<div class="akismet-card">
