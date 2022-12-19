78e88a4c3640e7f7a9b16ad4b18ad5f67e3036ff
Boone B Gorges <boonebgorges@gmail.com>
2022-12-19 16:37:13 -0600

    Revert "On login from toolbap, redirect to source URI."
    This reverts commit 1a22c14abb0d519c9f3938c38f102388704a07b8.

--- wp-content/mu-plugins/parts/persistent/loginform.php
+++ wp-content/mu-plugins/parts/persistent/loginform.php
@@ -1,14 +1,10 @@
-<?php
-$redirect_to = home_url( add_query_arg( array(), $GLOBALS['wp']->request ) );
-?>
-
 <div class="ab-sub-wrapper">
     <div class="ab-submenu">
         <form name="login-form" style="display:none;" id="sidebar-login-form" class="standard-form form" action="<?php echo site_url("wp-login.php", "login_post") ?>" method="post">
             <label><?php _e("Username", "buddypress") ?><br /><input type="text" name="log" id="dropdown-user-login" class="input form-control" value="" /></label><br />
             <label><?php _e("Password", "buddypress") ?><br /><input class="form-control" type="password" name="pwd" id="dropdown-user-pass" class="input" value="" /></label>
             <p class="forgetmenot checkbox"><label><input name="rememberme" type="checkbox" id="dropdown-rememberme" value="forever" /> <?php _e("Keep Me Logged In", "buddypress") ?></label></p>
-            <input type="hidden" name="redirect_to" value="<?php echo esc_url( $redirect_to ); ?>" />
+            <input type="hidden" name="redirect_to" value="<?php echo bp_get_root_domain() . $request_uri; ?>" />
             <input type="submit" name="wp-submit" id="dropdown-wp-submit" class="btn btn-primary sidebar-wp-submit" value="<?php _e("Log In"); ?>" tabindex="0" />
             <span class="exit"><a href="<?php echo wp_lostpassword_url(); ?>" class="lost-pw">Forgot Password?</a></span>
         </form>
