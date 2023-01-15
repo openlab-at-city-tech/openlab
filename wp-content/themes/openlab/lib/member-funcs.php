d27543ce2a0e13ac5701211f8132b05bb4768f48
boris.kuzmanov <boris.kuzmanov@haselt.com>
2023-01-15 18:28:11 +0100

    Change "Download file" to "View file" in the activity items

--- wp-content/themes/openlab/lib/member-funcs.php
+++ wp-content/themes/openlab/lib/member-funcs.php
@@ -1565,7 +1565,7 @@ function openlab_get_activity_view_button_label( $activity_type = '' ) {
 	switch ( $activity_type ) {
 		case 'edited_group_document' :
 		case 'added_group_document' :
-			return 'Download File';
+			return 'View File';
 
 		case 'bp_doc_created' :
 		case 'bp_doc_edited' :
