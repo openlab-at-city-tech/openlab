2f7e71dac740734539a2654417fa070400b2cddc
Boone B Gorges <boonebgorges@gmail.com>
2023-02-15 10:29:23 -0600

    Port exclusions for PHP linting.

--- developer/lint-php/lint-php.sh
+++ developer/lint-php/lint-php.sh
@@ -2,7 +2,12 @@
 
 excludes=(
 	'./wp-content/blogs.dir'
-	'./wp-content/uploads'
+	'./wp-content/uploads',
+	'./wp-content/plugins/advanced-gutenberg/vendor/pimple/ext/pimple/tests' # Test dir not loaded in production
+	'./wp-content/plugins/wp-post-to-pdf' # Plugin is deprecated and throws lots of false positives
+  './wp-content/plugins/anthologize/vendor/pear/pear/' # Not loaded because we have zlib.
+  './wp-content/plugins/anthologize/vendor/pear/file_archive/' # Not loaded because we have zlib.
+  './wp-content/plugins/out-of-the-box/vendors/phpThumb'
 )
 
 separator='--exclude '
