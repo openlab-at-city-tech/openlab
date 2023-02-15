113119bf2a5e624d3eca3bbf0ca42f5302200431
Boone B Gorges <boonebgorges@gmail.com>
2023-02-15 10:34:54 -0600

    Fix PHP linting exclusion path.

--- developer/lint-php/lint-php.sh
+++ developer/lint-php/lint-php.sh
@@ -3,7 +3,7 @@
 excludes=(
 	'./wp-content/blogs.dir'
 	'./wp-content/uploads',
-	'./wp-content/plugins/advanced-gutenberg/vendor/pimple/ext/pimple/tests' # Test dir not loaded in production
+	'./wp-content/plugins/advanced-gutenberg/vendor/pimple/pimple/ext/pimple/tests' # Test dir not loaded in production
 	'./wp-content/plugins/wp-post-to-pdf' # Plugin is deprecated and throws lots of false positives
   './wp-content/plugins/anthologize/vendor/pear/pear/' # Not loaded because we have zlib.
   './wp-content/plugins/anthologize/vendor/pear/file_archive/' # Not loaded because we have zlib.
