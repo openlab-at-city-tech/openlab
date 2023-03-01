b163b8700abe2d97f08062e859440d6dc86a7fa1
Boone B Gorges <boonebgorges@gmail.com>
2023-03-01 09:03:32 -0600

    Update plugin: tablepress.
    Name: TablePress
    New version: 2.0.4
    Previous version: 2.0.2

--- wp-content/plugins/tablepress/classes/class-import.php
+++ wp-content/plugins/tablepress/classes/class-import.php
@@ -134,6 +134,11 @@ class TablePress_Import {
 					return new WP_Error( 'table_import_url_host_blocked', '', $this->import_config['url'] );
 				}
 
+				/**
+				 * Load WP file functions to be sure that `download_url()` exists, in particular during Cron requests.
+				 */
+				require_once ABSPATH . 'wp-admin/includes/file.php';
+
 				// Download URL to local file.
 				$location = download_url( $this->import_config['url'] );
 				if ( is_wp_error( $location ) ) {
@@ -401,6 +406,7 @@ class TablePress_Import {
 
 		// Use the legacy import class, if the requirements for PHPSpreadsheet are not fulfilled.
 		$phpspreadsheet_requirements_fulfilled = PHP_VERSION_ID >= 70200
+			&& extension_loaded( 'mbstring' )
 			&& class_exists( 'ZipArchive', false )
 			&& class_exists( 'DOMDocument', false )
 			&& function_exists( 'simplexml_load_string' )
