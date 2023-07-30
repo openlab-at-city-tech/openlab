openlab-at-city-tech/openlab

commit cf84ebb54c84576cd466e2e7e16034502188ec27

Initial pass at group connections.
See #3207.

Squashed commit of the following:

commit d558072b4cf1a59eb540b0e77fdabe383f1cc5b4
Author: Boone B Gorges <boonebgorges@gmail.com>
Date:   Fri Jul 7 11:15:38 2023 -0500

    Pin openlab-connections.

commit 3aafb8650f86cb71de2617d5c010ca8c4f24ce67
Author: Boone B Gorges <boonebgorges@gmail.com>
Date:   Fri Jul 7 11:05:44 2023 -0500

    Connections sidebar and activity panel.

    See #3207.

commit 539be8adc6a9600b392fbccda56d5da4c00c5294
Author: Boone B Gorges <boonebgorges@gmail.com>
Date:   Fri Jul 7 09:56:58 2023 -0500

    Reorganize "no connection" text.

    See #3207.

commit a8cf5348d45b3af0302dccf9eb65213395944517
Merge: 441439cf4 66160d8eb
Author: Boone B Gorges <boonebgorges@gmail.com>
Date:   Fri Jul 7 09:45:18 2023 -0500

    Merge branch '1.7.x' into group-connections

commit 441439cf491e49961198e817888bde08cc3cc28d
Author: Boone B Gorges <boonebgorges@gmail.com>
Date:   Fri Jun 30 08:13:40 2023 -0500

    PHPStan juggling.

commit b4347bad6c7ae0ff450408dcdbdce784d06aa3a4
Author: Boone B Gorges <boonebgorges@gmail.com>
Date:   Fri Jun 30 08:10:05 2023 -0500

    Disconnect button improvements.

commit 11bc5ca88982abcee157c7799407e14ab8418ccb
Author: Boone B Gorges <boonebgorges@gmail.com>
Date:   Thu Jun 29 15:06:10 2023 -0500

    Add openlab-connections as a submodule.

commit 5cfa6cdba11f33e0ff622ff4a3c72b1a677e9a3f
Author: Boone B Gorges <boonebgorges@gmail.com>
Date:   Thu Jun 29 14:59:34 2023 -0500

    Connections integration adjustments.

commit bf323f2bd9ea5309c9071d58a8be6bd5b568b138
Author: Boone B Gorges <boonebgorges@gmail.com>
Date:   Thu Jun 29 14:59:14 2023 -0500

    Some PHPStan adjustments.

commit 51aebba3067c10d3ea0d7c6c35528dc99aa57f79
Author: Boone B Gorges <boonebgorges@gmail.com>
Date:   Thu Jun 29 14:48:16 2023 -0500

    Main 'Connections' list view in group.

commit 2e445e330d85252367b394ee90b5d7ba32ed3d7a
Author: Boone B Gorges <boonebgorges@gmail.com>

wp-content/themes/openlab/lib/menus.php
@@ -334,6 +334,10 @@ function openlab_submenu_markup($type = '', $opt_var = NULL, $row_wrapper = true
             $submenu_text = 'Docs<span aria-hidden="true">:</span> ';
             $menu = openlab_group_docs_submenu();
             break;
+        case 'group-connections':
+            $submenu_text = 'Connections<span aria-hidden="true">:</span> ';
+            $menu = openlab_group_connections_submenu();
+            break;
         default:
             $submenu_text = 'My Settings<span aria-hidden="true">:</span> ';
             $menu = openlab_profile_settings_submenu();
@@ -668,9 +672,8 @@ function openlab_group_activity_submenu() {
     $base_url = bp_get_group_permalink( groups_get_current_group() ) . 'activity';
 
     $current_item = $base_url;
-	if ( ! empty( $_GET['type'] ) && in_array( $_GET['type'], [ 'mine', 'mentions', 'starred' ], true ) ) {
+	if ( ! empty( $_GET['type'] ) && in_array( $_GET['type'], [ 'mine', 'mentions', 'starred', 'connections' ], true ) ) {
 		$current_item .= '?type=' . $_GET['type'];
-
 	}
 
     $menu_list = [
@@ -679,6 +682,10 @@ function openlab_group_activity_submenu() {
         $base_url . '?type=mentions'    => '@Mentions',
         $base_url . '?type=starred'     => 'Starred'
     ];
+
+	if ( class_exists( 'OpenLab\Connections\Util' ) && \OpenLab\Connections\Util::is_connections_enabled_for_group( bp_get_current_group_id() ) ) {
+		$menu_list[ $base_url . '?type=connections' ] = 'Connections&nbsp;&nbsp;<i class="fa fa-rss connections-icon"></i>';
+	}
 
     return openlab_submenu_gen( $menu_list, false, $current_item );
 }
@@ -852,9 +859,6 @@ function openlab_filter_subnav_home($subnav_item) {
 
     $new_item = str_replace("Home", $new_label, $subnav_item);
 
-    //update "current" class to "current-menu-item" to unify site identification of current menu page
-    $new_item = str_replace("current selected", "current-menu-item", $new_item);
-
     //for mobile menu add course site and site dashboard (if available)
     $group_id = bp_get_current_group_id();
 
@@ -977,9 +981,6 @@ function openlab_filter_subnav_docs($subnav_item) {
 		$new_item = $subnav_item;
 	}
 
-    //update "current" class to "current-menu-item" to unify site identification of current menu page
-    $new_item = str_replace( "current selected", "current-menu-item", $new_item );
-
     return $new_item;
 }
 
@@ -990,9 +991,6 @@ function openlab_filter_subnav_nav_group_documents($subnav_item) {
     if ( ! openlab_is_files_enabled_for_group( bp_get_current_group_id() ) ) {
         return '';
     }
-
-    //update "current" class to "current-menu-item" to unify site identification of current menu page
-    $subnav_item = str_replace("current selected", "current-menu-item", $subnav_item);
 
     // Add count. @todo Better caching.
     $count = BP_Group_Documents::get_total(bp_get_current_group_id());
@@ -1006,7 +1004,6 @@ function openlab_filter_subnav_nav_group_documents($subnav_item) {
 
     return $subnav_item;
 }
-
 add_filter('bp_get_options_nav_group-documents', 'openlab_filter_subnav_nav_group_documents');
 
 /**
@@ -1014,7 +1011,6 @@ add_filter('bp_get_options_nav_group-documents', 'openlab_filter_subnav_nav_grou
  */
 function openlab_filter_subnav_forums($subnav_item) {
     // update "current" class to "current-menu-item" to unify site identification of current menu page
-    $subnav_item = str_replace('current selected', 'current-menu-item', $subnav_item);
     $subnav_item = str_replace('Forum', 'Discussion', $subnav_item);
 
     // Add count.
@@ -1040,24 +1036,11 @@ function openlab_filter_subnav_forums($subnav_item) {
 }
 add_filter( 'bp_get_options_nav_nav-forum', 'openlab_filter_subnav_forums' );
 
+// Disables some nav items.
+add_filter('bp_get_options_nav_nav-invite-anyone', '__return_empty_string');
+add_filter('bp_get_options_nav_nav-notifications', '__return_empty_string');
+add_filter('bp_get_options_nav_request-membership', '__return_empty_string');
 
-add_filter('bp_get_options_nav_nav-invite-anyone', 'openlab_filter_subnav_nav_invite_anyone');
-
-function openlab_filter_subnav_nav_invite_anyone($subnav_item) {
-    return "";
-}
-
-add_filter('bp_get_options_nav_nav-notifications', 'openlab_filter_subnav_nav_notifications');
-
-function openlab_filter_subnav_nav_notifications($subnav_item) {
-    return "";
-}
-
-add_filter('bp_get_options_nav_request-membership', 'openlab_filter_subnav_nav_request_membership');
-
-function openlab_filter_subnav_nav_request_membership($subnav_item) {
-    return "";
-}
 
 add_filter('bp_get_options_nav_nav-events', 'openlab_filter_subnav_nav_events');
 add_filter('bp_get_options_nav_nav-events-mobile', 'openlab_filter_subnav_nav_events');
@@ -1087,30 +1070,29 @@ add_filter('bp_get_options_nav_calendar', 'openlab_filter_subnav_nav_calendar');
 function openlab_filter_subnav_nav_calendar($subnav_item) {
     $subnav_item = str_replace('Calendar', 'All Events', $subnav_item);
 
-    $subnav_item = str_replace("current selected", "current-menu-item", $subnav_item);
-
     return $subnav_item;
 }
-
 add_filter('bp_get_options_nav_upcoming', 'openlab_filter_subnav_nav_upcoming');
 
-function openlab_filter_subnav_nav_upcoming($subnav_item) {
-
-    $subnav_item = str_replace("current selected", "current-menu-item", $subnav_item);
-
-    return $subnav_item;
-}
-
-add_filter('bp_get_options_nav_new-event', 'openlab_filter_subnav_nav_new_event');
-
 /**
- * Filters the 'Activity' option nav item.
+ * Changes BP's default "current selected" classes to our custom 'current-menu-item' in bp_get_options_nav().
+ *
+ * @param string $subnav_item Nav menu markup.
+ * @return string
  */
-function openlab_filter_subnav_nav_activity( $subnav_item ) {
+function openlab_set_current_menu_item_class_for_subnav_item( $subnav_item ) {
     return str_replace( 'current selected', 'current-menu-item', $subnav_item );
 }
-add_filter( 'bp_get_options_nav_activity', 'openlab_filter_subnav_nav_activity' );
-
+add_filter( 'bp_get_options_nav_admin', 'openlab_set_current_menu_item_class_for_subnav_item' );
+add_filter( 'bp_get_options_nav_activity', 'openlab_set_current_menu_item_class_for_subnav_item' );
+add_filter( 'bp_get_options_nav_new-event', 'openlab_set_current_menu_item_class_for_subnav_item' );
+add_filter( 'bp_get_options_nav_upcoming', 'openlab_set_current_menu_item_class_for_subnav_item' );
+add_filter( 'bp_get_options_nav_nav-forum', 'openlab_set_current_menu_item_class_for_subnav_item' );
+add_filter( 'bp_get_options_nav_group-documents', 'openlab_set_current_menu_item_class_for_subnav_item' );
+add_filter( 'bp_get_options_nav_nav-docs', 'openlab_set_current_menu_item_class_for_subnav_item' );
+add_filter( 'bp_get_options_nav_nav-connections', 'openlab_set_current_menu_item_class_for_subnav_item' );
+add_filter( 'bp_get_options_nav_nav-announcements', 'openlab_set_current_menu_item_class_for_subnav_item' );
+add_filter( 'bp_get_options_nav_home', 'openlab_set_current_menu_item_class_for_subnav_item' );
 /**
  * Filters the 'Announcements' option nav item.
  */
@@ -1137,7 +1119,7 @@ function openlab_filter_subnav_nav_announcements( $subnav_item ) {
         $subnav_item = str_replace('</a>', ' ' . $span . '</a>', $subnav_item);
 	}
 
-    return str_replace( 'current selected', 'current-menu-item', $subnav_item );
+    return $subnav_item;
 }
 add_filter( 'bp_get_options_nav_nav-announcements', 'openlab_filter_subnav_nav_announcements' );
 

.gitmodules
@@ -22,3 +22,6 @@
 [submodule "wp-content/plugins/openlab-modules"]
 	path = wp-content/plugins/openlab-modules
 	url = git@github.com:openlab-at-city-tech/openlab-modules
+[submodule "wp-content/plugins/openlab-connections"]
+	path = wp-content/plugins/openlab-connections
+	url = git@github.com:openlab-at-city-tech/openlab-connections

phpstan.neon
@@ -24,6 +24,7 @@ parameters:
     - wp-content/plugins/gravityperks
     - wp-content/plugins/invite-anyone
     - wp-content/plugins/openlab-badges
+    - wp-content/plugins/openlab-connections
     - wp-content/plugins/openlab-favorites
     - wp-content/plugins/papercite
     - wp-content/themes/genesis

wp-content/mu-plugins/openlab-toolbar.php
@@ -716,7 +716,20 @@ HTML;
 		$invites      = groups_get_invites_for_user();
 		$invite_count = isset( $invites['total'] ) ? (int) $invites['total'] : 0;
 
-		$total_count = openlab_admin_bar_counts( $request_count + $invite_count, ' sub-count' );
+		$user_groups = bp_get_user_groups( bp_loggedin_user_id(), [ 'is_admin' => true ] );
+		if ( $user_groups ) {
+			$connection_invitations = \OpenLab\Connections\Invitation::get(
+				[
+					'invitee_group_id' => array_keys( $user_groups ),
+				]
+			);
+
+			$connection_invitation_count = count( $connection_invitations );
+		} else {
+			$connection_invitation_count = 0;
+		}
+
+		$total_count = openlab_admin_bar_counts( $request_count + $invite_count + $connection_invitation_count, ' sub-count' );
 
 		$wp_admin_bar->add_menu(
 			array(
@@ -866,6 +879,75 @@ HTML;
 					),
 				)
 			);
+		}
+
+		/**
+		 * CONNECTIONS
+		 */
+		if ( defined( 'OPENLAB_CONNECTIONS_PLUGIN_URL' ) ) {
+			if ( $user_groups ) {
+				$title = 'Connections';
+
+				// "Connections" title
+				$wp_admin_bar->add_node(
+					array(
+						'parent' => 'invites',
+						'id'     => 'connections-title',
+						'title'  => $title,
+						'meta'   => array(
+							'class' => 'submenu-title bold',
+						),
+					)
+				);
+
+				if ( $connection_invitations ) {
+					foreach ( $connection_invitations as $connection_invitation ) {
+						$group_avatar = bp_core_fetch_avatar(
+							[
+								'item_id' => $connection_invitation->get_inviter_group_id(),
+								'object'  => 'group',
+								'type'    => 'full',
+								'html'    => false,
+							]
+						);
+
+						$inviter_group = groups_get_group( $connection_invitation->get_inviter_group_id() );
+
+						$invitation_id = $connection_invitation->get_invitation_id();
+
+						// Avatar.
+						$title = '<div class="ol-toolbar-row"><div class="col-sm-6"><div class="item-avatar"><a href="' . esc_url( bp_get_group_permalink( $inviter_group ) ) . '"><img class="img-responsive" src ="' . esc_url( $group_avatar ) . '" alt="Profile picture of ' . stripslashes( $inviter_group->name ) . '"/></a></div></div>';
+
+						// Group name and link.
+						$title .= '<div class="col-sm-18"><p class="item-title"><a class="bold" href="' . bp_get_group_permalink( $inviter_group ) . '">' . stripslashes( $inviter_group->name ) . '</a></p>';
+
+						// Accept/reject buttons.
+						$title .= '<p class="actions clearfix"><a class="btn btn-primary link-btn accept" href="' . esc_url( wp_nonce_url( $connection_invitation->get_accept_url(), 'accept-invitation-' . $invitation_id ) ) . '">' . __( 'Accept', 'buddypress' ) . '</a> &nbsp; <a class="btn btn-default link-btn reject" href="' . esc_url( wp_nonce_url( $connection_invitation->get_reject_url(), 'reject-invitation-' . $invitation_id ) ) . '">' . __( 'Reject', 'buddypress' ) . '</a></p></div></div>';
+
+						$wp_admin_bar->add_node(
+							array(
+								'parent' => 'invites',
+								'id'     => 'invitation-' . $inviter_group->id,
+								'title'  => $title,
+								'meta'   => array(
+									'class' => 'nav-content-item nav-invitation',
+								),
+							)
+						);
+					}
+				} else {
+					$wp_admin_bar->add_node(
+						array(
+							'parent' => 'invites',
+							'id'     => 'group-invites-none',
+							'title'  => '<div class="ol-toolbar-row"><div class="col-sm-24"><p>' . 'No connection invitations.' . '</p></div></div>',
+							'meta'   => array(
+								'class' => 'nav-no-items nav-content-item',
+							),
+						)
+					);
+				}
+			}
 		}
 	}
 

wp-content/themes/openlab/buddypress/groups/single/activity.php
@@ -40,7 +40,14 @@ $filter_options = [
 		<?php endif; ?>
 
 		<?php while ( bp_activities() ) : bp_the_activity(); ?>
-			<?php bp_get_template_part( 'parts/activity/entry-group' ); ?>
+			<?php
+			if ( 'connections' === $type ) {
+				$template = 'activity/entry';
+			} else {
+				$template = 'parts/activity/entry-group';
+			}
+			?>
+			<?php bp_get_template_part( $template ); ?>
 		<?php endwhile; ?>
 
 		<?php echo openlab_activities_pagination_links(); ?>

wp-content/themes/openlab/buddypress/groups/single/connections/index.php
@@ -1,0 +1,138 @@
+<?php
+// @todo Pagination.
+$connections = \OpenLab\Connections\Connection::get( [ 'group_id' => bp_get_current_group_id() ] );
+
+$group_site_id  = openlab_get_site_id_by_group_id( bp_get_current_group_id() );
+$group_site_url = get_site_url( $group_site_id );
+
+$site_categories = OpenLab\Connections\Util::fetch_taxonomy_terms_for_site( $group_site_url, 'category' );
+$site_tags       = OpenLab\Connections\Util::fetch_taxonomy_terms_for_site( $group_site_url, 'post_tag' );
+
+?>
+
+<?php do_action( 'template_notices' ); ?>
+
+<?php if ( $connections ) : ?>
+	<div class="connections-settings" data-group-id="<?php echo esc_attr( bp_get_current_group_id() ); ?>">
+		<?php foreach ( $connections as $connection ) : ?>
+			<?php
+			$other_group_id_array = array_filter(
+				$connection->get_group_ids(),
+				function( $group_id ) {
+					return $group_id !== bp_get_current_group_id();
+				}
+			);
+
+			$connected_group_id = reset( $other_group_id_array );
+
+			$connected_group = groups_get_group( $connected_group_id );
+
+			$connected_group_url = bp_get_group_permalink( $connected_group );
+
+			$connected_group_avatar = bp_core_fetch_avatar(
+				[
+					'item_id' => $connected_group_id,
+					'object'  => 'group',
+					'type'    => 'full',
+				]
+			);
+
+			$connection_settings = $connection->get_group_settings( bp_get_current_group_id() );
+
+			$all_is_checked = !  array_diff( [ 'post', 'comment' ], $connection_settings['content_types'] ) && ! array_diff( [ 'post_tag' => 'all', 'category' => 'all' ], $connection_settings['post_taxes'] );
+
+			$selected_categories = [];
+			if ( isset( $connection_settings['post_taxes']['category'] ) ) {
+				if ( 'all' === $connection_settings['post_taxes']['category'] ) {
+					$selected_categories = 'all';
+				} else {
+					$selected_categories = array_map( 'intval', $connection_settings['post_taxes']['category'] );
+				}
+			}
+
+			$selected_tags = [];
+			if ( isset( $connection_settings['post_taxes']['post_tag'] ) ) {
+				if ( 'all' === $connection_settings['post_taxes']['post_tag'] ) {
+					$selected_tags = 'all';
+				} else {
+					$selected_tags = array_map( 'intval', $connection_settings['post_taxes']['post_tag'] );
+				}
+			}
+
+			?>
+			<div class="connection-settings" id="connection-settings-<?php echo esc_attr( $connection->get_connection_id() ); ?>" data-connection-id="<?php echo esc_attr( $connection->get_connection_id() ); ?>">
+				<div class="avatar-column">
+					<a href="<?php echo esc_url( $connected_group_url ); ?>"><?php echo $connected_group_avatar; ?></a>
+				</div>
+
+				<div class="primary-column">
+					<div class="connected-group-link item-title h2">
+						<a class="no-deco" href="<?php echo esc_url( $connected_group_url ); ?>"><?php echo esc_html( $connected_group->name ); ?></a>
+					</div>
+
+					<div class="accordion">
+						<button class="accordion-toggle" aria-expanded="false" aria-controls="accordion-content">
+							<span class="accordion-caret">&gt;</span>
+							<span class="sr-only">Expand</span>
+
+							<?php echo wp_kses_post( __( '<strong>Sharing:</strong>&nbsp;Manage the content shared with this connection.', 'openlab-connections' ) ); ?>
+						</button>
+
+						<div class="accordion-content">
+							<div class="connection-setting-checkbox">
+								<input type="checkbox" <?php checked( $all_is_checked ); ?> class="connection-setting-all" id="connection-setting-<?php echo esc_attr( $connection->get_connection_id() ); ?>-all" /> <label for="connection-setting-<?php echo esc_attr( $connection->get_connection_id() ); ?>-all"><?php esc_html_e( 'All', 'openlab-connections' ); ?>
+							</div>
+
+							<div class="connection-setting-checkbox connection-setting-checkbox-post">
+								<input type="checkbox" <?php checked( in_array( 'post', $connection_settings['content_types'], true ) ); ?> class="connection-setting-post" id="connection-setting-<?php echo esc_attr( $connection->get_connection_id() ); ?>-post" name="connection-settings[content-type][post]" value="1" /> <label for="connection-setting-<?php echo esc_attr( $connection->get_connection_id() ); ?>-post"><?php esc_html_e( 'Posts', 'openlab-connections' ); ?></label>
+
+								<div class="connection-setting-checkbox-subsettings">
+									<div>
+										<input type="checkbox" <?php checked( isset( $connection_settings['post_taxes']['category'] ) ); ?> class="connection-setting-post-tax" id="connection-setting-<?php esc_attr( $connection->get_connection_id() ); ?>-post-category" name="connection-settings[post-taxes][category]" value="1" /> <label for="connection-setting-<?php echo esc_attr( $connection->get_connection_id() ); ?>-post-category" class=""><?php esc_html_e( 'Category', 'openlab-connections' ); ?></label>
+
+										<select name="connection-settings[post-tax-terms][category]" multiple id="connection-setting-<?php echo esc_attr( $connection->get_connection_id() ); ?>-category-terms" class="connection-tax-term-selector">
+											<option value="_all" <?php selected( 'all' === $selected_categories ); ?>><?php esc_html_e( 'All categories', 'openlab-connections' ); ?></option>
+
+											<?php foreach ( $site_categories as $site_category ) : ?>
+												<option value="<?php echo esc_attr( $site_category['id'] ); ?>" <?php selected( is_array( $selected_categories ) && in_array( $site_category['id'], $selected_categories, true ) ); ?>><?php echo esc_html( $site_category['name'] ); ?></option>
+											<?php endforeach; ?>
+
+										</select>
+									</div>
+
+									<div>
+										<input type="checkbox" <?php checked( isset( $connection_settings['post_taxes']['post_tag'] ) ); ?> class="connection-setting-post-tax" id="connection-setting-<?php esc_attr( $connection->get_connection_id() ); ?>-post-tag" name="connection-settings[post-taxes][post_tag]" value="1" /> <label for="connection-setting-<?php echo esc_attr( $connection->get_connection_id() ); ?>-post-tag" class=""><?php esc_html_e( 'Tag', 'openlab-connections' ); ?></label>
+
+										<select name="connection-settings[post-tax-terms][post_tag]" multiple id="connection-setting-<?php echo esc_attr( $connection->get_connection_id() ); ?>-tag-terms" class="connection-tax-term-selector">
+											<option value="_all" <?php selected( 'all' === $selected_tags ); ?>><?php esc_html_e( 'All tags', 'openlab-connections' ); ?></option>
+
+											<?php foreach ( $site_tags as $site_tag ) : ?>
+												<option value="<?php echo esc_attr( $site_tag['id'] ); ?>" <?php selected( is_array( $selected_tags ) && in_array( $site_tag['id'], $selected_tags, true ) ); ?>><?php echo esc_html( $site_tag['name'] ); ?></option>
+											<?php endforeach; ?>
+										</select>
+									</div>
+								</div>
+							</div>
+
+							<div class="connection-setting-checkbox">
+								<input type="checkbox" <?php checked( in_array( 'comment', $connection_settings['content_types'], true ) ); ?> class="connection-setting-comment" id="connection-setting-<?php echo esc_attr( $connection->get_connection_id() ); ?>-comment" name="connection-settings[content-type][comment]" value="1" /> <label for="connection-setting-<?php echo esc_attr( $connection->get_connection_id() ); ?>-comment"><?php esc_html_e( 'Comments', 'openlab-connections' ); ?>
+							</div>
+
+							<div class="connection-setting-checkbox">
+								<input type="checkbox" <?php checked( empty( $connection_settings['content_types'] ) ); ?> class="connection-setting-none" id="connection-setting-<?php echo esc_attr( $connection->get_connection_id() ); ?>-none" /> <label for="connection-setting-<?php echo esc_attr( $connection->get_connection_id() ); ?>-none"><?php esc_html_e( 'None', 'openlab-connections' ); ?>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<a href="<?php echo esc_url( wp_nonce_url( $connection->get_disconnect_url( bp_get_current_group_id() ), 'disconnect-' . $connection->get_connection_id() ) ); ?>" onclick="return confirm( '<?php echo esc_js( sprintf( __( 'Are you sure you want to disconnect from %s?', 'openlab-connections' ), $connected_group->name ) ); ?>' )" class="disconnect-button no-deco btn btn-primary" aria-label="<?php esc_attr_e( 'Disconnect', 'openlab-connections' ); ?>"><?php esc_html_e( 'Connected', 'openlab-connections' ); ?></a>
+
+				<?php wp_nonce_field( 'connection-settings-' . $connection->get_connection_id(), 'connection-settings-' . $connection->get_connection_id() . '-nonce', false ); ?>
+			</div>
+		<?php endforeach; ?>
+	</div>
+<?php else : ?>
+	<p><?php esc_html_e( 'This feature connects related spaces on the Openlab. It is useful for sharing site activity with cohorts, collaborators, and across course sections. Visit <a href="tk">OpenLab Help</a> for more information.', 'openlab-connections' ); ?></p>
+
+	<p><?php esc_html_e( 'This group does not have any connections.', 'openlab-connections' ); ?></p>
+<?php endif; ?>

wp-content/themes/openlab/buddypress/groups/single/connections/invitations.php
@@ -1,0 +1,57 @@
+<?php do_action( 'template_notices' ); ?>
+
+<?php
+$pending_invites = \OpenLab\Connections\Invitation::get(
+	[
+		'invitee_group_id' => bp_get_current_group_id(),
+		'pending_only'     => true,
+	]
+);
+?>
+
+<form class="form-panel">
+	<div class="panel panel-default">
+		<div class="panel-heading">Invitations</div>
+		<div class="panel-body">
+			<p>Accept or decline invitations to connect. Manage the activity shared out to your connected groups in <a href="<?php echo esc_url( bp_get_group_permalink( groups_get_current_group() ) . 'connections/' ); ?>">Manage Connections</a>.</p>
+
+			<?php if ( $pending_invites ) : ?>
+				<div class="pending-invitations connection-invitations">
+					<div class="pending-invitation connection-invitation connection-invitation-header">
+						<div class="actions"><span class="sr-only"><?php esc_html_e( 'Actions', 'openlab-connections' ); ?></span></div>
+						<div class="group"><?php esc_html_e( 'Group', 'text-domain' ); ?></div>
+						<div class="received"><?php esc_html_e( 'Received', 'text-domain' ); ?></div>
+					</div>
+
+					<?php foreach ( $pending_invites as $pending_invite ) : ?>
+						<?php
+
+						$group = groups_get_group( $pending_invite->get_invitee_group_id() );
+
+						$date_received = '0000-00-00 00:00:00' === $pending_invite->get_date_created() ? '' : date_i18n( get_option( 'date_format' ), strtotime( $pending_invite->get_date_created() ) );
+
+						$invitation_id = $pending_invite->get_invitation_id();
+
+						$accept_url = wp_nonce_url( $pending_invite->get_accept_url(), 'accept-invitation-' . $invitation_id );;
+						$reject_url = wp_nonce_url( $pending_invite->get_reject_url(), 'reject-invitation-' . $invitation_id );;
+
+						?>
+
+						<div class="pending-invitation connection-invitation">
+							<div class="actions">
+								<a class="btn btn-primary" href="<?php echo esc_url( $accept_url ); ?>"><?php esc_html_e( 'Accept' ); ?></a>
+								<a class="btn btn-default" href="<?php echo esc_url( $reject_url ); ?>"><?php esc_html_e( 'Reject' ); ?></a>
+							</div>
+
+							<div class="group"><?php echo esc_html( $group->name ); ?></div>
+							<div class="received"><?php echo esc_html( $date_received ); ?></div>
+
+						</div>
+					<?php endforeach; ?>
+				</div>
+			<?php else : ?>
+				<p><?php esc_html_e( 'You have no pending invitations at this time.', 'openlab-connections' ); ?></p>
+			<?php endif; ?>
+		</div>
+	</div>
+</form>

commit 813f7d9948876d973f1f9fa758255b885d715d04

Fix broken Calendar view in groups.
This was broken in cf84ebb54c84576cd466e2e7e16034502188ec27 when
the "upcoming" nav filter was removed, but the add_filter() reference
to the filter was not.

See #3165, #3207.

wp-content/themes/openlab/lib/menus.php
@@ -1063,14 +1063,10 @@ function openlab_filter_subnav_nav_events($subnav_item) {
     return $subnav_item;
 }
 
-add_filter('bp_get_options_nav_calendar', 'openlab_filter_subnav_nav_calendar');
-
-function openlab_filter_subnav_nav_calendar($subnav_item) {
-    $subnav_item = str_replace('Calendar', 'All Events', $subnav_item);
-
-    return $subnav_item;
+function openlab_filter_subnav_nav_calendar( $subnav_item ) {
+    return str_replace( 'Calendar', 'All Events', $subnav_item );
 }
-add_filter('bp_get_options_nav_upcoming', 'openlab_filter_subnav_nav_upcoming');
+add_filter( 'bp_get_options_nav_calendar', 'openlab_filter_subnav_nav_calendar' );
 
 /**
  * Changes BP's default "current selected" classes to our custom 'current-menu-item' in bp_get_options_nav().