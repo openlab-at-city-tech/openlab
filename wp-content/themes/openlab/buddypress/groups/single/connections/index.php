20953bd3de3a31dc93d4e265ee96201402105c65
Boone B Gorges <boonebgorges@gmail.com>
2023-08-31 10:09:07 -0500

    Div for showing save status of connection settings.
    See #3222.

--- wp-content/themes/openlab/buddypress/groups/single/connections/index.php
+++ wp-content/themes/openlab/buddypress/groups/single/connections/index.php
@@ -152,6 +152,8 @@ switch ( $current_group_site_status ) {
 
 					<a href="<?php echo esc_url( wp_nonce_url( $connection->get_disconnect_url( bp_get_current_group_id() ), 'disconnect-' . $connection->get_connection_id() ) ); ?>" onclick="return confirm( '<?php echo esc_js( sprintf( __( 'Are you sure you want to disconnect from %s?', 'openlab-connections' ), $connected_group->name ) ); ?>' )" class="disconnect-button no-deco btn btn-primary" aria-label="<?php esc_attr_e( 'Disconnect', 'openlab-connections' ); ?>"><?php esc_html_e( 'Connected', 'openlab-connections' ); ?></a>
 
+					<div class="connection-settings-save-status" id="connection-settings-save-status-<?php echo esc_attr( $connection->get_connection_id() ); ?>"></div>
+
 					<?php wp_nonce_field( 'connection-settings-' . $connection->get_connection_id(), 'connection-settings-' . $connection->get_connection_id() . '-nonce', false ); ?>
 				</div>
 			<?php endforeach; ?>
