2cb050106458b92c5757571fd958b5f0f106f2b9
Boone B Gorges <boonebgorges@gmail.com>
2023-09-06 16:41:47 +0000

    Change 'Community Team' heading.
    See #3237.

--- wp-content/themes/openlab/sidebar-help.php
+++ wp-content/themes/openlab/sidebar-help.php
@@ -31,7 +31,7 @@ wp_nav_menu($args);
 </div>
 </div>
 
-<h2 class="sidebar-help-title support-team-title">Community Team</h2>
+<h2 class="sidebar-help-title support-team-title">OpenLab Support</h2>
 <div class="sidebar-block padded-block">
 <div id="support-team">
     <div class="help-tags-copy"><p>The Community Team is here to answer all your OpenLab questions.</p></div>
