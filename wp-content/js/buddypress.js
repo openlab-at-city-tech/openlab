3ddc38c06a50e63185bfde790a37e1408f047a86
Boone B Gorges <boonebgorges@gmail.com>
2023-10-11 14:34:54 +0000

    JS build.

--- wp-content/js/buddypress.js
+++ wp-content/js/buddypress.js
@@ -418,19 +418,19 @@ function bp_get_querystring( n ) {
 
 	//Hide the sort form submit, we're gonna submit on change
 	$('#bp-group-documents-sort-form input[type=submit]').hide();
-	$('#bp-group-documents-sort-form select[name=order]').change(function(){
+	$('#bp-group-documents-sort-form select[name=order]').on( 'change', function(){
 		$('form#bp-group-documents-sort-form').submit();
 	});
 
 	//Hide the category form submit, we're gonna submit on change
 	$('#bp-group-documents-category-form input[type=submit]').hide();
-	$('#bp-group-documents-category-form select[name=category]').change(function(){
+	$('#bp-group-documents-category-form select[name=category]').on( 'change', function(){
 		$('form#bp-group-documents-category-form').submit();
 	});
 
 	//Hide the upload form by default, expand as needed
 	$('#bp-group-documents-upload-button').show();
-	$('#bp-group-documents-upload-button').click(function(){
+	$('#bp-group-documents-upload-button').on( 'click', function(){
 		$('#bp-group-documents').addClass('is-edit-mode');
 		return false;
 	});
@@ -440,12 +440,12 @@ function bp_get_querystring( n ) {
 	}
 
 	//prefill the new category field
-	$('input.bp-group-documents-new-category').val('New Category...').css('color','#999').focus(function(){
+	$('input.bp-group-documents-new-category').val('New Category...').css('color','#999').on( 'focus', function(){
 		$(this).val('').css('color','inherit');
 	});
 
 	//check for presence of a file before submitting form
-	$('form#bp-group-documents-form').submit(function(){
+	$('form#bp-group-documents-form').on( 'submit', function(){
 
 		//check for pre-filled values, and remove before sumitting
 		if( $('input.bp-group-documents-new-category').val() == 'New Category...' ) {
@@ -461,7 +461,7 @@ function bp_get_querystring( n ) {
 	});
 
 	//validate group admin form before submitting
-	$('form#group-settings-form').submit(function() {
+	$('form#group-settings-form').on( 'submit', function() {
 
 		//check for pre-filled values, and remove before sumitting
 		if( $('input.bp-group-documents-new-category').val() == 'New Category...' ) {
@@ -470,12 +470,12 @@ function bp_get_querystring( n ) {
 	});
 
 	//Make the user confirm when deleting a document
-	$('a#bp-group-documents-delete').click(function(){
+	$('a#bp-group-documents-delete').on( 'click', function(){
 		return confirm('Are you sure you wish to permanently delete this document?');
 	});
 
 	//Track when a user clicks a document via Ajax
-	$('a.group-documents-title').add($('a.group-documents-icon')).click(function(){
+	$('a.group-documents-title').add($('a.group-documents-icon')).on( 'click', function(){
 		dash_position = $(this).attr('id').lastIndexOf('-');
 		document_num = $(this).attr('id').substring(dash_position+1);
 
@@ -487,12 +487,12 @@ function bp_get_querystring( n ) {
 	});
 
 	//Make user confirm when deleting a category
-	$('a.group-documents-category-delete').click(function(){
+	$('a.group-documents-category-delete').on( 'click', function(){
 		return confirm('Are you sure you wish to permanently delete this category?');
 	});
 
 	//add new single categories in the group admin screen via ajax
-	$('#group-documents-group-admin-categories input[value=Add]').click(function(){
+	$('#group-documents-group-admin-categories input[value=Add]').on( 'click', function(){
 		$.post(ajaxurl, {
 			action:'group_documents_add_category',
 			category:$('input[name=bp_group_documents_new_category]').val()
@@ -503,7 +503,7 @@ function bp_get_querystring( n ) {
 	});
 
 	//delete single categories in the group admin screen via ajax
-	$('#group-documents-group-admin-categories a.group-documents-category-delete').click(function(){
+	$('#group-documents-group-admin-categories a.group-documents-category-delete').on( 'click', function(){
 		cat_id_string = $(this).parent('li').attr('id');
 		pos = cat_id_string.indexOf('-');
 		cat_id = cat_id_string.substring(pos+1);
