82f5fc37e87d3b5a51adf62348509d75e677764f
Boone B Gorges <boonebgorges@gmail.com>
2023-10-19 14:49:38 -0500

    Install plugin: polylang.
    Name: Polylang
    Version: 3.5.1
    
    See #3263.

---
+++ wp-content/plugins/polylang/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpsyntex/polylang',
+        'pretty_version' => '3.5.x-dev',
+        'version' => '3.5.9999999.9999999-dev',
+        'reference' => '5e1feae4deed8cadf8d597cfbfbafef7e5a66c31',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wpsyntex/polylang' => array(
+            'pretty_version' => '3.5.x-dev',
+            'version' => '3.5.9999999.9999999-dev',
+            'reference' => '5e1feae4deed8cadf8d597cfbfbafef7e5a66c31',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
