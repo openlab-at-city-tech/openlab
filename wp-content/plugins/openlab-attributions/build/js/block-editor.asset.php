78928bae6bffdc07422ec53e893e8bd290ce1148
Boone B Gorges <boonebgorges@gmail.com>
2023-12-26 06:55:46 -0600

    Use black for attribution modal credits area.
    See #3258.

--- wp-content/plugins/openlab-attributions/build/js/block-editor.asset.php
+++ wp-content/plugins/openlab-attributions/build/js/block-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-rich-text'), 'version' => '45b7232693e269e8');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-rich-text'), 'version' => 'c564dce8b85acb8c');
