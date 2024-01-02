openlab-at-city-tech/openlab

commit b51cc229c961d0865b9a0c8d9dbb13266a43c479

Retool toolbar code to avoid PHPStan notices.
It was saying that the keys of the array returned by
`get_post_types()` could not be unset, so instead we build
a new array.

wp-content/mu-plugins/openlab-toolbar.php
@@ -1344,10 +1344,17 @@ HTML;
 			$actions['post-new.php?post_type=page'] = array( $cpts['page']->labels->name_admin_bar, 'new-page' );
 		}
 
-		unset( $cpts['post'], $cpts['page'], $cpts['attachment'] );
+		$filtered_cpts = [];
+		foreach ( $cpts as $cpt_name => $cpt ) {
+			if ( in_array( $cpt, [ 'post', 'page', 'attachment' ], true ) ) {
+				continue;
+			}
+
+			$filtered_cpts[ $cpt_name ] = $cpt;
+		}
 
 		// Add any additional custom post types.
-		foreach ( $cpts as $cpt ) {
+		foreach ( $filtered_cpts as $cpt ) {
 			if ( ! current_user_can( $cpt->cap->create_posts ) ) {
 				continue;
 			}
@@ -1359,6 +1366,7 @@ HTML;
 			$key             = 'post-new.php?post_type=' . $cpt->name;
 			$actions[ $key ] = array( $cpt->labels->name_admin_bar, 'new-' . $cpt->name );
 		}
+
 		// Avoid clash with parent node and a 'content' post type.
 		if ( isset( $actions['post-new.php?post_type=content'] ) ) {
 			$actions['post-new.php?post_type=content'][1] = 'add-new-content';

commit a0b1f522e21492255c2e322716efc5b01ed85f21

Ensure that 'Media' doesn't appear twice in 'Add' toolbar dropdown.
Something about this broke in b51cc229c961d0865b9a0c8d9dbb13266a43c479.
We fix it by forcing a skip of 'attachment'.

See #3293.

wp-content/mu-plugins/openlab-toolbar.php
@@ -1363,6 +1363,11 @@ HTML;
 				continue;
 			}
 
+			// Skip 'attachment', as it's added separately.
+			if ( 'attachment' === $cpt->name ) {
+				continue;
+			}
+
 			$key             = 'post-new.php?post_type=' . $cpt->name;
 			$actions[ $key ] = array( $cpt->labels->name_admin_bar, 'new-' . $cpt->name );
 		}