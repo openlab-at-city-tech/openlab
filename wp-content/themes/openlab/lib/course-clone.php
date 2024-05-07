168d5fccc0ed6a61d667678445027c0f07f722fe
Boone B Gorges <boonebgorges@gmail.com>
2024-05-07 13:08:28 -0500

    Ensure that rewrite rules are flushed after clone.
    We must make sure the database flags for already-flushed-rules
    are not copied as part of the options table clone.
    
    See #3371.

--- wp-content/themes/openlab/lib/course-clone.php
+++ wp-content/themes/openlab/lib/course-clone.php
@@ -874,6 +874,8 @@ class Openlab_Clone_Course_Site {
 			'fileupload_url',
 			'oplb_gradebook_db_version', // This will force reinstallation of tables.
 			'duplicate_post_version', // Forces duplicate-post to initialize roles
+			'openlab_rewrite_rules_flushed',
+			'openlab_modules_rewrite_rules_flushed', // Triggers rewrite rule flush post-clone.
 		);
 
 		$preserve_prefix = [
