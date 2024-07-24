52752f9762854f1c1461a0b433c22e7ecb91047d
Boone B Gorges <boonebgorges@gmail.com>
2024-07-23 20:02:45 -0500

    Hide 'primary' column on bp-two-factor table.
    See #3344.

---
+++ wp-content/themes/openlab/js/bp-two-factor.js
@@ -0,0 +1,23 @@
+(function($){
+	$(document).ready(function(){
+		// Figure out the position of the 'col-primary' column. It doesn't have
+		// the proper class in the table body.
+		var col;
+		$('.two-factor-methods-table thead th').each(function( k, v ){
+			console.log(v)
+			if ( $(v).hasClass('col-primary') ) {
+				col = k;
+				return false;
+			}
+		});
+
+		if ( col !== undefined ) {
+			console.log('col: ' + col);
+			$('.two-factor-methods-table tbody tr').each(function(){
+				var $thisRowChildren = $(this).children();
+				$thisRowChildren.eq(col).addClass('col-primary');
+			});
+		}
+
+	});
+})(jQuery);
