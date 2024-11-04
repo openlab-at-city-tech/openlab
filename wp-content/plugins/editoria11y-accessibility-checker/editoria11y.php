a84096452c63efe92dda01fdb22bf438d6081000
Boone B Gorges <boonebgorges@gmail.com>
2024-11-04 11:44:06 -0600

    Update plugin: editoria11y-accessibility-checker.
    Name: Editoria11y Accessibility Checker
    New version: 1.0.21
    Previous version: 1.0.20

--- wp-content/plugins/editoria11y-accessibility-checker/editoria11y.php
+++ wp-content/plugins/editoria11y-accessibility-checker/editoria11y.php
@@ -5,10 +5,10 @@
  *
  * Plugin Name:       Editoria11y Accessibility Checker
  * Plugin URI:        https://wordpress.org/plugins/editoria11y-accessibility-checker/
- * Version:           1.0.20
+ * Version:           1.0.21
  * Requires PHP:      7.2
  * Requires at least: 6.0
- * Tested up to:      6.6
+ * Tested up to:      6.7
  * Author:            Princeton University, WDS
  * Author URI:        https://wds.princeton.edu/team
  * License:           GPL v2 or later
