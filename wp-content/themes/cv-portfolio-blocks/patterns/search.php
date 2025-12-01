d20161f023de85f5bd618d5ce3e62469fbe89f8a
Boone B Gorges <boonebgorges@gmail.com>
2025-12-01 11:16:25 -0600

    Update theme: cv-portfolio-blocks.
    Name: CV Portfolio Blocks
    New version: 1.3.4
    Previous version: 1.2.9

--- wp-content/themes/cv-portfolio-blocks/patterns/search.php
+++ wp-content/themes/cv-portfolio-blocks/patterns/search.php
@@ -16,9 +16,9 @@ return array(
 
 <!-- wp:group {"layout":{"type":"constrained","contentSize":"90%"}} -->
 <div class="wp-block-group"><!-- wp:columns -->
-<div class="wp-block-columns"><!-- wp:column {"width":"70%"} -->
-<div class="wp-block-column" style="flex-basis:70%"><!-- wp:query {"queryId":46,"query":{"perPage":10,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true}} -->
-<div class="wp-block-query"><!-- wp:post-template {"className":"search-post","layout":{"type":"grid","columnCount":3}} -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:query {"queryId":46,"query":{"perPage":10,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true}} -->
+<div class="wp-block-query"><!-- wp:post-template {"layout":{"type":"grid","columnCount":3}} -->
 <!-- wp:post-featured-image {"isLink":true} /-->
 
 <!-- wp:group {"style":{"color":{"background":"#f9f9f9"}},"layout":{"type":"constrained"}} -->
@@ -34,19 +34,31 @@ return array(
 <!-- wp:query-pagination-numbers /-->
 
 <!-- wp:query-pagination-next {"fontSize":"small"} /-->
-<!-- /wp:query-pagination --></div>
+<!-- /wp:query-pagination -->
+
+<!-- wp:query-no-results -->
+<!-- wp:group {"className":"no-results","style":{"spacing":{"padding":{"top":"60px","bottom":"60px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group no-results" style="padding-top:60px;padding-bottom:60px"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">'. esc_html__(' No results found','cv-portfolio-blocks') .'</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"> '. esc_html__('  We could not find any posts matching your search. Try different keywords.','cv-portfolio-blocks') .'</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+<!-- /wp:query-no-results --></div>
 <!-- /wp:query -->
 
-<!-- wp:group {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"textColor":"secondary","layout":{"type":"constrained","contentSize":""},"fontSize":"medium"} -->
+<!-- wp:group {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"textColor":"secondary","fontSize":"medium","layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-secondary-color has-text-color has-link-color has-medium-font-size" style="font-style:normal;font-weight:700"><!-- wp:post-navigation-link {"type":"previous"} /-->
 
 <!-- wp:post-navigation-link /--></div>
 <!-- /wp:group --></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"width":"30%"} -->
-<div class="wp-block-column" style="flex-basis:30%"><!-- wp:template-part {"slug":"sidebar","theme":"cv-portfolio-blocks","tagName":"div"} /--></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
-<!-- /wp:group -->',
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"50px"} -->
+<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->',
 );
