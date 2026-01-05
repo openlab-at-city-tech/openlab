df2cc0d38c6682724bdaeddf9e79f71e307364ef
Boone B Gorges <boonebgorges@gmail.com>
2026-01-05 14:05:40 -0600

    Wrap openlab-footer element in footer tag.
    See #3609.

--- wp-content/mu-plugins/parts/persistent/footer.php
+++ wp-content/mu-plugins/parts/persistent/footer.php
@@ -4,60 +4,62 @@
  */
 ?>
 
-<div id="openlab-footer" class="oplb-bs <?php echo ($blog_id === 1 ? ' page-table-row' : '') ?><?php echo ($placeholder ? ' placeholder' : ''); ?>" <?php echo ($placeholder ? 'aria-hidden="true" tabindex="-1"' : ''); ?>>
-    <div class="oplb-bs">
-        <div class="footer-wrapper">
-            <div class="container-fluid footer-desktop">
-                <div class="ol-footer-row row-footer">
-                    <div class="col-sm-12 ol-footer-col">
-                        <div class="col-logos ol-footer-col">
-                            <h2><span>The OpenLab at City Tech:</span><span>A place to learn, work, and share</span></h2>
-                            <div class="logos-wrapper clearfix">
-                                <p class="statement semibold"><span class="semibold">The OpenLab</span> is an open-source, digital platform designed to support teaching and learning at City Tech (New York City College of Technology), and to promote student and faculty engagement in the intellectual and social life of the college community.</p>
-                                <a class="pull-left citytech-logo" href="http://www.citytech.cuny.edu/" target="_blank"><img class="img-responsive" src="<?php echo bp_root_domain(); ?>/wp-content/mu-plugins/css/images/ctnyc_seal.png" alt="New York City College of Technology" border="0" /></a>
-                                <a class="pull-left cuny-logo " href="http://www.cuny.edu/" target="_blank"><img class="img-responsive" src="<?php echo bp_root_domain(); ?>/wp-content/mu-plugins/css/images/cuny_logo.png" alt="City University of New York" border="0" /></a>
-                            </div>
-                        </div>
-                        <div class="ol-footer-row row-copyright hidden-xs hidden-sm ol-footer-col">
-                            <div class="col-sm-24 ol-footer-col">
-                                <p><span><a class="no-deco roll-over-color" href="http://www.citytech.cuny.edu/" target="_blank">New York City College of Technology</a></span> <span class="horiz-divider">|</span> <span><a class="no-deco roll-over-color" href="http://www.cuny.edu" target="_blank">City University of New York</a></span></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="horiz-bar-wrapper"><div class="horiz-bar"></div></div>
-                    <div class="col-sm-8 col-links semibold ol-footer-col">
-                        <h2 class="first-header">Support</h2>
-                        <a class="no-deco roll-over-color" href="<?php echo $site; ?>/blog/help/openlab-help/">Help</a> <span class="horiz-divider">|</span> <a class="no-deco roll-over-color" href="<?php echo $site; ?>/about/contact-us/">Contact Us</a> <span class="horiz-divider">|</span> <a class="no-deco roll-over-color" href="http://cuny.edu/website/privacy.html" target="_blank">Privacy Policy</a> <span class="horiz-divider">|</span> <a class="no-deco roll-over-color" href="<?php echo $site; ?>/about/terms-of-service/">Terms of Use</a> <span class="horiz-divider">|</span> <a class="no-deco roll-over-color" href="<?php echo $site; ?>/about/credits/">Credits</a>
+<footer>
+	<div id="openlab-footer" class="oplb-bs <?php echo ($blog_id === 1 ? ' page-table-row' : '') ?><?php echo ($placeholder ? ' placeholder' : ''); ?>" <?php echo ($placeholder ? 'aria-hidden="true" tabindex="-1"' : ''); ?>>
+		<div class="oplb-bs">
+			<div class="footer-wrapper">
+				<div class="container-fluid footer-desktop">
+					<div class="ol-footer-row row-footer">
+						<div class="col-sm-12 ol-footer-col">
+							<div class="col-logos ol-footer-col">
+								<h2><span>The OpenLab at City Tech:</span><span>A place to learn, work, and share</span></h2>
+								<div class="logos-wrapper clearfix">
+									<p class="statement semibold"><span class="semibold">The OpenLab</span> is an open-source, digital platform designed to support teaching and learning at City Tech (New York City College of Technology), and to promote student and faculty engagement in the intellectual and social life of the college community.</p>
+									<a class="pull-left citytech-logo" href="http://www.citytech.cuny.edu/" target="_blank"><img class="img-responsive" src="<?php echo bp_root_domain(); ?>/wp-content/mu-plugins/css/images/ctnyc_seal.png" alt="New York City College of Technology" border="0" /></a>
+									<a class="pull-left cuny-logo " href="http://www.cuny.edu/" target="_blank"><img class="img-responsive" src="<?php echo bp_root_domain(); ?>/wp-content/mu-plugins/css/images/cuny_logo.png" alt="City University of New York" border="0" /></a>
+								</div>
+							</div>
+							<div class="ol-footer-row row-copyright hidden-xs hidden-sm ol-footer-col">
+								<div class="col-sm-24 ol-footer-col">
+									<p><span><a class="no-deco roll-over-color" href="http://www.citytech.cuny.edu/" target="_blank">New York City College of Technology</a></span> <span class="horiz-divider">|</span> <span><a class="no-deco roll-over-color" href="http://www.cuny.edu" target="_blank">City University of New York</a></span></p>
+								</div>
+							</div>
+						</div>
+						<div class="horiz-bar-wrapper"><div class="horiz-bar"></div></div>
+						<div class="col-sm-8 col-links semibold ol-footer-col">
+							<h2 class="first-header">Support</h2>
+							<a class="no-deco roll-over-color" href="<?php echo $site; ?>/blog/help/openlab-help/">Help</a> <span class="horiz-divider">|</span> <a class="no-deco roll-over-color" href="<?php echo $site; ?>/about/contact-us/">Contact Us</a> <span class="horiz-divider">|</span> <a class="no-deco roll-over-color" href="http://cuny.edu/website/privacy.html" target="_blank">Privacy Policy</a> <span class="horiz-divider">|</span> <a class="no-deco roll-over-color" href="<?php echo $site; ?>/about/terms-of-service/">Terms of Use</a> <span class="horiz-divider">|</span> <a class="no-deco roll-over-color" href="<?php echo $site; ?>/about/credits/">Credits</a>
 
-						<h2 class="second-header">Accessibility</h2>
-						<p class="statement">Our goal is to make the OpenLab accessible for all users.</p>
-						<p class="statement"><a class="deco roll-over-color" href="<?php echo esc_attr( $accessibility_link ) ?>">Learn more about accessibility on the OpenLab</a></p>
+							<h2 class="second-header">Accessibility</h2>
+							<p class="statement">Our goal is to make the OpenLab accessible for all users.</p>
+							<p class="statement"><a class="deco roll-over-color" href="<?php echo esc_attr( $accessibility_link ) ?>">Learn more about accessibility on the OpenLab</a></p>
 
-                    </div>
-                    <div class="horiz-bar-wrapper"><div class="horiz-bar"></div></div>
-                    <div class="col-sm-3 col-copyright ol-footer-col">
-                        <h2>Copyright</h2>
-                        <h3 class="third-header"><a href="https://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons</a></h3>
-                        <ul>
-                            <li>- Attribution</li>
-                            <li>- NonCommercial</li>
-                            <li>- ShareAlike</li>
-                        </ul>
-                        <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/"><img src="<?php echo bp_root_domain() ?>/wp-content/mu-plugins/css/images/by-nc-sa.png" alt="Creative Commons" /></a>
-                    </div>
-                </div>
-                <div class="ol-footer-row row-copyright hidden-md hidden-lg">
-                    <div class="col-sm-24 ol-footer-col">
-                        <p><span>&copy; <a class="no-deco roll-over-color" href="http://www.citytech.cuny.edu/" target="_blank">New York City College of Technology</a></span> <span class="horiz-divider">|</span> <span><a class="no-deco roll-over-color" href="http://www.cuny.edu" target="_blank">City University of New York</a></span></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <?php if (!$placeholder): ?>
-        <a class="visible-xs" id="go-to-top" href="#"><span class="fa fa-chevron-circle-up"></span><br />top</a>
-    <?php endif; ?>
-</div>
+						</div>
+						<div class="horiz-bar-wrapper"><div class="horiz-bar"></div></div>
+						<div class="col-sm-3 col-copyright ol-footer-col">
+							<h2>Copyright</h2>
+							<h3 class="third-header"><a href="https://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons</a></h3>
+							<ul>
+								<li>- Attribution</li>
+								<li>- NonCommercial</li>
+								<li>- ShareAlike</li>
+							</ul>
+							<a href="https://creativecommons.org/licenses/by-nc-sa/3.0/"><img src="<?php echo bp_root_domain() ?>/wp-content/mu-plugins/css/images/by-nc-sa.png" alt="Creative Commons" /></a>
+						</div>
+					</div>
+					<div class="ol-footer-row row-copyright hidden-md hidden-lg">
+						<div class="col-sm-24 ol-footer-col">
+							<p><span>&copy; <a class="no-deco roll-over-color" href="http://www.citytech.cuny.edu/" target="_blank">New York City College of Technology</a></span> <span class="horiz-divider">|</span> <span><a class="no-deco roll-over-color" href="http://www.cuny.edu" target="_blank">City University of New York</a></span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php if (!$placeholder): ?>
+			<a class="visible-xs" id="go-to-top" href="#"><span class="fa fa-chevron-circle-up"></span><br />top</a>
+		<?php endif; ?>
+	</div>
+</footer>
 
 <?php if (!$placeholder): ?>
 
