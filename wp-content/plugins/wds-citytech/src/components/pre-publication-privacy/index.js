5f71ff75d783ad05d9c7f459bdee0c86dcd16ce3
Boone B Gorges <boonebgorges@gmail.com>
2026-04-17 16:10:11 -0500

    More work on pre-publication check panel.
    The comps have this moving higher up in the panel.

--- wp-content/plugins/wds-citytech/src/components/pre-publication-privacy/index.js
+++ wp-content/plugins/wds-citytech/src/components/pre-publication-privacy/index.js
@@ -1,43 +1,66 @@
+/* global openlabBlocksPostVisibility */
+
 import { registerPlugin } from '@wordpress/plugins';
 import { PluginPrePublishPanel } from '@wordpress/editor';
+import { useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
-import { select } from '@wordpress/data';
 
 import './style.scss';
 
-registerPlugin(
-	'openlab-pre-publication-privacy',
-	{
-		render: () => {
+const PrePublicationPrivacy = () => {
+	const { blogPublic, prePubShareOnlyWithGroup } = openlabBlocksPostVisibility;
+	const blogPublicInt = parseInt( blogPublic );
 
-			const { blogPublic } = window.openlabBlocks
-			const blogPublicMessage = () => {
-				switch ( blogPublic ) {
-					case '1' :
-					case '0' :
-						return __( 'You are publishing on a public site.', 'wds-citytech' );
-				}
-			}
+	const { postVisibility } = useSelect( ( select ) => {
+		const postMeta = select( 'core/editor' ).getEditedPostAttribute( 'meta' );
+		const defaultVisibility = blogPublicInt >= 0 ? 'default' : 'members-only';
 
-			const currentPostPostStatus = select( 'core/editor' ).getCurrentPost().status;
-			console.log(currentPostPostStatus);
+		return {
+			postVisibility: postMeta.openlab_post_visibility || defaultVisibility,
+		};
+	}, [ blogPublicInt ] );
 
-			return (
+	const getBlogPublicMessage = () => {
+		switch ( blogPublicInt ) {
+			case 1:
+				return __( 'You are publishing on a public site. It may be included in web search results.', 'wds-citytech' );
+			case 0:
+				return __( 'You are publishing on a public site. Search engines are asked not to include this site in web search results.', 'wds-citytech' );
+			case -1:
+				return __( 'You are publishing on an OpenLab only site. It is visible only to registered members of City Tech OpenLab.', 'wds-citytech' );
+			case -2:
+				return __( 'You are publishing on a private site. It is visible only to registered members of this site.', 'wds-citytech' );
+			case -3:
+				return __( 'You are publishing on a hidden site. It is visible only to site administrators.', 'wds-citytech' );
+			default:
+				return '';
+		}
+	};
 
-				<PluginPrePublishPanel
-					className="openlab-pre-publication-privacy-panel"
-					initialOpen={ true }
-					title="Visibility Status Alert"
-				>
-					<ul>
-						<li>{ blogPublicMessage() }</li>
-
-						<li>
-						</li>
-					</ul>
-				</PluginPrePublishPanel>
-			)
-		},
+	const getPostVisibilityMessage = () => {
+		switch ( postVisibility ) {
+			case 'default':
+			default:
+				return __( 'Everyone who can view this site can see this post. You can change the post visibility settings below.', 'wds-citytech' );
+			case 'group-members-only':
+				return prePubShareOnlyWithGroup;
+			case 'members-only':
+				return __( 'Only logged-in OpenLab members can see this post. This will override the Public visibility setting above. You can change the post visibility settings below.', 'wds-citytech' );
+		}
 	}
 
-)
+	return (
+		<PluginPrePublishPanel
+			className="openlab-pre-publication-privacy-panel"
+			initialOpen={ true }
+			title={ __( 'Visibility Status Alert', 'wds-citytech' ) }
+		>
+			<p>{ getBlogPublicMessage() }</p>
+			<p>{ getPostVisibilityMessage() }</p>
+		</PluginPrePublishPanel>
+	);
+};
+
+registerPlugin( 'openlab-pre-publication-privacy', {
+	render: PrePublicationPrivacy,
+} );
