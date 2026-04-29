3cb1cee1a7df9c6c0fe65f5bfc01bbcaee4dbc62
Boone B Gorges <boonebgorges@gmail.com>
2026-04-29 07:12:39 -0500

    Update plugin: duplicate-post.
    Name: Yoast Duplicate Post
    New version: 4.6
    Previous version: 4.5

--- wp-content/plugins/duplicate-post/vendor/composer/installed.php
+++ wp-content/plugins/duplicate-post/vendor/composer/installed.php
@@ -1,43 +1,31 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
+        'name' => 'yoast/duplicate-post',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b6a92eabe9e2b2f8e46f17c3e89ddfe10983df74',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '66efa2fa0d3b8a0b3af956da9385531f2b6e1ae7',
-        'name' => 'yoast/duplicate-post',
         'dev' => false,
     ),
     'versions' => array(
         'composer/installers' => array(
-            'pretty_version' => 'v1.12.0',
-            'version' => '1.12.0.0',
+            'pretty_version' => 'v2.3.0',
+            'version' => '2.3.0.0',
+            'reference' => '12fb2dfe5e16183de69e784a7b84046c43d97e8e',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/./installers',
             'aliases' => array(),
-            'reference' => 'd20a64ed3c94748397ff5973488761b22f6d3f19',
             'dev_requirement' => false,
-        ),
-        'roundcube/plugin-installer' => array(
-            'dev_requirement' => false,
-            'replaced' => array(
-                0 => '*',
-            ),
-        ),
-        'shama/baton' => array(
-            'dev_requirement' => false,
-            'replaced' => array(
-                0 => '*',
-            ),
         ),
         'yoast/duplicate-post' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b6a92eabe9e2b2f8e46f17c3e89ddfe10983df74',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '66efa2fa0d3b8a0b3af956da9385531f2b6e1ae7',
             'dev_requirement' => false,
         ),
     ),
