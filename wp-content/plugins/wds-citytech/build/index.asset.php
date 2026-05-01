2c533d6dda6cc5f46bbb225b713c9604f5e780e6
Boone B Gorges <boonebgorges@gmail.com>
2026-05-01 11:40:39 -0500

    Asset build.

--- wp-content/plugins/wds-citytech/build/index.asset.php
+++ wp-content/plugins/wds-citytech/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'f6ad24182050f5ce339a');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'fbe5458253b3c2e6e20b');
