88b533f0bdce10bce81eb0277e1f3e276e836c21
Boone B Gorges <boonebgorges@gmail.com>
2026-05-05 05:39:51 -0500

    Change icon used for 'More visibility options' pre-publish panel.
    See #3640.

--- wp-content/plugins/wds-citytech/build/index.asset.php
+++ wp-content/plugins/wds-citytech/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'fbe5458253b3c2e6e20b');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '9c6d0509e79d7ec0027e');
